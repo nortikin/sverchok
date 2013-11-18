a8d401be2dbb85c725d01b0d9a9f56ca5061accb
Cfyzzz <Cfyzzz@narod.ru>
2013-11-18 14:25:27 +0600

    Перевел на новый алгоритм
    Signed-off-by: Cfyzzz <Cfyzzz@narod.ru>

--- node_CrossSection.py
+++ node_CrossSection.py
@@ -14,11 +14,38 @@ def section(cut_me_vertices, cut_me_edges, mx, pp, pno):
     Returns: Mesh - the resulting mesh of the section if any or
              Boolean - False if no section exists""" 
     
+    
+    def equation_plane(point, normal_dest):
+        #получаем коэффициенты уравнения плоскости по точке и нормали
+        normal = normal_dest.normalized()
+        A = normal.x
+        B = normal.y
+        C = normal.z
+        D = (A*point.x+B*point.y+C*point.z)*-1
+        
+        if A<0.0:
+            A *= -1
+            B *= -1
+            C *= -1
+            D *= -1
+            
+        return (A,B,C,D)
+
+    
+    def point_on_plane(v1, ep):
+        formula = ep[0]*v1.x+ep[1]*v1.y+ep[2]*v1.z+ep[3]
+        if formula==0.0:
+            return True
+        else:
+            return False
+    
+    
     verts = []
     ed_xsect = {}
     ed_xsect_2 = {}
     x_me = {}
-
+    
+    ep = equation_plane(pp, pno)
     cut_me_polygons=[]
     if len(cut_me_edges[0])>2:
         cut_me_polygons = cut_me_edges.copy() 
@@ -34,39 +61,25 @@ def section(cut_me_vertices, cut_me_edges, mx, pp, pno):
         
         vert1 = ed.vertices[0]
         v1 = new_me.vertices[vert1].co * mx.transposed() 
-        co1 = v1 - pp
+
         vert2 = ed.vertices[1]
         v2 = new_me.vertices[vert2].co * mx.transposed()
-        co2 = v2 - pp
-
-        # projecting them on the normal vector
-        proj1 = co1.project(pno).length
-        proj2 = co2.project(pno).length
-
-        if (proj1 != 0):
-            rad1 = co1.angle(pno)
-            angle1 = round((180 * rad1 / pi),4)
-
-        else: angle1 = 0
-        if (proj2 != 0):
-            rad2 = co2.angle(pno)
-            angle2 = round((180 * rad2 / pi),4)
-
-        else: angle2 = 0
         
-        if ((proj1 == 0) or (proj2 == 0) or  \
-            (angle1 > 90) != (angle2 > 90)) and  \
-            (proj1+proj2 > 0):
-            
-            proj1 /= proj1+proj2
-            co = ((v2-v1)*proj1)+v1
-            verts.append(co)
-            
+        vec = v2-v1
+        mul = vec * pno
+        if mul==0.0:
+            if not point_on_plane(v1, ep):
+                #parallel and not on plane
+                continue
+        
+        t0 = -(ep[0]*v1.x+ep[1]*v1.y+ep[2]*v1.z + ep[3]) / (ep[0]*vec.x + ep[1]*vec.y + ep[2]*vec.z)
+        pq = vec*t0+v1
+        if (pq-v1).length <= vec.length:
+            verts.append(pq)
             ed_xsect[ed.key] = len(ed_xsect)
             ed_xsect_2[ed_idx] = ((ed.vertices[0], ed.vertices[1]),len(verts)-1)
-
+        
     edges = []
-    #print('new_me.polygons',len(new_me.polygons))
     for f in new_me.polygons:
         # get the edges that the intersecting points form
         # to explain this better:
@@ -76,21 +89,6 @@ def section(cut_me_vertices, cut_me_edges, mx, pp, pno):
 
         if len(ps) == 2:
             edges.append(tuple(ps))
-    '''
-    if len(new_me.polygons)==0:
-        edges = set()
-        for f in cut_me_edges:
-            ps=set()
-            for v in f:
-                for ed in ed_xsect_2.keys():
-                    if v in ed_xsect_2[ed][0]:
-                        ps.add((ed, ed_xsect_2[ed][1]))
-        
-            lps=list(ps)
-            if len(lps)==2:
-                edges.add((lps[0][1], lps[1][1]))
-                
-        edges = list(edges)'''
     
     x_me['Verts'] = verts
     x_me['Edges'] = edges
@@ -171,8 +169,8 @@ class CrossSectionNode(Node, SverchCustomTreeNode):
                 self.outputs['edges'].StringsProperty = str(edges_out) 
             
         else:
-            self.outputs['vertices'].VerticesProperty = str([[]])
-            self.outputs['edges'].StringsProperty = str([[]]) 
+            self.outputs['vertices'].VerticesProperty = str([])
+            self.outputs['edges'].StringsProperty = str([])
         
 
     def update_socket(self, context):
@@ -186,7 +184,6 @@ def unregister():
 
 if __name__ == "__main__":
     register()
-
 
 
 
