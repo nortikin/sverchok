6479bde873ed117907a5bf6423250b0c23902283
ly29 <linusyng@live.com>
2014-01-22 14:27:42 +0100

    Works
    Layout changes. Bugfixes.
    The node has to be active, otherwise load does not work.

--- node_Text.py
+++ node_Text.py
@@ -32,7 +32,9 @@ class TextInNode(Node, SverchCustomTreeNode):
     bl_label = 'Text Input'
     bl_icon = 'OUTLINER_OB_EMPTY'
 
-    csv_data = collections.OrderedDict()
+# why is this shared between instances?
+
+    csv_data = {}
     
     def avail_texts(self,context):
         texts = bpy.data.texts
@@ -55,81 +57,92 @@ class TextInNode(Node, SverchCustomTreeNode):
         pass
                 
     def draw_buttons(self, context, layout):
-        layout.operator('node.sverchok_text_input', text='Load')
         layout.prop(self,"text","Text Select:")
-        row = layout.row()
-        row.prop(self,'columns','Columns?')
-        row.prop(self,'names','Named fields?')
+        layout.prop(self,'columns','Columns')
+        layout.prop(self,'names','Named fields?')
+        layout.operator('node.sverchok_text_input', text='Load')
+
         # should be able to select external file
 
     def update(self): 
                  
-        for name in self.csv_data:
+        for name in self.csv_data[self.name]:
             if name in self.outputs and len(self.outputs[name].links)>0:
                 if not self.outputs[name].node.socket_value_update:
                     self.outputs[name].node.update()
-                self.outputs[name].StringsProperty = str([self.csv_data[name]])
+                self.outputs[name].StringsProperty = str([self.csv_data[self.name][name]])
  
                         
     def update_socket(self, context):
         self.update()
 
     def load(self):
-        #reset 
-        for name in self.csv_data: 
-            del self.csv_data[name]
-            
+        
+        csv_data = collections.OrderedDict() 
+        
+        if self.name in self.csv_data:
+            del self.csv_data[self.name]
+                    
         f = bpy.data.texts[self.text].as_string()
         # should be able to select external file
         reader = csv.reader(io.StringIO(f),delimiter=',')
         
+
         if self.columns:
-            for i,row in enumerate(reader):
-             
+            for i,row in enumerate(reader):         
                 if i == 0: #setup names
                     if self.names:
                         for name in row:
                             tmp = name
-                            while not tmp in self.csv_data:
-                                j = 1
-                                tmp = name+str(j)
-                                j += 1
-                            self.csv_data[str(tmp)] = []
+                            print(name,row)
+                            c = 1
+                            while tmp in csv_data:
+                                tmp = name+str(c)
+                                c += 1
+                                print(tmp,name,c)
+    
+                            csv_data[str(tmp)] = []
                         continue #first row is names    
                     else:
                         for j in range(len(row)):
-                            self.csv_data["Col "+str(j)] = []
-                # load data               
-                for j,name in enumerate(self.csv_data):
+                            csv_data["Col "+str(j)] = []
+                # load data 
+                              
+                for j,name in enumerate(csv_data):
                     try:
-                        self.csv_data[name].append(float(row[j]))   
+                        csv_data[name].append(float(row[j]))   
                     except (ValueError, IndexError):
-                        pass #discard strings other than first
-        #rows            
-        else: 
+                        pass #discard strings other than first row
+                        
+        else:         #rows            
             for i,row in enumerate(reader):
                 name = []
                 out = []
-    
                 for j,obj in enumerate(row):
                     nr = []
                     try:
                         out.append(float(obj))   
                     except ValueError:
-                        if j == 0 and not self.names:
-                            name = row[0]
+                        if j == 0 and self.names:
+                            tmp = row[0]
+                            c = 1    
+                            while tmp in csv_data:
+                                tmp = row[0]+str(c)
+                                c += 1
+                            name = tmp
                         else:
-                            pass #discard strings other than first
+                            pass #discard strings other than first column
 
                 if not name:
                     name = "Row "+ str(i)
-                self.csv_data[name] = out   
-        
+                csv_data[name] = out   
+                
+        self.csv_data[self.name]=csv_data
         #remove sockets
         for out in self.outputs:
             self.outputs.remove(out)
         # create sockets with names, maybe implement update in future       
-        for name in self.csv_data:
+        for name in csv_data:
             self.outputs.new('StringsSocket', name, name)                 
                   
  
