d126e401da729953ea0407bdcd0e7e29d056395d
Dealga McArdle <zeffii@users.noreply.github.com>
2014-04-26 14:59:46 +0200

    added KDTree preliminary node.

--- __init__.py
+++ __init__.py
@@ -144,6 +144,7 @@ if "bpy" in locals():
     imp.reload(node_MeshJoin)
     imp.reload(node_VertSort)
     imp.reload(node_ConvexHull)
+    imp.reload(node_KDTree)
 
 else:
     import node_s
@@ -231,6 +232,7 @@ else:
     import node_MeshJoin
     import node_VertSort
     import node_ConvexHull
+    import node_KDTree
     
 def register():
     import bpy
@@ -317,6 +319,7 @@ def register():
     node_MeshJoin.register()
     node_VertSort.register()
     node_ConvexHull.register()
+    node_KDTree.register()
         
     if 'SVERCHOK' not in nodeitems_utils._node_categories:
         nodeitems_utils.register_node_categories("SVERCHOK", node_s.make_categories())
@@ -326,6 +329,7 @@ def unregister():
     import bpy
     import nodeitems_utils
     
+    node_KDTree.unregister()
     node_ConvexHull.unregister()
     node_VertSort.unregister()
     node_MeshJoin.unregister()
