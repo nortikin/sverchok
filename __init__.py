35c6be73a59340619a6117dbc4bfc8dcdd7bc074
ly29 <ly29@users.noreply.github.com>
2014-04-26 09:55:00 +0200

    KDTree node requires 2.70
    So we bump the version number.

--- __init__.py
+++ __init__.py
@@ -33,8 +33,8 @@
 bl_info = {
     "name": "Sverchok",
     "author": "Nedovizin Alexander, Gorodetskiy Nikita, Linus Yng, Agustin Jimenez, Dealga McArdle",
-    "version": (0, 2, 7),
-    "blender": (2, 6, 9), 
+    "version": (0, 2, 8),
+    "blender": (2, 7, 0), 
     "location": "Nodes > CustomNodesTree > Add user nodes",
     "description": "Do parametric node-based geometry programming",
     "warning": "requires nodes window",
