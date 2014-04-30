8b0c576262940b59a2cb4803b3c267cd2ca87165
Dealga McArdle <zeffii@users.noreply.github.com>
2014-04-30 19:29:09 +0200

    Update vector_fx.py

--- node_script_templates/vector_fx.py
+++ node_script_templates/vector_fx.py
@@ -20,7 +20,9 @@ def sv_main(verts=[], dist=1.1, strength=1.0):
     vert_add = verts_out.append
     for idx, v in enumerate(verts):
         vc = Vector(v)
-        nvc = vc + Vector((0,0,dist))
+        fdist = vc.length
+        new_z = ((2/(fdist+dist))*-1)*strength
+        nvc = vc + Vector((0,0,new_z))
         vert_add(nvc.to_tuple())
         
     out_sockets[0][2] = [verts_out]
