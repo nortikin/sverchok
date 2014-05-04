300d74427d5bba04ebeac851609f80a44b8fa337
Dealga McArdle <zeffii@users.noreply.github.com>
2014-05-04 13:13:03 +0200

    fixes for default values, (more details in ext)
    This breaks existing kdtree Edges node trees. You will have to delete and replace kdtree Edges by hand. Such is life!
    there is no such thing as `self[some_prop_name]`, so that needed to go anyway. Using a loop to set values now.

--- node_KDTree_Edges.py
+++ node_KDTree_Edges.py
@@ -54,15 +54,19 @@ class SvKDTreeEdgesNode(Node, SverchCustomTreeNode):
         except (IndexError, KeyError) as e:
             return
 
-        optional_sockets = ['mindist', 'maxdist', 'maxNum', 'skip']
+        optional_sockets = {
+            'mindist': self.mindist,
+            'maxdist': self.maxdist,
+            'maxNum': self.maxNum,
+            'skip': self.skip}
+
         socket_inputs = []
-        for socket in optional_sockets:
-            try:
-                socket_input = SvGetSocketAnyType(self, inputs[socket])[0][0]
-            except:
-                socket_input = self[socket]
-            finally:
-                socket_inputs.append(socket_input)
+        for s, s_default_value in optional_sockets.items():
+            if s in inputs and inputs[s].links:
+                sock_input = int(SvGetSocketAnyType(self, inputs[s])[0][0])
+            else:
+                sock_input = s_default_value
+            socket_inputs.append(sock_input)
 
         self.run_kdtree(socket_inputs, verts)
 
