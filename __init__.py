e1983f51970f6630bc4a4ebb0058376dcdb42273
ly29 <linusyng@live.com>
2014-05-16 15:50:01 +0200

    Bugfix unregister, AdaptiveEdges.

--- __init__.py
+++ __init__.py
@@ -413,7 +413,7 @@ def unregister():
     node_Solidify.unregister()
     node_Bisect.unregister()
     node_CrossSection.unregister()
-    node_AdaptiveEdges()
+    node_AdaptiveEdges.unregister()
     node_AdaptivePolygons.unregister()
     node_Tools.unregister()
     node_Formula2.unregister()
