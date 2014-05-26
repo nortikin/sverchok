be45c80cdf7be52aefbffc0e7c8ec8b5b7fb744a
Dealga McArdle <zeffii@users.noreply.github.com>
2014-05-26 17:10:37 +0200

    with demo file link

---
+++ node_script_templates/zeffii/max_x_filter.py
@@ -0,0 +1,37 @@
+# https://github.com/zeffii/bpy_script/blob/master/geom_daily_recodes/491_orbits_001.blend
+
+def sv_main(verts=[], edges=[], max_x=0.2):
+
+    in_sockets = [
+        ['v', 'verts', verts],
+        ['s', 'edges', edges],
+        ['s', 'max_x', max_x]]
+
+    out_sockets = [ ['v', 'Vecs', []], ['s', 'Edges', []] ]
+
+    if not verts and edges:
+        return out_sockets
+    
+    # - first find all vertices with x component larger than max_x
+    mask_indices = []
+    mask_index = mask_indices.append
+    
+    # print(verts)
+    
+    for idx, v in enumerate(verts[0]):
+        if v[0] > max_x:
+            mask_index(idx)
+    
+    out_sockets[0][2] = [verts]
+    
+    out_edges = []
+    for edge in edges[0]:
+        a, b = edge
+        if (a or b) in mask_indices:
+            continue
+        out_edges.append(edge)
+        
+    out_sockets[1][2] = [out_edges]
+
+
+    return in_sockets, out_sockets
