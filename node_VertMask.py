bf321f418e9e831d97faa7a1b2d39bd38764b2f4
ly29 <linusyng@live.com>
2014-05-29 08:23:21 +0200

    VertMask uses dataCorrect now

--- node_VertMask.py
+++ node_VertMask.py
@@ -1,7 +1,7 @@
 from node_s import *
 from util import *
 import itertools
-from itertools import cycle
+from itertools import cycle, islice
 
 class SvVertMaskNode(Node, SverchCustomTreeNode):
     '''Delete verts from mesh'''
@@ -26,28 +26,34 @@ class SvVertMaskNode(Node, SverchCustomTreeNode):
         if self.inputs['Vertices'].links and self.inputs['Poly Egde'].links:
             verts = SvGetSocketAnyType(self,self.inputs['Vertices'])
             poly = SvGetSocketAnyType(self,self.inputs['Poly Egde'])
-            
+            verts = dataCorrect(verts)
+            poly = dataCorrect(poly)
             if self.inputs['Mask'].links:
                 mask = SvGetSocketAnyType(self,self.inputs['Mask'])
             else:
                 mask = [[1,0]]
-                
+            has_true_out = True
+            has_false_out = False    
             verts_out = []
             poly_edge_out = []
             for ve,pe,ma in zip(verts,poly,repeat_last(mask)):
-                current_mask = (m for m,v in zip(cycle(ma),ve))
+                current_mask = islice(cycle(ma),len(ve))
                 vert_index = [i for i,m in enumerate(current_mask) if m]
                 if len(vert_index) < len(ve):
                     index_set = set(vert_index)
-                    f_pe = filter(lambda p:all((i in index_set for i in p)),pe)
-                    vert_dict = {j:i for i,j in enumerate(vert_index)}
-                    new_vert = [ve[i] for i in vert_index]
-                    new_pe = [[vert_dict[n] for n in fe] for fe in f_pe]
-                    verts_out.append(new_vert)
-                    poly_edge_out.append(new_pe)
+                    if has_true_out:
+                        vert_dict = {j:i for i,j in enumerate(vert_index)}
+                        new_vert = [ve[i] for i in vert_index]
+                        is_ss = index_set.issuperset
+                        new_pe = [[vert_dict[n] for n in fe] for fe in pe 
+                                                if is_ss(fe)]
+                        verts_out.append(new_vert)
+                        poly_edge_out.append(new_pe)
+                        
                 else: #no reprocessing needed
                     verts_out.append(ve)
                     poly_edge_out.append(pe)
+                
 
             if 'Vertices' in self.outputs and self.outputs['Vertices'].links:
                 SvSetSocketAnyType(self, 'Vertices',verts_out)
