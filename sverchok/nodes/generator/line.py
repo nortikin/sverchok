a7292d190e94ef6f907a6151e07308f96f8935a4
nikitron <nikitron@gmail.com>
2014-06-17 18:09:25 +0300

    line vectorized

--- sverchok/nodes/generator/line.py
+++ sverchok/nodes/generator/line.py
@@ -23,26 +23,25 @@ from node_tree import SverchCustomTreeNode
 from data_structure import (updateNode, fullList,
                             SvSetSocketAnyType, SvGetSocketAnyType)
 
-
 class LineNode(bpy.types.Node, SverchCustomTreeNode):
     ''' Line '''
     bl_idname = 'LineNode'
     bl_label = 'Line'
     bl_icon = 'OUTLINER_OB_EMPTY'
-
-    int_ = IntProperty(name='N Verts', description='Nº Vertices',
-                       default=2, min=2,
-                       options={'ANIMATABLE'}, update=updateNode)
-    step_ = FloatProperty(name='Step', description='Step length',
-                          default=1.0,
-                          options={'ANIMATABLE'}, update=updateNode)
+    
+    int_ = IntProperty(name = 'N Verts', description='Nº Vertices', 
+                    default=2, min=2,
+                    options={'ANIMATABLE'}, update=updateNode)
+    step_ = FloatProperty(name = 'Step', description='Step length',
+                    default=1.0, options={'ANIMATABLE'},
+                    update=updateNode)
 
     def init(self, context):
         self.inputs.new('StringsSocket', "Nº Vertices").prop_name = 'int_'
         self.inputs.new('StringsSocket', "Step").prop_name = 'step_'
         self.outputs.new('VerticesSocket', "Vertices", "Vertices")
         self.outputs.new('StringsSocket', "Edges", "Edges")
-
+    
     def draw_buttons(self, context, layout):
         pass
         #layout.prop(self, "int_", text="Nº Vert")
@@ -51,46 +50,49 @@ class LineNode(bpy.types.Node, SverchCustomTreeNode):
     def update(self):
         # inputs
         if 'Nº Vertices' in self.inputs and self.inputs['Nº Vertices'].links:
-            Integer = int(SvGetSocketAnyType(self, self.inputs['Nº Vertices'])[0][0])
+            Integer_ = SvGetSocketAnyType(self,self.inputs['Nº Vertices'])[0]
         else:
-            Integer = self.int_
+            Integer_ = [self.int_]
 
         if 'Step' in self.inputs and self.inputs['Step'].links:
-            Step = SvGetSocketAnyType(self, self.inputs['Step'])[0]
-
-            if len(Step) < Integer:
-                fullList(Step, Integer)
-
-            listVert = []
-            for i in range(Integer):
-                listVert.append(i*Step[i])
-            X = listVert
+            Step_ = SvGetSocketAnyType(self,self.inputs['Step'])[0]
+            
+            if len(Integer_) > len(Step_):
+                fullList(Step_, len(Integer_))
+            X=[]
+            for j, k in enumerate(Integer_):
+                listVert = []
+                for i in range(k):
+                    listVert.append(i*Step_[j])
+                X.append(listVert)
 
         else:
             Step = self.step_
-            listVert = [Step*(i) for i in range(Integer)]
-            X = listVert
-
+            X = [[Step*(i) for i in range(Integer)] for Integer in Integer_]
+            
         # outputs
         if 'Vertices' in self.outputs and self.outputs['Vertices'].links:
 
-            Y = [0.0]
-            Z = [0.0]
-            max_num = len(X)
-            fullList(Y, max_num)
-            fullList(Z, max_num)
-
-            points = list(zip(X, Y, Z))
-            SvSetSocketAnyType(self, 'Vertices', [points])
+            points = []
+            for X_obj in X:
+                Y = [0.0]
+                Z = [0.0]
+                max_num = len(X_obj)
+                fullList(Y,max_num)
+                fullList(Z,max_num)
+                points.append(list(zip(X_obj,Y,Z)))
+            SvSetSocketAnyType(self, 'Vertices',points)
 
         if 'Edges' in self.outputs and self.outputs['Edges'].links:
 
-            listEdg = []
-            for i in range(Integer-1):
-                listEdg.append((i, i+1))
+            edg = []
+            for Integer in Integer_:
+                listEdg_obj = []
+                for i in range(Integer-1):
+                    listEdg_obj.append((i, i+1))
+                edg.append(list(listEdg_obj))
 
-            edg = list(listEdg)
-            SvSetSocketAnyType(self, 'Edges', [edg])
+            SvSetSocketAnyType(self, 'Edges',edg)
 
     def update_socket(self, context):
         self.update()
