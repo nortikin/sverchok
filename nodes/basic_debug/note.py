5a115fb9c7af76f31d3eea10ed13a94feab9dc8e
ly29 <linusyng@live.com>
2014-07-08 12:37:10 +0200

    Rewritten node

--- nodes/basic_debug/note.py
+++ nodes/basic_debug/note.py
@@ -16,64 +16,19 @@
 #
 # ##### END GPL LICENSE BLOCK #####
 from ast import literal_eval
+import textwrap
 
 import bpy
-from bpy.props import StringProperty
+from bpy.props import StringProperty, IntProperty
 
 from node_tree import SverchCustomTreeNode
-from data_structure import SvSetSocketAnyType, updateNode
-
-Sv_handle_Note = {}
+from data_structure import SvSetSocketAnyType, updateNode, node_id
 
 
-class SverchokNote(bpy.types.Operator):
-    """Sverchok Note"""
-    bl_idname = "node.sverchok_note_button"
-    bl_label = "Sverchok notes"
-    bl_options = {'REGISTER', 'UNDO'}
+TEXT_WIDTH = 6
 
-    text = StringProperty(name='text',
-                          default='')
-
-    def execute(self, context):
-        name = context.screen.name
-        areas = bpy.data.screens[name].areas
-        text = literal_eval(self.text)
-
-        Sv_handle_Note[text[0]] = True
-        out = []
-        length = len(text[1])
-        width = min(47, length)  # min(round(text[2]/10), lenth)
-        text2 = ''
-        k = 1
-        for i, t in enumerate(text[1]):
-            text2 += t
-            if k == width or i == len(text[1])-1:
-                out.append(text2)
-                text2 = ''
-                k = 1
-            k += 1
-        Sv_handle_Note[text[0]+'text'] = str(out)
-        return {'FINISHED'}
-
-
-class SverchokUnNote(bpy.types.Operator):
-    """Sverchok UnNote"""
-    bl_idname = "node.sverchok_note_unbutton"
-    bl_label = "Sverchok Un notes"
-    bl_options = {'REGISTER', 'UNDO'}
-
-    text = StringProperty(name='text',
-                          default='')
-
-    def execute(self, context):
-        name = context.screen.name
-        areas = bpy.data.screens[name].areas
-        text = literal_eval(self.text)
-        
-        Sv_handle_Note[text[0]] = False
-        Sv_handle_Note[text[0]+'text'] = str(['your text here'])
-        return {'FINISHED'}
+def format_text(text, width):
+    return textwrap.wrap(text, width // TEXT_WIDTH)
 
 
 class NoteNode(bpy.types.Node, SverchCustomTreeNode):
@@ -81,63 +36,69 @@ class NoteNode(bpy.types.Node, SverchCustomTreeNode):
     bl_idname = 'NoteNode'
     bl_label = 'Note'
     bl_icon = 'OUTLINER_OB_EMPTY'
-
+    
+    def update_width(self, context):
+        global t_w
+        t_w = self.text_width
+        
+    def update_text(self, context):
+        self.format_text()
+        updateNode(self, context)
+        
     text = StringProperty(name='text',
                           default='your text here',
-                          update=updateNode)
+                          update=update_text)
+    text_cache = {}
+    n_id = StringProperty(default='')
+    
+    text_width = IntProperty(min=1, update=update_width)
 
+    def format_text(self):
+        n_id = node_id(self)
+        tl = format_text(self.text, self.width)
+        self.text_cache[n_id] = (self.width, tl)
+        return tl
+        
     def init(self, context):
-        if self.text != 'your text here':
-            self.use_custom_color = True
-            self.color = (0.5,0.5,1)
-        else:
-            self.use_custom_color = True
-            self.color = (0.05,0.05,0.1)
+        n_id = node_id(self)
         self.width = 400
         self.outputs.new('StringsSocket', "Text", "Text")
     
     def draw_buttons(self, context, layout):
-        global Sv_handle_Note
-        if self.name not in Sv_handle_Note:
-            Sv_handle_Note[self.name] = False
-            name = context.screen.name
-            areas = bpy.data.screens[name].areas
-            for ar in areas:
-                if ar.type == 'NODE_EDITOR':
-                    ar.spaces.active.node_tree.nodes[self.name].width_hidden = 320
-                    ar.spaces.active.node_tree.nodes[self.name].location[0] += 10
-            # not works in this context. Have to be rewrited.
-            # aim - to esteblish width of node 320
+            
+        row = layout.row()
+        row.prop(self, "text")
+        if self.n_id in self.text_cache:
+            data = self.text_cache.get(self.n_id)
+            if data and data[0] == self.width:
+                for line in data[1]:
+                    print(line)
+                    layout.label(text=line)
+                return
+                
+        text_lines = format_text(self.text, self.width )
+        for line in text_lines:
+            layout.label(text=line)
 
-        if not Sv_handle_Note[self.name]:
-            row = layout.row(align=True)
-            row.prop(self, 'text', text='')
-            row = layout.row(align=True)
-            row.operator('node.sverchok_note_button', text='MIND').text = str([self.name, self.text])
-
-        else:
-            ev = literal_eval(Sv_handle_Note[self.name+'text'])
-            for t in ev:
-                row = layout.row(align=True)
-                row.label(t)
-            row = layout.row(align=True)
-            row.operator('node.sverchok_note_unbutton', text='CHANGE').text = str([self.name, self.text])
-
+    def draw_buttons_ext(self, context, layout):
+        layout.prop(self, "text_width")
+        
     def update(self):
+        n_id = node_id(self)
+        
         if 'Text' in self.outputs and self.outputs['Text'].links:
-            text = [[a] for a in self.text.split()]
+            text = self.text_cache.get(n_id)
+            if not text:
+                text = self.format_text()
             SvSetSocketAnyType(self, 'Text', [text])
-
+    
+    def copy(self, node):
+        n_id = ''
 
 def register():
     bpy.utils.register_class(NoteNode)
-    bpy.utils.register_class(SverchokNote)
-    bpy.utils.register_class(SverchokUnNote)
-
 
 def unregister():
-    bpy.utils.unregister_class(SverchokUnNote)
-    bpy.utils.unregister_class(SverchokNote)
     bpy.utils.unregister_class(NoteNode)
 
 if __name__ == '__main__':
