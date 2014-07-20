6948cefb7932fb30829d1a8d966952a6d2e540de
zeffii <tetha.z@gmail.com>
2014-07-20 11:06:31 +0200

    not sure -- thought I had committed this last night

--- nodes/generator/profile.py
+++ nodes/generator/profile.py
@@ -19,6 +19,7 @@
 ''' by Dealga McArdle | 2014 '''
 
 import parser
+from ast import literal_eval
 from math import *
 from string import ascii_lowercase
 
@@ -26,18 +27,14 @@ import bpy
 from bpy.props import BoolProperty, StringProperty
 
 from node_tree import SverchCustomTreeNode
-from data_structure import (sv_Vars, updateNode, multi_socket, changable_sockets,
-                            dataSpoil, dataCorrect, levelsOflist,
-                            SvSetSocketAnyType, SvGetSocketAnyType)
-
+from data_structure import (updateNode, dataCorrect, SvSetSocketAnyType, SvGetSocketAnyType)
 
 
 idx_map = {i: j for i, j in enumerate(ascii_lowercase)}
 
 
-
 class SvProfileNode(bpy.types.Node, SverchCustomTreeNode):
-    ''' 
+    '''
     SvProfileNode generates a (set of) profiles or elevation segments using
     assignments, variables and a string descriptor like in SVG.
 
@@ -47,7 +44,6 @@ class SvProfileNode(bpy.types.Node, SverchCustomTreeNode):
     bl_icon = 'OUTLINER_OB_EMPTY'
 
     profile_str = StringProperty(default="", update=updateNode)
-
 
     def draw_buttons(self, context, layout):
         row = layout.row(align=True)
@@ -61,12 +57,8 @@ class SvProfileNode(bpy.types.Node, SverchCustomTreeNode):
         self.outputs.new('StringsSocket', "Edges", "Edges")
 
     def adjust_inputs(self):
-        ''' 
-        take care of adding new inputs when the last is full 
-        -- if needed
-        
-        warning. 26 inputs is max, you probably want to approach 
-        profile creation in a different way.
+        '''
+        takes care of adding new inputs until reaching 26, think of using SN or EK
         '''
         inputs = self.inputs
         if inputs[-1].links:
@@ -80,7 +72,6 @@ class SvProfileNode(bpy.types.Node, SverchCustomTreeNode):
         elif not inputs[-2].links:
             inputs.remove(inputs[-1])
 
-
     def update(self):
         if not ('Edges' in self.outputs):
             return
@@ -90,10 +81,41 @@ class SvProfileNode(bpy.types.Node, SverchCustomTreeNode):
             return
 
         self.adjust_inputs()
+
+        outputs = self.outputs
+        if not outputs[0].links:
+            # 0 = verts, this at least needs a connection
+            return
+
         self.process()
 
 
+    def meta_getter(self, idx, fallback):
+        pass
+
+    def homogenize_input(self):
+        '''
+        this function finds the longest input list, and adjusts all others to match it.
+        '''
+        segments = {}
+        for i, input_ in enumerate(self.inputs):
+
+            # not using .get for dict lookup because they should all be valid and known.
+            letter = idx_map[i]
+
+
+            segments[letter] = if input_.links
+
+
     def process(self):
+        segments = self.homogenize_input()
+
+        for segment in segments:
+            fstr = {}
+
+            result = literal_eval(self.profile_str)
+
+
         pass
 
 
