51eadbe9b62de7c18b6f845b03dd72c73fa8336f
zeffii <tetha.z@gmail.com>
2014-08-11 15:01:40 +0200

    move into own class

--- nodes/generator/profile.py
+++ nodes/generator/profile.py
@@ -36,179 +36,14 @@ from data_structure import fullList, updateNode, dataCorrect, SvSetSocketAnyType
 idx_map = {i: j for i, j in enumerate(ascii_lowercase)}
 
 
-class SvProfileNode(bpy.types.Node, SverchCustomTreeNode):
-    '''
-    SvProfileNode generates one or more profiles / elevation segments using;
-    assignments, variables, and a string descriptor similar to SVG.
+class PathParser(object):
 
-    This node expects simple input, or vectorized input.
-    - sockets with no input are automatically 0, not None
-    - The longest input array will be used to extend the shorter ones, using last value repeat.
-    '''
-    bl_idname = 'SvProfileNode'
-    bl_label = 'ProfileNode'
-    bl_icon = 'OUTLINER_OB_EMPTY'
-
-    def mode_change(self, context):
-        if not (self.selected_axis == self.current_axis):
-            self.label = self.selected_axis
-            self.current_axis = self.selected_axis
-            updateNode(self, context)
-
-    axis_options = [
-        ("X", "X", "", 0),
-        ("Y", "Y", "", 1),
-        ("Z", "Z", "", 2)
-    ]
-    current_axis = StringProperty(default='Z')
-
-    selected_axis = EnumProperty(
-        items=axis_options,
-        name="Type of axis",
-        description="offers basic axis output vectors X|Y|Z",
-        default="Z",
-        update=mode_change)
-
-    # profile_str = StringProperty(default="", update=updateNode)
-    profile_file = StringProperty(default="", update=updateNode)
-    filename = StringProperty(default="")
-    posxy = FloatVectorProperty(default=(0.0, 0.0), size=2)  # update=updateNode)
-    state_idx = IntProperty(default=0)
-    previous_command = StringProperty(default="START")
-
-    def draw_buttons(self, context, layout):
-        row = layout.row()
-        row.prop(self, 'selected_axis', expand=True)
-        row = layout.row(align=True)
-        row.prop(self, "profile_file", text="")
-
-    def init(self, context):
-        self.inputs.new('StringsSocket', "a", "a")
-        self.inputs.new('StringsSocket', "b", "b")
-
-        self.outputs.new('VerticesSocket', "Verts", "Verts")
-        self.outputs.new('StringsSocket', "Edges", "Edges")
-
-    def adjust_inputs(self):
-        '''
-        takes care of adding new inputs until reaching 26,
-        think of using SN or EK if you get that far.
-        '''
-        inputs = self.inputs
-        if inputs[-1].links:
-            new_index = len(inputs)
-            new_letter = idx_map.get(new_index, None)
-            if new_letter:
-                inputs.new('StringsSocket', new_letter, new_letter)
-            else:
-                print('this implementation goes up to 26 chars only, use SN or EK')
-                print('- or contact Dealga')
-        elif not inputs[-2].links:
-            inputs.remove(inputs[-1])
-
-    def update(self):
-        '''
-        update analyzes the state of the node and returns if the criteria to start processing
-        are not met.
-        '''
-
-        # keeping the file internal for now.
-        self.filename = self.profile_file.strip()
-        if not (self.filename in bpy.data.texts):
-            return
-
-        if not ('Edges' in self.outputs):
-            return
-
-        elif len([1 for inputs in self.inputs if inputs.links]) == 0:
-            ''' must have at least one input... '''
-            return
-
-        self.adjust_inputs()
-
-        # 0 == verts, this is a minimum requirement.
-        if not self.outputs[0].links:
-            return
-
-        self.process()
-
-    def meta_get(self, s_name, fallback, level):
-        '''
-        private function for the processing function, accepts level 0..2
-        - if socket has no links, then return fallback value
-        - s_name can be an index instead of socket name
-        '''
-        inputs = self.inputs
-        if inputs[s_name].links:
-            socket_in = SvGetSocketAnyType(self, inputs[s_name])
-            if level == 1:
-                data = dataCorrect(socket_in)[0]
-            elif level == 2:
-                data = dataCorrect(socket_in)[0][0]
-            else:
-                data = dataCorrect(socket_in)
-            return data
-        else:
-            return fallback
-
-    def homogenize_input(self, segments, longest):
-        '''
-        edit segments in place, extend all to match length of longest
-        '''
-        for letter, letter_dict in segments.items():
-            if letter_dict['length'] < longest:
-                fullList(letter_dict['data'], longest)
-
-    def get_input(self):
-        '''
-        collect all input socket data, and track the longest sequence.
-        '''
-        segments = {}
-        longest = 0
-        for i, input_ in enumerate(self.inputs):
-            letter = idx_map[i]
-
-            ''' get socket data, or use a fallback '''
-            data = self.meta_get(i, [0], 2)
-
-            num_datapoints = len(data)
-            segments[letter] = {'length': num_datapoints, 'data': data}
-
-            if num_datapoints > longest:
-                longest = num_datapoints
-
-        return segments, longest
-
-    def process(self):
-        segments, longest = self.get_input()
-
-        if longest < 1:
-            print('logic error, longest < 1')
-            return
-
-        self.homogenize_input(segments, longest)
-
-        full_result_verts = []
-        full_result_edges = []
-
-        for idx in range(longest):
-            result, edges = self.parse_path_file(segments, idx)
-
-            axis_fill = {
-                'X': lambda coords: (0, coords[0], coords[1]),
-                'Y': lambda coords: (coords[0], 0, coords[1]),
-                'Z': lambda coords: (coords[0], coords[1], 0)
-                }.get(self.current_axis)
-
-            result = list(map(axis_fill, result))
-            full_result_verts.append(result)
-            full_result_edges.append(edges)
-
-        if full_result_verts:
-            SvSetSocketAnyType(self, 'Verts', full_result_verts)
-
-            if self.outputs['Edges'].links:
-                SvSetSocketAnyType(self, 'Edges', full_result_edges)
+    def __init__(self, filename):
+        self.posxy = (0, 0)
+        self.previos_posxy = (0, 0)
+        self.filename = filename
+        self.state_idx = 0
+        self.previous_command = "START"
 
     def parse_path_file(self, segments, idx):
         '''
@@ -527,6 +362,183 @@ class SvProfileNode(bpy.types.Node, SverchCustomTreeNode):
             self.posxy = tuple(line_data[-1])
 
         return temp_edges
+
+
+
+class SvProfileNode(bpy.types.Node, SverchCustomTreeNode):
+    '''
+    SvProfileNode generates one or more profiles / elevation segments using;
+    assignments, variables, and a string descriptor similar to SVG.
+
+    This node expects simple input, or vectorized input.
+    - sockets with no input are automatically 0, not None
+    - The longest input array will be used to extend the shorter ones, using last value repeat.
+    '''
+    bl_idname = 'SvProfileNode'
+    bl_label = 'ProfileNode'
+    bl_icon = 'OUTLINER_OB_EMPTY'
+
+    def mode_change(self, context):
+        if not (self.selected_axis == self.current_axis):
+            self.label = self.selected_axis
+            self.current_axis = self.selected_axis
+            updateNode(self, context)
+
+    axis_options = [
+        ("X", "X", "", 0),
+        ("Y", "Y", "", 1),
+        ("Z", "Z", "", 2)
+    ]
+    current_axis = StringProperty(default='Z')
+
+    selected_axis = EnumProperty(
+        items=axis_options,
+        name="Type of axis",
+        description="offers basic axis output vectors X|Y|Z",
+        default="Z",
+        update=mode_change)
+
+    # profile_str = StringProperty(default="", update=updateNode)
+    profile_file = StringProperty(default="", update=updateNode)
+    filename = StringProperty(default="")
+    posxy = FloatVectorProperty(default=(0.0, 0.0), size=2)  # update=updateNode)
+    # state_idx = IntProperty(default=0)
+    # previous_command = StringProperty(default="START")
+
+    def draw_buttons(self, context, layout):
+        row = layout.row()
+        row.prop(self, 'selected_axis', expand=True)
+        row = layout.row(align=True)
+        row.prop(self, "profile_file", text="")
+
+    def init(self, context):
+        self.inputs.new('StringsSocket', "a", "a")
+        self.inputs.new('StringsSocket', "b", "b")
+
+        self.outputs.new('VerticesSocket', "Verts", "Verts")
+        self.outputs.new('StringsSocket', "Edges", "Edges")
+
+    def adjust_inputs(self):
+        '''
+        takes care of adding new inputs until reaching 26,
+        think of using SN or EK if you get that far.
+        '''
+        inputs = self.inputs
+        if inputs[-1].links:
+            new_index = len(inputs)
+            new_letter = idx_map.get(new_index, None)
+            if new_letter:
+                inputs.new('StringsSocket', new_letter, new_letter)
+            else:
+                print('this implementation goes up to 26 chars only, use SN or EK')
+                print('- or contact Dealga')
+        elif not inputs[-2].links:
+            inputs.remove(inputs[-1])
+
+    def update(self):
+        '''
+        update analyzes the state of the node and returns if the criteria to start processing
+        are not met.
+        '''
+
+        # keeping the file internal for now.
+        self.filename = self.profile_file.strip()
+        if not (self.filename in bpy.data.texts):
+            return
+
+        if not ('Edges' in self.outputs):
+            return
+
+        elif len([1 for inputs in self.inputs if inputs.links]) == 0:
+            ''' must have at least one input... '''
+            return
+
+        self.adjust_inputs()
+
+        # 0 == verts, this is a minimum requirement.
+        if not self.outputs[0].links:
+            return
+
+        self.process()
+
+    def meta_get(self, s_name, fallback, level):
+        '''
+        private function for the processing function, accepts level 0..2
+        - if socket has no links, then return fallback value
+        - s_name can be an index instead of socket name
+        '''
+        inputs = self.inputs
+        if inputs[s_name].links:
+            socket_in = SvGetSocketAnyType(self, inputs[s_name])
+            if level == 1:
+                data = dataCorrect(socket_in)[0]
+            elif level == 2:
+                data = dataCorrect(socket_in)[0][0]
+            else:
+                data = dataCorrect(socket_in)
+            return data
+        else:
+            return fallback
+
+    def homogenize_input(self, segments, longest):
+        '''
+        edit segments in place, extend all to match length of longest
+        '''
+        for letter, letter_dict in segments.items():
+            if letter_dict['length'] < longest:
+                fullList(letter_dict['data'], longest)
+
+    def get_input(self):
+        '''
+        collect all input socket data, and track the longest sequence.
+        '''
+        segments = {}
+        longest = 0
+        for i, input_ in enumerate(self.inputs):
+            letter = idx_map[i]
+
+            ''' get socket data, or use a fallback '''
+            data = self.meta_get(i, [0], 2)
+
+            num_datapoints = len(data)
+            segments[letter] = {'length': num_datapoints, 'data': data}
+
+            if num_datapoints > longest:
+                longest = num_datapoints
+
+        return segments, longest
+
+    def process(self):
+        segments, longest = self.get_input()
+
+        if longest < 1:
+            print('logic error, longest < 1')
+            return
+
+        self.homogenize_input(segments, longest)
+
+        full_result_verts = []
+        full_result_edges = []
+
+        for idx in range(longest):
+            path_object = PathParser(self.filename)
+            result, edges = path_object.parse_path_file(segments, idx)
+
+            axis_fill = {
+                'X': lambda coords: (0, coords[0], coords[1]),
+                'Y': lambda coords: (coords[0], 0, coords[1]),
+                'Z': lambda coords: (coords[0], coords[1], 0)
+                }.get(self.current_axis)
+
+            result = list(map(axis_fill, result))
+            full_result_verts.append(result)
+            full_result_edges.append(edges)
+
+        if full_result_verts:
+            SvSetSocketAnyType(self, 'Verts', full_result_verts)
+
+            if self.outputs['Edges'].links:
+                SvSetSocketAnyType(self, 'Edges', full_result_edges)
 
 
 def register():
