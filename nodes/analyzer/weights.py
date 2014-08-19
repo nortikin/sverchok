5227ecc7f62aeab8f639f6b9590363f74c4f5337
nikitron <nikitron@gmail.com>
2014-08-19 15:22:55 +0300

    weights preserving tail

--- nodes/analyzer/weights.py
+++ nodes/analyzer/weights.py
@@ -30,12 +30,17 @@ class SvVertexGroupNode(bpy.types.Node, SverchCustomTreeNode):
 
 
     allind = BoolProperty(name='All vertices', description='use all vertices of object', default=False, update=updateNode)
+    clear = BoolProperty(name='clear unused', description='clear weight of unindexed vertices', default=False, update=updateNode)
+    
+    
     formula = StringProperty(name='formula', description='name of object to operate on', default='Cube', update=updateNode)
 
     def draw_buttons(self, context, layout):
         layout.prop(self, "formula", text="")
         row = layout.row(align=True)
         row.prop(self, "allind", text="All vertices")
+        row.prop(self, "clear", text="clear unused")
+        
 
     x_ = IntProperty(name='VertIND' , description='indixes of corresponding vertices', default=1, update=updateNode)
     y_ = FloatProperty(name='Weights', description='weights for corresponding vertices', default=0.0, precision=3, update=updateNode)
@@ -45,40 +50,45 @@ class SvVertexGroupNode(bpy.types.Node, SverchCustomTreeNode):
         self.inputs.new('StringsSocket', "VertIND").prop_name = 'x_'
         self.inputs.new('StringsSocket', "Weights").prop_name = 'y_'
 
-
+    
     def update(self):
         
+        obj= bpy.data.objects[self.formula]
+        if self.allind:
+            verts = [[i.index for i in obj.data.vertices]]
+        else:
+            
             if 'VertIND' in self.inputs and self.inputs['VertIND'].links and \
                type(self.inputs['VertIND'].links[0].from_socket) == StringsSocket:
                 verts = SvGetSocketAnyType(self, self.inputs['VertIND'])
             else:
                 verts = [[self.x_]]
             
-            if self.allind:
-                verts = [[i.index for i in bpy.data.objects[self.formula].data.vertices]]
+        if 'Weights' in self.inputs and self.inputs['Weights'].links and \
+           type(self.inputs['Weights'].links[0].from_socket) == StringsSocket:
+            if len(SvGetSocketAnyType(self, self.inputs['Weights'])[0])>1:
+                 wei = SvGetSocketAnyType(self, self.inputs['Weights'])
+            else:
+                 wei = [SvGetSocketAnyType(self, self.inputs['Weights'])[0]*len(verts[0])]
+        else:
+            wei = [[self.y_]*len(verts[0])]
 
+
+        
+        obj.data.update()
+        
+
+        if obj.vertex_groups.active and obj.vertex_groups.active.name.find( 'Sv_VGroup' ) != -1:
     
-            if 'Weights' in self.inputs and self.inputs['Weights'].links and \
-               type(self.inputs['Weights'].links[0].from_socket) == StringsSocket:
-                if len(SvGetSocketAnyType(self, self.inputs['Weights'])[0])>1:
-                     wei = SvGetSocketAnyType(self, self.inputs['Weights'])
-                else:
-                     wei = [SvGetSocketAnyType(self, self.inputs['Weights'])[0]*len(verts[0])]
-            else:
-                wei = [[self.y_]*len(verts[0])]
-
-
-            obj= bpy.data.objects[self.formula]
-            obj.data.update()
-            if obj.vertex_groups.active and obj.vertex_groups.active.name.find( 'Sv_VGroup' ) != -1:
-        
-                g=0
-                while g!= len(wei[0]):
-                    obj.vertex_groups.active.add([verts[0][g]], wei[0][g], "REPLACE")
-                    g=g+1
-        
-            else:
-                obj.vertex_groups.active= obj.vertex_groups.new(name='Sv_VGroup')
+            if self.clear:
+                obj.vertex_groups.active.add([i.index for i in obj.data.vertices], 0, "REPLACE")
+            g=0
+            while g!= len(wei[0]):
+                obj.vertex_groups.active.add([verts[0][g]], wei[0][g], "REPLACE")
+                g=g+1
+    
+        else:
+            obj.vertex_groups.active= obj.vertex_groups.new(name='Sv_VGroup')
 
     def update_socket(self, context):
         self.update()
@@ -88,4 +98,3 @@ def register():
 
 def unregister():
     bpy.utils.unregister_class(SvVertexGroupNode)
-
