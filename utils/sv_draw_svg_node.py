443bd389173189f493ee93cda36494897e67aa67
Agustin Jiménez <ajb.azrael@gmail.com>
2014-08-20 20:08:50 +0200

    More params added
    Only few nodes left to be able to draw. Added text, button, two booleans in a row, more than one value in a slider.
    
    Is really funny to draw in this way! :)

--- utils/sv_draw_svg_node.py
+++ utils/sv_draw_svg_node.py
@@ -18,21 +18,28 @@
 
 import svgwrite
 
-
 # Sample dictionary
-#    Params can be any ot this: 'Box', 'Slider', 'Enum', 'Enum2', 'Bool'
+#    Params can be any ot this: 'Box', 'Slider', 'Enum', 'Enum2', 'Bool', 'Text', 'Button'
 #    Params structure is: ('name', 'value', 'type of param')
-#    For 'Enum2', 'name' value is: ('option1', 'option2', 'option3', etc.)
+#    Enum param: (['t1', 't2', 't3'], 't2', 'Enum')
+#    One boolean in a row: ('Mode', True, 'Bool')
+#    Two booleans in same row: (['Mode', 'Mode2'], [True, False], 'Bool')
+#    If value of an input is empty, only socket and name are displayed
+#    For more than one slider value in a row: (['A', 'B', 'C'], ['35', '45', '63'], 'Slider')
 circle_def = {'name': 'Circle',
               'outputs': [('Vertices', '', 'VerticesSocket'),
                           ('Edges', '', 'StringsSocket'),
                           ('Polygons', '', 'StringsSocket'),],
-              'params': [('Mode', True, 'Bool'),
-                         (('pr1', 'pr2', 'pr3'), 'pr2', 'Enum'),
-                         ('Order', 'pr1', 'Enum2'),
-                         ('Order', 'pr1', 'Box'),
-                         ('Prueba', '35', 'Slider'),],
-              'inputs': [('Radius', '1.00', 'StringsSocket'),
+              'params': [(['Mode'], [True], 'Bool'),
+                         (['t1', 't2', 't3'], 't2', 'Enum'),
+                         ('Order', 'test', 'Enum2'),
+                         (['Mode', 'Mode2'], [True, False], 'Bool'),
+                         ('Order', '', 'Box'),
+                         ('Testing:', '', 'Text'),
+                         ('Button Test', '', 'Button'),
+                         (['A', 'B', 'C'], ['35', '45', '63'], 'Slider'),
+                         ('Test', '35', 'Slider'),],
+              'inputs': [('Radius', '', 'StringsSocket'),
                          ('N Vertices', '24', 'StringsSocket'),
                          ('Degrees', '360º', 'StringsSocket'),]
                          }
@@ -49,39 +56,70 @@ col_boolean = '#414141'
 col_enum = '#414141'
 col_active = '#5781c3'
 col_box = '#bfbfbf'
+col_button = '#838383'
+VerticesSocket = '#e59933'
+StringsSocket = '#99ff99'
+MatrixSocket = '#33cccc'
 width = 265
 
-def slider(dwg, name, value, pos, width):
+def slider(dwg, parameter, pos, width):
     dwg.add(dwg.rect(insert=(20, pos-17), size=(width-40, 34), rx=18, ry=18, fill=col_slider, stroke=col_stroke, stroke_width=1))
-    dwg.add(dwg.path("M 30 "+str(pos)+" l 0 0 l 10 8 l 0 -16 M "+str(width-30)+" "+str(pos)+" l 0 0 l -10 8 l 0 -16", fill=col_arrows))
-    dwg.add(dwg.text(name+':', insert=(50, pos+7), fill=col_whitetext, font_size=20))
-    dwg.add(dwg.text(value, insert=(width-50, pos+7), fill=col_whitetext, font_size=20, text_anchor='end'))
+    if type(parameter[1]) is list:
+        x = (width-40)/len(parameter[1])
+        for i, v in enumerate(parameter[0]):
+            dwg.add(dwg.text(v+':', insert=(50+x*i, pos+7), fill=col_whitetext, font_size=20))
+        for i, v in enumerate(parameter[1]):
+            dwg.add(dwg.text(v, insert=(x*i+x-10, pos+7), fill=col_whitetext, font_size=20, text_anchor='end'))
+            dwg.add(dwg.path("M "+str(30+x*i)+" "+str(pos)+" l 0 0 l 10 8 l 0 -16 M "+str(x*i+x+10)+" "+str(pos)+" l 0 0 l -10 8 l 0 -16", fill=col_arrows))
+        for i in range(len(parameter[1])-1):
+            dwg.add(dwg.line(start=(20+x*(i+1), pos+17), end=(20+x*(i+1), pos-17), stroke=col_background, stroke_width=0.5))
+    else:
+        dwg.add(dwg.path("M 30 "+str(pos)+" l 0 0 l 10 8 l 0 -16 M "+str(width-30)+" "+str(pos)+" l 0 0 l -10 8 l 0 -16", fill=col_arrows))
+        dwg.add(dwg.text(parameter[0]+':', insert=(50, pos+7), fill=col_whitetext, font_size=20))
+        dwg.add(dwg.text(parameter[1], insert=(width-50, pos+7), fill=col_whitetext, font_size=20, text_anchor='end'))
 
-def boolean(dwg, name, value, pos, width):
-    dwg.add(dwg.rect(insert=(20, pos-14), size=(28, 28), rx=4, ry=4, fill=col_boolean, stroke=col_stroke, stroke_width=0.8))
-    dwg.add(dwg.text(name, insert=(60, pos+7), fill=col_darktext, font_size=20))
-    if value:
-        dwg.add(dwg.text(u'\u2713', insert=(20, pos+12), fill=col_whitetext, font_size=40))
+def boolean(dwg, parameter, pos, width):
+    a = [2 if len(parameter[0]) == 2 else 1]
+    for i in range(a[0]):
+        dwg.add(dwg.rect(insert=(20+i*(width/2-20), pos-14), size=(28, 28), rx=4, ry=4, fill=col_boolean, stroke=col_stroke, stroke_width=0.8))
+        dwg.add(dwg.text(parameter[0][i], insert=(60+i*(width/2-20), pos+7), fill=col_darktext, font_size=20))
+        if parameter[1][i]:
+            dwg.add(dwg.text(u'\u2713', insert=(20+i*(width/2-20), pos+12), fill=col_whitetext, font_size=40))
 
-def enum(dwg, name, value, pos, width):
+def enum(dwg, parameter, pos, width):
     dwg.add(dwg.rect(insert=(20, pos-17), size=(width-40, 34), rx=0, ry=0, fill=col_enum, stroke=col_stroke, stroke_width=0.8))
-    x = (width-40)/len(name)
-    for i, v in enumerate(name):
-        if v == value:
+    x = (width-40)/len(parameter[0])
+    for i, v in enumerate(parameter[0]):
+        if v == parameter[1]:
             dwg.add(dwg.rect(insert=(20+x*i, pos-16.5), size=(x, 33), fill=col_active, stroke=col_stroke, stroke_width=0))
         dwg.add(dwg.text(v, insert=(20+x/2+x*i, pos+7), fill=col_whitetext, font_size=20, text_anchor='middle'))
-    for i in range(len(name)-1):
+    for i in range(len(parameter[0])-1):
         dwg.add(dwg.line(start=(20+x*(i+1), pos+17), end=(20+x*(i+1), pos-17), stroke=col_stroke, stroke_width=0.8))
 
-def enum2(dwg, name, value, pos, width):
-    dwg.add(dwg.text(name+':', insert=(width/2-20, pos+7), fill=col_darktext, font_size=20, text_anchor='end'))
+def enum2(dwg, parameter, pos, width):
+    dwg.add(dwg.text(parameter[0]+':', insert=(width/2-20, pos+7), fill=col_darktext, font_size=20, text_anchor='end'))
     dwg.add(dwg.rect(insert=(width/2, pos-17), size=(width/2-20, 34), rx=8, ry=8, fill=col_enum, stroke=col_stroke, stroke_width=0.8))
-    dwg.add(dwg.text(value, insert=(width/2+20, pos+7), fill=col_whitetext, font_size=20))
+    dwg.add(dwg.text(parameter[1], insert=(width/2+20, pos+7), fill=col_whitetext, font_size=20))
     dwg.add(dwg.path("M "+str(width-35)+" "+str(pos-12)+" l 0 0 l 5 9 l -10 0 z M "+str(width-35)+" "+str(pos+12)+" l 0 0 l 5 -9 l -10 0 z", fill=col_arrows))
 
-def box(dwg, name, value, pos, width):
+def box(dwg, parameter, pos, width):
     dwg.add(dwg.rect(insert=(20, pos-17), size=(width-40, 34), rx=8, ry=8, fill=col_box, stroke=col_stroke, stroke_width=1))
-    dwg.add(dwg.text(value, insert=(40, pos+7), fill=col_darktext, font_size=20))
+    dwg.add(dwg.text(parameter[0], insert=(40, pos+7), fill=col_darktext, font_size=20))
+
+def text(dwg, parameter, pos, width):
+    dwg.add(dwg.text(parameter[0], insert=(30, pos+7), fill=col_darktext, font_size=20))
+
+def button(dwg, parameter, pos, width):
+    dwg.add(dwg.rect(insert=(20, pos-17), size=(width-40, 34), rx=8, ry=8, fill=col_button, stroke=col_stroke, stroke_width=1))
+    dwg.add(dwg.text(parameter[0], insert=(width/2, pos+7), fill=col_darktext, font_size=20, text_anchor='middle'))
+
+methods = {'Bool': boolean,
+           'Enum': enum,
+           'Enum2': enum2,
+           'Box': box,
+           'Slider': slider,
+           'Text': text,
+           'Button': button}
 
 def draw_node(node):
     for i in node:
@@ -101,28 +139,23 @@ def draw_node(node):
 
     for i, v in enumerate(outputs):
         y = 40 + 40*(i+1)
-        col = ['#e59933' if v[2] == 'VerticesSocket' else '#33cccc' if v[2] == 'MatrixSocket' else '#99ff99']
+        col = [VerticesSocket if v[2] == 'VerticesSocket' else MatrixSocket if v[2] == 'MatrixSocket' else StringsSocket]
         dwg.add(dwg.circle(center=(width, y), r=10, fill=col[0], stroke=col_stroke, stroke_width=1))
         dwg.add(dwg.text(v[0], insert=(width-25, y+5), fill='black', font_size=20, text_anchor='end'))
 
     for i, v in enumerate(params):
         y = 90 + 40*(len(outputs) - 1) + 40*(i+1)
-        if v[2] == 'Bool':
-            boolean(dwg, v[0], v[1], y, width)
-        elif v[2] == 'Slider':
-            slider(dwg, v[0], v[1], y, width)
-        elif v[2] == 'Enum':
-            enum(dwg, v[0], v[1], y, width)
-        elif v[2] == 'Enum2':
-            enum2(dwg, v[0], v[1], y, width)
-        elif v[2] == 'Box':
-            box(dwg, v[0], v[1], y, width)
+        if v[2] in methods:
+            methods[v[2]](dwg, v, y, width)
 
     for i, v in enumerate(inputs):
         y = 140 + 40*(len(outputs) + len(params) - 2) + 40*(i+1)
-        col = ['#e59933' if v[2] == 'VerticesSocket' else '#33cccc' if v[2] == 'MatrixSocket' else '#99ff99']
+        col = [VerticesSocket if v[2] == 'VerticesSocket' else MatrixSocket if v[2] == 'MatrixSocket' else StringsSocket]
         dwg.add(dwg.circle(center=(0, y), r=10, fill=col[0], stroke=col_stroke, stroke_width=1))
-        slider(dwg, v[0], v[1], y, width)
+        if len(v[1]) == 0:
+            dwg.add(dwg.text(v[0], insert=(30, y+7), fill=col_darktext, font_size=20))
+        else:
+            slider(dwg, v, y, width)            
                 
     dwg.save()
 
