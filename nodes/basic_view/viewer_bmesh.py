035b9252484f7886bc61f796dbb0868ee62cb0cf
zeffii <tetha.z@gmail.com>
2014-08-27 14:05:26 +0200

    totally overkill, but needed for sane random name ops.

--- nodes/basic_view/viewer_bmesh.py
+++ nodes/basic_view/viewer_bmesh.py
@@ -18,6 +18,7 @@
 
 import itertools
 import random
+import re
 
 import bpy
 from bpy.props import BoolProperty, StringProperty
@@ -28,10 +29,28 @@ from data_structure import dataCorrect, fullList, updateNode, SvGetSocketAnyType
 from utils.sv_bmesh_utils import bmesh_from_pydata
 
 
-def get_random_init():
+def natural_plus_one(object_names):
 
+    ''' sorts ['Alpha', 'Alpha1', 'Alpha11', 'Alpha2', 'Alpha23']
+        into ['Alpha', 'Alpha1', 'Alpha2', 'Alpha11', 'Alpha23']
+        and returns (23+1)
+    '''
+
+    def extended_sort(a):
+        ''' finds the digit trailing, or 0 if no digits '''
+        k = re.split('(\d*)', a)
+        return 0 if len(k) == 1 else int(k[1])
+
+    natural_sort = sorted(object_names, key=extended_sort)
+    last = natural_sort[-1]
+    num = extended_sort(last)
+    return num+1
+
+
+def get_random_init():
     # this must eventually include a check on bpy.data.objects
-    # to not return names that are present the current .blend
+    # to not return names that are present in the current .blend
+    objects = bpy.data.objects
 
     greek_alphabet = [
         'Alpha', 'Beta', 'Gamma', 'Delta',
@@ -40,7 +59,27 @@ def get_random_init():
         'Nu', 'Xi', 'Omicron', 'Pi',
         'Rho', 'Sigma', 'Tau', 'Upsilon',
         'Phi', 'Chi', 'Psi', 'Omega']
-    return random.choice(greek_alphabet)
+
+    with_underscore = lambda obj: '_' in obj.name and obj.type == 'MESH'
+    names_with_underscores = list(filter(with_underscore, objects))
+    print(names_with_underscores)
+    set_of_names_pre_underscores = set([n.name.split('_')[0] for n in names_with_underscores])
+    if '' in set_of_names_pre_underscores:
+        set_of_names_pre_underscores.remove('')
+
+    n = random.choice(greek_alphabet)
+
+    # not picked yet.
+    if not n in set_of_names_pre_underscores:
+        return n
+
+    # at this point the name was already picked, we don't want to overwrite
+    # existing obj/meshes and instead append digits onto the greek letter
+    # if Alpha is present already a new one will be Alpha2, Alpha3 etc..
+    # (not Alpha002, or Alpha.002)
+    similar_names = [name for name in set_of_names_pre_underscores if n in name]
+    plus_one = natural_plus_one(similar_names)
+    return n + str(plus_one)
 
 
 def default_mesh(name):
@@ -144,11 +183,15 @@ class BmeshViewerNode(bpy.types.Node, SverchCustomTreeNode):
     state_render = BoolProperty(default=True)
     state_select = BoolProperty(default=True)
     select_state_mesh = BoolProperty(default=False)
+
     fixed_verts = BoolProperty(
         default=False,
         name="Fixed vertices",
         description="Use only with unchanging topology")
-    autosmooth = BoolProperty(default=False, update=updateNode)
+    autosmooth = BoolProperty(
+        default=False,
+        update=updateNode,
+        description="This auto sets all faces to smooth shade")
 
     def init(self, context):
         self.use_custom_color = True
