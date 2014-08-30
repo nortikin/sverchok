4ab511ffa33a7fa88714849eda702f7951f0c5ba
zeffii <tetha.z@gmail.com>
2014-08-30 17:36:42 +0200

    adding break instead of continue

--- utils/text_editor_plugins.py
+++ utils/text_editor_plugins.py
@@ -113,13 +113,11 @@ class SvNodeRefreshFromTextEditor(bpy.types.Operator):
                 continue
 
             node_types = [node.bl_idname for node in ng.nodes]
-
             if 'SvProfileNode' in node_types:
                 nodes = [n for n in ng.nodes if n.bl_idname == 'SvProfileNode']
                 for n in nodes:
                     if n.filename == text_file_name:
                         ng.update()
-                        #continue
                         break
 
             if 'SvScriptNode' in node_types:
@@ -128,7 +126,6 @@ class SvNodeRefreshFromTextEditor(bpy.types.Operator):
                     if n.script_name == text_file_name:
                         n.load()
                         ng.update()
-                        #continue
                         break
 
         return {'FINISHED'}
