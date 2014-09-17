94af66493534c3e8b2dfed2cbd49d6434c49b061
zeffii <tetha.z@gmail.com>
2014-09-17 13:51:50 +0200

    rudimentary addition

--- nodes/modifier_change/edges_intersect_remove.py
+++ nodes/modifier_change/edges_intersect_remove.py
@@ -46,25 +46,48 @@ def select_nonintersecting(bm, d_edges):
         bm.edges[edge].select = True
 
 
+def get_nonintersection_dictionary(bm, edge_indices):
+    permutations = ei.get_valid_permutations(bm, edge_indices)
+
+    k = defaultdict(list)
+    d = defaultdict(list)
+
+    for edges in permutations:
+        raw_vert_indices = cm.vertex_indices_from_edges_tuple(bm, edges)
+        vert_vectors = cm.vectors_from_indices(bm, raw_vert_indices)
+
+        points = LineIntersect(*vert_vectors)
+
+        # some can be skipped.    (NaN, None, not on both edges)
+        if ei.can_skip(points, vert_vectors):
+            continue
+
+        # reaches this point only when an intersection happens on both edges.
+        [k[edge].append(points[0]) for edge in edges]
+
+    # k will contain a dict of edge indices and points found on those edges.
+    for edge_idx, unordered_points in k.items():
+        tv1, tv2 = bm.edges[edge_idx].verts
+        v1 = bm.verts[tv1.index].co
+        v2 = bm.verts[tv2.index].co
+        ordered_points = ei.order_points((v1, v2), unordered_points)
+        d[edge_idx].extend(ordered_points)
+
+    return d
+
+
 class SvNonIntersectEdgesNode(SvIntersectEdgesNode):
 
     bl_idname = 'SvNonIntersectEdgesNode'
     bl_label = 'Non Intersect Edges Node'
     bl_icon = 'OUTLINER_OB_EMPTY'
 
-    # def draw_buttons(self, context, layout):
-    #     pass
-
-    # def init(self, context):
-    #     self.inputs.new('VerticesSocket', 'Verts_in', 'Verts_in')
-    #     self.inputs.new('StringsSocket', 'Edges_in', 'Edges_in')
-
-    #     self.outputs.new('VerticesSocket', 'Verts_out', 'Verts_out')
-    #     self.outputs.new('StringsSocket', 'Edges_out', 'Edges_out')
-
     def update(self):
         inputs = self.inputs
         outputs = self.outputs
+
+        if not (len(outputs) == 2):
+            return
 
         try:
             verts_in = SvGetSocketAnyType(self, inputs['Verts_in'])[0]
@@ -83,7 +106,7 @@ class SvNonIntersectEdgesNode(SvIntersectEdgesNode):
         for edge in bm.edges:
             edge.select = False
 
-        d = ei.get_intersection_dictionary(bm, edge_indices)
+        d = get_nonintersection_dictionary(bm, edge_indices)
         select_nonintersecting(bm, d.keys())
 
         verts_out = [v.co.to_tuple() for v in bm.verts]
@@ -91,9 +114,6 @@ class SvNonIntersectEdgesNode(SvIntersectEdgesNode):
 
         SvSetSocketAnyType(self, 'Verts_out', [verts_out])
         SvSetSocketAnyType(self, 'Edges_out', [edges_out])
-
-    # def update_socket(self, context):
-    #     self.update()
 
 
 def register():
