c9c408ead8465e6458693f448420d6d17bdb8e47
zeffii <tetha.z@gmail.com>
2014-09-27 15:02:20 +0200

    temporary  :)

--- core/handlers.py
+++ core/handlers.py
@@ -7,6 +7,16 @@ from utils import viewer_draw_mk2
 from utils import index_viewer_draw
 from utils import nodeview_bgl_viewer_draw
 
+sv_ascii_logo = """\
+      ::::::::  :::     ::: :::::::::: :::::::::   ::::::::  :::    :::  ::::::::  :::    ::: 
+    :+:    :+: :+:     :+: :+:        :+:    :+: :+:    :+: :+:    :+: :+:    :+: :+:   :+:   
+   +:+        +:+     +:+ +:+        +:+    +:+ +:+        +:+    +:+ +:+    +:+ +:+  +:+     
+  +#++:++#++ +#+     +:+ +#++:++#   +#++:++#:  +#+        +#++:++#++ +#+    +:+ +#++:++       
+        +#+  +#+   +#+  +#+        +#+    +#+ +#+        +#+    +#+ +#+    +#+ +#+  +#+       
+#+#    #+#   #+#+#+#   #+#        #+#    #+# #+#    #+# #+#    #+# #+#    #+# #+#   #+#       
+########      ###     ########## ###    ###  ########  ###    ###  ########  ###    ###       
+"""
+
 
 @persistent
 def sv_update_handler(scene):
@@ -89,7 +99,8 @@ def set_frame_change(mode):
     elif mode == "PRE":
         print("Removed Sverchok handler pre")
         pre.append(sv_update_handler)
-    print("Have a nice day with Sverchok")
+    print("Have a nice day with sverchok")
+    print(sv_ascii_logo)    
     print("****** Sverchok loaded ******\n")
 
 
