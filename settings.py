73a348e0c6366fb3ce3b934c0efd56d5fe2af365
zeffii <tetha.z@gmail.com>
2014-10-10 08:53:48 +0200

    rejigg

--- settings.py
+++ settings.py
@@ -23,47 +23,53 @@ class SverchokPreferences(AddonPreferences):
     def set_frame_change(self, context):
         handlers.set_frame_change(self.frame_change_mode)
 
-    show_debug = BoolProperty(name="Print update timings",
-                              description="Print update timings in console",
-                              default=False, subtype='NONE',
-                              update=update_debug_mode)
+    show_debug = BoolProperty(
+        name="Print update timings",
+        description="Print update timings in console",
+        default=False, subtype='NONE',
+        update=update_debug_mode)
 
-    heat_map = BoolProperty(name="Heat map",
-                            description="Color nodes according to time",
-                            default=False, subtype='NONE',
-                            update=update_heat_map)
+    heat_map = BoolProperty(
+        name="Heat map",
+        description="Color nodes according to time",
+        default=False, subtype='NONE',
+        update=update_heat_map)
 
-    heat_map_hot = FloatVectorProperty(name="Heat map hot", description='',
-                                       size=3, min=0.0, max=1.0,
-                                       default=(.8, 0, 0), subtype='COLOR')
+    heat_map_hot = FloatVectorProperty(
+        name="Heat map hot", description='',
+        size=3, min=0.0, max=1.0,
+        default=(.8, 0, 0), subtype='COLOR')
 
-    heat_map_cold = FloatVectorProperty(name="Heat map cold", description='',
-                                        size=3, min=0.0, max=1.0,
-                                        default=(1, 1, 1), subtype='COLOR')
+    heat_map_cold = FloatVectorProperty(
+        name="Heat map cold", description='',
+        size=3, min=0.0, max=1.0,
+        default=(1, 1, 1), subtype='COLOR')
 
-    frame_change_modes = \
-            [("PRE", "Pre", "Update Sverchok before frame change", 0),
-             ("POST", "Post", "Update Sverchok after frame change", 1),
-             ("NONE", "None", "Sverchok doesn't update on frame change", 2)]
- 
-    frame_change_mode = EnumProperty(items=frame_change_modes, 
-                            name="Frame change",
-                            description="Select frame change handler", 
-                            default="POST",
-                            update=set_frame_change)
-    
+    frame_change_modes = [
+        ("PRE", "Pre", "Update Sverchok before frame change", 0),
+        ("POST", "Post", "Update Sverchok after frame change", 1),
+        ("NONE", "None", "Sverchok doesn't update on frame change", 2)
+    ]
+
+    frame_change_mode = EnumProperty(
+        items=frame_change_modes,
+        name="Frame change",
+        description="Select frame change handler",
+        default="POST",
+        update=set_frame_change)
+
     show_icons = BoolProperty(
         name="show_icons",
         default=False,
         description="Use icons in menu")
- 
+
     def draw(self, context):
         layout = self.layout
 
         col = layout.column()
         col.label(text="General")
         col.label(text="Frame change handler:")
-        row =  col.row()
+        row = col.row()
         row.prop(self, "frame_change_mode", expand=True)
         col.separator()
         col.label(text="Debug")
@@ -76,16 +82,17 @@ class SverchokPreferences(AddonPreferences):
         row.prop(self, "heat_map_cold")
 
         col.separator()
-        row=layout.row()
+        row = layout.row()
         row.operator('wm.url_open', text='Home!').url = 'http://nikitron.cc.ua/blend_scripts.html'
         if sv_tools.sv_new_version:
             row.operator('node.sverchok_update_addon', text='Upgrade Sverchok addon')
         else:
             row.operator('node.sverchok_check_for_upgrades', text='Check for new version')
 
+
 def register():
     bpy.utils.register_class(SverchokPreferences)
-    
+
 
 def unregister():
     bpy.utils.unregister_class(SverchokPreferences)
