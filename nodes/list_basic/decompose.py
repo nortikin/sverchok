ac29ebd3c03c60dddfb15ad1c1fbc6257018b278
nortikin <nikitron@gmail.com>
2014-10-23 00:42:04 +0300

    decomposer list completed and ok. test it. need manual data_structure sync with 0.5

--- nodes/list_basic/decompose.py
+++ nodes/list_basic/decompose.py
@@ -21,7 +21,8 @@ from bpy.props import BoolProperty, IntProperty, StringProperty
 
 from node_tree import SverchCustomTreeNode
 from data_structure import (levelsOflist, multi_socket, changable_sockets,
-                            get_socket_type_full, SvSetSocket, SvGetSocketAnyType)
+                            get_socket_type_full, SvSetSocket, SvGetSocketAnyType,
+                            updateNode)
 
 
 class SvListDecomposeNode(bpy.types.Node, SverchCustomTreeNode):
@@ -34,65 +35,51 @@ class SvListDecomposeNode(bpy.types.Node, SverchCustomTreeNode):
     base_name = 'data'
     multi_socket_type = 'StringsSocket'
 
-    # two veriables for adaptive socket
     typ = StringProperty(name='typ',
                          default='')
     newsock = BoolProperty(name='newsock',
                            default=False)
-
+                           
     level = IntProperty(name='level',
-                        default=1, min=0)
+                        default=1, min=1, update=updateNode)
 
     def draw_buttons(self, context, layout):
         col = layout.column(align=True)
         col.prop(self, 'level')
 
     def init(self, context):
-        # initial socket, is defines type of output
         self.inputs.new('StringsSocket', "data", "data")
-        # adaptive multy socket
         self.outputs.new('StringsSocket', "data", "data")
 
     def update(self):
         if 'data' in self.inputs and self.inputs['data'].links:
-            # get any type socket from input:
             data = SvGetSocketAnyType(self, self.inputs['data'])
 
-            # Process data
-            leve = min((levelsOflist(data)-2), self.level)
-            result = self.beat(data, leve, leve)
+            leve = levelsOflist(data)
+            if leve+1 < self.level:
+                self.level = leve+1
+            result = self.beat(data, self.level)
+            
 
-            # multisocket - from util(formula node)
+            self.multi_socket_type = get_socket_type_full(self, 'data')
             multi_socket(self, min=1, start=2, breck=True, output=len(result))
-
-            # adaptive socket - from util(mask list node)
-            # list to pack and change type of multysockets in output... maybe not so quick
             outputsocketname = [name.name for name in self.outputs]
             changable_sockets(self, 'data', outputsocketname)
-            self.multi_socket_type = get_socket_type_full(self, 'data')
 
-            # how to assign correct property to adaptive output:
-            # in nearest future with socket's data' dictionary we will send
-            # only node_name+layout_name+socket_name in str() format
-            # and will make separate definition to easyly assign and
-            # get and recognise data from dictionary
             for i, out in enumerate(result):
+                if i > 30:
+                    break
                 SvSetSocket(self.outputs[i], out)
-                if i >= 32: break
 
-    def beat(self, data, level, left):
+    def beat(self, data, level, count=1):
         out = []
-        if left:
+        if level > 1:
             for objects in data:
-                out.extend(self.beat(objects, level, left-1))
-        elif level:
-            if type(data) not in (int, float):
-                for objects in data:
-                    out.append([self.beat(objects, level-1, 0)])
-            else:
-                return data
+                out.extend(self.beat(objects, level-1, count+1))
         else:
-            out.extend([data])
+            for i in range(count):
+                data = [data]
+            return data
         return out
 
 
@@ -104,3 +91,5 @@ def unregister():
     bpy.utils.unregister_class(SvListDecomposeNode)
 
 
+if __name__ == '__main__':
+    register()
