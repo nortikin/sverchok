e02a9ca38f0da48ffe21fa313993b1b8f7ed6f63
nortikin <nikitron@gmail.com>
2014-11-04 17:58:24 +0200

    viewer text but some bugs left

--- nodes/basic_view/viewer_text.py
+++ nodes/basic_view/viewer_text.py
@@ -22,10 +22,6 @@ from bpy.props import StringProperty
 from sverchok.node_tree import SverchCustomTreeNode
 from sverchok.data_structure import levelsOflist, SvGetSocketAnyType
 
-# global cache_viewer_slot1, cache_viewer_slot2, cache_viewer_slot3
-cache_viewer_slot1 = {}  # {'veriable':'None \n'}
-cache_viewer_slot2 = {}  # {'veriable':'None \n'}
-cache_viewer_slot3 = {}  # {'veriable':'None \n'}
 
 
 class SverchokViewer(bpy.types.Operator):
@@ -34,158 +30,100 @@ class SverchokViewer(bpy.types.Operator):
     bl_label = "Sverchok viewer"
     bl_options = {'REGISTER', 'UNDO'}
 
-    nodename = StringProperty(name='nodename',
-                              default='None')
+    nodename = StringProperty(name='nodename')
+    treename = StringProperty(name='treename')
 
     def execute(self, context):
-        global cache_viewer_slot1
-        global cache_viewer_slot2
-        global cache_viewer_slot3
+        inputs = bpy.data.node_groups[self.treename].nodes[self.nodename].inputs
 
+        # vertices socket
+        out_verts = 'None \n'
+        if inputs['vertices'].links:
+            if type(inputs['vertices'].links[0].from_socket) == bpy.types.VerticesSocket:
+                evaverti = inputs['vertices'].sv_get()
+                
+                deptl = levelsOflist(evaverti)
+                if deptl and deptl > 2:
+                    a = self.readFORviewer_sockets_data(evaverti, deptl, len(evaverti))
+                elif deptl:
+                    a = self.readFORviewer_sockets_data_small(evaverti, deptl, len(evaverti))
+                else:
+                    a = 'None \n'
+                out_verts = a
+
+        # edges/faces socket
+        out_edgpol = 'None \n'
+        edpotype = '\n\ndata \n'
+        if inputs['edg_pol'].links:
+            if type(inputs['edg_pol'].links[0].from_socket) == bpy.types.StringsSocket:
+                evaline_str = inputs['edg_pol'].sv_get()
+
+                if evaline_str:
+                    edpotype = self.edgDef(evaline_str)
+
+                deptl = levelsOflist(evaline_str)
+                if deptl and deptl > 2:
+                    b = self.readFORviewer_sockets_data(evaline_str, deptl, len(evaline_str))
+                elif deptl:
+                    b = self.readFORviewer_sockets_data_small(evaline_str, deptl, len(evaline_str))
+                else:
+                    b = 'None \n'
+                out_edgpol = str(b)
+
+        # matrix socket
+        out_matrix = 'None \n'
+        if inputs['matrix'].links:
+            if type(inputs['matrix'].links[0].from_socket) == bpy.types.MatrixSocket:
+                eva = inputs['matrix'].sv_get()
+
+                deptl = levelsOflist(eva)
+                if deptl and deptl > 2:
+                    c = self.readFORviewer_sockets_data(eva, deptl, len(eva))
+                elif deptl:
+                    c = self.readFORviewer_sockets_data_small(eva, deptl, len(eva))
+                else:
+                    c = 'None \n'
+                out_matrix = str(c)
+
+        self.do_text(out_verts, out_edgpol, out_matrix, edpotype)
+        return {'FINISHED'}
+
+    def do_text(self,vertices,edgspols,matrices,edpotype):
         texts = bpy.data.texts.items()
         exists = False
         for t in texts:
             if bpy.data.texts[t[0]].name == 'Sverchok_viewer':
                 exists = True
                 break
-        
-        #bpy.context.area.type = 'TEXT_EDITOR'
+
         if not exists:
             bpy.data.texts.new('Sverchok_viewer')
-        #bpy.ops.text.select_all()
-        podpis = '\n' + '\n' \
+        podpis = '\n'*2 \
                 + '**************************************************' + '\n' \
                 + '                     The End                      '
-
-
-        if cache_viewer_slot1['veriable'+self.nodename] or cache_viewer_slot2['veriable'+self.nodename] or cache_viewer_slot3['veriable'+self.nodename]:
-            for_file = 'node name: ' + self.nodename \
-                        + '\n\nvertices: \n' \
-                        + cache_viewer_slot1['veriable'+self.nodename] \
-                        + cache_viewer_slot2['type'+self.nodename] \
-                        + cache_viewer_slot2['veriable'+self.nodename] \
-                        + '\n\nmatrixes: \n' \
-                        + cache_viewer_slot3['veriable'+self.nodename] \
-                        + podpis
-        else:
-            for_file = 'vertices: \nNone' \
-                        + '\ndata: \nNone' \
-                        + '\nmatrixes: \nNone' + podpis
+        for_file = 'node name: ' + self.nodename \
+                    + '\n\nvertices: \n' \
+                    + vertices \
+                    + edpotype \
+                    + edgspols \
+                    + '\n\nmatrixes: \n' \
+                    + matrices \
+                    + podpis
         bpy.data.texts['Sverchok_viewer'].clear()
         bpy.data.texts['Sverchok_viewer'].write(for_file)
-        #before was ugly solution
-        #bpy.data.texts['Sverchok_viewer'].from_string(for_file)
-        #bpy.context.area.type = 'NODE_EDITOR'
-        #print (cache_viewer_slot1['veriable'], cache_viewer_slot2['veriable'], cache_viewer_slot3['veriable'])
-        #cache_viewer_slot1['veriable'] = 'None \n'
-        #cache_viewer_slot2['veriable'] = 'None \n'
-        #cache_viewer_slot3['veriable'] = 'None \n'
-        return {'FINISHED'}
-
-
-class ViewerNode_text(bpy.types.Node, SverchCustomTreeNode):
-    ''' Viewer Node text '''
-    bl_idname = 'ViewerNode_text'
-    bl_label = 'Viewer text'
-    bl_icon = 'OUTLINER_OB_EMPTY'
-
-    def sv_init(self, context):
-        self.use_custom_color = True
-        self.color = (0.05, 0.05, 0.1)
-        self.inputs.new('VerticesSocket', 'vertices', 'vertices')
-        self.inputs.new('StringsSocket', 'edg_pol', 'edg_pol')
-        self.inputs.new('MatrixSocket', 'matrix', 'matrix')
-
-    def draw_buttons(self, context, layout):
-        row = layout.row()
-        row.scale_y = 4.0
-        row.operator('node.sverchok_viewer_button', text='V I E W').nodename = self.name
-
-    def process(self):
-        # vertices socket
-
-        global cache_viewer_slot1
-        global cache_viewer_slot2
-        global cache_viewer_slot3
-        if 'vertices' in self.inputs and 'edg_pol' in self.inputs and 'matrix' in self.inputs:
-            if self.inputs['vertices'].links:
-
-                if type(self.inputs['vertices'].links[0].from_socket) == bpy.types.VerticesSocket:
-                    evaverti = SvGetSocketAnyType(self, self.inputs['vertices'])
-                    #evaverti = eval(verti)
-                    deptl = levelsOflist(evaverti)
-                    #print(str(evaverti))
-                    #print (deptl, ' text viewer')
-                    if deptl and deptl > 2:
-                        a = self.readFORviewer_sockets_data(evaverti, deptl, len(evaverti))
-                    elif deptl:
-                        a = self.readFORviewer_sockets_data_small(evaverti, deptl, len(evaverti))
-                    else:
-                        a = 'None \n'
-                    cache_viewer_slot1['veriable'+self.name] = a
-                    #print ('viewer text input1')
-            else:
-                cache_viewer_slot1['veriable'+self.name] = 'None \n'
-            # edges/faces socket
-            if self.inputs['edg_pol'].links:
-
-                if type(self.inputs['edg_pol'].links[0].from_socket) == bpy.types.StringsSocket:
-                    evaline_str = SvGetSocketAnyType(self, self.inputs['edg_pol'])
-                    #print (line_str)
-
-                    if evaline_str:
-                        cache_viewer_slot2['type'+self.name] = str(self.edgDef(evaline_str))
-                    deptl = levelsOflist(evaline_str)
-                    #print(str(evaline_str))
-                    #print (deptl, ' text viewer')
-                    if deptl and deptl > 2:
-                        b = self.readFORviewer_sockets_data(evaline_str, deptl, len(evaline_str))
-                    elif deptl:
-                        b = self.readFORviewer_sockets_data_small(evaline_str, deptl, len(evaline_str))
-                    else:
-                        b = 'None \n'
-                    cache_viewer_slot2['veriable'+self.name] = str(b)
-                    #print ('viewer text input2')
-            else:
-                cache_viewer_slot2['veriable'+self.name] = 'None \n'
-                cache_viewer_slot2['type'+self.name] = '\n\ndata \n'
-            # matrix socket
-            if self.inputs['matrix'].links:
-
-                if type(self.inputs['matrix'].links[0].from_socket) == bpy.types.MatrixSocket:
-                    eva = SvGetSocketAnyType(self, self.inputs['matrix'])
-                    deptl = levelsOflist(eva)
-                    #print (deptl, ' text viewer')
-                    if deptl and deptl > 2:
-                        c = self.readFORviewer_sockets_data(eva, deptl, len(eva))
-                    elif deptl:
-                        c = self.readFORviewer_sockets_data_small(eva, deptl, len(eva))
-                    else:
-                        c = 'None \n'
-                    cache_viewer_slot3['veriable'+self.name] = str(c)
-                    #print ('viewer text input3')
-            else:
-                cache_viewer_slot3['veriable'+self.name] = 'None \n'
-
-            if self.inputs['matrix'].links or self.inputs['vertices'].links or \
-                        self.inputs['edg_pol'].links:
-                self.use_custom_color = True
-                self.color = (0.5, 0.5, 1)
-            else:
-                self.use_custom_color = True
-                self.color = (0.05, 0.05, 0.1)
 
     def edgDef(self, l):
         t = '\n\ndata: \n'
-        if l[0] and type(l[0]) in [int, float, str]:
-            if len(l) > 2:
-                t = '\n\npolygons: \n'
+        if l[0]:
+            # checking failed :(
+            if isinstance(l[0], str) or isinstance(l[0], int) or isinstance(l[0], float):
+                print('type')
+                if len(l) > 2:
+                    t = '\n\npolygons: \n'
+                else:
+                    t = '\n\nedges: \n'
             else:
-                t = '\n\nedges: \n'
-        elif l[0]:
-            t = self.edgDef(l[0])
-        else:
-            pass
+                t = self.edgDef(l[0])
         return t
 
     def readFORviewer_sockets_data(self, data, dept, le):
@@ -219,6 +157,39 @@ class ViewerNode_text(bpy.types.Node, SverchCustomTreeNode):
             for k, val in enumerate(data):
                 output += ('\n' + str(val))
         return cache + output
+
+class ViewerNode_text(bpy.types.Node, SverchCustomTreeNode):
+    ''' Viewer Node text '''
+    bl_idname = 'ViewerNode_text'
+    bl_label = 'Viewer text'
+    bl_icon = 'OUTLINER_OB_EMPTY'
+
+    def sv_init(self, context):
+        self.use_custom_color = True
+        self.color = (0.05, 0.05, 0.1)
+        self.inputs.new('VerticesSocket', 'vertices', 'vertices')
+        self.inputs.new('StringsSocket', 'edg_pol', 'edg_pol')
+        self.inputs.new('MatrixSocket', 'matrix', 'matrix')
+
+    def draw_buttons(self, context, layout):
+        row = layout.row()
+        row.scale_y = 4.0
+        do_text = row.operator('node.sverchok_viewer_button', text='V I E W')
+        do_text.nodename = self.name
+        do_text.treename = self.id_data.name
+
+    def process(self):
+        pass
+    def update(self):
+        if 'vertices' in self.inputs and 'edg_pol' in self.inputs and 'matrix' in self.inputs:
+            if self.inputs['matrix'].links or self.inputs['vertices'].links or \
+                        self.inputs['edg_pol'].links:
+                self.use_custom_color = True
+                self.color = (0.5, 0.5, 1)
+            else:
+                self.use_custom_color = True
+                self.color = (0.05, 0.05, 0.1)
+
 
 
 def register():
