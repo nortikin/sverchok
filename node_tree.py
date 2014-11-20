e16fd09d00b758e78b16575d0e8233b372fcc3e5
ly29 <linusyng@live.com>
2014-11-20 09:14:24 +0100

    Remove sockets from node_tree, common socket interface

--- node_tree.py
+++ node_tree.py
@@ -25,19 +25,17 @@ from bpy.props import StringProperty, BoolProperty, FloatVectorProperty, IntProp
 from bpy.types import NodeTree, NodeSocket, NodeSocketStandard
 
 from sverchok import data_structure
-from sverchok.data_structure import (SvGetSocketInfo, SvGetSocket,
-                                     SvSetSocket, updateNode,
+from sverchok.data_structure import (updateNode,
                                      get_other_socket, SvNoDataError)
+
+# only import these to keep compability for now
+from sverchok.sockets import MatrixSocket, VerticesSocket, StringsSocket
 
 from sverchok.core.update_system import (build_update_list, process_from_node,
                                          process_tree, get_update_lists)
 from sverchok.ui import color_def
 
-sentinel = object()
 
-
-def process_from_socket(self, context):
-    self.node.process_node(context)
 
 
 # this property group is only used by the old viewer draw
@@ -50,197 +48,9 @@ class SvColors(bpy.types.PropertyGroup):
         update=updateNode)
 
 
-class SvObjectSocket(NodeSocket):
-    bl_idname = "SvObjectSocket"
-    bl_label = "Object Socket"
-    
-    object_ref = StringProperty(update=process_from_socket)
-    
-    def sv_get(self, default=sentinel, deepcopy=False):
-        if self.is_linked and not self.is_output:
-            return SvGetSocket(self, deepcopy)
-        elif self.object_ref:
-            obj = bpy.data.objects.get(self.object_ref)
-            if obj:
-                return [obj]
-            else:
-                raise SvNoDataError
-        else:
-            raise SvNoDataError
-
-    def sv_set(self, data):
-        SvSetSocket(self, data)
-
-    def draw(self, context, layout, node, text):
-        if self.is_linked:
-            layout.label(text + '. ' + SvGetSocketInfo(self))
-        elif self.is_output:
-            layout.label(text)
-        else:
-            layout.prop_search(self, 'object_ref', bpy.data, 'objects', text=text)
-
-    def draw_color(self, context, node):
-        return(.9, .8, .8, 1.0)
-    
-    
-class SvTextSocket(NodeSocket):
-    bl_idname = "SvTextSocket"
-    bl_label = "Text Socket"
-    
-    text = StringProperty(update=process_from_socket)
-    
-    def sv_get(self, default=None, deepcopy=False):
-        if self.is_linked and not self.is_output:
-            return SvGetSocket(self, deepcopy)
-        else:
-            return [self.text]
-
-    def sv_set(self, data):
-        SvSetSocket(self, data)
-
-    def draw(self, context, layout, node, text):
-        if self.is_linked:
-            layout.label(text + '. ' + SvGetSocketInfo(self))
-        elif self.is_output:
-            layout.label(text)
-        else:
-            layout.prop(self, "text")
-
-    def draw_color(self, context, node):
-        '''if self.is_linked:
-            return(.8,.3,.75,1.0)
-        else: '''
-        return(.1, .1, .1, 1.0)
-    
-    
-    
-class MatrixSocket(NodeSocket):
-    '''4x4 matrix Socket_type'''
-    # ref: http://urchn.org/post/nodal-transform-experiment
-    bl_idname = "MatrixSocket"
-    bl_label = "Matrix Socket"
-    prop_name = StringProperty(default='')
-    
-
-    def sv_get(self, default=sentinel, deepcopy=False):
-        if self.is_linked and not self.is_output:
-            return SvGetSocket(self, deepcopy)
-        elif default is sentinel:
-            raise SvNoDataError
-        else:
-            return default
-
-    def sv_set(self, data):
-        SvSetSocket(self, data)
-
-    def draw(self, context, layout, node, text):
-        if self.is_linked:
-            layout.label(text + '. ' + SvGetSocketInfo(self))
-        else:
-            layout.label(text)
-
-    def draw_color(self, context, node):
-        '''if self.is_linked:
-            return(.8,.3,.75,1.0)
-        else: '''
-        return(.2, .8, .8, 1.0)
 
 
-class VerticesSocket(NodeSocket):
-    '''String Vertices - one string'''
-    bl_idname = "VerticesSocket"
-    bl_label = "Vertices Socket"
-
-    prop = FloatVectorProperty(default=(0, 0, 0), size=3, update=process_from_socket)
-    prop_name = StringProperty(default='')
-    use_prop = BoolProperty(default=False)
-    
-    
-    def sv_get(self, default=sentinel, deepcopy=False):
-        if self.is_linked and not self.is_output:
-            return SvGetSocket(self, deepcopy)
-        if self.prop_name:
-            return [[getattr(self.node, self.prop_name)[:]]]
-        elif self.use_prop:
-            return [[self.prop[:]]]
-        elif default is sentinel:
-            raise SvNoDataError
-        else:
-            return default
-            
-    def sv_set(self, data):
-        SvSetSocket(self, data)
-
-    def draw(self, context, layout, node, text):
-        if not self.is_output and not self.is_linked:
-            if self.prop_name:
-                layout.template_component_menu(node, self.prop_name, name=self.name)
-            elif self.use_prop:
-                layout.template_component_menu(self, "prop", name=self.name)
-            else:
-                layout.label(text)
-        elif self.is_linked:
-            layout.label(text + '. ' + SvGetSocketInfo(self))
-        else:
-            layout.label(text)
-
-    def draw_color(self, context, node):
-        return(0.9, 0.6, 0.2, 1.0)
-
-
-class StringsSocket(NodeSocketStandard):
-    '''String any type - one string'''
-    bl_idname = "StringsSocket"
-    bl_label = "Strings Socket"
-
-    prop_name = StringProperty(default='')
-
-    prop_type = StringProperty(default='')
-    prop_index = IntProperty()
-
-
-    def sv_get(self, default=sentinel, deepcopy=False):
-        if self.is_linked and not self.is_output:
-            return SvGetSocket(self, deepcopy)
-        elif self.prop_name:
-            return [[getattr(self.node, self.prop_name)]]
-        elif self.prop_type:
-            return [[getattr(self.node, self.prop_type)[self.prop_index]]]
-        elif default is not sentinel:
-            return default
-        else:
-            raise SvNoDataError
-
-    def sv_set(self, data):
-        SvSetSocket(self, data)
-
-    def draw(self, context, layout, node, text):
-        if self.prop_name:
-            if self.is_output:
-                t = text
-                msg = "Warning output socket: {name} in node: {node} has property attached"
-                print(msg.format(name=self.name, node=node.name))
-            else:
-                prop = node.rna_type.properties.get(self.prop_name, None)
-                t = prop.name if prop else text
-        else:
-            t = text
-
-        if not self.is_output and not self.is_linked:
-            if self.prop_name and not self.prop_type:
-                layout.prop(node, self.prop_name)
-            elif self.prop_type:
-                layout.prop(node, self.prop_type, index=self.prop_index, text=self.name)
-            else:
-                layout.label(t)
-        elif self.is_linked:
-            layout.label(t + '. ' + SvGetSocketInfo(self))
-        else:
-            layout.label(t)
-
-    def draw_color(self, context, node):
-        return(0.6, 1.0, 0.6, 1.0)
-
+# Node tree 
 
 class SvNodeTreeCommon(object):
     '''
@@ -401,11 +211,6 @@ classes = [
     SvColors,
     SverchCustomTree,
     SverchGroupTree,
-    MatrixSocket,
-    StringsSocket,
-    VerticesSocket,
-    SvTextSocket,
-    SvObjectSocket
 ]
 
 def register():
