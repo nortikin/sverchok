efc6f04b13511e947c482a3bbc5febf9f82aa952
ly29 <linusyng@live.com>
2014-12-01 11:59:43 +0100

    crash prevention

--- core/update_system.py
+++ core/update_system.py
@@ -62,7 +62,13 @@ def make_dep_dict(node_tree, down=False):
                      for name, node in ng.nodes.items()
                      if node.bl_idname == 'WifiInNode'}
         
-    for link in ng.links:
+    for i,link in enumerate(list(ng.links)):
+        #  this proctects against a rare occurance where 
+        #  a link is considered valid without a to_socket
+        #  and from_socket. proctects against a blender crash
+        if not (link.to_socket and link.from_socket):
+            ng.links.remove(link)
+            raise ValueError("Invalid link found!, please report this file")
         if not link.is_valid:
             return collections.defaultdict(set)  # this happens more often than one might think
         if link.is_hidden:
