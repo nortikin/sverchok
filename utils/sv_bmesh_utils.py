6ae42988d0707cd6c8640a9f060c0ea5a447b2e4
Dealga McArdle <zeffii@users.noreply.github.com>
2014-12-24 15:25:48 +0100

    reverse direction

--- utils/sv_bmesh_utils.py
+++ utils/sv_bmesh_utils.py
@@ -49,3 +49,10 @@ def bmesh_from_pydata(verts=[], edges=[], faces=[]):
         bm.edges.index_update()
 
     return bm
+
+
+def pydata_from_bmesh(bm):
+    v = [v.co[:] for v in bm.verts]
+    e = [[i.index for i in e.verts] for e in bm.edges[:]]
+    p = [[i.index for i in p.verts] for p in bm.faces[:]]
+    return v, e, p
