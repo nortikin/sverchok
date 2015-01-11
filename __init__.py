7d6d8886f56d4c7ca5ffb6e64449ebe679ebdc85
nikitron <nortikin@users.noreply.github.com>
2015-01-11 14:32:51 +0300

    Update __init__.py
    version

--- __init__.py
+++ __init__.py
@@ -42,7 +42,7 @@ bl_info = {
         "Cfyzzz, Nikitron, Ly29, "
         "AgustinJB, Zeffii, Kosvor,"
     ),
-    "version": (0, 5, 0, 8),
+    "version": (0, 5, 1, 0),
     "blender": (2, 7, 2),
     "location": "Nodes > CustomNodesTree > Add user nodes",
     "description": "Parametric node-based geometry programming",
