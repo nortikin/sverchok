f81ea211cb7aac8a70de3c8eae69be5369701248
zeffii <tetha.z@gmail.com>
2015-01-15 21:34:12 +0100

    test

--- inlet.js
+++ inlet.js
@@ -105,6 +105,7 @@ window.addEventListener("popstate", function(e) {
 
 window.addEventListener("hashchange", function(e) {
     var thash = window.location.hash;
+    console.log(thash);
     var pagename = thash.slice(1);
     var _div4 = d3.select("div.sv_html");
     var obtained_html = read_content(pagename + '.md');
