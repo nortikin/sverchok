b535b754431d7ee0dd32c5f8e4da8f04f694cdc0
Kosvor <Kosvor@users.noreply.github.com>
2015-02-10 19:05:32 +0000

    Update Barycentric_Transform.py

--- node_scripts/templates/ko/Barycentric_Transform.py
+++ node_scripts/templates/ko/Barycentric_Transform.py
@@ -8,23 +8,26 @@ def sv_main(Points=[], SourceTri=[], TargetTri=[]):
         ]
 
     import mathutils
-    from mathutils import Vector
+    from sverchok.data_structure import (match_long_cycle)
 
     out = []
-
     if SourceTri and TargetTri and Points:
 
-        st = [Vector(x) for x in SourceTri[0][:3]]
-        tt = [Vector(x) for x in TargetTri[0][:3]]
-        Points = [Vector(x) for x in Points[0]]
+        Points, SourceTri, TargetTri = match_long_cycle([Points, SourceTri, TargetTri])
 
-        for i in Points:
-            out.append(mathutils.geometry.barycentric_transform(i, st[0], st[1], st[2], tt[0], tt[1], tt[2])[:])
+        g = 0
+        while g < len(Points):
+
+            st = SourceTri[g][:3]
+            tt = TargetTri[g][:3]
+            P = Points[g]
+
+            out.append([mathutils.geometry.barycentric_transform(i, st[0], st[1], st[2], tt[0], tt[1], tt[2])[:] for i in P])
+
+            g = g+1
 
     out_sockets = [
-        ['v', 'TransformedPoints', [out]]
-
+        ['v', 'TransformedPoints', out]
     ]
 
     return in_sockets, out_sockets
-    
