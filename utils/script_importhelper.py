7dfb367af6b7fb3413de0fc10e72e9b7eb7dd027
ly29 <linusyng@live.com>
2015-04-24 09:26:31 +0200

    SN2 load logic fix

--- utils/script_importhelper.py
+++ utils/script_importhelper.py
@@ -1,17 +1,26 @@
 import inspect
 from sverchok.utils.sv_script import SvScript
 
-def load_script(source, name):
-    from_file = '<{}>'.format(name)
+def load_script(source, file_name):
+    from_file = '<{}>'.format(file_name)
     code = compile(source, from_file, 'exec', optimize=0)
     # insert classes that we can inherit from
     local_space = {cls.__name__:cls for cls in SvScript.__subclasses__()}
+    base_classes = set(cls.__name__ for cls in SvScript.__subclasses__())
     local_space["SvScript"] = SvScript
     
     exec(code, globals(),local_space)
 
     script = None
+    
     for name in code.co_names:
+        # filter out inherited
+        if not name in local_space:
+            continue
+        # skip base classes
+        if name in base_classes:
+            continue        
+            
         try: 
             script_class = local_space.get(name)
             if inspect.isclass(script_class):
@@ -20,9 +29,12 @@ def load_script(source, name):
                     print("Script Node found script {}".format(name))
                     script = script
                     globals().update(local_space)
+                    
         except Exception as Err:
             print("Script Node couldn't load {0}".format(name))
             print(Err) 
-            raise Err
+            
+    if not script:
+        raise ImportWarning("Couldn't find script in {}".format(name))  
     return script
     
