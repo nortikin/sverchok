c613a14e6369299028cf82bb215b3b8f78fd2a1a
Kosvor <Kosvor@users.noreply.github.com>
2015-08-12 09:03:51 +0300

    Update BMOperators.py

--- nodes/basic_data/BMOperators.py
+++ nodes/basic_data/BMOperators.py
@@ -20,7 +20,7 @@ import bpy
 import bmesh
 from bpy.props import EnumProperty
 from sverchok.node_tree import SverchCustomTreeNode
-from sverchok.data_structure import (updateNode, enum_item as e)
+from sverchok.data_structure import (updateNode, enum_item as e, second_as_first_cycle as safc)
 
 
 class SvBMOpsNode(bpy.types.Node, SverchCustomTreeNode):
@@ -30,7 +30,7 @@ class SvBMOpsNode(bpy.types.Node, SverchCustomTreeNode):
     bl_icon = 'OUTLINER_OB_EMPTY'
 
     PV = ['remove_doubles(bm,verts=e,dist=v[0])',
-          'collapse(bm,edges=e)',
+          'collapse(bm,edges=e,uvs=v[0])',
           'unsubdivide(bm,verts=e,iterations=v[0])',
           'holes_fill(bm,edges=e,sides=v[0])',
           'bridge_loops(bm,edges=e,use_pairs=v[0],use_cyclic=v[1],use_merge=v[2],merge_factor=v[3],twist_offset=v[4])',
@@ -39,14 +39,16 @@ class SvBMOpsNode(bpy.types.Node, SverchCustomTreeNode):
           'dissolve_edges(bm,edges=e,use_verts=v[0],use_face_split=v[1])',
           'dissolve_faces(bm,faces=e,use_verts=v[0])',
           'triangulate(bm,faces=e,quad_method=v[0],ngon_method=v[1])',
-          'join_triangles(bm,faces=e,cmp_sharp=v[0],cmp_uvs=v[1],cmp_vcols=v[2],cmp_materials=v[3],limit=v[4])',
+          'join_triangles(bm,faces=e,cmp_seam=v[0],cmp_sharp=v[1],cmp_uvs=v[2],cmp_vcols=v[3],cmp_materials=v[4],angle_face_threshold=v[5],angle_shape_threshold=v[6])',
           'connect_verts_concave(bm,faces=e)',
           'connect_verts_nonplanar(bm,angle_limit=v[0],faces=e)',
           'subdivide_edgering(bm,edges=e,interp_mode=v[0],smooth=v[1],cuts=v[2],profile_shape=v[3],profile_shape_factor=v[4])',
           'inset_individual(bm,faces=e,thickness=v[0],depth=v[1],use_even_offset=v[2],use_interpolate=v[3],use_relative_offset=v[4])',
+          'inset_region(bm,faces=e,use_boundary=v[0],use_even_offset=v[1],use_interpolate=v[2],use_relative_offset=v[3],use_edge_rail=v[4],thickness=v[5],depth=v[6],use_outset=v[7])',
           'grid_fill(bm,edges=e,mat_nr=v[0],use_smooth=v[1],use_interp_simple=v[2])',
-          'edgenet_fill(bm, edges=e, mat_nr=v[0], use_smooth=v[1], sides=v[2])',
-          'rotate_edges(bm, edges=e, use_ccw=v[0])'
+          'rotate_edges(bm, edges=e, use_ccw=v[0])',
+          'recalc_face_normals(bm,faces=e)',
+          'poke(bm,faces=e,offset=v[0],center_mode=v[1],use_relative_offset=v[2])'
           ]
 
     oper = EnumProperty(name="BMop", default=PV[0], items=e(PV), update=updateNode)
@@ -65,16 +67,9 @@ class SvBMOpsNode(bpy.types.Node, SverchCustomTreeNode):
         if not self.outputs['bmesh_list'].is_linked:
             return
         bml, val, e = self.inputs
-        obj = bml.sv_get()
-        v = val.sv_get([[1]*12])
+        obj, v = safc(bml.sv_get(), val.sv_get([[1]*12]))
         outp = []
         op = "bmesh.ops."+self.oper
-        if "verts=e" in op:
-            cur = "verts"
-        elif "edges=e" in op:
-            cur = "edges"
-        elif "faces=e" in op:
-            cur = "faces"
         if e.is_linked:
             element = e.sv_get()
             for bm, v, e in zip(obj,v, element):
@@ -82,6 +77,12 @@ class SvBMOpsNode(bpy.types.Node, SverchCustomTreeNode):
                 outp.append(bm.copy())
                 bm.free()
         else:
+            if "verts=e" in op:
+                cur = "verts"
+            elif "edges=e" in op:
+                cur = "edges"
+            elif "faces=e" in op:
+                cur = "faces"
             for bm, v in zip(obj,v):
                 e = getattr(bm, cur)
                 exec(op)
