c640aebd25bc5c540a19468643bc3608cfc2635c
zeffii <tetha.z@gmail.com>
2016-08-02 10:39:32 +0200

    redact UIList experiment

--- nodes/basic_data/group_exp.py
+++ nodes/basic_data/group_exp.py
@@ -35,26 +35,6 @@ socket_types = [
 reverse_lookup = {'outputs': 'inputs', 'inputs': 'outputs'}
 
 
-class SvNodeGroupInputList(bpy.types.UIList):
-    bl_idname = "SvNodeGroupInputList"
-
-    def draw_item(self, context, layout, data, item, icon, active_data, active_propname, index, flt_flag):
-        s = item
-        node = s.node
-        sockets = getattr(node, 'outputs')
-
-        # not an attempt make this fully featured yet..
-        if index == (len(sockets) - 1):
-            return
-        
-        layout.template_node_socket(color=s.draw_color(s.node, context))
-        layout.prop(item, "name", text="", emboss=False)
-        # layout.prop(item, "base_type", text="")
-        # props = layout.operator(NodeGroupItemRemove.bl_idname, text="", icon='X')
-        # props.index = index
-        # props.in_out = 'IN'
-
-
 def find_node(id_name, ng):
     for n in ng.nodes:
         if n.bl_idname == id_name:
@@ -458,19 +438,8 @@ class SvCustomGroupInterface(Panel):
         for i, s in enumerate(out_node.inputs):
             draw_socket_row(column2, s, i)
 
-        # UI templates.... 
-        row = layout.row()
-        col = row.column()
-        col.template_list(
-            SvNodeGroupInputList.bl_idname, 
-            "inputs", in_node, "outputs", ntree, "active_input"
-        )
-
-
-
 
 classes = [
-    SvNodeGroupInputList,
     SvMoveSocketOpExp,
     SvRenameSocketOpExp,
     SvEditSocketOpExp,
