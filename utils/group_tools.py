159691482d48b1e96b1e47e0a3c96dd068796b74
ly29 <linusyng@live.com>
2016-08-17 09:30:30 +0200

    disable old groups but keep nodes
    The nodes are kept for auto upgrade purposes, all code in them could be
    removed…

--- utils/group_tools.py
+++ utils/group_tools.py
@@ -16,6 +16,11 @@
 #
 # ##### END GPL LICENSE BLOCK #####
 
+"""
+old file, see monad file instead
+can be removed, kept for a while 2016-08-17
+"""
+
 import json
 import os
 import re
@@ -40,9 +45,9 @@ class SvNodeGroupCreator(bpy.types.Operator):
 
     bl_idname = "node.sv_group_creator"
     bl_label = "Create node group from selected"
- 
+
     def execute(self, context):
-        
+
         ng = context.space_data.node_tree
         ng.freeze(hard=True)
         is_process = ng.sv_process
@@ -53,15 +58,15 @@ class SvNodeGroupCreator(bpy.types.Operator):
             self.report({"CANCELLED"}, "No nodes selected")
             return {'CANCELLED'}
         # collect links outside of of selected nodes
-        test_in = lambda l: bool(l.to_node in nodes) and bool(l.from_node not in nodes) 
+        test_in = lambda l: bool(l.to_node in nodes) and bool(l.from_node not in nodes)
         test_out = lambda l: bool(l.from_node in nodes) and bool(l.to_node not in nodes)
         out_links = [l for l in ng.links if test_out(l)]
         in_links = [l for l in ng.links if test_in(l)]
         locx = [n.location.x for n in nodes]
         locy = sum(n.location.y for n in nodes)/len(nodes)
-        
+
         # crete node_group nodes
-        
+
         group_in = ng.nodes.new("SvGroupInputsNode")
         group_in.location = (min(locx)-300, locy)
         group_out = ng.nodes.new("SvGroupOutputsNode")
@@ -77,13 +82,13 @@ class SvNodeGroupCreator(bpy.types.Operator):
             other = get_other_socket(in_socket)
             gn_socket = group_node.inputs.new(other.bl_idname, s_name )
             gi_socket = group_in.outputs.new(other.bl_idname, s_name)
-            
+
             ng.links.remove(l)
             ng.links.new(in_socket, gi_socket)
             ng.links.new(gn_socket, out_socket)
-        
+
         out_links_sockets = set(l.from_socket for l in out_links)
-        
+
         for i, from_socket in enumerate(out_links_sockets):
             to_sockets = [l.to_socket for l in from_socket.links]
             s_name = "{}:{}".format(i, from_socket.name)
@@ -96,8 +101,8 @@ class SvNodeGroupCreator(bpy.types.Operator):
                 ng.links.remove(l)
                 ng.links.new(go_socket, from_socket)
                 ng.links.new(to_socket, gn_socket)
-        
-        # collect sockets for node group in out    
+
+        # collect sockets for node group in out
         group_in.collect()
         group_out.collect()
         # deselect all
@@ -105,36 +110,36 @@ class SvNodeGroupCreator(bpy.types.Operator):
             n.select = False
         nodes.add(group_in)
         nodes.add(group_out)
-        
+
         # select nodes to move
         for n in nodes:
             n.select = True
-        
+
         nodes_json = create_dict_of_tree(ng, {}, selected=True)
         print(nodes_json)
-        
+
         for n in nodes:
             ng.nodes.remove(n)
-        
+
         group_ng = bpy.data.node_groups.new("SvGroup", 'SverchGroupTreeType')
-        
+
         group_node.group_name = group_ng.name
         group_node.select = True
         group_ng.use_fake_user = True
         import_tree(group_ng, "", nodes_json)
-        
+
         ng.unfreeze(hard=True)
         ng.sv_process = is_process
         ng.update()
         self.report({"INFO"}, "Node group created")
         return {'FINISHED'}
 
-class SvNodeGroupEdit(bpy.types.Operator):  
+class SvNodeGroupEdit(bpy.types.Operator):
     bl_idname = "node.sv_node_group_edit"
     bl_label = "Edit group"
-    
+
     group_name = StringProperty()
-    
+
     def execute(self, context):
         ng = context.space_data.node_tree
         node = context.node
@@ -157,12 +162,12 @@ class SvNodeGroupEdit(bpy.types.Operator):
         ng["Group Node"] = node.name
         return {'FINISHED'}
 
-class SvNodeGroupEditDone(bpy.types.Operator):  
+class SvNodeGroupEditDone(bpy.types.Operator):
     bl_idname = "node.sv_node_group_done"
     bl_label = "Save group"
-    
+
     frame_name = StringProperty()
-    
+
     def execute(self, context):
         print("Saving node group")
         ng = context.space_data.node_tree
@@ -170,25 +175,25 @@ class SvNodeGroupEditDone(bpy.types.Operator):
         if not frame:
             return {'CANCELLED'}
         nodes = [n for n in ng.nodes if n.parent == frame]
-        
+
         g_node = ng.nodes[ng["Group Node"]]
-        
+
         for n in ng.nodes:
             n.select = False
         for n in nodes:
             n.select = True
         in_out = [n for n in nodes if n.bl_idname in {'SvGroupInputsNode', 'SvGroupOutputsNode'}]
-        
+
         in_out.sort(key=lambda n:n.bl_idname)
         for n in in_out:
             n.collect()
         g_node.adjust_sockets(in_out)
-            
+
         frame.select = True
         group_ng = bpy.data.node_groups[ng[frame.name]]
         del ng[frame.name]
         group_ng.name = frame.label
-        
+
         ng.freeze(hard=True)
         ng.nodes.remove(frame)
         nodes_json = create_dict_of_tree(ng, {}, selected=True)
@@ -198,7 +203,7 @@ class SvNodeGroupEditDone(bpy.types.Operator):
         ng.unfreeze(hard=True)
         group_ng.nodes.clear()
         import_tree(group_ng, "", nodes_json)
-        
+
         self.report({"INFO"}, "Node group save")
         return {'FINISHED'}
 
@@ -222,20 +227,20 @@ class SverchokGroupLayoutsMenu(bpy.types.Panel):
     def draw(self, context):
         layout = self.layout
         layout.operator("node.sv_group_creator")
-        
+
         #for ng in bpy.data.node_groups:
         #    if ng.bl_idname == 'SverchGroupTreeType':
         #        layout.label(ng.name)
         #        op = layout.operator("node.sv_node_group_edit", text="Edit")
         #        op.group_name = ng.name
-                
+
 classes = [
     SverchokGroupLayoutsMenu,
     SvNodeGroupCreator,
     SvNodeGroupEdit,
     SvNodeGroupEditDone
 ]
-
+"""
 def register():
     for class_name in classes:
         bpy.utils.register_class(class_name)
@@ -243,3 +248,4 @@ def register():
 def unregister():
     for class_name in reversed(classes):
         bpy.utils.unregister_class(class_name)
+"""
