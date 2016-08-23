09d7d127bda4a08a0cb47b3274aa21d311ff8617
ly29 <linusyng@live.com>
2016-08-23 09:32:46 +0200

    two funds

--- ui/autolayout.py
+++ ui/autolayout.py
@@ -2,7 +2,7 @@ import collections
 
 import bpy
 from bpy.types import Panel, Operator
-from bpy.props import FloatProperty
+from bpy.props import FloatProperty, StringProperty
 
 
 class SvAutoLayoutPanel(Panel):
@@ -23,11 +23,12 @@ class SvAutoLayoutPanel(Panel):
     def draw(self, context):
         layout = self.layout
         layout.label("Autolayout options")
-        op = layout.operator("node.sverchok_autolayout")
+        for key in SvAutoLayoutOp.order_funcs.keys():
+            layout.operator("node.sverchok_autolayout", text=key).func_name = key
 
-#def update_value(self, context):
-#    tree = context.space_data.edit_tree
-#    order_nodes(tree, self.x_spread)
+def update_value(self, context):
+    tree = context.space_data.edit_tree
+    order_nodes_2(tree, self.x_spread)
 
 class SvAutoLayoutOp(Operator):
     """Sort layout"""
@@ -43,15 +44,25 @@ class SvAutoLayoutOp(Operator):
             return False
 
     x_spread = FloatProperty(default=300, soft_min=0, update=update_value)
+    func_name = StringProperty(default="Topo up")
+
+    order_funcs = {"Topo up": order_nodes_1,
+                   "Topo down": order_nodes_2,
+                }
 
     def execute(self, context):
-        print(dir(context))
+        func = self.order_funcs.get(self.func_name)
         tree = context.space_data.edit_tree
-        order_nodes(tree, self.x_spread)
-        return {'FINISHED'}
+        if func:
+            func(tree, self.x_spread)
+            return {'FINISHED'}
+        else:
+            print("no func {}".format(self.func_name))
+            return {'CANCELLED'}
 
     def invoke(self, context, event):
         return context.window_manager.invoke_props_dialog(self)
+
 
 
 def topo_sort(nodes, links, start):
@@ -63,22 +74,38 @@ def topo_sort(nodes, links, start):
     weights = collections.defaultdict(lambda : -1)
     def visit(node, weight):
         weights[node] = max(weight, weights[node])
-        for from_node in links[node]:
-            visit(from_node, weight + 1)
+        for from_node, link_weight in links[node]:
+            visit(from_node, weight + link_weight)
     visit(start, 0)
     return sorted(weights.items(), key= lambda x:x[1])
 
-def collect_links_up(ng):
+def collect_links_up(ng, weight_func=lambda n0, n1: 1):
     rawlinks = [(l.from_node.name, l.to_node.name) for l in ng.links]
     links = collections.defaultdict(list)
-    for from_node, to_node in rawlinks:
-        links[to_node].append(from_node)
+    for l in ng.links:
+        from_node = l.from_node
+        to_node = l.to_node
+        weight = weight_func(from_node, to_node)
+        links[to_node].append((from_node, weight))
     from_set = {from_node for from_node, _ in rawlinks}
     to_set = {to_node for _, to_node in rawlinks}
     return links, from_set, to_set
 
-def generate_weights(ng):
-    links, f, t = collect_links_up(ng)
+
+def collect_links_down(ng, weight_func=lambda n0, n1: 1):
+    rawlinks = [(l.to_node.name, l.from_node.name) for l in ng.links]
+    links = collections.defaultdict(list)
+    for l in ng.links:
+        from_node = l.to_node
+        to_node = l.from_node
+        weight = weight_func(from_node, to_node)
+        links[to_node.name].append((from_node.name, weight))
+    from_set = {from_node for from_node, _ in rawlinks}
+    to_set = {to_node for _, to_node in rawlinks}
+    return links, from_set, to_set
+
+def add_weights(links, from_set, to_set):
+    f, t = from_set, to_set
     nodes = list(f | t)
     tot_weights = [topo_sort(nodes, links, start) for start in (t - f)]
     out = collections.defaultdict(lambda :0)
@@ -89,13 +116,24 @@ def generate_weights(ng):
         #print(sorted(out.items(), key=lambda x:x[1]))
     return out
 
-def order_nodes(ng, delta=300):
-    weights = generate_weights(ng)
+def order_nodes_1(ng, delta=300):
+    weights = add_weights(*collect_links_up(ng))
     max_x = max(n.location.x for n in ng.nodes)
     max_y = max(n.location.y for n in ng.nodes)
     y_count = [0 for n in range(len(ng.nodes))]
     for node, weight in weights.items():
         x = max_x - weight * delta
+        y = max_y - y_count[weight] * delta
+        y_count[weight] += 1
+        ng.nodes[node].location = (x, y)
+
+def order_nodes_2(ng, delta=300):
+    weights = add_weights(*collect_links_down(ng))
+    min_x = min(n.location.x for n in ng.nodes)
+    max_y = max(n.location.y for n in ng.nodes)
+    y_count = [0 for n in range(len(ng.nodes))]
+    for node, weight in weights.items():
+        x = min_x + weight * delta
         y = max_y - y_count[weight] * delta
         y_count[weight] += 1
         ng.nodes[node].location = (x, y)
