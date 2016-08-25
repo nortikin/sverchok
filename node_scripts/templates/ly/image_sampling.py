6208ec93e9a185d7d4198398022e6e489255d9bd
ly29 <linusyng@live.com>
2016-08-25 17:19:20 +0200

    fixes

--- node_scripts/templates/ly/image_sampling.py
+++ node_scripts/templates/ly/image_sampling.py
@@ -14,13 +14,7 @@ def sv_main(img_name=0, p=[[[0,0,0]]]):
        ['s', "Image name", img_name],
        ['v', 'Point', p],
     ]
-    def get_name(img_name):
-        if isinstance(img_name, (str, int)):
-            return img_name
-        elif img_name:
-            return get_name(img_name[0])
-        else:
-            0
+
     def get_name(img_name):
         if isinstance(img_name, (str, int)):
             return img_name
@@ -32,7 +26,6 @@ def sv_main(img_name=0, p=[[[0,0,0]]]):
     # if you want to look to a specific image
     #img_name = "Emperor_Penguin_Manchot_empereur.jpg"
 
-
     dim_x, dim_y = bpy.data.images[img_name].size
     tmp = np.array(bpy.data.images[img_name].pixels)
     image = tmp.reshape(dim_y, dim_x, 4)
@@ -42,9 +35,7 @@ def sv_main(img_name=0, p=[[[0,0,0]]]):
             x,y, _ = point
             u_i = clip(0, dim_x - 1, int(x * dim_x))
             v_i = clip(0, dim_y - 1, int(y*dim_y))
-
             color = image[v_i, u_i][:3]
-
             out.append(color)
 
 
