83ac70a2bc96caf924f212a88d8a5ff33f347f01
ly29 <linusyng@live.com>
2016-09-04 16:53:20 +0200

    small adjustments

--- node_scripts/SN3/image_sampling.py
+++ node_scripts/SN3/image_sampling.py
@@ -12,26 +12,26 @@ def draw(self, context, layout):
     layout.prop_search(self, "image_name", bpy.data, "images")
 
 @node_script(draw_buttons=draw)
-def sample_image(image_name: StringP() = "",
-                 points: Vertex("Uv points") = None) -> (
-                 Vertex("Colors"),
-                 Float("Alpha")):
+def sample_image(
+        image_name: StringP(name="Image") = "",
+        points: Vertex("Uv points") = None
+        ) -> (
+        Vertex("Colors"),
+        Float("Alpha")):
 
     bpy_image = bpy.data.images[image_name]
 
     dim_x, dim_y = bpy_image.size
-    tmp = np.array(bpy_image.pixels)
-    image = tmp.reshape(dim_y, dim_x, 4)
+    image = np.array(bpy_image.pixels[:]).reshape(dim_y, dim_x, 4)
 
     out = []
     alpha = []
     points = np.array(points[0])
     uv = points * (dim_x, dim_y, 0)
-    #
+
     for u, v, _ in uv:
         color = image[v % dim_y, u % dim_x]
         out.append(color[:3].tolist())
         alpha.append(color[-1])
-
 
     return out, alpha
