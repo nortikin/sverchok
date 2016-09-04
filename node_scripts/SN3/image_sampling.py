ceafca0478abe525c407fff6d7bf7a531d9e9396
ly29 <linusyng@live.com>
2016-09-04 11:27:52 +0200

    test script

---
+++ node_scripts/SN3/image_sampling.py
@@ -0,0 +1,37 @@
+import bpy
+import numpy as np
+
+
+"""
+Sample an image at u,v coordinates, repeatin the texture
+Images can either be selected by name or int.
+Outputs image data at uv as rgb
+"""
+
+def draw(self, context, layout):
+    layout.prop_search(self, "image_name", bpy.data, "images")
+
+@node_script(draw_buttons=draw)
+def sample_image(image_name: StringP() = "",
+                 points: Vertex("Uv points") = None) -> (
+                 Vertex("Colors"),
+                 Float("Alpha")):
+
+    bpy_image = bpy.data.images[image_name]
+
+    dim_x, dim_y = bpy_image.size
+    tmp = np.array(bpy_image.pixels)
+    image = tmp.reshape(dim_y, dim_x, 4)
+
+    out = []
+    alpha = []
+    points = np.array(points[0])
+    uv = points * (dim_x, dim_y, 0)
+    #
+    for u, v, _ in uv:
+        color = image[v % dim_y, u % dim_x]
+        out.append(color[:3].tolist())
+        alpha.append(color[-1])
+
+
+    return out, alpha
