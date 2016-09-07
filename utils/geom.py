1c099baec7b777bb4fd9853524a9c4f438f88061
zeffii <tetha.z@gmail.com>
2016-09-07 13:03:26 +0200

    add final simple mode for circle

--- utils/geom.py
+++ utils/geom.py
@@ -54,12 +54,13 @@ N = identity_matrix
 def circle(radius=1.0, phase=0, verts=20, matrix=None, mode='pydata'):
     vertices = []
     theta = TAU / verts
-    for i in range(verts)
+    for i in range(verts):
         rad = i * theta
         vertices.append((math.sin(rad + phase) , math.cos(rad + phase), 0))
-    edges = [[i, i+1] for i in range(verts-1)] + [verts-1, 0]
-    faces = [[i for i in range(verts)]]
-    pass
+    edges = [[i, i+1] for i in range(verts-1)] + [[verts-1, 0]]
+    faces = [i for i in range(verts)] + [0]
+    return vertices, edges, [faces]
+
 
 def arc(radius=1.0, phase=0, angle=TAU, verts=20, matrix=None, mode='pydata'):
     pass
