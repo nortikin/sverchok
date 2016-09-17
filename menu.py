a0e7ee4c59b0c77020e058cb8401e43f9e8a6064
zeffii <tetha.z@gmail.com>
2016-09-17 10:48:25 +0200

    index.md added..seems ok

--- menu.py
+++ menu.py
@@ -17,9 +17,10 @@
 #
 # ##### END GPL LICENSE BLOCK #####
 
-from collections import OrderedDict
-import re
+import os
 import pprint
+from os.path import dirname
+from collections import OrderedDict
 
 import bpy
 from nodeitems_utils import NodeCategory, NodeItem, NodeItemCustom
@@ -33,265 +34,41 @@ class SverchNodeCategory(NodeCategory):
 
 
 def make_node_cats():
+    '''
+    this loads the index.md file and converts it to an OrderedDict of node categories.
+
+    ## category name
+        bl_idname,      shortname,       <icon> (optional)
+        ...             ...              ...
+
+    becomes
+
+        node_cats[category name] = [['bl_idname', 'shortname' <, 'icon' >],...... ]
+
+    '''
+
+    index_path = os.path.join(dirname(__file__), 'index.md')
 
     node_cats = OrderedDict()
-    '''  bl_idname, shortname, <icon> (optional) '''
+    with open(index_path) as md:
+        category = None
+        temp_list = []
+        for line in md:
+            if not line.strip():
+                continue
+            elif line.startswith('##'):
+                if category:
+                    node_cats[category] = temp_list
+                category = line[2:].strip()
+                temp_list = []
+            else:
+                line_without_padding = line.strip()
+                non_padded_list = [i.strip() for i in line_without_padding.split(',')]
+                temp_list.append(non_padded_list)
+        # final append
+        node_cats[category] = temp_list
+    
 
-# blue-green
-    node_cats["Generators"] = [
-        "LineNode,              Line,                          GRIP",
-        "PlaneNode,             Plane,                   MESH_PLANE",
-        "SvNGonNode,            NGon,                      RNDCURVE",
-        "SvBoxNode,             Box,                      MESH_CUBE",
-        "SvCircleNode,          Circle,                 MESH_CIRCLE",
-        "CylinderNode,          Cylinder,             MESH_CYLINDER",
-        "SphereNode,            Sphere,               MESH_UVSPHERE",
-        "BasicSplineNode,       2pt Spline,          CURVE_BEZCURVE",
-        "svBasicArcNode,        3pt Arc,                SPHERECURVE",
-        "RandomVectorNode,      Random Vector,             RNDCURVE"
-    ]
-
-    node_cats["Extended Generators"] = [
-        "SvBoxRoundedNode,      Rounded Box",
-        "HilbertNode,           Hilbert",
-        "Hilbert3dNode,         Hilbert3d",
-        "HilbertImageNode,      Hilbert image",
-        "ImageNode,             Image,                   FILE_IMAGE",
-        "SvProfileNode,         ProfileParametric",
-        "SvGenerativeArtNode,   Generative Art",
-        "SvScriptNode,          Scripted Node,        SCRIPTPLUGINS",
-        "SvScriptNodeMK3,       Script 3 Node,        SCRIPTPLUGINS"
-    ]
-
-    node_cats["Analyzers"] = [
-        "SvBBoxNode,            Bounding box",
-        "SvVolumeNode,          Volume",
-        "AreaNode,              Area",
-        "DistancePPNode,        Distance",
-        "CentersPolsNodeMK2,    Centers Polygons 2",
-        "CentersPolsNodeMK3,    Centers Polygons 3",
-        "GetNormalsNode,        Calculate normals",
-        "VectorNormalNode,      Vertex Normal",
-        "SvKDTreeNode,          KDT Closest Verts",
-        "SvKDTreeEdgesNode,     KDT Closest Edges"
-    ]
-
-    node_cats["Transforms"] = [
-        "SvRotationNode,        Rotation,                   MAN_ROT",
-        "SvScaleNode,           Scale,                    MAN_SCALE",
-        "VectorMoveNode,        Move,                     MAN_TRANS",
-        "SvMirrorNode,          Mirror,                  MOD_MIRROR",
-        "MatrixApplyNode,       Matrix Apply"
-    ]
-
-    node_cats["Modifier Change"] = [
-        # modifiers deforms and reorganize and reconstruct data
-        "PolygonBoomNode,            Polygon Boom",
-        "Pols2EdgsNode,              Polygons to Edges",
-        "SvMeshJoinNode,             Mesh Join",
-        "SvRemoveDoublesNode,        Remove Doubles",
-        "SvDeleteLooseNode,          Delete Loose",
-        "SvSeparateMeshNode,         Separate Loose Parts",
-        "SvExtrudeSeparateNode,      Extrude Separate Faces",
-        "SvRandomizeVerticesNode,    Randomize input vertices",
-        "SvVertMaskNode,             Mask Vertices",
-        "SvFillsHoleNode,            Fill Holes",
-        "SvLimitedDissolve,          Limited Dissolve",
-        "SvIntersectEdgesNode,       Intersect Edges",
-        "SvIterateNode,              Iterate matrix transformation"
-    ]
-
-    node_cats["Modifier Make"] = [
-        # bl_idname, shortname, <icon> (optional)
-        'LineConnectNodeMK2,           UV Connection',
-        'AdaptivePolsNode,             Adaptive Polygons',
-        'SvAdaptiveEdgeNode,           Ad aptive Edges',
-        'CrossSectionNode,             Cross Section',
-        'SvBisectNode,                 Bisect',
-        'SvSolidifyNode,               Solidify',
-        'SvWireframeNode,              Wireframe',
-        'DelaunayTriangulation2DNode,  Delaunay 2D',
-        'Voronoi2DNode,                Voronoi 2D',
-        'SvPipeNode,                   Pipe',
-        "SvDuplicateAlongEdgeNode,     Duplicate objects along edge",
-        "SvWafelNode,                  Wafel",
-        "SvConvexHullNode,             Convex Hull",
-        "SvLatheNode,                  Lathe,                   MOD_SCREW"
-    ]
-
-    node_cats["List Masks"] = [
-        "MaskListNode,           List Mask (out)",
-        "SvMaskJoinNode,         List Mask Join (in)"
-    ]
-
-    node_cats["List main"] = [
-        "ListJoinNode,           List Join",
-        "ZipNode,                List Zip",
-        "ListLevelsNode,         List Del Levels",
-        "ListLengthNode,         List Length",
-        "ListSumNodeMK2,         List Sum",
-        "ListMatchNode,          List Match",
-        "ListFuncNode,           List Math"
-    ]
-
-    node_cats["List struct"] = [
-        "ShiftNodeMK2,           List Shift",
-        "ListRepeaterNode,       List Repeater",
-        "ListSliceNode,          List Slice",
-        "SvListSplitNode,        List Split",
-        "ListFLNode,             List First&Last",
-        "ListItem2Node,          List Item",
-        "ListReverseNode,        List Reverse",
-        "ListShuffleNode,        List Shuffle",
-        "ListSortNodeMK2,        List Sort",
-        "ListFlipNode,           List Flip"
-    ]
-
-    node_cats["Number"] = [
-        'GenListRangeIntNode,    Range Int',
-        'SvGenFloatRange,        Range Float',
-        'SvListInputNode,        List Input',
-        'RandomNode,             Random,                RNDCURVE',
-        'FloatNode,              Float',
-        'IntegerNode,            Int',
-        'Float2IntNode,          Float 2 Int',
-        'Formula2Node,           Formula',
-        'ScalarMathNode,         Math',
-        'SvMapRangeNode,         Map Range',
-        'SvEasingNode,           Easing 0..1',
-        'SvGenFibonacci,         Fibonacci sequence',
-        'SvGenExponential,       Exponential sequence'
-    ]
-
-    node_cats["Vector"] = [
-        'GenVectorsNode,           Vector in',
-        'VectorsOutNode,           Vector out',
-        'VectorMathNode,           Vector Math',
-        'VectorDropNode,           Vector Drop',
-        "VectorPolarInNode,        Vector polar input",
-        "VectorPolarOutNode,       Vector polar output",
-        'VertsDelDoublesNode,      Vector X Doubles',
-        'EvaluateLineNode,         Vector Evaluate',
-        'SvInterpolationNode,      Vector Interpolation',
-        'SvInterpolationNodeMK2,   Vector Interpolation mk2',
-        'SvInterpolationNodeMK3,   Vector Interpolation mk3',
-        'SvVertSortNode,           Vector Sort,                 SORTSIZE',
-        'SvNoiseNode,              Vector Noise,        FORCE_TURBULENCE',
-        'svAxisInputNode,          Vector X | Y | Z,             MANIPUL'
-    ]
-
-    node_cats["Matrix"] = [
-        "MatrixGenNode,             Matrix in",
-        "MatrixOutNode,             Matrix out",
-        "MatrixDeformNode,          Matrix Deform",
-        "SvMatrixValueIn,           Matrix Input",
-        "SvMatrixEulerNode,         Matrix Euler",
-        "MatrixShearNode,           Matrix Shear",
-        "MatrixInterpolationNode,   Matrix Interpolation"
-    ]
-
-    node_cats["Logic"] = [
-        "SvLogicNode,               Logic",
-        "SvSwitchNode,              Switch",
-        "SvNeuroElman1LNode,        Neuro"
-    ]
-
-# orange
-    node_cats["Viz"] = [
-        "ViewerNode2,               Viewer Draw,           RETOPO",
-        "SvBmeshViewerNodeMK2,      Viewer BMeshMK2",
-        "IndexViewerNode,           Viewer Index",
-        "Sv3DviewPropsNode,         3dview Props"
-    ]
-
-# greish blue
-    node_cats["Text"] = [
-        "ViewerNode_text,           Viewer text",
-        "SvTextInNode,              Text in",
-        "SvTextOutNode,             Text out",
-        "NoteNode,                  Note",
-        "GTextNode,                 GText",
-        "SvDebugPrintNode,          Debug print",
-        "SvStethoscopeNode,         Stethoscope"
-    ]
-
-# green
-    node_cats["Scene"] = [
-        "ObjectsNode,               Objects in",
-        "SvFrameInfoNode,           Frame info",
-        "SvEmptyOutNode,            Empty out,            OUTLINER_OB_EMPTY",
-        "SvDupliInstancesMK3,       Dupli instancer",
-        "SvInstancerNode,           Mesh instancer",
-        "SvGetPropNode,             Get property,              FORCE_VORTEX",
-        "SvSetPropNode,             Set property,              FORCE_VORTEX",
-        "SvVertexGroupNode,         Vertex group",
-        "SvRayCastSceneNode,        Scene Raycast",
-        "SvRayCastObjectNode,       Object ID Raycast",
-        "SvVertexColorNode,         Vertex color",
-        "SvVertexColorNodeMK2,      Vertex color new"
-    ]
-
-# violet
-    node_cats["Layout"] = [
-        "WifiInNode,                Wifi in",
-        "WifiOutNode,               Wifi out",
-        "NodeReroute,               Reroute Point",
-        "ConverterNode,             SocketConvert"
-    ]
-
-    node_cats['Network'] = [
-        "UdpClientNode,             UDP Client"
-    ]
-
-    node_cats["Beta Nodes"] = [
-        # for testing convenience, and while no documentation
-        "SvBevelNode,               Bevel",
-        "SvExtrudeEdgesNode,        Extrude Edges",
-        "SvOffsetNode,              Offset",
-        "SvRecalcNormalsNode,       Recalc normals",
-        "SvEdgeAnglesNode,          Angles at the edges",
-        "SvListDecomposeNode,       List Decompose",
-        "SvFormulaShapeNode,        Formula shape,                      IPO",
-        "SvScriptNodeMK2,           Script 2",
-        "SvMeshFilterNode,          Mesh filter",
-        "SvTriangulateNode,         Triangulate mesh",
-        "SvHeavyTriangulateNode,    Triangulate mesh (heavy)",
-        "SvBricksNode,              Bricks grid",
-        "SvMatrixApplyJoinNode,     Apply matrix to mesh",
-        "SvMatrixTubeNode,          Matrix Tube"
-    ]
-
-    node_cats["Alpha Nodes"] = [
-        "SvCurveViewerNode,         Curve Viewer,             MOD_CURVE",
-        "SvCurveViewerNodeAlt,      Curve Viewer 2D,          MOD_CURVE",
-        "SvPolylineViewerNode,      Polyline Viewer,          MOD_CURVE",
-        "SvTypeViewerNode,          Typography Viewer",
-        "SvImageComponentsNode,     Image Decompose,         GROUP_VCOL",
-        "SvJoinTrianglesNode,       Join Triangles",
-        "SvCacheNode,               Cache",
-        "SvInsetSpecial,            Inset Special",
-        "SkinViewerNode,            Skin Mesher",
-        "SvCSGBooleanNode,          CSG Boolean",
-        "SvNumpyArrayNode,          Numpy Array",
-        "SvNodeRemoteNode,          Node Remote (Control)",
-        "SvGetDataObjectNode,       Object ID Get",
-        "SvSetDataObjectNode,       Object ID Set",
-        "SvSortObjsNode,            Object ID Sort",
-        "SvObjectToMeshNode,        Object ID Out",
-        "SvFilterObjsNode,          Object ID Filter",
-        "SvPointOnMeshNode,         Object ID Point on Mesh",
-        "SvBMVertsNode,             BMesh Props",
-        "SvBMOpsNode,               BMesh Ops",
-        "SvBMinputNode,             BMesh In",
-        "SvBMoutputNode,            BMesh Out",
-        "SvBMtoElementNode,         BMesh Elements"
-        #  ["SvBVHtreeNode",        "BVH Tree In"],
-        #  ["SvBVHRaycastNode",     "BVH Tree Raycast"],
-        #  ["SvBvhOverlapNode",     "BVH Tree Overlap"],
-        #  ["SvBVHnearNode",        "BVH Tree Nearest"],
-    ]
-    for k, v in node_cats.items():
-        node_cats[k] = [[i.strip() for i in j.split(',')] for j in v]
 
     pprint.pprint(node_cats)
 
@@ -303,10 +80,10 @@ def juggle_and_join(node_cats):
     this step post processes the extended catagorization used
     by ctrl+space dynamic menu, and attempts to merge previously
     joined catagories. Why? Because the default menu gets very
-    long if there are too many catagories.
+    long if there are too many categories.
 
     The only real alternative to this approach is to write a
-    replacement for nodeitems_utils which respects catagories
+    replacement for nodeitems_utils which respects categories
     and submenus.
 
     '''
