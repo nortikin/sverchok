c8a701fc2913bc151087accda77e6b719637a1f3
zeffii <tetha.z@gmail.com>
2016-09-18 20:02:20 +0200

    add ignore_list filtering

--- nodes/__init__.py
+++ nodes/__init__.py
@@ -2,6 +2,7 @@ import os
 from os.path import dirname
 from os.path import basename
 from collections import defaultdict
+import pprint
 
 directory = dirname(__file__)
 
@@ -21,11 +22,22 @@ def automatic_collection():
         for file in files:
             if file == '__init__.py':
                 continue
-            if file.endswith('dict'):
+            if not file.endswith('.py'):
                 continue
             nodes_dict[current_dir].append(file[:-3])
+
+    # remove items found in ignore_list
+    for k, v in ignore_list.items():
+        items = nodes_dict.get(k)
+        if items:
+            for filename in v:
+                try:
+                    items.remove(filename)
+                except:
+                    print('failed to remove', filename, 'from', k, ' : check your spelling')
 
     # may not be used, but can be.
     return nodes_dict
 
 automatic_collection()
+
