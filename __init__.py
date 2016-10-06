192d56a3a1c55bbc00358225d573c07858975e3d
Dealga McArdle <zeffii@users.noreply.github.com>
2016-10-06 09:52:20 +0200

    bump minor version

--- __init__.py
+++ __init__.py
@@ -45,7 +45,7 @@ bl_info = {
         "AgustinJB, Zeffii, Kosvor, "
         "Portnov, Elfnor"
     ),
-    "version": (0, 5, 6, 5),
+    "version": (0, 5, 6, 6),
     "blender": (2, 7, 2),
     "location": "Nodes > CustomNodesTree > Add user nodes",
     "description": "Parametric node-based geometry programming",
