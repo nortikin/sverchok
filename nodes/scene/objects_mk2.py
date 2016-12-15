fc5561f942e796b0c98b1062ba02fc9a5daa3079
zeffii <tetha.z@gmail.com>
2016-12-15 10:12:42 +0100

    restructure draw_obj_names

--- nodes/scene/objects_mk2.py
+++ nodes/scene/objects_mk2.py
@@ -171,13 +171,14 @@ class ObjectsNodeMK2(bpy.types.Node, SverchCustomTreeNode):
         # display names currently being tracked, stop at the first 5..
         handle = handle_read(self.name + self.id_data.name)
         if self.objects_local and handle[0]:
+            
             remain = len(handle[1]) - 5
-            for i, o in enumerate(handle[1]):
-                layout.label(o)                
+            more_items = '... {0} more item' + ('' if remain == 1 else 's')
+
+            for i, obj_name in enumerate(handle[1]):
+                layout.label(obj_name)
                 if i > 4 and remain > 0:
-                    dots = ' . . . '
-                    s = '' if remain == 1 else 's'
-                    layout.label(dots + str(remain) + ' more item' + s)
+                    layout.label(more_items.format(remain))
                     break
         else:
             layout.label('--None--')
