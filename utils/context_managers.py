1ff2284fc68d4ff716c4660592f69d8993381821
zeffii <tetha.z@gmail.com>
2017-01-01 01:55:10 +0100

    add explanation for the context manager

--- utils/context_managers.py
+++ utils/context_managers.py
@@ -8,6 +8,18 @@ def hard_freeze(self):
     '''
     This prevents properties from calling updateNode when edited.
     it Freezes the node-tree until control flow leaves the context.
+
+    usage  (when self is a reference to a node)
+
+        from sverchok.utils.context_managers import hard_freeze
+
+        ...
+        ...
+
+        with hard_freeze(self) as node:
+            node.some_prop = 'some_value_change'
+            node.some_other_prop = 'some_other_value_change'
+
     '''
     self.id_data.freeze(hard=True)
     yield self
