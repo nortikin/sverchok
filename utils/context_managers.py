1538fb8d71cda2991c732fb5e3dc1a62460fb3f3
zeffii <tetha.z@gmail.com>
2017-01-01 01:52:26 +0100

    fixes code formatting

--- utils/context_managers.py
+++ utils/context_managers.py
@@ -6,7 +6,8 @@ from contextlib import contextmanager
 @contextmanager
 def hard_freeze(self):
     '''
-    This prevents properties from calling updateNode when edited. it Freezes the node-tree until control flow leaves the context.
+    This prevents properties from calling updateNode when edited.
+    it Freezes the node-tree until control flow leaves the context.
     '''
     self.id_data.freeze(hard=True)
     yield self
