2b67ee529c6f2d9656124e20fd0ee281e2b4a671
zeffii <tetha.z@gmail.com>
2017-01-12 21:50:00 +0100

    remove svset/Getsocket solidify -- medium pass

--- nodes/modifier_make/solidify.py
+++ nodes/modifier_make/solidify.py
@@ -21,10 +21,7 @@ from bpy.props import FloatProperty
 import bmesh
 
 from sverchok.node_tree import SverchCustomTreeNode
-from sverchok.data_structure import (updateNode, Vector_generate, repeat_last,
-                            SvSetSocketAnyType, SvGetSocketAnyType,
-                            fullList)
-
+from sverchok.data_structure import updateNode, repeat_last, fullList
 from sverchok.utils.sv_bmesh_utils import bmesh_from_pydata
 # by Linus Yng
 
@@ -38,11 +35,10 @@ def soldify(vertices, faces, t, verlen):
         return False
 
     bm = bmesh_from_pydata(vertices, [], faces)
-
     geom_in = bm.verts[:]+bm.edges[:]+bm.faces[:]
 
     bmesh.ops.recalc_face_normals(bm, faces=bm.faces[:])
-    res = bmesh.ops.solidify(bm, geom=geom_in, thickness=t[0])
+    bmesh.ops.solidify(bm, geom=geom_in, thickness=t[0])
 
     edges = []
     faces = []
@@ -69,67 +65,51 @@ class SvSolidifyNode(bpy.types.Node, SverchCustomTreeNode):
     bl_label = 'Solidify'
     bl_icon = 'OUTLINER_OB_EMPTY'
 
-    thickness = FloatProperty(name='Thickness', description='Shell thickness',
-                              default=0.1,
-                              update=updateNode)
+    thickness = FloatProperty(
+        name='Thickness', description='Shell thickness',
+        default=0.1, update=updateNode)
 
     def sv_init(self, context):
-
         self.inputs.new('StringsSocket', 'thickness').prop_name = 'thickness'
-        self.inputs.new('VerticesSocket', 'vertices', 'vertices')
-        self.inputs.new('StringsSocket', 'polygons', 'polygons')
+        self.inputs.new('VerticesSocket', 'vertices')
+        self.inputs.new('StringsSocket', 'polygons')
 
-        self.outputs.new('VerticesSocket', 'vertices', 'vertices')
-        self.outputs.new('StringsSocket', 'edges', 'edges')
-        self.outputs.new('StringsSocket', 'polygons', 'polygons')
-        self.outputs.new('StringsSocket', 'newpols', 'newpols')
+        self.outputs.new('VerticesSocket', 'vertices')
+        self.outputs.new('StringsSocket', 'edges')
+        self.outputs.new('StringsSocket', 'polygons')
+        self.outputs.new('StringsSocket', 'newpols')
 
     def process(self):
         if not any((s.is_linked for s in self.outputs)):
             return
 
-        if 'vertices' in self.inputs and self.inputs['vertices'].is_linked and \
-           'polygons' in self.inputs and self.inputs['polygons'].is_linked:
+        if not (self.inputs['vertices'].is_linked and self.inputs['polygons'].is_linked):
+            return
 
-            verts = Vector_generate(SvGetSocketAnyType(self, self.inputs['vertices']))
-            polys = SvGetSocketAnyType(self, self.inputs['polygons'])
-            if 'thickness' in self.inputs:
-                thickness = self.inputs['thickness'].sv_get()
-            else:
-                thickness = [[self.thickness]]
+        verts = self.inputs['vertices'].sv_get()
+        polys = self.inputs['polygons'].sv_get()
+        thickness = self.inputs['thickness'].sv_get()
 
-            #print (verts,polys)
-
-            verts_out = []
-            edges_out = []
-            polys_out = []
-            newpo_out = []
-            fullList(thickness, len(verts))
-            for v, p, t in zip(verts, polys, thickness):
-                verlen = set(range(len(v)))
-                res = soldify(v, p, t, verlen)
-            
-                if not res:
-                    return
-                verts_out.append(res[0])
-                edges_out.append(res[1])
-                polys_out.append(res[2])
-                newpo_out.append(res[3])
-            
-            if 'vertices' in self.outputs and self.outputs['vertices'].is_linked:
-                SvSetSocketAnyType(self, 'vertices', verts_out)
-            
-            if 'edges' in self.outputs and self.outputs['edges'].is_linked:
-                SvSetSocketAnyType(self, 'edges', edges_out)
-            
-            if 'polygons' in self.outputs and self.outputs['polygons'].is_linked:
-                SvSetSocketAnyType(self, 'polygons', polys_out)
-                
-            if 'newpols' in self.outputs and self.outputs['newpols'].is_linked:
-                SvSetSocketAnyType(self, 'newpols', newpo_out)
-
-    def update_socket(self, context):
-        self.update()
+        verts_out = []
+        edges_out = []
+        polys_out = []
+        newpo_out = []
+        fullList(thickness, len(verts))
+        for v, p, t in zip(verts, polys, thickness):
+            verlen = set(range(len(v)))
+            res = soldify(v, p, t, verlen)
+        
+            if not res:
+                return
+            verts_out.append(res[0])
+            edges_out.append(res[1])
+            polys_out.append(res[2])
+            newpo_out.append(res[3])
+        
+        self.outputs['vertices'].sv_set(verts_out)
+        self.outputs['edges'].sv_set(edges_out)
+        self.outputs['polygons'].sv_set(polys_out)
+        self.outputs['newpols'].sv_set(newpo_out)
 
 
 def register():
@@ -138,6 +118,3 @@ def register():
 
 def unregister():
     bpy.utils.unregister_class(SvSolidifyNode)
-
-if __name__ == '__main__':
-    register()
