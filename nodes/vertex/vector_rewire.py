a8a7cdf2fb23ba0a66b6383b6619e1a766f51fa7
zeffii <tetha.z@gmail.com>
2017-01-23 16:10:58 +0100

    add scalar socket

--- nodes/vertex/vector_rewire.py
+++ nodes/vertex/vector_rewire.py
@@ -23,7 +23,7 @@ from sverchok.node_tree import SverchCustomTreeNode
 from sverchok.data_structure import dataCorrect
 from sverchok.data_structure import updateNode
 
-options = 'X Y Z -1 0 1'.split(' ')
+options = 'X Y Z -1 0 1 Scalar'.split(' ')
 options2 = 'X Y Z'.split(' ')
 mode_options = [(n, n, '', idx) for idx, n in enumerate(options)]
 mode_options2 = [(n, n, '', idx) for idx, n in enumerate(options2)]
@@ -44,10 +44,13 @@ class SvVectorRewire(bpy.types.Node, SverchCustomTreeNode):
         items=mode_options2,
         description="offers....",
         default="Z", update=updateNode
-    )    
+    )
+
+    scalar = FloatProperty(default=0.0, update=updateNode)
     
     def sv_init(self, context):
         self.inputs.new('VerticesSocket', "Vectors")
+        self.inputs.new('StringsSocket', "Scalar").prop_name = "scalar"
         self.outputs.new('VerticesSocket', "Vectors")
 
     def draw_buttons(self, context, layout):
@@ -57,8 +60,9 @@ class SvVectorRewire(bpy.types.Node, SverchCustomTreeNode):
         row.prop(self, 'selected_mode_to', text='')
 
     def process(self):
-        vectors_out = self.outputs[0]
         vectors_in = self.inputs[0]
+        scalar_in = self.inputs[1]
+        vectors_out = self.outputs[0]
 
         if not all([vectors_out.is_linked, vectors_in.is_linked]):
             return
@@ -80,13 +84,14 @@ class SvVectorRewire(bpy.types.Node, SverchCustomTreeNode):
         series_vec = []
         for obj in xyz:
 
-            # handles xy xz yx yz zx zy
             if sorted_tuple in rewire_dict.keys():
+                # handles xy xz yx yz zx zy
                 x, y, z = rewire_dict.get(sorted_tuple)
                 coords = ([v[x], v[y], v[z]] for v in obj)
                 series_vec.append(list(coords))
-                continue
+
             elif switching[0] in {3, 4, 5}:
+                # handles -1 0 1 -> xyz
                 value = {3: -1, 4: 0, 5: 1}.get(switching[0])
                 if switching[1] == 0:
                     coords = ([value, v[1], v[2]] for v in obj)
@@ -95,6 +100,12 @@ class SvVectorRewire(bpy.types.Node, SverchCustomTreeNode):
                 else:  # 2
                     coords = ([v[0], v[1], value] for v in obj)
                 series_vec.append(list(coords))
+
+            else switching[0] == 6:
+                # handles socket s. -> xyz
+                scalar_data = scalar_in.sv_get()
+                if isinstance(scalar_data, list) and len(scalar_data) > 0:
+                    
 
 
         vectors_out.sv_set(series_vec)                    
