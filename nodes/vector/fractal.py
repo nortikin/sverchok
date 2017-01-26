ceef74a51d145ba4d10f0bc596d59de179a2591a
kalwalt <info@kalwaltart.it>
2017-01-26 20:40:07 +0100

    prints works in the fractals

--- nodes/vector/fractal.py
+++ nodes/vector/fractal.py
@@ -50,24 +50,51 @@ fractal_options = [
     ('HYBRID_MULTI_FRACTAL', 4),
 ]
 
+h_factor = 0.0
+lacunarity = 1.0
+octaves = 3
+offset = 0.0
+gain = 0.5
+
 def deepnoise(v, _noise_type):
-    u = noise.noise_vector(v, _noise_type)[:]
+    u = noise.noise_vector(v,  _noise_type)[:]
     a = u[0], u[1], u[2]-1   # a = u minus (0,0,1)
     return sqrt((a[0] * a[0]) + (a[1] * a[1]) + (a[2] * a[2])) * 0.5
 
-def fractal(v, _noise_type):
-    print("This is a test but, Fbm is an awesome add!!")
-    data = 1.0
-    return data
+def fractal(v, h_factor,  lacunarity, octaves, offset, gain, _noise_type):
 
-def multifractal():
-    return
-def hetero():
-    return
-def ridged():
-    return
-def hybrid():
-    return
+    h_factor=1.0
+    lacunarity=1.0
+    octaves=3
+    print("This is a test but, Fbm is an awesome add!!")
+    data = []
+    verts = []
+
+    if v and v[0]:
+        for verts in v[0]:
+             out = noise.fractal(verts, h_factor, lacunarity, octaves, _noise_type)
+             data.append(out)
+    return out
+
+def multifractal(v, h_factor,  lacunarity, octaves, offset, gain, _noise_type):
+    out = 0.0
+    print("hey, i'am a multifractal!")
+    return out
+
+def hetero(v, h_factor,  lacunarity, octaves, offset, gain, _noise_type):
+    out = 0.0
+    print("hetero fractal works")
+    return out
+
+def ridged(v, h_factor,  lacunarity, octaves, offset, gain, _noise_type):
+    out = 0.0
+    print("ridged fractal!")
+    return out
+
+def hybrid(v, h_factor,  lacunarity, octaves, offset, gain, _noise_type):
+    out = 0.0
+    print("you got a hybrid fractal!")
+    return out
 
 noise_dict = {t[0]: t[1] for t in noise_options}
 avail_noise = [(t[0], t[0].title(), t[0].title(), '', t[1]) for t in noise_options]
@@ -88,6 +115,8 @@ class SvVectorFractal(bpy.types.Node, SverchCustomTreeNode):
     bl_idname = 'SvVectorFractal'
     bl_label = 'Vector Fractal'
     bl_icon = 'FORCE_TURBULENCE'
+
+
 
     def changeMode(self, context):
         outputs = self.outputs
@@ -170,7 +199,7 @@ class SvVectorFractal(bpy.types.Node, SverchCustomTreeNode):
 
             noise.seed_set(seed_val)
             #out.append([noise_function(v, _noise_type) for v in obj])
-            out.append([fractal_function(v, _noise_type) for v in obj])
+            out.append([fractal_function(v, h_factor,  lacunarity, octaves, offset, gain, _noise_type) for v in obj])
 
         #musgrave fractals outputs only float so why we need Vector?
         #if 'Noise V' in outputs:
