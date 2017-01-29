c3ca6ea203ec7fb86758ab519921a691345f99f1
kalwalt <info@kalwaltart.it>
2017-01-29 21:38:17 +0100

    cleaning and deleting turbulence function

--- nodes/vector/turbulence.py
+++ nodes/vector/turbulence.py
@@ -18,14 +18,13 @@
 
 import inspect
 import operator
-from math import sqrt
 
 import bpy
-from bpy.props import EnumProperty, IntProperty, FloatProperty
-from mathutils import noise,Vector
+from bpy.props import EnumProperty, IntProperty, FloatProperty, BoolProperty
+from mathutils import noise
 
 from sverchok.node_tree import SverchCustomTreeNode
-from sverchok.data_structure import (updateNode, Vector_degenerate, match_long_repeat)
+from sverchok.data_structure import (updateNode, Vector_degenerate )
 
 # noise nodes
 # from http://www.blender.org/documentation/blender_python_api_current/mathutils.noise.html
@@ -43,22 +42,21 @@ noise_options = [
     ('VORONOI_CRACKLE', 8),
     ('CELLNOISE', 14)
 ]
-
+'''
 def turbulence( verts, octaves, hard, _noise_type, amp, freq ):
     return noise.turbulence( verts, octaves, hard, _noise_type, amp, freq )
-
+'''
 noise_dict = {t[0]: t[1] for t in noise_options}
 avail_noise = [(t[0], t[0].title(), t[0].title(), '', t[1]) for t in noise_options]
 
-turbulence_f = {'SCALAR': turbulence, 'VECTOR': noise.turbulence_vector}
- 
- 
+turbulence_f = {'SCALAR': noise.turbulence, 'VECTOR': noise.turbulence_vector}
+
 class SvTurbulenceNode(bpy.types.Node, SverchCustomTreeNode):
     '''Vector Turbulence node'''
     bl_idname = 'SvTurbulenceNode'
     bl_label = 'Vector Turbulence'
     bl_icon = 'FORCE_TURBULENCE'
-      
+
     def changeMode(self, context):
         outputs = self.outputs
         if self.out_mode == 'SCALAR':
@@ -69,28 +67,28 @@ class SvTurbulenceNode(bpy.types.Node, SverchCustomTreeNode):
             if 'Noise V' not in outputs:
                 outputs[0].replace_socket('VerticesSocket', 'Noise V')
                 return
-                  
+
     out_modes = [
         ('SCALAR', 'Scalar', 'Scalar output', '', 1),
         ('VECTOR', 'Vector', 'Vector output', '', 2)]
-         
+
     out_mode = EnumProperty(
         items=out_modes,
         default='VECTOR',
         description='Output type',
         update=changeMode)
-         
+
     noise_type = EnumProperty(
         items=avail_noise,
         default='STDPERLIN',
         description="Noise type",
         update=updateNode)
-          
+
     octaves = IntProperty(default=3, min=0, max=6, description='Octaves', name='Octaves', update=updateNode)
-    hard = IntProperty(default=0, min=0, max=1, description="Hard (sharp transitions) or soft (smooth transitions)", name="Hard", update=updateNode)
+    hard = BoolProperty(default=True, description="Hard (sharp transitions) or soft (smooth transitions)", name="Hard", update=updateNode)
     amp = FloatProperty(default=0.25, description="The amplitude scaling factor", name="Amplitude", update=updateNode)
     freq = FloatProperty(default=0.03, description="The frequency scaling factor", name="Frequency", update=updateNode)
-      
+
     def sv_init(self, context):
         self.inputs.new('VerticesSocket', 'Vertices')
         self.inputs.new('StringsSocket', 'Octaves').prop_name = 'octaves'
@@ -108,30 +106,30 @@ class SvTurbulenceNode(bpy.types.Node, SverchCustomTreeNode):
 
         if not outputs[0].is_linked:
             return
-         
+
         out = []
         verts = inputs['Vertices'].sv_get()
-          
+
         m_octaves = inputs['Octaves'].sv_get()[0][0]
         m_hard = inputs['Hard'].sv_get()[0][0]
         m_amp = inputs['Amplitude'].sv_get()[0][0]
         m_freq = inputs['Frequency'].sv_get()[0][0]
-         
-        
+
+
         _noise_type = noise_dict[self.noise_type]
         turbulence_function = turbulence_f[self.out_mode]
-            
+
         vertices = []
         for vertices in verts[:]:
-            
+
             out.append([turbulence_function(v, m_octaves, m_hard, _noise_type, m_amp, m_freq) for v in vertices])
-                                
+
         if 'Noise V' in outputs:
             outputs['Noise V'].sv_set(Vector_degenerate(out))
         else:
             outputs['Noise S'].sv_set(out)
-            
-             
+
+
 def register():
     bpy.utils.register_class(SvTurbulenceNode)
 
