0dbc7f7af85c187ac9485993a4f7752f9784980d
zeffii <tetha.z@gmail.com>
2017-02-20 11:27:38 +0100

    corrects indexing

--- nodes/vector/color_out.py
+++ nodes/vector/color_out.py
@@ -88,24 +88,6 @@ class SvColorsOutNode(bpy.types.Node, SverchCustomTreeNode):
         else:
             data = [[self.unit_color[:]]]
 
-        # A, B, C, D = [], [], [], []
-        # if self.use_alpha:
-        #     for obj in data:
-        #         a_, b_, c_, d_ = (list(x) for x in zip(*obj))
-        #         A.append(a_)
-        #         B.append(b_)
-        #         C.append(c_)
-        #         D.append(d_)
-        #     for i, socket in enumerate(self.outputs):
-        #         self.outputs[socket.name].sv_set([A, B, C, D][i])
-        # else:
-        #     for obj in data:
-        #         a_, b_, c_ = (list(x) for x in zip(*obj))
-        #         A.append(a_)
-        #         B.append(b_)
-        #         C.append(c_)
-        #     for i, socket in enumerate(self.outputs[:3]):
-        #         self.outputs[socket.name].sv_set([A, B, C][i])
 
         values = [[], [], [], []]
         for obj in data:
@@ -114,10 +96,10 @@ class SvColorsOutNode(bpy.types.Node, SverchCustomTreeNode):
                 values[idx].append(v)
         for i, socket in enumerate(self.outputs):
 
-            if len(values[3]) == 0 or (not self.use_alpha and i == 3):
-                break 
+            if i == 3 and len(values[3]) == 0 or (not self.use_alpha):
+                socket.sv_set([[]])
             else:
-                socket.sv_set([values][i])
+                socket.sv_set(values[i])
 
     
     
