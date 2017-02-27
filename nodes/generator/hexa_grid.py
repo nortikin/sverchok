9ef79861689ad7184f5da4928dc9dcce5ecd843c
Marius Giurgi <marius.giurgi@gmail.com>
2017-02-26 19:54:38 -0500

    Cleaned up the hexagrid code and fix minor centering bug

--- nodes/generator/hexa_grid.py
+++ nodes/generator/hexa_grid.py
@@ -19,9 +19,7 @@
 import bpy
 from bpy.props import IntProperty, FloatProperty, BoolProperty, EnumProperty
 
-from math import sin, cos, pi, sqrt, radians
-from random import random
-import time
+from math import sqrt
 
 from sverchok.node_tree import SverchCustomTreeNode
 from sverchok.data_structure import updateNode, match_long_repeat
@@ -31,8 +29,8 @@ DEBUG = False
 
 gridTypeItems = [
     ("RECTANGLE", "Rectangle", "", custom_icon("SV_HEXA_GRID_RECTANGLE"), 0),
-    ("DIAMOND", "Diamond", "", custom_icon("SV_HEXA_GRID_DIAMOND"), 1),
-    ("TRIANGLE", "Triangle", "", custom_icon("SV_HEXA_GRID_TRIANGLE"), 2),
+    ("TRIANGLE", "Triangle", "", custom_icon("SV_HEXA_GRID_TRIANGLE"), 1),
+    ("DIAMOND", "Diamond", "", custom_icon("SV_HEXA_GRID_DIAMOND"), 2),
     ("HEXAGON", "Hexagon", "", custom_icon("SV_HEXA_GRID_HEXAGON"), 3)]
 
 
@@ -40,24 +38,19 @@ def get_hexagon_grid(r, level, center):
     dx = r * 3 / 2
     dy = r * sqrt(3)
 
+    num = level - 1
+    numX = 2 * num + 1
+    numY = list(map(lambda x: 2 * num - abs(num - x) + 1, range(numX)))
+
+    cx = num * dx if center else 0
+    cy = num * dy / 2 if center else 0
+
     grid_values = []
     grid_x = 0
     grid_y = 0
 
-    # 0 1 2 3 4 3 2 1 0
-    num = level - 1
-    numX = 2 * num + 1
-    numY = list(map(lambda x: 2*num - abs(num - x) + 1, range(numX)))
-
-    print(numX)
-    print(numY)
-
-    cx = level * r if center else 0
-    # cy = level * dy if center else 0
-    cy = 0
-
     for i in range(numX):
-        grid_y = - (numY[i] - 1) * dy / 2
+        grid_y = - (numY[i] - numY[0]) * dy / 2
         for j in range(numY[i]):
             grid_values.append([grid_x - cx, grid_y - cy, 0])
             grid_y += dy
@@ -71,21 +64,16 @@ def get_diamond_grid(r, level, center):
     dx = r * 3 / 2
     dy = r * sqrt(3)
 
-    grid_values = []
-    grid_x = 0
-    grid_y = 0
-
-    # 0 1 2 3 4 3 2 1 0
     num = level - 1
     numX = 2 * num + 1
     numY = list(map(lambda x: num - abs(num - x) + 1, range(numX)))
 
-    print(numX)
-    print(numY)
-
     cx = num * dx if center else 0
-    # cy = (numy - 1) * r  if center else 0
     cy = 0
+
+    grid_values = []
+    grid_x = 0
+    grid_y = 0
 
     for i in range(numX):
         grid_y = - (numY[i] - 1) * dy / 2
@@ -103,8 +91,8 @@ def get_triangle_grid(r, level, center):
     dy = r * sqrt(3)
 
     num = level
+
     cx = (num - 1) * r if center else 0
-    # cy = (numy - 1) * r  if center else 0
     cy = 0
 
     grid_values = []
@@ -113,8 +101,7 @@ def get_triangle_grid(r, level, center):
 
     for i in range(num):
         grid_y = -int((i + 1) / 2) * dy
-        if (i % 2 != 0):
-            grid_y += dy / 2
+        grid_y += dy / 2 if (i % 2 != 0) else 0
         for j in range(i + 1):
             grid_values.append([grid_x - cx, grid_y - cy, 0])
             grid_y += dy
@@ -125,26 +112,23 @@ def get_triangle_grid(r, level, center):
 
 
 def get_rectangle_grid(r, numx, numy, center):
-
     dx = r * 3 / 2
     dy = r * sqrt(3)
 
     cx = (numx - 1) * dx / 2 if center else 0
-    cy = (numy - 1) * dy / 2 if center else 0
+    cy = (numy - 1.0 + 0.5 * (numx > 1)) * dy / 2 if center else 0
 
     grid_values = []
     grid_x = 0
     grid_y = 0
 
     for i in range(numx):
-        if (i % 2 != 0):
-            grid_y += dy / 2
+        grid_y = dy / 2 if (i % 2 != 0) else 0
         for j in range(numy):
             grid_values.append([grid_x - cx, grid_y - cy, 0])
             grid_y += dy
 
         grid_x += dx
-        grid_y = 0
 
     return grid_values
 
@@ -181,33 +165,28 @@ class SvHexaGridNode(bpy.types.Node, SverchCustomTreeNode):
         update=update_type)
 
     level = IntProperty(
-        name="Level",
+        name="Level", description="Number of levels in non rectangular patterns",
         default=3, min=1, soft_min=1,
-        description="Number of levels",
         update=updateNode)
 
     numx = IntProperty(
-        name="NumX",
+        name="NumX", description="Number of points along X",
         default=7, min=1, soft_min=1,
-        description="Number of points along X",
         update=updateNode)
 
     numy = IntProperty(
-        name="NumY",
+        name="NumY", description="Number of points along Y",
         default=6, min=1, soft_min=1,
-        description="Number of points along Y",
         update=updateNode)
 
     radius = FloatProperty(
-        name="Radius",
+        name="Radius", description="Radius of the grid tile",
         default=1.0, min=0.0, soft_min=0.0,
-        description="Radius of the grid",
         update=updateNode)
 
     center = BoolProperty(
-        name="Center",
-        default=False,
-        description="Center grid around origin",
+        name="Center", description="Center grid around origin",
+        default=True,
         update=updateNode)
 
     def sv_init(self, context):
@@ -230,9 +209,6 @@ class SvHexaGridNode(bpy.types.Node, SverchCustomTreeNode):
         if not any(s.is_linked for s in self.outputs):
             return
 
-        gridType = next(x[-1] for x in gridTypeItems if x[0] == self.gridType)
-        print("type: ", gridType)
-
         # input values lists
         input_level = self.inputs["Level"].sv_get()[0]
         input_numx = self.inputs["NumX"].sv_get()[0]
@@ -248,15 +224,8 @@ class SvHexaGridNode(bpy.types.Node, SverchCustomTreeNode):
         if self.outputs['Grid'].is_linked:
             gridList = []
 
-            if self.gridType == "DIAMOND":
-                parameters = match_long_repeat([input_level, input_radius])
-                for n, r in zip(*parameters):
-                    grid = get_diamond_grid(r, n, self.center)
-                    gridList.append(grid)
-
-            elif self.gridType == "RECTANGLE":
-                parameters = match_long_repeat(
-                    [input_numx, input_numy, input_radius])
+            if self.gridType == "RECTANGLE":
+                parameters = match_long_repeat([input_numx, input_numy, input_radius])
                 for nx, ny, r in zip(*parameters):
                     grid = get_rectangle_grid(r, nx, ny, self.center)
                     gridList.append(grid)
@@ -265,6 +234,12 @@ class SvHexaGridNode(bpy.types.Node, SverchCustomTreeNode):
                 parameters = match_long_repeat([input_level, input_radius])
                 for n, r in zip(*parameters):
                     grid = get_triangle_grid(r, n, self.center)
+                    gridList.append(grid)
+
+            if self.gridType == "DIAMOND":
+                parameters = match_long_repeat([input_level, input_radius])
+                for n, r in zip(*parameters):
+                    grid = get_diamond_grid(r, n, self.center)
                     gridList.append(grid)
 
             elif self.gridType == "HEXAGON":
