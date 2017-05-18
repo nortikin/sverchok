f3a8815c090dfec100992fe3577f7d7364ce2c0c
zeffii <tetha.z@gmail.com>
2017-05-18 11:28:14 +0200

    list convert from ziplongest

--- nodes/list_struct/slice_lite.py
+++ nodes/list_struct/slice_lite.py
@@ -28,7 +28,7 @@ def grouper(iterable, n, fillvalue=None):
     "Collect data into fixed-length chunks or blocks"
     # grouper('ABCDEFG', 3, 'x') --> ABC DEF Gxx"
     args = [iter(iterable)] * n
-    return zip_longest(*args, fillvalue=fillvalue)
+    return list(zip_longest(*args, fillvalue=fillvalue))
 
 
 def list_split(sublist, num_slices):
