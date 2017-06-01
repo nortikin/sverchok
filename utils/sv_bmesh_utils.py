e0488e38e8beb9e9bc9455ae5d319868b9c37b4c
Dealga McArdle <zeffii@users.noreply.github.com>
2017-06-01 20:01:23 +0200

    add bmesh_join function (#1695)
    * add bmesh_join function
    
    * adds final endline

--- utils/sv_bmesh_utils.py
+++ utils/sv_bmesh_utils.py
@@ -109,3 +109,45 @@ def with_bmesh(method):
     real_process.__name__ = method.__name__
     real_process.__doc__ = method.__doc__
     return real_process
+
+
+def bmesh_join(list_of_bmeshes, normal_update=False):
+    """ takes as input a list of bm references and outputs a single merged bmesh 
+
+    allows an additional 'normal_update=True' (default False) to force normal calculations
+
+    """
+
+    bm = bmesh.new()
+    add_vert = bm.verts.new
+    add_face = bm.faces.new
+    add_edge = bm.edges.new
+
+    for bm_to_add in list_of_bmeshes:
+        offset = len(bm.verts)
+
+        for v in bm_to_add.verts:
+            add_vert(v.co)
+
+        bm.verts.index_update()
+        bm.verts.ensure_lookup_table()
+
+        if bm_to_add.faces:
+            for face in bm_to_add.faces:
+                add_face(tuple(bm.verts[i.index+offset] for i in face.verts))
+            bm.faces.index_update()
+
+        if bm_to_add.edges:
+            for edge in bm_to_add.edges:
+                edge_seq = tuple(bm.verts[i.index+offset] for i in edge.verts)
+                try:
+                    add_edge(edge_seq)
+                except ValueError:
+                    # edge exists!
+                    pass
+            bm.edges.index_update()
+
+    if normal_update:
+        bm.normal_update()
+    
+    return bm
