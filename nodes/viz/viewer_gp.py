d171d354ed708a78d3958249b86ad31e84a5a965
Dealga McArdle <zeffii@users.noreply.github.com>
2017-06-21 21:56:40 +0200

    add color where it should be added (#1723)
    * add color where it should be added
    
    * add checks for color
    
    * add checks for color 2
    
    * add correct removal of unused colors = optional in ext
    
    * add correct fill and color code - almost finished
    
    * add rougher rounding

--- nodes/viz/viewer_gp.py
+++ nodes/viz/viewer_gp.py
@@ -29,13 +29,13 @@ from sverchok.utils.context_managers import new_input
 nodule_color = (0.899, 0.8052, 0.0, 1.0)
 
 
-def set_correct_stroke_count(strokes, coords):
+def set_correct_stroke_count(strokes, coords, BLACK):
     """ ensure that the number of strokes match the sets of coordinates """
     diff = len(strokes) - len(coords)
     if diff < 0:
         # add new strokes
         for _ in range(abs(diff)):
-            strokes.new()
+            strokes.new(colorname=BLACK.name)
     elif diff > 0:
         # remove excess strokes
         for _ in range(diff):
@@ -67,6 +67,52 @@ def match_points_and_pressures(pressure_set, num_points):
     return pressure_set
 
 
+def get_palette(grease_pencil, palette_name=None):
+    palettes = grease_pencil.palettes
+    if not palette_name in palettes:
+        palette = palettes.new(palette_name)
+    else:
+        palette = palettes.get(palette_name)
+    return palette
+
+
+def remove_unused_colors(palette, strokes):
+    """
+    optional cleanup step, probably best to not have this switched on by default
+    """
+    named_colors = [stroke.colorname for stroke in strokes] + [str([0,0,0])]
+    unused_named_colors = {color.name for color in palette.colors} - set(named_colors)
+    for unused_color in unused_named_colors:
+        palette.colors.remove(palette.colors[unused_color])
+
+
+def ensure_color_in_palette(node, palette, color, named_color=None, fill=None):
+
+    if not named_color:
+        if fill:
+            rounded_color = str([round(i, 4) for i in color[:4]]) + str([round(i, 4) for i in fill[:4]])
+        else:
+            rounded_color = str([round(i, 4) for i in color[:4]])
+        named_color = str(rounded_color)
+    else:
+        named_color = 'BLACK'
+
+    if not named_color in palette.colors:
+        new_color = palette.colors.new()
+        new_color.name = named_color
+        new_color.color = color[:3]
+        new_color.alpha = color[3]
+        new_color.use_hq_fill = node.use_hq_fill
+        if fill:
+            new_color.fill_color = fill[:3]
+            new_color.fill_alpha = fill[3]
+
+        # print('made', new_color.name)
+        return new_color
+    else:
+        return palette.colors[named_color]
+
+
 class SvGreasePencilStrokes(bpy.types.Node, SverchCustomTreeNode):
     ''' Make GreasePencil Strokes '''
     bl_idname = 'SvGreasePencilStrokes'
@@ -90,6 +136,9 @@ class SvGreasePencilStrokes(bpy.types.Node, SverchCustomTreeNode):
         update=updateNode, name='Fill', default=(0.2, 0.6, 0.9, 1.0),
         size=4, min=0.0, max=1.0, subtype='COLOR'
     )
+
+    auto_cleanup_colors = bpy.props.BoolProperty(default=True, update=updateNode)
+    use_hq_fill = bpy.props.BoolProperty(default=False, update=updateNode)
 
     draw_cyclic = bpy.props.BoolProperty(default=True, update=updateNode)
     pressure = bpy.props.FloatProperty(default=2.0, min=0.1, max=8.0, update=updateNode)
@@ -117,7 +166,11 @@ class SvGreasePencilStrokes(bpy.types.Node, SverchCustomTreeNode):
 
     def draw_buttons(self, context, layout):
         layout.prop(self, 'draw_mode', expand=True)
+        layout.prop(self, 'use_hq_fill', expand=True)
 
+    def draw_buttons_ext(self, context, layout):
+        layout.prop(self, 'auto_cleanup_colors', text='auto remove unused colors')
+    
 
     def get_pressures(self):
         pressures = self.inputs["pressure"].sv_get()
@@ -139,21 +192,31 @@ class SvGreasePencilStrokes(bpy.types.Node, SverchCustomTreeNode):
     def process(self):
         frame = self.inputs[0]
         coordinates = self.inputs[1]
-        coloors = self.inputs[4]
+        colors = self.inputs["stroke color"]
+        fills = self.inputs["fill color"]
+
         if frame.is_linked and coordinates.is_linked:
 
             strokes = frame.sv_get()
+            GP_DATA = strokes.id_data
+            PALETTE = get_palette(GP_DATA, "drafting_" + self.name)
+            BLACK = ensure_color_in_palette(self, PALETTE, [0,0,0], None, None)
+
             coords = coordinates.sv_get()
             self.num_strokes = len(coords)
-            set_correct_stroke_count(strokes, coords)
-            cols = coloors.sv_get()[0]
-             
+            set_correct_stroke_count(strokes, coords, BLACK)
+            cols = colors.sv_get()[0]
+            fill_cols = fills.sv_get()[0]
+
             cyclic_socket_value = self.inputs["draw cyclic"].sv_get()[0]
             fullList(cyclic_socket_value, self.num_strokes)
-
+            fullList(cols, self.num_strokes)
+            fullList(fill_cols, self.num_strokes)
             pressures = self.get_pressures()
 
-            for idx, (stroke, coord_set, color) in enumerate(zip(strokes, coords, cols)):
+            for idx, (stroke, coord_set, color, fill) in enumerate(zip(strokes, coords, cols, fill_cols)):
+                color_from_palette = ensure_color_in_palette(self, PALETTE, color=color, named_color=None, fill=fill)
+
                 stroke.draw_mode = self.draw_mode
                 stroke.draw_cyclic = cyclic_socket_value[idx]
 
@@ -163,13 +226,13 @@ class SvGreasePencilStrokes(bpy.types.Node, SverchCustomTreeNode):
                 flat_pressures = match_points_and_pressures(pressures[idx], num_points)
                 pass_pressures_to_stroke(stroke, flat_pressures)
 
-                print(color)
-                col = stroke.color.color
-                col.r, col.g, col.b = color[0:3]
-                print(col.r, col.g, col.b)
-                # color.fill_alpha
-                # color.alpha
+                stroke.line_width = 1
+                try:
+                    stroke.colorname = color_from_palette.name
+                except:
+                    print('stroke with index', idx, 'is not generated yet.')
 
+            remove_unused_colors(PALETTE, strokes)
             self.outputs[0].sv_set(strokes)
 
 
