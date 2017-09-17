3384e610b8d5fb0512bbb793b5261ae4efddd66a
zeffii <tetha.z@gmail.com>
2017-09-17 15:56:05 +0200

    add monad_cls_template_dict ref for readability

--- menu.py
+++ menu.py
@@ -140,7 +140,8 @@ def sv_group_items(context):
         if cls_ref and monad.cls_bl_idname:
             yield NodeItem(monad.cls_bl_idname, monad.name)
         elif monad.cls_bl_idname:
-            yield NodeItem("SvMonadGenericNode", monad.name, {"cls_bl_idname": "str('{}')".format(monad.cls_bl_idname)})
+            monad_cls_template_dict = {"cls_bl_idname": "str('{}')".format(monad.cls_bl_idname)}
+            yield NodeItem("SvMonadGenericNode", monad.name, monad_cls_template_dict)
 
 
 
