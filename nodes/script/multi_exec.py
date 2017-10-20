fbab2cf3e50321a281e7a11a72cd40d495b1a8a6
zeffii <tetha.z@gmail.com>
2017-10-20 13:53:07 +0200

    add cc code from node to clipboard

--- nodes/script/multi_exec.py
+++ nodes/script/multi_exec.py
@@ -108,6 +108,9 @@ class SvExecNodeMod(bpy.types.Node, SverchCustomTreeNode):
         col.operator(callback_id, text='copy to node').cmd = 'copy_from_text'
         col.prop_search(self, 'text', bpy.data, "texts", text="")
 
+        row = layout.row()
+        col.operator(callback_id, text='cc code to clipboard').cmd = 'copy_node_text_to_clipboard'
+
     def add_new_line(self, context):
         self.dynamic_strings.add().line = ""
 
@@ -143,8 +146,17 @@ class SvExecNodeMod(bpy.types.Node, SverchCustomTreeNode):
                 sds.add().line = ""
 
     def copy_from_text(self, context):
+        """ make sure self.dynamic_strings has enough strings to do this """
         for i, i2 in zip(self.dynamic_strings, bpy.data.texts[self.text].lines):
             i.line = i2.body
+
+    def copy_node_text_to_clipboard(self, context):
+        lines = [d.line for d in self.dynamic_strings]
+        if not lines:
+            return
+        str_lines = "\n".join(lines)
+        bpy.context.window_manager.clipboard = str_lines
+
 
     def insert_line(self, op_props):
 
@@ -160,6 +172,7 @@ class SvExecNodeMod(bpy.types.Node, SverchCustomTreeNode):
                 if op_props.idx == tidx:
                     sds.add().line = ""                
                 sds.add().line = i
+
 
 
     def sv_init(self, context):
