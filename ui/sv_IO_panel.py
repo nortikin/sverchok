6ac2da2bea61074adeecb0b8500cbb7f2edee988
zeffii <tetha.z@gmail.com>
2017-12-10 21:28:07 +0100

    add ui icon

--- ui/sv_IO_panel.py
+++ ui/sv_IO_panel.py
@@ -74,7 +74,7 @@ class SverchokIOLayoutsMenu(bpy.types.Panel):
 
             ziprow = box.row(align=True)
             ziprow.operator('node.blend_to_archive', text='archive blend as .zip').archive_ext = 'zip'
-            ziprow.operator('node.blend_to_archive', text='.gz').archive_ext = 'gz'
+            ziprow.operator('node.blend_to_archive', text='', icon='FILE_BACKUP').archive_ext = 'gz'
 
         else:
             ''' import '''
