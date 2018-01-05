44ab6478b15b8b81fef8dca1d3184c24b262d84d
Ilya Portnov <portnov@bk.ru>
2018-01-05 12:04:02 +0500

    Move to tests/ directory.
    Panels.
    
    Move to utils.testing.

--- utils/testing.py
+++ utils/testing.py
@@ -1,10 +1,12 @@
 
 import bpy
 
+from sverchok.utils.context_managers import sv_preferences
+
 def generate_node_definition(node):
     result = """
-get_or_create_node_tree()
-node = create_node("{}")
+tree = get_or_create_node_tree()
+node = create_node("{}", tree.name)
 """.format(node.bl_idname)
     
     for k, v in node.items():
@@ -49,4 +51,63 @@ def get_node(node_name, tree_name=None):
     if tree_name is None:
         tree_name = "TestingTree"
     return bpy.data.node_groups[tree_name].nodes[node_name]
+
+def nodes_are_equal(actual, reference):
+    if actual.bl_idname != reference.bl_idname:
+        raise AssertionError("Actual node {} has bl_idname `{}', but reference has `{}'".format(actual, actual.bl_idname, reference.bl_idname))
+    for k, v in actual.items():
+        if k not in reference:
+            raise AssertionError("Property `{}' is present is actual node {}, but is not present in reference".format(k, actual))
+        if v != reference[k]:
+            raise AssertionError("Property `{}' has value `{}' in actual node {}, but in reference it has value `{}'".format(k, actual, reference[k]))
+
+    for k in reference.keys():
+        if k not in actual:
+            raise AssertionError("Property `{}' is present in reference node, but is not present in actual node {}".format(k, actual))
+
+class SvRunTests(bpy.types.Operator):
+    """
+    Run all tests.
+    """
+
+    bl_idname = "node.sv_run_all_tests"
+    bl_label = "Run all tests"
+    bl_options = {'INTERNAL'}
+
+    def execute(self, context):
+        pass
+        return {'FINISHED'}
+
+class SvTestingPanel(bpy.types.Panel):
+    bl_idname = "SvTestingPanel"
+    bl_label = "Testing"
+    bl_space_type = 'NODE_EDITOR'
+    bl_region_type = 'UI'
+    bl_category = 'Sverchok'
+    use_pin = True
+
+    @classmethod
+    def poll(cls, context):
+        try:
+            if context.space_data.edit_tree.bl_idname != 'SverchCustomTreeType':
+                return False
+            with sv_preferences() as prefs:
+                return prefs.developer_mode
+        except:
+            return False
+
+    def draw(self, context):
+        layout = self.layout
+        layout.operator("node.sv_run_all_tests")
+
+classes = [SvRunTests, SvTestingPanel]
+
+def register():
+    for clazz in classes:
+        bpy.utils.register_class(clazz)
+
+def unregister():
+    for clazz in reversed(classes):
+        bpy.utils.unregister_class(clazz)
+
 
