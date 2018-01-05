2dfd832ab7795b1cd735d3c73fa80944dd5a9aa1
Ilya Portnov <portnov@bk.ru>
2018-01-05 20:47:49 +0500

    Add test for exporting tree with Profile node.

--- tests/tree_export_tests.py
+++ tests/tree_export_tests.py
@@ -5,11 +5,19 @@ import json
 from sverchok.utils.testing import *
 from sverchok.utils.sv_IO_panel_tools import create_dict_of_tree
 
-class ExportTreeTests(ReferenceTreeTestCase):
+class ScriptUvExportTest(ReferenceTreeTestCase):
 
     reference_file_name = "script_uv_ref.blend.gz"
 
-    def test_export(self):
+    def test_script_uv_export(self):
+        export_result = create_dict_of_tree(self.tree)
+        print(json.dumps(export_result))
+
+class ProfileExportTest(ReferenceTreeTestCase):
+
+    reference_file_name = "profile_ref.blend.gz"
+
+    def test_profile_export(self):
         export_result = create_dict_of_tree(self.tree)
         print(json.dumps(export_result))
 
