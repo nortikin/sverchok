dd906b26f2ca9d2676cd03a0e8a8897f94acfc10
Ilya Portnov <portnov@bk.ru>
2018-01-05 18:46:54 +0500

    Test torus export.

--- tests/simple_export_tests.py
+++ tests/simple_export_tests.py
@@ -37,7 +37,6 @@ class ExportSingleSimpleNode(SverchokTestCase):
         node.subd_ = 1
 
         export_result = create_dict_of_tree(self.tree)
-        print(json.dumps(export_result))
 
         expected_result = json.loads("""
         {"export_version": "0.07", "update_lists": [], "groups": {},
@@ -47,6 +46,36 @@ class ExportSingleSimpleNode(SverchokTestCase):
             "cap_": 0, "subd_": 1, "radBot_": 1.0299999713897705, "vert_": 33},
         "bl_idname": "CylinderNode", "label": "", "height": 100.0, "width":
         140.0, "hide": false}}}
+        """)
+
+        self.assert_json_equals(export_result, expected_result)
+
+    def test_torus_export(self):
+        node = create_node("SvTorusNode", self.tree.name)
+        node.mode = "MAJOR_MINOR"
+        node.Separate = 0
+        node.torus_eR = 1.2799999713897705
+        node.torus_R = 1.0299999713897705
+        node.torus_r = 0.25
+        node.torus_iR = 0.7799999713897705
+        node.torus_n1 = 33
+        node.torus_n2 = 17
+        node.torus_rP = 0.029999999329447746
+        node.torus_sP = 0.029999999329447746
+        node.torus_sT = 1
+
+        export_result = create_dict_of_tree(self.tree)
+
+        expected_result = json.loads("""
+        {"framed_nodes": {}, "update_lists": [], "groups": {}, "nodes":
+        {"Torus": {"height": 100.0, "width": 140.0, "hide": false, "location":
+        [0.0, 0.0], "color": [0.0, 0.5, 0.5], "params": {"torus_r": 0.25,
+        "torus_sP": 0.029999999329447746, "torus_eR": 1.2799999713897705,
+        "torus_R": 1.0299999713897705, "torus_iR": 0.7799999713897705,
+        "torus_n1": 33, "torus_n2": 17, "torus_sT": 1, "mode": "MAJOR_MINOR",
+        "Separate": 0, "torus_rP": 0.029999999329447746}, "label": "",
+        "use_custom_color": true, "bl_idname": "SvTorusNode"}},
+        "export_version": "0.07"}
         """)
 
         self.assert_json_equals(export_result, expected_result)
