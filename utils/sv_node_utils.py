259b7ad8282b439d766f5aa8763f68f4fae2b2ed
zeffii <tetha.z@gmail.com>
2018-01-14 10:44:52 +0100

    add frame/nested checking for rclick

--- utils/sv_node_utils.py
+++ utils/sv_node_utils.py
@@ -1,11 +1,10 @@
- """
-  * This file is part of project Sverchok. It's copyrighted by the contributors
-  * recorded in the version control history of the file, available from
-  * its original location https://github.com/nortikin/sverchok/commit/master
-  * 
-  * SPDX-License-Identifier: GPL3
-  * License-Filename: LICENSE
-"""
+# This file is part of project Sverchok. It's copyrighted by the contributors
+# recorded in the version control history of the file, available from
+# its original location https://github.com/nortikin/sverchok/commit/master
+#  
+# SPDX-License-Identifier: GPL3
+# License-Filename: LICENSE
+
 
 
 def recursive_framed_location_finder(node, loc_xy):
