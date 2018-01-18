ca167f28d435a2e2b358987a5dfd2ca56173e094
Victor Doval <produccionesluminosas@gmail.com>
2018-01-18 17:19:21 +0100

    Update edges_intersect_mk2
    Intergrated last Portnov updates with the 2D implementation.
    Links to updates:
    https://github.com/nortikin/sverchok/commit/354ecf1cb81fa396e3398291bde7c99ba3fc20f1#diff-067b2f92748fccfdc85dc714d8c981a8
    and https://github.com/nortikin/sverchok/commit/0615caf8f4ddc018615cf0e23b85b5c67f407c9b#diff-067b2f92748fccfdc85dc714d8c981a8

--- nodes/modifier_change/edges_intersect_mk2.py
+++ nodes/modifier_change/edges_intersect_mk2.py
@@ -35,9 +35,7 @@ modeItems = [
     ("2D", "2D", "", 0),
     ("3D", "3D", "", 1)]
 
-
 ''' helpers '''
-
 
 def order_points(edge, point_list):
     ''' order these edges from distance to v1, then
@@ -95,8 +93,24 @@ def get_intersection_dictionary(cm, bm, edge_indices):
     d = defaultdict(list)
 
     for edges in permutations:
-        raw_vert_indices = cm.vertex_indices_from_edges_tuple(bm, edges)
-        vert_vectors = cm.vectors_from_indices(bm, raw_vert_indices)
+        vert_vectors = cm.vectors_from_edges_tuple(bm, edges)
+        v1, v2, v3, v4 = vert_vectors
+
+        # Edges obviously can not intersect if their bounding
+        # boxes do not intersect
+        if (max(v1.x, v2.x) < min(v3.x, v4.x) or
+            max(v1.y, v2.y) < min(v3.y, v4.y) or
+            max(v1.z, v2.z) < min(v3.z, v4.z)):
+                continue
+        if (max(v3.x, v4.x) < min(v1.x, v2.x) or
+            max(v3.y, v4.y) < min(v1.y, v2.y) or
+            max(v3.z, v4.z) < min(v1.z, v2.z)):
+                continue
+
+        # Edges can not intersect if they do not lie in
+        # the same plane
+        if not cm.is_coplanar(vert_vectors):
+            continue
 
         points = intersect_line_line(*vert_vectors)
 
@@ -142,17 +156,20 @@ def unselect_nonintersecting(bm, d_edges, edge_indices):
         # print("unselected {}, non intersecting edges".format(reserved_edges))
 
 
-def intersect_edges(verts, edges, epsi):
-    bm = bmesh_from_pydata(verts, edges, [])
+def intersect_edges_3d(verts_in, edges_in, s_epsilon):
+
+    bm = bmesh_from_pydata(verts_in, edges_in, [])
 
     edge_indices = [e.index for e in bm.edges]
     trim_indices = len(edge_indices)
     for edge in bm.edges:
         edge.select = True
 
-    cm = CAD_ops(epsilon=epsi)
+    cm = CAD_ops(epsilon=s_epsilon)
+
     d = get_intersection_dictionary(cm, bm, edge_indices)
     unselect_nonintersecting(bm, d.keys(), edge_indices)
+
     # store non_intersecting edge sequencer
     add_back = [[i.index for i in edge.verts] for edge in bm.edges if not edge.select]
 
@@ -169,9 +186,10 @@ def intersect_edges(verts, edges, epsi):
 
 
 def edges_from_ed_inter(ed_inter):
-    '''create edges from intersecctions'''
+    '''create edges from intersecctions library'''
     edges_out = []
     for e in ed_inter:
+        # sort by first element of tuple (distances)
         e_s = sorted(e)
         for i in range(1, len(e_s)):
             edges_out.append((e_s[i-1][1], e_s[i][1]))
@@ -179,16 +197,16 @@ def edges_from_ed_inter(ed_inter):
 
 
 def intersect_edges_2d(verts, edges):
-    '''iterate through edges and find intersecctions'''
+    '''Iterate through edges  and expose them to intersect_line_line_2d'''
     verts_in = [Vector(v) for v in verts]
     ed_lengths = [(verts_in[e[1]] - verts_in[e[0]]).length for e in edges]
     verts_out = verts
     edges_out = []
     ed_inter = [[] for e in edges]
     for e, d, i in zip(edges, ed_lengths, range(len(edges))):
-
+        # if there is no intersections this will create a normal edge
         ed_inter[i].append([0.0, e[0]])
-        ed_inter[i].append([1.0, e[1]])
+        ed_inter[i].append([d, e[1]])
         v1 = verts_in[e[0]]
         v2 = verts_in[e[1]]
         if d == 0:
@@ -209,8 +227,9 @@ def intersect_edges_2d(verts, edges):
                 d_to_2 = (vx - v3.to_2d()).length
 
                 new_id = len(verts_out)
-                ed_inter[i].append([d_to_1 / d, new_id])
-                ed_inter[j].append([d_to_2 / d2, new_id])
+                # first item stores distance to origin, second the vertex id
+                ed_inter[i].append([d_to_1, new_id])
+                ed_inter[j].append([d_to_2, new_id])
                 verts_out.append((vx.x, vx.y, v1.z))
 
     edges_out = edges_from_ed_inter(ed_inter)
@@ -269,7 +288,7 @@ class SvIntersectEdgesNodeMK2(bpy.types.Node, SverchCustomTreeNode):
             return
 
         if self.mode == "3D":
-            verts_out, edges_out = intersect_edges(verts_in, edges_in, self.epsilon)
+            verts_out, edges_out = intersect_edges_3d(verts_in, edges_in, self.epsilon)
         else:
             verts_out, edges_out = intersect_edges_2d(verts_in, edges_in)
 
