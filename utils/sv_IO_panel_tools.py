4de01a7a1171a5c37cf8cc17f3a4bf25c61e86f5
zeffii <tetha.z@gmail.com>
2018-01-21 16:39:41 +0100

    pushes as much as possible to sensible functions

--- utils/sv_IO_panel_tools.py
+++ utils/sv_IO_panel_tools.py
@@ -183,19 +183,66 @@ def collect_custom_socket_properties(node, node_dict):
     # print("**\n")
 
 
+def can_skip_property(node, k):
+    """
+    n_id:
+        used to store the hash of the current Node,
+        this is created along with the Node anyway. skip.
+    typ, newsock:
+        reserved variables for changeable sockets
+    dynamic_strings:
+        reserved by exec node
+    frame_collection_name / type_collection_name both store Collection properties..avoiding for now
+    """
+
+    if k in {'n_id', 'typ', 'newsock', 'dynamic_strings', 'frame_collection_name', 'type_collection_name'}:
+        return True
+
+    elif node.bl_idname == 'SvProfileNodeMK2' and k in {'SvLists', 'SvSubLists'}:
+        # these are CollectionProperties, populated later.
+        return True
+
+    elif node.bl_idname == 'ObjectsNode' and (k == "objects_local"):
+        # this silences the import error when items not found.
+        return True
+
+    elif node.bl_idname == 'SvObjectsNodeMK3' and (k == 'object_names'):
+        # this supresses this k, in favour of hitting node.storage_get_data later
+        return True
+
+    elif node.bl_idname in PROFILE_NODES and (k == "filename"):
+        return True
+
+
+def display_introspection_info(node, k, v):
+    if not isinstance(v, (float, int, str)):
+        debug('//')
+        debug("%s -> property: %s: %s", node.name, k, type(v))
+        if k in node.bl_rna.properties:
+            debug(type(node.bl_rna.properties[k]))
+        elif k in node:
+            # something like node['lp']  , ID Property directly on the node instance.
+            debug(type(node[k]))
+        else:
+            error('%s is not bl_rna or IDproperty.. please report this', k)
+
+        debug('\\\\')
+
+
 def create_dict_of_tree(ng, skip_set={}, selected=False):
     nodes = ng.nodes
     layout_dict = {}
     nodes_dict = {}
     groups_dict = {}
     texts = bpy.data.texts
+
     if not skip_set:
         skip_set = {'Sv3DviewPropsNode'}
 
     if selected:
         nodes = list(filter(lambda n: n.select, nodes))
 
-    ''' get nodes and params '''
+    # get nodes and params
     for node in nodes:
 
         if node.bl_idname in skip_set:
@@ -205,57 +252,19 @@ def create_dict_of_tree(ng, skip_set={}, selected=False):
         node_items = {}
         node_enums = find_enumerators(node)
 
-        ObjectsNode = (node.bl_idname == 'ObjectsNode')
-        ObjectsNode3 = (node.bl_idname == 'SvObjectsNodeMK3')
-        ProfileParamNode = (node.bl_idname == 'SvProfileNode')
         IsGroupNode = (node.bl_idname == 'SvGroupNode')
-        IsMonadInstanceNode = (node.bl_idname.startswith('SvGroupNodeMonad'))
-        
         TextInput = (node.bl_idname in {'SvTextInNode', 'SvTextInNodeMK2'})
+        IsMonadInstanceNode = (node.bl_idname.startswith('SvGroupNodeMonad'))
 
         for k, v in node.items():
 
-            if not isinstance(v, (float, int, str)):
-                debug('//')
-                debug("%s -> property: %s: %s", node.name, k, type(v))
-                if k in node.bl_rna.properties:
-                    debug(type(node.bl_rna.properties[k]))
-                elif k in node:
-                    # something like node['lp']  , ID Property directly on the node instance.
-                    debug(type(node[k]))
-                else:
-                    error('%s is not bl_rna or IDproperty.. please report this', k)
+            display_introspection_info(node, k, v)
 
-                debug('\\\\')
-
-            # heavy handed skipping for testing.
-            if node.bl_idname == 'SvProfileNodeMK2':
-                if k in {'SvLists', 'SvSubLists'}:
-                    continue
-
-            if k in {'n_id', 'typ', 'newsock', 'dynamic_strings', 'frame_collection_name', 'type_collection_name'}:
-                """
-                n_id:
-                    used to store the hash of the current Node,
-                    this is created along with the Node anyway. skip.
-                typ, newsock:
-                    reserved variables for changeable sockets
-                dynamic_strings:
-                    reserved by exec node
-                frame_collection_name / type_collection_name both store Collection properties..avoiding for now
-                """
+            if can_skip_property(node, k):
                 continue
-
-            if has_state_switch_protection(node, k):
+            elif has_state_switch_protection(node, k):
                 continue
-
-            if ObjectsNode and (k == "objects_local"):
-                # this silences the import error when items not found.
-                continue
-            elif ObjectsNode3 and (k == 'object_names'):
-                node_dict['object_names'] = [o.name for o in node.object_names]
-                continue
-
+            
             if TextInput and (k == 'current_text'):
                 node_dict['current_text'] = node.text
                 node_dict['textmode'] = node.textmode
@@ -267,10 +276,6 @@ def create_dict_of_tree(ng, skip_set={}, selected=False):
                     node_dict['text_lines']['stored_as_json'] = json_as_dict
                 else:
                     node_dict['text_lines'] = texts[node.text].as_string()
-
-            if node.bl_idname in PROFILE_NODES and (k == "filename"):
-                '''add file content to dict'''
-                node_dict['path_file'] = texts[node.filename].as_string()
 
             if IsGroupNode and (k == "group_name"):
                 if v not in groups_dict:
