a4b32d121c403f86cd18ca4e8d8a13b2cc325416
Ilya Portnov <portnov@iportnov.ru>
2018-01-21 15:30:34 +0500

    Test for disable + enable again Sverchok addon.

--- tests/init_tests.py
+++ tests/init_tests.py
@@ -1,5 +1,6 @@
 
 import bpy
+import addon_utils
 
 from sverchok.utils.testing import *
 from sverchok.utils.logging import debug, info
@@ -9,4 +10,8 @@ class InitTests(SverchokTestCase):
     def test_core_exists(self):
         bpy.ops.script.reload()
         import sverchok.core
+
+    def test_disable_enable(self):
+        addon_utils.disable("sverchok")
+        addon_utils.enable("sverchok")
 
