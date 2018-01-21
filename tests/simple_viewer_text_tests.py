d6789cd23c2081db9d30c3ae5f884d52d7b66670
Ilya Portnov <portnov@bk.ru>
2018-01-21 13:19:57 +0500

    Test case for TextViewer Mk3.

--- tests/simple_viewer_text_tests.py
+++ tests/simple_viewer_text_tests.py
@@ -1,23 +1,31 @@
 
-from sverchok.core.socket_conversions import ImplicitConversionProhibited
+import bpy
 from sverchok.utils.testing import *
 from sverchok.utils.logging import debug, info
 
-class SocketConversionTests(EmptyTreeTestCase):
+class TextViewerTest(EmptyTreeTestCase):
     
-    def test_vertices_to_matrices(self):
-        """
-        Test that cyl -> viewer text works.
-        """
+    def test_text_viewer(self):
 
         cyl = create_node("CylinderNode")
         cyl.Separate = True
         viewer_text = create_node("ViewerNodeTextMK3")
 
-        # Connect NGon node to MatrixApply node
+        # Connect Cylinder -> Viewer
         self.tree.links.new(cyl.outputs['Vertices'], viewer_text.inputs[0])
 
-        # Trigger processing of NGon node
+        # Trigger processing of Cylinder node
         cyl.process()
-        # Read what ViewerText node sees
-        data = viewer_text.inputs[0].sv_get()
+        # Invoke "VIEW" operator
+        bpy.ops.node.sverchok_viewer_buttonmk1('EXEC_DEFAULT', treename=self.tree.name, nodename=viewer_text.name)
+
+        # Read what the operator has written to the text buffer.
+        text = bpy.data.texts['Sverchok_viewer'].as_string()
+        
+        # Test that text in buffer equals exactly to the text saved
+        # in the file from the first test run.
+        with open(self.get_reference_file_path("text_viewer_out.txt"), "r") as f:
+            expected_text = f.read()
+            self.assertEquals(text, expected_text)
+
+
