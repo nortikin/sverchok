4e3cc8ae0b845c1d17a75a790b0e14e64e45ff61
zeffii <tetha.z@gmail.com>
2018-02-09 10:28:09 +0100

    add more caution to json-storage code

--- nodes/generators_extended/profile_mk2.py
+++ nodes/generators_extended/profile_mk2.py
@@ -946,17 +946,24 @@ class SvProfileNodeMK2(bpy.types.Node, SverchCustomTreeNode):
         self.id_data.unfreeze(hard=True)
 
 
-    def storage_get_data(self, node_dict):
-        local_storage = {'knots': [], 'knotsnames': []}
+    def storage_get_data(self, node_dict): 
+        local_storage = {'knots': [], 'knotsnames': []} 
 
-        for knot in self.SvLists['knots'].SvSubLists:
-            local_storage['knots'].append([knot.SvX, knot.SvY, knot.SvZ])
+        if "knots" in self.SvLists:
+            # implies "knotsnames" will be found too.. because that's how it works..
+            for knot in self.SvLists['knots'].SvSubLists: 
+                local_storage['knots'].append([knot.SvX, knot.SvY, knot.SvZ]) 
 
-        for outname in self.SvLists['knotsnames'].SvSubLists:
-            local_storage['knotsnames'].append(outname.SvName)
+            for outname in self.SvLists['knotsnames'].SvSubLists: 
+                local_storage['knotsnames'].append(outname.SvName) 
         
-        node_dict['profile_sublist_storage'] = json.dumps(local_storage, sort_keys=True)
-        node_dict['path_file'] = bpy.data.texts[self.filename].as_string()
+        # store anyway  
+        node_dict['profile_sublist_storage'] = json.dumps(local_storage, sort_keys=True) 
+        
+        if self.filename:
+            node_dict['path_file'] = bpy.data.texts[self.filename].as_string() 
+        else:
+            node_dict['path_file'] = ""
 
 
 
