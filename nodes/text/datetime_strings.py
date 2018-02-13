78c82ae153c6b1771c079bb091e56096f25da28a
zeffii <tetha.z@gmail.com>
2018-02-13 13:30:28 +0100

    adds reference to the node, created by toggling the npanel Make Ref

--- nodes/text/datetime_strings.py
+++ nodes/text/datetime_strings.py
@@ -30,6 +30,49 @@ import bpy
 from sverchok.node_tree import SverchCustomTreeNode
 from sverchok.data_structure import updateNode, match_long_repeat
 
+reference_table = """\
+Note: Examples are based (http://strftime.org/ ) on datetime.datetime(2013, 9, 30, 7, 6, 5)
+Python Docs: https://docs.python.org/3.6/library/datetime.html#strftime-and-strptime-behavior
++-----+------------+---------------------
+|Code | Catches    | Meaning Example
++=====+============+=====================
+| %a  | Mon        | Weekday as locale’s abbreviated name.
+| %A  | Monday     | Weekday as locale’s full name.
+| %w  | 1          | Weekday as a decimal number, where 0 is Sunday and 6 is Saturday.
+| %d  | 30         | Day of the month as a zero-padded decimal number.
+| %-d | 30         | Day of the month as a decimal number. (Platform specific)
+| %b  | Sep        | Month as locale’s abbreviated name.
+| %B  | September  | Month as locale’s full name.
+| %m  | 09         | Month as a zero-padded decimal number.
+| %-m | 9          | Month as a decimal number. (Platform specific)
+| %y  | 13         | Year without century as a zero-padded decimal number.
+| %Y  | 2013       | Year with century as a decimal number.
+| %H  | 07         | Hour (24-hour clock) as a zero-padded decimal number.
+| %-H | 7          | Hour (24-hour clock) as a decimal number. (Platform specific)
+| %I  | 07         | Hour (12-hour clock) as a zero-padded decimal number.
+| %-I | 7          | Hour (12-hour clock) as a decimal number. (Platform specific)
+| %p  | AM         | Locale’s equivalent of either AM or PM.
+| %M  | 06         | Minute as a zero-padded decimal number.
+| %-M | 6          | Minute as a decimal number. (Platform specific)
+| %S  | 05         | Second as a zero-padded decimal number.
+| %-S | 5          | Second as a decimal number. (Platform specific)
+| %f  | 000000     | Microsecond as a decimal number, zero-padded on the left.
+| %z  | ?          | UTC offset in the form +HHMM or -HHMM (empty string if the the object is naive).
+| %Z  | ?          | Time zone name (empty string if the object is naive).
+| %j  | 273        | Day of the year as a zero-padded decimal number.
+| %-j | 273        | Day of the year as a decimal number. (Platform specific)
+| %U  | 39         | Week number of the year (Sunday as the first day of the week) as a zero padded decimal number.
+|     |            | All days in a new year preceding the first Sunday are considered to be in week 0.
+| %W  | 39         | Week number of the year (Monday as the first day of the week) as a decimal number.
+|     |            | All days in a new year preceding the first Monday are considered to be in week 0.
+| %c  | Mon Sep 30 07:06:05 2013 | Locale’s appropriate date and time representation.
+| %x  | 09/30/13                 | Locale’s appropriate date representation.
+| %X  | 07:06:05                 | Locale’s appropriate time representation.
+| %%  | %                        | A literal '%' character.
++-----+--------------------------+
+
+"""
+
 
 class SvDatetimeStrings(bpy.types.Node, SverchCustomTreeNode):
     ''' a SvDatetimeStrings f '''
@@ -37,8 +80,19 @@ class SvDatetimeStrings(bpy.types.Node, SverchCustomTreeNode):
     bl_label = 'Datetime Strings'
     bl_icon = 'SORTTIME'
 
+    def show_short_reference(self, context):
+        """ just keep writing to the table if it exists. """
+
+        doc_name = "[DOC] strptime / strftime"
+        texts = bpy.data.texts
+        if not doc_name in texts:
+            texts.new(doc_name)
+        texts[doc_name].from_string(reference_table)
+
+
     date_pattern = bpy.props.StringProperty(default="%m/%d/%Y", description="date formatting information", name="Date Time String Formatter")
     time_offset = bpy.props.StringProperty(default="01/01/2018", description="for graphing purposes you might need to subtract a start date", name='offset')
+    make_reference = bpy.props.BoolProperty(name="Make Reference in Texts", update=show_short_reference)
 
     def sv_init(self, context):
         self.inputs.new("StringsSocket", "times")
@@ -47,6 +101,9 @@ class SvDatetimeStrings(bpy.types.Node, SverchCustomTreeNode):
 
     def draw_buttons(self, context, layout):
         layout.prop(self, "date_pattern", text="", icon="SORTTIME")
+
+    def draw_buttons_ext(self, context, layout):
+        layout.prop(self, "make_reference")
 
     def process(self):
 
