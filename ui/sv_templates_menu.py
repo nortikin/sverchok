7a01b404a9ae80ce81ee9849d71d4a66e7c58800
zeffii <tetha.z@gmail.com>
2018-03-12 23:33:25 +0100

    dynamically generate submenu too

--- ui/sv_templates_menu.py
+++ ui/sv_templates_menu.py
@@ -1,27 +1,53 @@
-# ##### BEGIN GPL LICENSE BLOCK #####
-#
-#  This program is free software; you can redistribute it and/or
-#  modify it under the terms of the GNU General Public License
-#  as published by the Free Software Foundation; either version 2
-#  of the License, or (at your option) any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with this program; if not, write to the Free Software Foundation,
-#  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
-#
-# ##### END GPL LICENSE BLOCK #####
+# This file is part of project Sverchok. It's copyrighted by the contributors
+# recorded in the version control history of the file, available from
+# its original location https://github.com/nortikin/sverchok/commit/master
+#  
+# SPDX-License-Identifier: GPL3
+# License-Filename: LICENSE
 
+from pathlib import Path
 
 import bpy
 import os
 from sverchok.utils.sv_update_utils import sv_get_local_path
 
 sv_path = os.path.dirname(sv_get_local_path()[0])
+
+
+def get_paths():
+    paths = {}
+    dataset_root = Path(sv_path)
+    analyzer_path = dataset_root / 'json_examples'
+    for listed_path in analyzer_path.iterdir():
+        if listed_path.is_dir():
+            paths[listed_path.name] = str(listed_path)
+
+    return paths
+
+paths = get_paths()
+
+
+def draw_item(self, context, item):
+
+    ntree = context.space_data.node_tree
+    if not ntree:
+        ntree = lambda: None
+        ntree.name = '____make_new____'
+
+    self.path_menu(searchpaths=[paths.get(item)], operator='node.tree_importer_silent', props_default={"id_tree": ntree.name})
+
+# quick class factory.
+def make_class(bl_label):
+    bl_idname = "SvPyMenu" + bl_label
+    draw_func = lambda s, c: draw_item(s, c, bl_label)
+    return type(bl_idname, (bpy.types.Menu,), {'bl_label': bl_label, 'draw': draw_func, 'bl_idname': bl_idname})
+
+menu_classes = []
+for catname in paths.keys():
+    locals()['SvPyMenu'+ catname] = make_class(catname) 
+    menu_classes.append(locals()['SvPyMenu'+ catname])
+
+
 
 # Node Examples Menu
 class SV_MT_layouts_templates(bpy.types.Menu):
@@ -30,16 +56,6 @@ class SV_MT_layouts_templates(bpy.types.Menu):
     bl_label = "Examples"
     bl_description = "List of Sverchok Examples"
 
-
-    def avail_templates():
-        fullpath = [sv_path, "json_examples"]
-        templates_path = os.path.join(*fullpath)
-        items = [[os.path.join(templates_path,t),t] for t in next(os.walk(templates_path))[2]]
-        items = [item for item in items if not item[0].endswith(".zip")]
-        items.sort()
-        return items
-
-    sv_templates = avail_templates()
 
     @classmethod
     def poll(cls, context):
@@ -50,18 +66,11 @@ class SV_MT_layouts_templates(bpy.types.Menu):
 
     def draw(self, context):
         layout = self.layout
-        ntree = context.space_data.node_tree
-        if not ntree:
-            ntree = lambda: None
-            ntree.name = '____make_new____'
 
-        for svt in self.sv_templates:
-            a = layout.operator(
-                'node.tree_importer_silent',
-                text=str(svt[1]),
-                icon='RNA')
-            a.id_tree = ntree.name
-            a.filepath = svt[0]
+        scene = context.scene
+        for cls in menu_classes:
+            layout.menu(cls.__name__)
+
 
 
 def node_templates_pulldown(self, context):
@@ -69,22 +78,22 @@ def node_templates_pulldown(self, context):
         layout = self.layout
         row = layout.row(align=True)
         row.scale_x = 1.3
-        row.menu("SV_MT_layouts_templates",
-                 icon="RNA")
-
-from sverchok.ui import sv_panels
+        row.menu("SV_MT_layouts_templates", icon="RNA")
 
 
+# from sverchok.ui import sv_panels
 
+classes = menu_classes + [SV_MT_layouts_templates]
 
 def register():
-    bpy.utils.register_class(SV_MT_layouts_templates)
+    _ = [bpy.utils.register_class(cls) for cls in classes]
     bpy.types.NODE_HT_header.append(node_templates_pulldown)
 
 
 def unregister():
     bpy.types.NODE_HT_header.remove(node_templates_pulldown)
-    bpy.utils.unregister_class(SV_MT_layouts_templates)
+    _ = [bpy.utils.register_class(cls) for cls in reversed(classes)]
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     register()
