387e0cbe6100c498fd05a160b5252b062f42d332
Victor Doval <10011941+vicdoval@users.noreply.github.com>
2018-03-23 19:27:54 +0100

    List Math warp output (#2168)
    * added warp to output
    
    * added trigger and tooltip
    
    * quotes typo correction
    
    * draw ext property to wrap

--- nodes/list_main/func.py
+++ nodes/list_main/func.py
@@ -19,7 +19,7 @@
 from itertools import accumulate
 
 import bpy
-from bpy.props import EnumProperty, IntProperty
+from bpy.props import EnumProperty, IntProperty, BoolProperty
 
 from sverchok.node_tree import SverchCustomTreeNode
 from sverchok.data_structure import updateNode
@@ -28,7 +28,10 @@ def acc(l):
     return list(accumulate(l))
 
 class ListFuncNode(bpy.types.Node, SverchCustomTreeNode):
-    ''' List function '''
+    '''
+    Triggers: List functions
+    Tooltip: Operations with list, sum, average, min, max
+    '''
     bl_idname = 'ListFuncNode'
     bl_label = 'List Math'
     bl_icon = 'OUTLINER_OB_EMPTY'
@@ -46,12 +49,17 @@ class ListFuncNode(bpy.types.Node, SverchCustomTreeNode):
     level = IntProperty(name='level_to_count',
                         default=1, min=0,
                         update=updateNode)
+    wrap = BoolProperty(name='wrap', description='extra level add', 
+                        default=False,update=updateNode)
 #    typ = bpy.props.StringProperty(name='typ', default='')
 #    newsock = bpy.props.BoolProperty(name='newsock', default=False)
 
     def draw_buttons(self, context, layout):
         layout.prop(self, "level", text="level")
         layout.prop(self, "func_", "Functions:")
+
+    def draw_buttons_ext(self, context, layout):
+        layout.prop(self, "wrap", text="wrap")
 
     def sv_init(self, context):
         self.inputs.new('StringsSocket', "Data", "Data")
@@ -74,7 +82,7 @@ class ListFuncNode(bpy.types.Node, SverchCustomTreeNode):
                 else:
                     out = self.count(data, self.level, func)
 
-                self.outputs['Function'].sv_set(out)
+                self.outputs['Function'].sv_set([out] if self.wrap else out)
 
     def count(self, data, level, func):
         out = []
