e108f109af8360aa9bf775782ef507d479974319
Durman <sv@soluyanov.ru>
2018-05-18 23:21:49 +0400

    Update Set_UVMap.py
    Added new matrix input socket, warnings about difference number of input UV points and real UV points of mesh

--- nodes/scene/Set_UVMap.py
+++ nodes/scene/Set_UVMap.py
@@ -18,23 +18,70 @@
 
 import bpy
 from bpy.props import StringProperty
+from mathutils import Vector, Matrix
 
 from sverchok.node_tree import SverchCustomTreeNode
-from sverchok.data_structure import updateNode
+from sverchok.data_structure import updateNode, repeat_last
+
+WARNING = []
 
 def create_uvmap(obj_in, uv_name):
     #Create basic UV map for object in scene with current name
     [obj.data.uv_textures.new(uv_name) for obj in obj_in 
         if not obj.data.uv_textures.get(uv_name, False)]
+        
+def remove_uv(obj_in, uv_name):
+    #Remove UV map from objects with current name of map
+    [obj.data.uv_textures.remove(obj.data.uv_textures[uv_name]) for obj in obj_in 
+        if obj.data.uv_textures.get(uv_name, False)]
+        
+def make_worning(bo):
+    w = lambda n: print("Set UVMap node - input data have wrong size in %d.object." % n)
+    [w(n) for n,b in enumerate(bo) if b]
     
-def set_uvmap(uv_name, loop_ver, obj_in):
+def set_uvmap(uv_name, loop_ver, obj_in, mat_in):
     #Set custom UV maps with current name for current objects in scene
-    for uv_obj, obj in zip(loop_ver, obj_in):
+    for uv_obj, obj, mat_obj in zip(loop_ver, obj_in, repeat_last(mat_in)):
+        
+        len_loop = len(obj.data.loops)
+        len_uv_in = len(uv_obj)
+        WARNING.append(False if len_loop == len_uv_in else True)
+        
+        if len_loop < len_uv_in:
+            uv_obj = uv_obj[0:len_loop]
+        elif len_loop > len_uv_in:
+            uv_obj = [uv for _, uv in zip(range(len_loop), repeat_last(uv_obj))]
         
         xy_uv = [(u,v) for u,v,_ in uv_obj]
+        
+        if mat_obj:
+            xy_uv = [(mat * Vector(uv).to_3d())[:2] for uv, mat in zip(xy_uv, repeat_last(mat_obj))]
+        
         per_loop_list = [uv for pair in xy_uv for uv in pair]
     
         obj.data.uv_layers[uv_name].data.foreach_set("uv", per_loop_list)
+        
+def deform_baseuv(uv_name, obj_in, mat_in):
+    #It removes and creates new map each time before make deform
+    for obj, mat in zip(obj_in, repeat_last(mat_in)):
+
+        obj.data.uv_textures.remove(obj.data.uv_textures[uv_name])
+        obj.data.uv_textures.new(uv_name)
+        
+        uv_co = [l.uv for l in obj.data.uv_layers[uv_name].data]
+        
+        xy_uv = [(m * l.to_3d())[:2] for l, m in zip(uv_co, repeat_last(mat))]
+        per_loop_list = [uv for pair in xy_uv for uv in pair]
+        
+        obj.data.uv_layers[uv_name].data.foreach_set("uv", per_loop_list)
+        
+def check_data_in(soc):
+    #puts data in nested list if necessary
+    
+    if type(soc.sv_get()[0]) == list:
+        return soc.sv_get()
+    else:
+        return [soc.sv_get()]
 
 class SvSetUVMap(bpy.types.Node, SverchCustomTreeNode):
     """
@@ -52,6 +99,7 @@ class SvSetUVMap(bpy.types.Node, SverchCustomTreeNode):
     
     def sv_init(self, context):
         self.inputs.new('VerticesSocket', 'Loops vertices')
+        self.inputs.new('MatrixSocket', 'Matrix')
         self.inputs.new('SvObjectSocket', 'Object')
         self.outputs.new('SvObjectSocket', 'Object')
         
@@ -60,16 +108,28 @@ class SvSetUVMap(bpy.types.Node, SverchCustomTreeNode):
         
     def process(self):
             
-        if self.inputs['Object'].is_linked or len(self.inputs['Object'].values()[0]) != 0:
-            obj_in  = self.inputs['Object'].sv_get()
-            uv_name = self.UV_name
+        if not (self.inputs['Object'].is_linked or len(self.inputs['Object'].values()[0]) != 0):
+            return
+
+        obj_in  = self.inputs['Object'].sv_get()
+        uv_name = self.UV_name
+        mat_in  = (check_data_in(self.inputs['Matrix']) 
+                   if self.inputs['Matrix'].is_linked else [False])
+        loop_ver = (self.inputs['Loops vertices'].sv_get()
+                    if self.inputs['Loops vertices'].is_linked else None)
             
-            create_uvmap(obj_in, uv_name)
+        create_uvmap(obj_in, uv_name)
     
-            if self.inputs['Loops vertices'].is_linked:
-                loop_ver = self.inputs['Loops vertices'].sv_get()
+        if self.inputs['Loops vertices'].is_linked:
+            #Mode with own unwrapping
+            set_uvmap(uv_name, loop_ver, obj_in, mat_in)
+            make_worning(WARNING)
                 
-                set_uvmap(uv_name, loop_ver, obj_in)
+        elif self.inputs['Matrix'].is_linked:
+            #Mode with bas unwrapping
+            deform_baseuv(uv_name, obj_in, mat_in)
+                
+        self.outputs['Object'].sv_set(self.inputs['Object'].sv_get())
 
     
 def register():
