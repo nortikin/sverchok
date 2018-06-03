52d12d264fde789192b46814647e7fc8a34c1041
nortikin <nikitronn@yandex.ru>
2018-06-03 15:36:41 +0300

    print remove

--- node_scripts/SNLite_templates/flip_polygons.py
+++ node_scripts/SNLite_templates/flip_polygons.py
@@ -11,7 +11,6 @@ if pols:
     po = ar(pols)
     sha = np.shape(po)
     sh = [sha[0],U,int(sha[1]/U),sha[2]]
-    print(sh)
     pols_after = np.reshape(po,sh)
     pols_out = np.reshape(pols_after.transpose(0,2,1,3),sha).tolist()
 else:
