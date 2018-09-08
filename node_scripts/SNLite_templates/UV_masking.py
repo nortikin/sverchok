1ef7bd2a93adcdb49b9dc6efcfea1634b2059453
nortikin <nikitronn@yandex.ru>
2018-09-09 00:22:15 +0300

    viewer index fixing - now texts shares to edges/faces same as to verts

--- node_scripts/SNLite_templates/UV_masking.py
+++ node_scripts/SNLite_templates/UV_masking.py
@@ -1,10 +1,12 @@
 """
-in   pols    s   .=[]   n=0
-in   U_mask     s   .=[]    n=0
-in   V_mask     s   .=[]    n=0
+in   pols         s   .=[]    n=0
+in   U_mask       s   .=[]    n=0
+in   V_mask       s   .=[]    n=0
+in   U            s   .=16    n=2
+in   V            s   .=8     n=2
 out  pols_True    s
 out  pols_False   s
-out  mask   s
+out  mask         s
 """
 # it makes filter ranges of 0 and 1 chains.
 # input U is i.e. [[2,10,40,4]]
@@ -30,10 +32,17 @@ for j in V_mask[0]:
     V_.extend([tmblr for k in range(j)])
     tmblr = not tmblr
 z = 0
+z1 = (V-1)*6-1
+z2 = (V-1)*(U-1)-(V-1)*5
+print(z1,z2)
 l = len(pols[0])
 for q in U_:
+    tmblr = True
     for w in V_:
-        t = q and w
+        t = q and w and tmblr
+        tmblr = not tmblr
+        if z1<z<z2:
+            tmblr = True
         z += 1
         mask.append(t)
         if t:
