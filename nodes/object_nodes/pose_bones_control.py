af3c6a8e6086573f1ed3e5f7a92ba055caa884a9
Kosvor2 <kosvor.v2016@yandex.ru>
2018-09-19 09:59:16 +0500

    Update pose_bones_control.py

--- nodes/object_nodes/pose_bones_control.py
+++ nodes/object_nodes/pose_bones_control.py
@@ -17,10 +17,9 @@
 # ##### END GPL LICENSE BLOCK #####
 
 import bpy
-from math import radians
 from bpy.props import IntProperty, StringProperty
 from sverchok.node_tree import SverchCustomTreeNode
-from sverchok.data_structure import match_long_cycle as mlc, updateNode
+from sverchok.data_structure import updateNode
 
 
 class SvArmatureControlNode(bpy.types.Node, SverchCustomTreeNode):
@@ -34,28 +33,18 @@ class SvArmatureControlNode(bpy.types.Node, SverchCustomTreeNode):
     def sv_init(self, context):
         self.inputs.new('SvObjectSocket', 'Armature Object')
         self.inputs.new('StringsSocket', 'bone select index').prop_name = "Sindex"
-        self.inputs.new('VerticesSocket', "rotation XYZ")
         self.inputs.new('MatrixSocket', "matrix basis")
         
 
     def process(self):
-        armobj, selind, rot, rotmat = self.inputs
-        
-        numbb = selind.sv_get()[0]
-        armat = [armobj.sv_get()[0].pose.bones[i] for i in numbb]
+        armobjL, selind, rotmat = self.inputs
+        R = rotmat.sv_get()
 
-
-        if rot.is_linked:
-            R = rot.sv_get()[0]
-            for obarm, r in zip(armat, R):
-                obarm.rotation_mode = 'XYZ'    
-                obarm.rotation_euler = (radians(r[0]), radians(r[1]), radians(r[2]))
-
-
-        elif rotmat.is_linked:
-            R = rotmat.sv_get()
-            for obarm, r in zip(armat, R):
-                obarm.matrix_basis = r
+        for ilist, armobj in zip(selind.sv_get(), armobjL.sv_get()):
+            armatbl = [armobj.pose.bones[i] for i in ilist]
+            
+            for bone, r in zip(armatbl, R):
+                bone.matrix_basis = r
 
 
 
