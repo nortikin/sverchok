5155ece7acfacd371f90b9fa44a3a8a021ec7a3a
zeffii <tetha.z@gmail.com>
2018-11-22 21:36:55 +0100

    add vd experimental node file

---
+++ nodes/viz/vd_draw_experimental.py
@@ -0,0 +1,207 @@
+# This file is part of project Sverchok. It's copyrighted by the contributors
+# recorded in the version control history of the file, available from
+# its original location https://github.com/nortikin/sverchok/commit/master
+#  
+# SPDX-License-Identifier: GPL3
+# License-Filename: LICENSE
+
+
+import bpy
+import gpu
+from gpu_extras.batch import batch_for_shader
+
+# import mathutils
+from mathutils import Vector, Matrix
+from mathutils.geometry import tessellate_polygon as tessellate
+
+import sverchok
+from bpy.props import StringProperty, BoolProperty, FloatVectorProperty
+from sverchok.node_tree import SverchCustomTreeNode
+from sverchok.data_structure import node_id, updateNode
+from sverchok.ui.bgl_callback_3dview import callback_disable, callback_enable
+from sverchok.utils.sv_batch_primitives import MatrixDraw28
+from sverchok.utils.geom import multiply_vectors_deep
+
+
+def screen_v3dMatrix(context, args):
+    mdraw = MatrixDraw28()
+    for matrix in args[0]:
+        mdraw.draw_matrix(matrix)
+
+# def screen_v3dBGL(context, args):
+#     # region = context.region
+#     # region3d = context.space_data.region_3d
+    
+#     shader = args[0]
+#     batch = args[1]
+#     line4f = args[2]
+
+#     shader.bind()
+#     shader.uniform_float("color", line4f)
+#     batch.draw(shader)
+
+def draw_edges(context, args):
+    geom, line4f = args
+    coords, indices = geom.verts, geom.edges
+
+    shader = gpu.shader.from_builtin('3D_UNIFORM_COLOR')
+    batch = batch_for_shader(shader, 'LINES', {"pos" : coords}, indices=indices)
+    shader.bind()
+    shader.uniform_float("color", line4f)
+    batch.draw(shader)
+
+def draw_faces(context, args):
+    geom, line4f = args
+    coords, indices = geom.verts, geom.faces
+
+    new_indices = []
+    concat = new_indices.append
+    for idxset in indices:
+        num_verts = len(idxset)
+        if num_verts == 3:
+            concat(tuple(idxset))
+        elif num_verts == 4:
+            # a b c d  ->  [a, b, c], [a, c, d]
+            concat((idxset[0], idxset[1], idxset[2]))
+            concat((idxset[0], idxset[2], idxset[3]))
+        else:
+            subcoords = [Vector(coords[idx]) for idx in idxset]
+            for pol in tessellate([subcoords]):
+                concat([idxset[i] for i in pol])
+
+    shader = gpu.shader.from_builtin('3D_UNIFORM_COLOR')
+    batch = batch_for_shader(shader, 'TRIS', {"pos" : coords}, indices=new_indices)
+    shader.bind()
+    shader.uniform_float("color", line4f)
+    batch.draw(shader)
+
+
+
+class SvVDExperimental(bpy.types.Node, SverchCustomTreeNode):
+    """
+    Triggers: exp vd
+    Tooltip: experimental drawing node
+    
+    not a very exciting node kids.
+    """
+
+    bl_idname = 'SvVDExperimental'
+    bl_label = 'VD Experimental'
+    bl_icon = 'GREASEPENCIL'
+
+    n_id: StringProperty(default='')
+    activate: BoolProperty(name='Show', description='Activate', default=True, update=updateNode)
+
+    edge_color: FloatVectorProperty(
+        subtype='COLOR', min=0, max=1,
+        default=(0.3, 0.3, 0.3, 1.0), name='edge color', size=4, update=updateNode)
+
+
+    def sv_init(self, context):
+        inew = self.inputs.new
+        inew('VerticesSocket', 'verts')
+        inew('StringsSocket', 'edges')
+        inew('StringsSocket', 'faces')
+        inew('MatrixSocket', 'matrix')
+
+    def draw_buttons(self, context, layout):
+        layout.row().prop(self, "activate", text="ACTIVATE")
+        r1 = layout.row(align=True)
+        r1.label(icon="UV_EDGESEL")
+        r1.prop(self, "edge_color", text='')
+
+    def process(self):
+        if not (self.id_data.sv_show and self.activate):
+            callback_disable(node_id(self))
+            return
+
+        n_id = node_id(self)
+        callback_disable(n_id)
+
+        verts_socket, edges_socket, faces_socket, matrix_socket = self.inputs[:4]
+
+        if verts_socket.is_linked: 
+            geom = lambda: None
+            
+            propv = verts_socket.sv_get(deepcopy=False, default=[])
+            coords = propv[0]
+
+            if edges_socket.is_linked:
+                prope = edges_socket.sv_get(deepcopy=False, default=[])
+                indices = prope[0]
+            elif faces_socket.is_linked:
+                propf = faces_socket.sv_get(deepcopy=False, default=[])
+                indices = propf[0]
+
+            if matrix_socket.is_linked:
+                # for now just deal with first
+                m = matrix_socket.sv_get(deepcopy=False, default=[Matrix()])[0]
+                coords = multiply_vectors_deep(m, coords)
+      
+            geom.verts = coords
+            
+            if edges_socket.is_linked:
+                geom.edges = indices
+                line4f = self.edge_color[:]
+
+                draw_data = {
+                    'tree_name': self.id_data.name[:],
+                    'custom_function': draw_edges,
+                    'args': (geom, line4f)
+                } 
+                callback_enable(n_id, draw_data)
+                return
+
+            elif faces_socket.is_linked:
+                geom.faces = indices
+                line4f = self.edge_color[:]
+
+                draw_data = {
+                    'tree_name': self.id_data.name[:],
+                    'custom_function': draw_faces,
+                    'args': (geom, line4f)
+                } 
+                callback_enable(n_id, draw_data)
+                return
+
+
+            return
+
+        elif matrix_socket.is_linked:
+            matrices = matrix_socket.sv_get(deepcopy=False, default=[Matrix()])
+
+            draw_data = {
+                'tree_name': self.id_data.name[:],
+                'custom_function': screen_v3dMatrix,
+                'args': (matrices,)
+            }            
+
+            callback_enable(n_id, draw_data)
+
+    def copy(self, node):
+        self.n_id = ''
+
+    @property
+    def fully_enabled(self):
+        return "matrix" in self.inputs
+
+    def update(self):
+        if not self.fully_enabled:
+            return
+
+        try:
+            socket_one_has_upstream_links = self.inputs[0].other
+            socket_two_has_upstream_links = self.inputs[1].other
+            
+            if not socket_one_has_upstream_links:
+                callback_disable(node_id(self))
+        except:
+            print('vd basic lines update holdout', self.n_id)
+
+
+def register():
+    bpy.utils.register_class(SvVDExperimental)
+
+
+def unregister():
+    bpy.utils.unregister_class(SvVDExperimental)
