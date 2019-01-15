807ee6ada63a732b5ed6d4893e503de047bcf1d7
zeffii <tetha.z@gmail.com>
2019-01-15 15:51:07 +0100

    reduce intro printing

--- utils/ascii_print.py
+++ utils/ascii_print.py
@@ -6,10 +6,10 @@ from sverchok.ui.development import get_version_string
 # pylint: disable=w1401
 
 def logo():
-    l1 = " ______ _    _ _______  ______ _______ _     _  _____  _     _"
-    l2 = "/______  \  /  |______ |_____/ |       |_____| |     | |____/"
-    l3 = "______/   \/   |______ |    \_ |_____  |     | |_____| |    \_"
-    l4 = "initialized."
+    l1 = r" ______ _    _ _______  ______ _______ _     _  _____  _     _"
+    l2 = r"/______  \  /  |______ |_____/ |       |_____| |     | |____/ "
+    l3 = r"______/   \/   |______ |    \_ |_____  |     | |_____| |    \_"
+    l4 = r"initialized."
     lines = [l1, l2, l3, l4]
 
     can_paint = os.name in {'posix'}
@@ -20,5 +20,4 @@ def logo():
 
 def show_welcome():
     print("** version: ", get_version_string()," **")
-    print("** Have a nice day with sverchok  **\n")
     logo()
