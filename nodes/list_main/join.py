7f28e4a1fe38dc0229615f20802f27f20504f209
zeffii <tetha.z@gmail.com>
2019-01-19 15:44:05 +0100

    shuffle join code

--- nodes/list_main/join.py
+++ nodes/list_main/join.py
@@ -1,20 +1,10 @@
-# ##### BEGIN GPL LICENSE BLOCK #####
-#
-#  This program is free software; you can redistribute it and/or
-#  modify it under the terms of the GNU General Public License
-#  as published by the Free Software Foundation; either version 2
-#  of the License, or (at your option) any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with this program; if not, write to the Free Software Foundation,
-#  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
-#
-# ##### END GPL LICENSE BLOCK #####
+# This file is part of project Sverchok. It's copyrighted by the contributors
+# recorded in the version control history of the file, available from
+# its original location https://github.com/nortikin/sverchok/commit/master
+#  
+# SPDX-License-Identifier: GPL3
+# License-Filename: LICENSE
+
 
 import bpy
 from bpy.props import BoolProperty, IntProperty, StringProperty
@@ -29,19 +19,20 @@ class ListJoinNode(bpy.types.Node, SverchCustomTreeNode):
     bl_label = 'List Join'
     bl_icon = 'OUTLINER_OB_EMPTY'
 
-    JoinLevel: IntProperty(name='JoinLevel', description='Choose join level of data (see help)',
-                            default=1, min=1,
-                            update=updateNode)
-    mix_check: BoolProperty(name='mix', description='Grouping similar to zip()',
-                             default=False,
-                             update=updateNode)
-    wrap_check: BoolProperty(name='wrap', description='Grouping similar to append(list)',
-                              default=False,
-                              update=updateNode)
-    typ: StringProperty(name='typ',
-                         default='')
-    newsock: BoolProperty(name='newsock',
-                           default=False)
+    JoinLevel: IntProperty(
+        name='JoinLevel', description='Choose join level of data (see help)',
+        default=1, min=1,  update=updateNode)
+
+    mix_check: BoolProperty(
+        name='mix', description='Grouping similar to zip()',
+        default=False, update=updateNode)
+    
+    wrap_check: BoolProperty(
+        name='wrap', description='Grouping similar to append(list)',
+        default=False, update=updateNode)
+
+    typ: StringProperty(name='typ', default='')
+    newsock: BoolProperty(name='newsock', default=False)
 
     base_name = 'data '
     multi_socket_type = 'StringsSocket'
@@ -65,29 +56,31 @@ class ListJoinNode(bpy.types.Node, SverchCustomTreeNode):
             changable_sockets(self, inputsocketname, outputsocketname)
 
     def process(self):
-        if 'data' in self.outputs and self.outputs['data'].is_linked:
-            slots = []
-            for socket in self.inputs:
-                if socket.is_linked:
-                    slots.append(socket.sv_get())
-            if len(slots) == 0:
-                return
+        if not self.outputs['data'].is_linked:
+            return 
 
-            list_result = joiner(slots, self.JoinLevel)
-            result = list_result.copy()
+        slots = []
+        for socket in self.inputs:
+            if socket.is_linked:
+                slots.append(socket.sv_get())
+        if len(slots) == 0:
+            return
+
+        list_result = joiner(slots, self.JoinLevel)
+        result = list_result.copy()
+        if self.mix_check:
+            list_mix = myZip_2(slots, self.JoinLevel)
+            result = list_mix.copy()
+
+        if self.wrap_check:
+            list_wrap = wrapper_2(slots, list_result, self.JoinLevel)
+            result = list_wrap.copy()
+
             if self.mix_check:
-                list_mix = myZip_2(slots, self.JoinLevel)
-                result = list_mix.copy()
+                list_wrap_mix = wrapper_2(slots, list_mix, self.JoinLevel)
+                result = list_wrap_mix.copy()
 
-            if self.wrap_check:
-                list_wrap = wrapper_2(slots, list_result, self.JoinLevel)
-                result = list_wrap.copy()
-
-                if self.mix_check:
-                    list_wrap_mix = wrapper_2(slots, list_mix, self.JoinLevel)
-                    result = list_wrap_mix.copy()
-
-            self.outputs[0].sv_set(result)
+        self.outputs[0].sv_set(result)
 
 
     def draw_label(self):
