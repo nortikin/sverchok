471fc12b380fb6e6426a5f5ea0716922193113f1
zeffii <tetha.z@gmail.com>
2019-02-25 10:11:31 +0100

    fix attractor avg vec code

--- nodes/vector/attractor.py
+++ nodes/vector/attractor.py
@@ -28,8 +28,7 @@ def get_avg_vector(vectors):
     result = Vector((0,0,0))
     for vector in vectors:
         result += vector
-    result = (1.0/float(len(vectors))) @ result
-    return result
+    return result / len(vectors)
 
 def inverse(c, x):
     return 1.0/x
