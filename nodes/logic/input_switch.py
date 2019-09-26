9e102944fb5e9bc3d365776756f47a0ea5231373
zeffii <tetha.z@gmail.com>
2019-09-26 16:57:07 +0200

    commonize socket hide/unhide functions

--- nodes/logic/input_switch.py
+++ nodes/logic/input_switch.py
@@ -25,6 +25,21 @@ SEPARATOR_SOCKET = "SvSeparatorSocket"
 MAX_SET_SIZE = 9
 MAX_NUM_SWITCHES = 9
 
+def get_indices_that_should_be_visible(max_groups, num_visible_groups, max_items_per_group, num_items_per_group):
+    socket_index = 1
+    node = {}
+    node[0] = True # selector always visible, first socket
+    for group_index in range(max_groups):
+        node[socket_index] = group_index < num_visible_groups
+        socket_index += 1
+        for set_item in range(max_items_per_group):
+            node[socket_index] = (group_index < num_visible_groups and set_item < num_items_per_group) 
+            socket_index += 1
+
+    g = "".join(["01"[k] for k in node.values()])
+    print(g)
+    return node
+
 class SvInputSwitchNode(bpy.types.Node, SverchCustomTreeNode):
     """
     Triggers: Sets, Switch, Select
@@ -34,6 +49,13 @@ class SvInputSwitchNode(bpy.types.Node, SverchCustomTreeNode):
     bl_idname = 'SvInputSwitchNode'
     bl_label = 'Input Switch'
     sv_icon = 'SV_INPUT_SWITCH'
+
+    def interface_fully_initialized(self):
+        return len(self.outputs) == MAX_SET_SIZE
+
+    def replace_socket_if_needed(self, input_socket):
+        if input_socket.bl_idname != input_socket.other.bl_idname:
+            input_socket.replace_socket(input_socket.other.bl_idname)
 
     def update_node_sockets_per_set(self, context):
         print('doing', inspect.stack()[0][3])
@@ -48,40 +70,20 @@ class SvInputSwitchNode(bpy.types.Node, SverchCustomTreeNode):
         # reconfigure _ input _ sockets
         pass
 
-    num_sockets_per_set: IntProperty(
-        name="Num Sockets per set", description="Number of inputs in a set",
-        default=2, min=1, max=MAX_SET_SIZE, update=update_node_sockets_per_set)
+    def adjust_input_socket_bl_idname_to_match_linked_input(self):
+        for input_socket in self.inputs:
+            if input_socket.is_linked:
+                self.replace_socket_if_needed(input_socket)
 
-    num_switches: IntProperty(
-        name="Num Switches", description="Number of switch items (no update associated)",
-        default=2, min=2, max=MAX_NUM_SWITCHES)
+    def adjust_output_sockets_bl_idname_to_match_selected_set(self, remap_indices):
+        for out_idx, in_idx in enumerate(remap_indices):
+            input_bl_idname = self.inputs[in_idx].bl_idname
+            if input_bl_idname != self.outputs[out_idx].bl_idname:
+                self.outputs[out_idx].replace_socket(input_bl_idname)
 
-    num_available_switches: IntProperty(
-        default=2, min=2, description='keep track of current state (no update associated)')
-
-    selected: IntProperty(
-        name="Selected", description="Selected Set",
-        default=0, min=0, update=updateNode)
-
-
-    def unhide_sockets_to_cope_with_switchnum_expansion(self):
-        inew = self.inputs.new
-        onew = self.outputs.new
-        
-        self.num_switches += 1
-        ## old # inew(SEPARATOR_SOCKET, f"Separator {self.num_switches}")
-        ## old # for i in range(self.num_sockets_per_set):
-        ## old #     inew(GENERIC_SOCKET, f"{self.label_of_set(self.num_switches)} {i + 1}")
-
-
-    def hide_extraneous_input_sockets_in_all_visible_sets(self):
-        pass
-
-    def unhide_necessary_input_sockets_in_all_visible_sets(self):
-        pass
-
-    def interface_fully_initialized(self):
-        return len(self.outputs) == MAX_SET_SIZE
+    @property
+    def not_already_maxed_out(self):
+        return self.num_switches < MAX_NUM_SWITCHES
 
     @property
     def any_sockets_of_last_input_set_connected(self):
@@ -99,40 +101,14 @@ class SvInputSwitchNode(bpy.types.Node, SverchCustomTreeNode):
 
         return any([self.inputs[idx].is_linked for idx in range(first_index, last_index)])
 
-    @property
-    def not_already_maxed_out(self):
-        return self.num_switches < MAX_NUM_SWITCHES
-
-    def interface_unhide_inputs_to_handle_new_set_if_needed():
-        if self.not_already_maxed_out and self.any_sockets_of_last_input_set_connected:
-            self.unhide_sockets_to_cope_with_switchnum_expansion()
-
-    def update(self):
-        if not self.interface_fully_initialized():
-            return
-
-        # self.interface_unhide_inputs_to_handle_new_set_if_needed()
-      
-    def draw_buttons(self, context, layout):
-        layout.prop(self, "num_sockets_per_set")
-
     def initialize_input_sockets(self):
         inew = self.inputs.new
         inew(GENERIC_SOCKET, "Selected").prop_name = "selected"
-
-        # the idea here is to add all sockets in advance, and hide the extraneous ones to begin with.
-
         for group in range(MAX_NUM_SWITCHES):
-            separator_socket = inew(SEPARATOR_SOCKET, f"Separator {group}")
-            if group >= self.num_switches:
-                separator_socket.hide_safe = True
-
+            socket = inew(SEPARATOR_SOCKET, f"Separator {group}")
             for set_item in range(MAX_SET_SIZE):
-                socket = inew(GENERIC_SOCKET, f"{GREEK_LABELS[group]} {set_item}")
-                if (set_item >= self.num_sockets_per_set) or (group >= self.num_switches):
-                    socket.hide_safe = True
-
-
+                inew(GENERIC_SOCKET, f"{GREEK_LABELS[group]} {set_item}")
+        
     def initialize_output_sockets(self):
         """ create all needed output sockets, but hide beyond set size """
         onew = self.outputs.new
@@ -141,29 +117,53 @@ class SvInputSwitchNode(bpy.types.Node, SverchCustomTreeNode):
             if i >= self.num_sockets_per_set:
                 sock.hide_safe = True
 
-    def sv_init(self, context):
-        self.initialize_input_sockets()
-        self.initialize_output_sockets()
-
-    def replace_socket_if_needed(self, input_socket):
-        if input_socket.bl_idname != input_socket.other.bl_idname:
-            input_socket.replace_socket(input_socket.other.bl_idname)
-
     def collect_input_indices_to_map(self):
         """ which input socket indices are associated with the selected set """
         print('doing', inspect.stack()[0][3])
         return [2, 3]
 
-    def adjust_input_socket_bl_idname_to_match_linked_input(self):
-        for input_socket in self.inputs:
-            if input_socket.is_linked:
-                self.replace_socket_if_needed(input_socket)
+    def unhide_sockets_to_cope_with_switchnum(self):
+        tndict = get_indices_that_should_be_visible(MAX_NUM_SWITCHES, self.num_switches, MAX_SET_SIZE, self.num_sockets_per_set)
+        for key, value in tndict.items():
+            socket = self.inputs[key]
+            desired_hide_state = not(value)
+            if not socket.hide == desired_hide_state:
+                socket.hide_safe = desired_hide_state
 
-    def adjust_output_sockets_bl_idname_to_match_selected_set(self, remap_indices):
-        for out_idx, in_idx in enumerate(remap_indices):
-            input_bl_idname = self.inputs[in_idx].bl_idname
-            if input_bl_idname != self.outputs[out_idx].bl_idname:
-                self.outputs[out_idx].replace_socket(input_bl_idname)
+    def interface_unhide_inputs_to_handle_new_set_if_needed(self):
+        if self.not_already_maxed_out and self.any_sockets_of_last_input_set_connected:
+            self.num_switches += 1
+            self.unhide_sockets_to_cope_with_switchnum()
+
+    num_sockets_per_set: IntProperty(
+        name="Num Sockets per set", description="Number of inputs in a set",
+        default=2, min=1, max=MAX_SET_SIZE, update=update_node_sockets_per_set)
+
+    num_switches: IntProperty(
+        name="Num Switches", description="Number of switch items (no update associated)",
+        default=2, min=2, max=MAX_NUM_SWITCHES)
+
+    num_available_switches: IntProperty(
+        default=2, min=2, description='keep track of current state (no update associated)')
+
+    selected: IntProperty(
+        name="Selected", description="Selected Set",
+        default=0, min=0, update=updateNode)
+
+
+    def draw_buttons(self, context, layout):
+        layout.prop(self, "num_sockets_per_set")
+
+    def update(self):
+        if not self.interface_fully_initialized():
+            return
+
+        # self.interface_unhide_inputs_to_handle_new_set_if_needed()
+
+    def sv_init(self, context):
+        self.initialize_input_sockets()
+        self.unhide_sockets_to_cope_with_switchnum()
+        self.initialize_output_sockets()
 
     def process(self):
         print('num_switches (from process func)', self.num_switches)
