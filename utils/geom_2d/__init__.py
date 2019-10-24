00f1e81e5040a4473e682e9f5efa25da80640430
Durman <sv@soluyanov.ru>
2019-10-24 15:56:26 +0400

    add unit test for make monotone polygon node

---
+++ utils/geom_2d/__init__.py
@@ -0,0 +1,5 @@
+# After changes which can impact on several algorithm tests of the package should be done.
+# Run strings below from blender text editor and check result in console.
+
+# from sverchok.utils.testing import run_test_from_file
+# run_test_from_file("geom_2d_tests.py")
