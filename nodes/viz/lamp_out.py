8e930788a5e0bceb53474b65d908e838816b5be4
Ilya Portnov <portnov@bk.ru>
2020-01-04 23:58:40 +0500

    Fix export/import of "Lamp Out" node.
    It appears that "type" is reserved property name, but other than for
    export/import it works correctly anyway...

--- nodes/viz/lamp_out.py
+++ nodes/viz/lamp_out.py
@@ -132,6 +132,8 @@ class SvLampOutNode(bpy.types.Node, SverchCustomTreeNode):
         name="Emission Node", description="Name of Emission node in the lamp shader, that contains Sthrength and Color inputs",
         default="Emission", update=updateNode)
 
+    properties_to_skip_iojson = ['type']
+
     def sv_init(self, context):
         self.inputs.new('SvMatrixSocket', 'Origin')
         self.inputs.new('SvStringsSocket', 'Size').prop_name = 'size'
@@ -303,6 +305,12 @@ class SvLampOutNode(bpy.types.Node, SverchCustomTreeNode):
         # delete associated lamps data
         for object_name in objs:
             lamps_data.remove(lamps_data[object_name])
+
+    def storage_get_data(self, storage):
+        storage["lamp_type"] = self.type
+
+    def storage_set_data(self, storage):
+        self.type = storage.get("lamp_type", "POINT")
 
 def register():
     bpy.utils.register_class(SvLampOutNode)
