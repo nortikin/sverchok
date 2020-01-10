cad2c6036a87cc764a805dbb00d664efda2a43cf
durman <sv@soluyanov.ru>
2020-01-10 09:32:41 +0400

    fix update object after data changes

--- nodes/object_nodes/set_custom_uv_map.py
+++ nodes/object_nodes/set_custom_uv_map.py
@@ -5,6 +5,7 @@
 # SPDX-License-Identifier: GPL3
 # License-Filename: LICENSE
 
+
 from itertools import cycle
 import numpy as np
 
@@ -13,60 +14,51 @@ from mathutils import Vector
 
 from sverchok.node_tree import SverchCustomTreeNode
 
-from sverchok.data_structure import repeat_last, updateNode
+from sverchok.data_structure import updateNode
 
 
 def set_custom_map(obj, verts=None, faces=None, uv_name='SVMap', matrix=None):
-    # if uv_name in obj.data.uv_layers:
-    #     obj.data.uv_layers.remove(obj.data.uv_layers[uv_name])
+    """
+    Cretes new UV layer if need and apply given coordinates (XY) to UV layer
+    :param obj: Blender mesh object
+    :param verts: list of tuple(float, float, float)
+    :param faces: list of list of int
+    :param uv_name: name of UV layer
+    :param matrix: mathutils Matrix if deformation is needed
+    :return: None
+    """
     if uv_name not in obj.data.uv_layers:
         obj.data.uv_layers.new(name=uv_name)
-    set_uv(verts, faces, obj, uv_name)
+    set_uv(verts, faces, obj, uv_name, matrix)
+    obj.data.update()
 
 
 def set_uv(verts, faces, obj, uv_name, matrix=None):
+    # Apply coordinates to UV layer
     unpack_uv = np.zeros((len(obj.data.loops) * 2), dtype=float)
     if verts and faces:
         for i, co in zip(range(0, len(obj.data.loops) * 2, 2), (verts[i] for face in faces for i in face)):
-            uv = Vector(co[:2])
+            uv = Vector(co[:2]) if matrix is None else (matrix @ Vector(co))[:2]
             unpack_uv[i: i+2] = uv
-    # if matrix:
-    #     loop_points = ((mat @ Vector(co))[:2] for co, mat in zip((verts[i] for face in faces for i in face),
-    #                                                              cycle([matrix])))
-    # else:
-    #     loop_points = (Vector(co)[:2] for co in (verts[i] for face in faces for i in face))
     obj.data.uv_layers[uv_name].data.foreach_set("uv", unpack_uv)
-
-
-def deform_uv(obj, uv_name, matrix):
-
-    uv_co = [l.uv for l in obj.data.uv_layers[uv_name].data]
-
-    xy_uv = [(m @ l.to_3d())[:2] for l, m in zip(uv_co, repeat_last([matrix]))]
-    per_loop_list = [uv for pair in xy_uv for uv in pair]
-
-    obj.data.uv_layers[uv_name].data.foreach_set("uv", per_loop_list)
 
 
 class SvSetCustomUVMap(bpy.types.Node, SverchCustomTreeNode):
     """
-    Triggers: ...
+    Triggers: Set custom UV map to Blender mesh
 
-    ...
-    ...
+    3D coordinates can be putted,
+    it will works as flat projection
     """
     bl_idname = 'SvSetCustomUVMap'
     bl_label = 'Set custom UV map'
     bl_icon = 'GROUP_UVS'
 
-    active: bpy.props.BoolProperty(name='Make active', default=True, update=updateNode)
     uv_name: bpy.props.StringProperty(name='Uv name', default='SVMap', description='Name of UV layer',
                                       update=updateNode)
 
     def draw_buttons(self, context, layout):
-        row = layout.column(align=True)
-        row.prop(self, 'uv_name', text='', icon='GROUP_UVS')
-        # row.prop(self, 'active', toggle=True)
+        layout.prop(self, 'uv_name', text='', icon='GROUP_UVS')
 
     def sv_init(self, context):
         self.inputs.new('SvObjectSocket', 'Objects')
