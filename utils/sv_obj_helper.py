7d21d188a2b161106bcc4d84a071ca1c7f95367c
Dealga McArdle <zeffii@users.noreply.github.com>
2020-01-11 12:57:33 +0100

    B28 svobj collections (#2810)
    * implement to_collection
    
    * apply to_collection to obj nodes that currently call to_group
    
    * update comment
    
    * add first working version of children/collections
    
    * add comment about possible route for ungroup
    
    * remove old to_group entirely
    
    * add group_state_update_handler
    
    * correct comment
    
    * correct syntax of with-statement
    
    * confirm grouping works
    
    * apply nicer solution
    
    * add throttle to CurveViewer and update func
    
    * correct typo - unreported bug
    
    * apply grouping to polylineviewr
    
    * fix unreported bug hahah..no one uses this

--- utils/sv_obj_helper.py
+++ utils/sv_obj_helper.py
@@ -168,18 +168,51 @@ class SvObjHelper():
         objs = [obj for obj in objects if obj.type == self.data_kind]
         return [o for o in objs if o.get('basedata_name') == self.basedata_name]
 
-    def to_group(self, objs):
-        groups = bpy.data.groups
-        named = self.basedata_name
+    def group_state_update_handler(self, context):
+        """
+        since this is technically a scene/admin code controlling heirarchy, pressing
+        the button should result in assymetric behaviour depending on the new state of
+        "self.grouping".
 
-        # alias group, or generate new group and alias that
-        group = groups.get(named)
-        if not group:
-            group = groups.new(named)
+        + - - - + - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - +
+        | state | desired behaviour                                             |
+        + ----- + --------------------------------------------------------------+
+        | True  | add all objects associated with the node to the collection    |
+        + ----- + --------------------------------------------------------------+
+        | False | remove collection, if present, and association with object    |
+        + - - - + - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - +
+        """
+        if self.grouping:
+            updateNode(self, context)
+        else:
+            with self.sv_throttle_tree_update():
+                self.clear_collection()
+
+    def to_collection(self, objs):
+        collections = bpy.data.collections
+        named = self.custom_collection_name or self.basedata_name
+
+        # alias collection, or generate new collection and alias that
+        collection = collections.get(named)
+        if not collection:
+            collection = collections.new(named)
+            bpy.context.scene.collection.children.link(collection)
 
         for obj in objs:
-            if obj.name not in group.objects:
-                group.objects.link(obj)
+            if obj.name not in collection.objects:
+                collection.objects.link(obj)
+
+    def clear_collection(self):
+        collections = bpy.data.collections
+        named = self.custom_collection_name or self.basedata_name
+
+        # alias collection, or generate new collection and alias that
+        collection = collections.get(named)
+        if not collection:
+            """ seems the collection is already gone, this is a no op """
+            return
+        else:
+            collections.remove(collection)
 
     def ensure_parent(self):
         if self.parent_to_empty:
@@ -235,6 +268,10 @@ class SvObjHelper():
 
     parent_to_empty: BoolProperty(name='parent to empty', default=False, update=updateNode)
     parent_name: StringProperty(name='parent name')  # calling updateNode would recurse.    
+    
+    custom_collection_name: StringProperty(
+        name='collection name', update=updateNode,
+        description='custom collection name, will default to the basename of the node first')
 
     def sv_init_helper_basedata_name(self):
         """ 
