d9aa59dd579f1a923112f95ed42670601cb086de
Durman <sv@soluyanov.ru>
2020-01-15 21:19:24 +0400

    add custom space and 3d panel draw

--- nodes/vector/vector_normal_in.py
+++ nodes/vector/vector_normal_in.py
@@ -8,9 +8,21 @@
 from itertools import chain, cycle
 
 import bpy
+from mathutils import Quaternion, Vector
 
 from sverchok.node_tree import SverchCustomTreeNode
 from sverchok.data_structure import updateNode
+
+
+quat_x90 = Quaternion((1, 0, 0), 1.57)
+
+
+def get_costume_norm(normal, space_normal):
+    quat_rotation_xy = Vector((0, -1, 0)).rotation_difference(Vector(space_normal[:2] + (0,)))
+    quat_rotation_z = Vector(space_normal[:2] + (0,)).rotation_difference(Vector(space_normal))
+    quat_rotation = quat_rotation_z @ quat_rotation_xy
+    quat = quat_rotation @ quat_x90
+    return [(quat @ normal)[:]]
 
 
 class SvVectorNormalIn(bpy.types.Node, SverchCustomTreeNode):
@@ -36,21 +48,34 @@ class SvVectorNormalIn(bpy.types.Node, SverchCustomTreeNode):
     modes: bpy.props.EnumProperty(items=[(n, n, '') for n in ('X', 'Y', 'Z', 'Custom')], default='Z',
                                   update=update_node)
     vector: bpy.props.FloatVectorProperty(default=(0.0, 0.0, 1.0), subtype='DIRECTION', update=updateNode)
+    custom_vector: bpy.props.FloatVectorProperty(default=(0.0, 0.0, 1.0), update=updateNode)
+    draw_3dpanel: bpy.props.BoolProperty(name='Show in 3d panel',
+                                         description='Show items of this node in 3d panel of 3d view screen')
 
     def draw_buttons(self, context, layout):
         layout.prop(self, 'vector', text='')
 
+    def draw_buttons_ext(self, context, layout):
+        layout.prop(self, 'draw_3dpanel')
+
     def draw_socket(self, socket, context, layout):
         layout.prop(self, 'modes', expand=True)
 
+    def draw_buttons_3dpanel(self, layout):
+        layout.label(text=f"{self.label or self.name}")
+        layout.prop(self, 'vector', text='')
+
     def sv_init(self, context):
         sock_in = self.inputs.new('SvVerticesSocket', 'Verts')
-        sock_in.prop_name = 'vector'
+        sock_in.prop_name = 'custom_vector'
         sock_in.hide = True
         self.outputs.new('SvVerticesSocket', 'Verts').custom_draw = 'draw_socket'
 
     def process(self):
-        self.outputs['Verts'].sv_set(self.inputs['Verts'].sv_get())
+        out = []
+        for space in self.inputs['Verts'].sv_get(deepcopy=False):
+            out.append(get_costume_norm(self.vector, space[0]))
+        self.outputs['Verts'].sv_set(out)
 
 
 def register():
