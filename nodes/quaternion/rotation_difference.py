1ef31c96ee698762d0ce72225df7ba1beead9989
durman <sv@soluyanov.ru>
2020-01-15 16:46:15 +0400

    add flatten list option

--- nodes/quaternion/rotation_difference.py
+++ nodes/quaternion/rotation_difference.py
@@ -12,9 +12,16 @@ import bpy
 from mathutils import Vector
 
 from sverchok.node_tree import SverchCustomTreeNode
+from sverchok.data_structure import updateNode
 
 
 def get_quaternion(verts_a, verts_b):
+    """
+    returns quaternion which represent rotation from vertices A to vertices B
+    :param verts_a: list of tuple(float, float, float)
+    :param verts_b: list of tuple(float, float, float)
+    :return: list of quaternions
+    """
     max_len = max([len(verts_a), len(verts_b)])
     verts_a = iter_last(verts_a)
     verts_b = iter_last(verts_b)
@@ -29,15 +36,18 @@ class SvRotationDifference(bpy.types.Node, SverchCustomTreeNode):
     """
     Triggers: Quaternion
 
-    ...
-    ...
+    creates quaternion which represent rotation from vertices A to vertices B
     """
     bl_idname = 'SvRotationDifference'
     bl_label = 'Rotation difference'
     bl_icon = 'MOD_BOOLEAN'
 
-    vec1: bpy.props.FloatVectorProperty(default=(1, 0, 0))
-    vec2: bpy.props.FloatVectorProperty(default=(0, 1, 0))
+    vec1: bpy.props.FloatVectorProperty(default=(1, 0, 0), update=updateNode)
+    vec2: bpy.props.FloatVectorProperty(default=(0, 1, 0), update=updateNode)
+    is_flatten: bpy.props.BoolProperty(name="Flat output", update=updateNode, default=True)
+
+    def draw_buttons_ext(self, context, layout):
+        layout.prop(self, 'is_flatten')
 
     def sv_init(self, context):
         self.inputs.new('SvVerticesSocket', 'Verts_A').prop_name = 'vec1'
@@ -47,7 +57,8 @@ class SvRotationDifference(bpy.types.Node, SverchCustomTreeNode):
     def process(self):
         max_len = max([len(sock.sv_get(deepcopy=False)) for sock in self.inputs])
         inputs = [iter_last(sock.sv_get(default=False)) for sock in self.inputs]
-        self.outputs['Quaternions'].sv_set([get_quaternion(v1, v2) for i, v1, v2 in zip(range(max_len), *inputs)])
+        out = [get_quaternion(v1, v2) for i, v1, v2 in zip(range(max_len), *inputs)]
+        self.outputs['Quaternions'].sv_set([q for l in out for q in l] if self.is_flatten else out)
 
 
 def register():
