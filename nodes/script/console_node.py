f0eed2b9b4c2c6e0e0b8cf51b3a837e15c9d9215
zeffii <tetha.z@gmail.com>
2020-01-22 14:04:13 +0100

    move comment to correct spot

--- nodes/script/console_node.py
+++ nodes/script/console_node.py
@@ -6,12 +6,13 @@
 # License-Filename: LICENSE
 
 import bpy
-# import mathutils
-# from mathutils import Vector
+from mathutils import Vector, Matrix
+
 # from bpy.props import FloatProperty, BoolProperty
 from sverchok.node_tree import SverchCustomTreeNode
 from sverchok.data_structure import updateNode
 from sverchok.utils.geom import grid
+
 
 def process_string_to_charmap(node, str):
     for line in str.split():
@@ -32,22 +33,9 @@ def triangles_from_quads(F):
     _ = [TF_add((poly[0], poly[1], poly[2]), (poly[0], poly[2], poly[3])) for poly in F]
     return TF
 
+
 def tri_grid(dim_x, dim_y, nx, ny):
-    V, E, F = grid(dim_x, dim_y, nx, ny, anchor=0, matrix=None, mode='pydata')
-    TF = triangles_from_quads(F)
-
-# this data need only be generated once, or at runtime at request.
-grid_data = {}
-grid_data[(30, 30, 120, 80)] = grid(30, 30, 120, 80)
-
-class SvConsoleNode(bpy.types.Node, SverchCustomTreeNode):
-    
     """
-    Triggers: SvConsoleNode
-    Tooltip: 
-    
-    This node prints the input to the nodeview using a fixedwidth character map.
-
     This generates 2d grid, into which we texture each polygon with it's associated character UV
 
     AA -  -  -AB -  -  -AC -  -  -AD -  -  - n
@@ -65,7 +53,28 @@ class SvConsoleNode(bpy.types.Node, SverchCustomTreeNode):
     etc
 
     There's a mapping between int(char) to uv coordinates, charmap will support ascii only.
+    """
 
+    neg_y = Matrix(((1, 0, 0, 0), (0, -1, 0, 0), (0, 0, 1, 0), (0, 0, 0, 1)))
+    V, E, F = grid(dim_x, dim_y, nx, ny, anchor=7, matrix=neg_y, mode='pydata')
+    TF = triangles_from_quads(F)
+    return V, TF
+
+
+V, F = tri_grid(dim_x, dim_y, nx, ny)
+
+
+# this data need only be generated once, or at runtime at request.
+grid_data = {}
+grid_data[(30, 30, 120, 80)] = grid(30, 30, 120, 80)
+
+class SvConsoleNode(bpy.types.Node, SverchCustomTreeNode):
+    
+    """
+    Triggers: SvConsoleNode
+    Tooltip: 
+
+    This node prints the input to the nodeview using a fixedwidth character map.
     """
 
     bl_idname = 'SvConsoleNode'
