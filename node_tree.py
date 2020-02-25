9d5cc6530dc398a8f6acd5a29fe33cadbaf1391c
zeffii <tetha.z@gmail.com>
2020-02-25 11:52:30 +0100

    fix indentation

--- node_tree.py
+++ node_tree.py
@@ -355,11 +355,11 @@ class SverchCustomTreeNode:
         contain spaces. This function takes a list of enums that the node currently holds, and 
         makes sure the stored enum has no spaces.
         """
-            for enum_property in enums:
-                current_value = getattr(self, enum_property)
-                if " " in current_value:
-                    with self.sv_throttle_tree_update():        
-                        setattr(self, enum_property, data_structure.no_space(current_value))
+        for enum_property in enums:
+            current_value = getattr(self, enum_property)
+            if " " in current_value:
+                with self.sv_throttle_tree_update():        
+                    setattr(self, enum_property, data_structure.no_space(current_value))
 
 
     def does_support_draft_mode(self):
