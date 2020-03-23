ee085c0bd7da8730060a8b716a4195e97901d1be
Ilya Portnov <portnov@bk.ru>
2020-03-23 22:54:06 +0500

    Circle node: add TMin/TMax.

--- nodes/curve/circle.py
+++ nodes/curve/circle.py
@@ -1,5 +1,6 @@
 
 import numpy as np
+from math import pi
 
 from mathutils import Matrix
 import bpy
@@ -24,9 +25,21 @@ class SvExCircleNode(bpy.types.Node, SverchCustomTreeNode):
         default = 1.0,
         update = updateNode)
 
+    t_min : FloatProperty(
+        name = "T Min",
+        default = 0.0,
+        update = updateNode)
+
+    t_max : FloatProperty(
+        name = "T Max",
+        default = 2*pi,
+        update = updateNode)
+
     def sv_init(self, context):
         self.inputs.new('SvMatrixSocket', "Center")
         self.inputs.new('SvStringsSocket', "Radius").prop_name = 'radius'
+        self.inputs.new('SvStringsSocket', "TMin").prop_name = 't_min'
+        self.inputs.new('SvStringsSocket', "TMax").prop_name = 't_max'
         self.outputs.new('SvExCurveSocket', "Curve").display_shape = 'DIAMOND'
 
     def process(self):
@@ -35,13 +48,18 @@ class SvExCircleNode(bpy.types.Node, SverchCustomTreeNode):
 
         center_s = self.inputs['Center'].sv_get(default=[Matrix()])
         radius_s = self.inputs['Radius'].sv_get()
+        t_min_s = self.inputs['TMin'].sv_get()
+        t_max_s = self.inputs['TMax'].sv_get()
         radius_s = ensure_nesting_level(radius_s, 2)
+        t_min_s = ensure_nesting_level(t_min_s, 2)
+        t_max_s = ensure_nesting_level(t_max_s, 2)
         center_s = ensure_nesting_level(center_s, 2, data_types=[Matrix])
 
         curves_out = []
-        for centers, radiuses in zip_long_repeat(center_s, radius_s):
-            for center, radius in zip_long_repeat(centers, radiuses):
+        for centers, radiuses, t_mins, t_maxs in zip_long_repeat(center_s, radius_s, t_min_s, t_max_s):
+            for center, radius, t_min, t_max in zip_long_repeat(centers, radiuses, t_mins, t_maxs):
                 curve = SvExCircle(center, radius)
+                curve.u_bounds = (t_min, t_max)
                 curves_out.append(curve)
 
         self.outputs['Curve'].sv_set(curves_out)
