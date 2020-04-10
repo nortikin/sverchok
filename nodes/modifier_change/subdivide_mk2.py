75c69269d02d4c0340bf76c01a2ee81ec536eb92
Victor Doval <10011941+vicdoval@users.noreply.github.com>
2020-04-10 17:28:41 +0200

    Removing MK2 from subdivide bl_label

--- nodes/modifier_change/subdivide_mk2.py
+++ nodes/modifier_change/subdivide_mk2.py
@@ -45,7 +45,7 @@ class SvSubdivideNodeMK2(bpy.types.Node, SverchCustomTreeNode):
     '''
 
     bl_idname = 'SvSubdivideNodeMK2'
-    bl_label = 'Subdivide MK2'
+    bl_label = 'Subdivide'
     bl_icon = 'OUTLINER_OB_EMPTY'
     sv_icon = 'SV_SUBDIVIDE'
 
