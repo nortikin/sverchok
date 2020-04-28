626d884289f5a9c64acfd7a7b41fb5b6459d2065
Victor Doval <10011941+vicdoval@users.noreply.github.com>
2020-04-28 18:58:04 +0200

    curve mapper to the animatable club

--- nodes/number/curve_mapper.py
+++ nodes/number/curve_mapper.py
@@ -20,6 +20,8 @@ import bpy
 from bpy.props import BoolProperty, FloatProperty, EnumProperty, StringProperty
 
 from sverchok.node_tree import SverchCustomTreeNode
+from sverchok.utils.nodes_mixins.sv_animatable_nodes import SvAnimatableNode
+
 from sverchok.data_structure import updateNode, list_match_func, numpy_list_match_modes, numpy_list_match_func
 from sverchok.utils.sv_itertools import recurse_f_level_control
 from sverchok.utils.sv_manual_curves_utils import get_valid_evaluate_function_legacy, get_valid_evaluate_function, get_valid_curve
@@ -39,7 +41,7 @@ def curve_mapper(params, constant, matching_f):
 
     return result
 
-class SvCurveMapperNode(bpy.types.Node, SverchCustomTreeNode):
+class SvCurveMapperNode(bpy.types.Node, SverchCustomTreeNode, SvAnimatableNode):
     """
     Triggers: Manual Curve remap
     Tooltip:  Map input list using a manually defined curve
@@ -49,56 +51,10 @@ class SvCurveMapperNode(bpy.types.Node, SverchCustomTreeNode):
     bl_label = 'Curve Mapper'
     bl_icon = 'NORMALIZE_FCURVES'
 
-    def update_sockets(self, context):
-        if not self.inputs["Old Min"].is_linked:
-            self.inputs["Old Min"].hide_safe = self.auto_limits
-        if not self.inputs["Old Max"].is_linked:
-            self.inputs["Old Max"].hide_safe = self.auto_limits
-        updateNode(self, context)
-
-    old_min: FloatProperty(
-        name='Old Min', description='Old Min',
-        default=0, update=updateNode)
-
-    old_max: FloatProperty(
-        name='Old Max', description='Old Max',
-        default=1, update=updateNode)
-
-    new_min: FloatProperty(
-        name='New Min', description='New Min',
-        default=0, update=updateNode)
-
-    new_max: FloatProperty(
-        name='New Max', description='New Max',
-        default=10, update=updateNode)
-
     value: FloatProperty(
         name='Value', description='New Max',
         default=.5, update=updateNode)
 
-    def update_mapper(self, context):
-        if self.update_curve:
-            self.update_curve = False
-            updateNode(self,context)
-
-    update_curve: BoolProperty(
-        name='Update', description='Update Node with updated curve',
-        default=False, update=update_mapper)
-
-    auto_limits: BoolProperty(
-        name='List limits', description='Use old min and old max from list',
-        default=False, update=update_sockets)
-
-    list_match: EnumProperty(
-        name="List Match",
-        description="Behavior on different list lengths",
-        items=numpy_list_match_modes, default="REPEAT",
-        update=updateNode)
-
-    output_numpy: BoolProperty(
-        name='Output NumPy',
-        description='Output NumPy arrays',
-        default=False, update=updateNode)
     n_id: StringProperty(default='')
 
     def sv_init(self, context):
@@ -116,13 +72,14 @@ class SvCurveMapperNode(bpy.types.Node, SverchCustomTreeNode):
             layout.label(text="Connect input to activate")
             return
         try:
+            self.animatable_buttons(layout, icon_only=True)
             n_id = self.node_id
             tnode = m.nodes['RGB Curves' + n_id]
             if not tnode:
                 layout.label(text="Connect input to activate")
                 return
             layout.template_curve_mapping(tnode, "mapping", type="NONE")
-            layout.prop(self, "update_curve", toggle=True)
+
         except AttributeError:
             layout.label(text="Connect input to activate")
             return
@@ -141,11 +98,10 @@ class SvCurveMapperNode(bpy.types.Node, SverchCustomTreeNode):
         outputs = self.outputs
         n_id = self.node_id
         evaluate = get_evaluator(node_group_name, 'RGB Curves'+n_id)
-        # no outputs, end early.
+
         if not outputs['Value'].is_linked:
             return
         result = []
-
         floats_in = inputs[0].sv_get(default=[[]], deepcopy=False)
 
         desired_levels = [2]
