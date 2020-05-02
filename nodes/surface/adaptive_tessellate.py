9e7d64a4c7119b1810309559918d88c79be677e4
Ilya Portnov <portnov@bk.ru>
2020-05-02 23:09:59 +0500

    "Adaptive Tessellate" node efforts.

---
+++ nodes/surface/adaptive_tessellate.py
@@ -0,0 +1,124 @@
+
+import numpy as np
+
+import bpy
+from bpy.props import EnumProperty, IntProperty, BoolProperty
+
+import sverchok
+from sverchok.node_tree import SverchCustomTreeNode, throttled
+from sverchok.data_structure import updateNode, zip_long_repeat, ensure_nesting_level, get_data_nesting_level
+from sverchok.utils.logging import info, exception
+from sverchok.utils.geom_2d.merge_mesh import crop_mesh_delaunay
+
+from sverchok.utils.curve import SvCurve
+from sverchok.utils.surface import SvSurface
+from sverchok.utils.adaptive_surface import adaptive_subdivide
+
+class SvAdaptiveTessellateNode(bpy.types.Node, SverchCustomTreeNode):
+    """
+    Triggers: Adaptive Tessellate Surface
+    Tooltip: Adaptively tessellate surface
+    """
+    bl_idname = 'SvAdaptiveTessellateNode'
+    bl_label = 'Adaptive Tessellate Surface'
+    bl_icon = 'OUTLINER_OB_EMPTY'
+    sv_icon = 'SV_TRIM_TESSELLATE'
+
+    samples_u : IntProperty(
+            name = "Samples U",
+            default = 25, min = 3,
+            update = updateNode)
+
+    samples_v : IntProperty(
+            name = "Samples V",
+            default = 25, min = 3,
+            update = updateNode)
+
+    min_ppf : IntProperty(
+            name = "Min per face",
+            description = "Minimum number of new points per rectangular grid face",
+            min = 0, default = 0,
+            update = updateNode)
+
+    max_ppf : IntProperty(
+            name = "Max per face",
+            description = "Minimum number of new points per rectangular grid face",
+            min = 1, default = 5,
+            update = updateNode)
+
+    seed : IntProperty(
+            name = "Seed",
+            description = "Random Seed value",
+            default = 0,
+            update = updateNode)
+
+    by_curvature : BoolProperty(
+            name = "By Curvature",
+            default = True,
+            update = updateNode)
+
+    by_area : BoolProperty(
+            name = "By Area",
+            default = False,
+            update = updateNode)
+
+    def draw_buttons(self, context, layout):
+        row = layout.row(align=True)
+        row.prop(self, 'by_curvature', toggle=True)
+        row.prop(self, 'by_area', toggle=True)
+
+    def sv_init(self, context):
+        self.inputs.new('SvSurfaceSocket', "Surface")
+        self.inputs.new('SvStringsSocket', "SamplesU").prop_name = 'samples_u'
+        self.inputs.new('SvStringsSocket', "SamplesV").prop_name = 'samples_v'
+        self.inputs.new('SvStringsSocket', "MinPpf").prop_name = 'min_ppf'
+        self.inputs.new('SvStringsSocket', "MaxPpf").prop_name = 'max_ppf'
+        self.inputs.new('SvStringsSocket', "Seed").prop_name = 'seed'
+        self.inputs.new('SvVerticesSocket', "AddUVPoints")
+        self.outputs.new('SvVerticesSocket', "Vertices")
+        self.outputs.new('SvStringsSocket', "Faces")
+
+    def process(self):
+        if not any(socket.is_linked for socket in self.outputs):
+            return
+
+        surfaces_s = self.inputs['Surface'].sv_get()
+        samples_u_s = self.inputs['SamplesU'].sv_get()
+        samples_v_s = self.inputs['SamplesV'].sv_get()
+        min_ppf_s = self.inputs['MinPpf'].sv_get()
+        max_ppf_s = self.inputs['MaxPpf'].sv_get()
+        seed_s = self.inputs['Seed'].sv_get()
+        add_points_s = self.inputs['AddUVPoints'].sv_get(default=[[]])
+
+        surfaces_s = ensure_nesting_level(surfaces_s, 2, data_types=(SvSurface,))
+        samples_u_s = ensure_nesting_level(samples_u_s, 2)
+        samples_v_s = ensure_nesting_level(samples_v_s, 2)
+        min_ppf_s = ensure_nesting_level(min_ppf_s, 2)
+        max_ppf_s = ensure_nesting_level(max_ppf_s, 2)
+        seed_s = ensure_nesting_level(seed_s, 2)
+        add_points_s = ensure_nesting_level(add_points_s, 4)
+
+        verts_out = []
+        faces_out = []
+        inputs = zip_long_repeat(surfaces_s, samples_u_s, samples_v_s, min_ppf_s, max_ppf_s, seed_s, add_points_s)
+        for surfaces, samples_u_i, samples_v_i, min_ppf_i, max_ppf_i, seed_i, add_points_i in inputs:
+            objects = zip_long_repeat(surfaces, samples_u_i, samples_v_i, min_ppf_i, max_ppf_i, seed_i, add_points_i)
+            for surface, samples_u, samples_v, min_ppf, max_ppf, seed, add_points in objects:
+                us, vs, new_faces = adaptive_subdivide(surface,
+                                        samples_u, samples_v,
+                                        self.by_curvature, self.by_area,
+                                        add_points,
+                                        min_ppf, max_ppf, seed)
+                new_verts = surface.evaluate_array(us, vs).tolist()
+                verts_out.append(new_verts)
+                faces_out.append(new_faces)
+
+        self.outputs['Vertices'].sv_set(verts_out)
+        self.outputs['Faces'].sv_set(faces_out)
+
+def register():
+    bpy.utils.register_class(SvAdaptiveTessellateNode)
+
+def unregister():
+    bpy.utils.unregister_class(SvAdaptiveTessellateNode)
+
---
+++ utils/adaptive_surface.py
@@ -0,0 +1,125 @@
+# This file is part of project Sverchok. It's copyrighted by the contributors
+# recorded in the version control history of the file, available from
+# its original location https://github.com/nortikin/sverchok/commit/master
+#  
+# SPDX-License-Identifier: GPL3
+# License-Filename: LICENSE
+
+import numpy as np
+import random
+from math import ceil
+
+from sverchok.utils.logging import info, exception
+from sverchok.utils.surface import SvSurface
+from sverchok.utils.geom_2d.merge_mesh import crop_mesh_delaunay
+from sverchok.utils.voronoi import computeDelaunayTriangulation, Site
+
+def populate_surface_uv(surface, samples_u, samples_v, by_curvature=True, by_area=True, min_ppf=1, max_ppf=5, seed=1):
+    u_min, u_max = surface.get_u_min(), surface.get_u_max()
+    v_min, v_max = surface.get_v_min(), surface.get_v_max()
+    us_range = np.linspace(u_min, u_max, num=samples_u)
+    vs_range = np.linspace(v_min, v_max, num=samples_v)
+    us, vs = np.meshgrid(us_range, vs_range)
+    us = us.flatten()
+    vs = vs.flatten()
+
+    if by_curvature:
+        curvatures = abs(surface.gauss_curvature_array(us, vs)).clip(0, 100)
+        curvatures = curvatures.reshape((samples_u, samples_v))
+
+        curvatures_0 = curvatures[:-1, :-1]
+        curvatures_du = curvatures[1:, :-1]
+        curvatures_dv = curvatures[:-1, 1:]
+        curvatures_du_dv = curvatures[1:, 1:]
+
+        max_curvatures = np.max([curvatures_0, curvatures_du, curvatures_dv, curvatures_du_dv], axis=0)
+        max_curvature = max_curvatures.max()
+        min_curvature = max_curvatures.min()
+        curvatures_range = max_curvature - min_curvature
+        info("Curvature range: %s - %s", min_curvature, max_curvature)
+        if curvatures_range == 0:
+            max_curvatures = np.zeros((samples_u-1, samples_v-1))
+        else:
+            max_curvatures = (max_curvatures - min_curvature) / curvatures_range
+    else:
+        max_curvatures = np.zeros((samples_u-1, samples_v-1))
+        curvatures_range = 0
+
+    if by_area:
+        surface_points = surface.evaluate_array(us, vs)
+        surface_points = surface_points.reshape((samples_u, samples_v, 3))
+
+        points_0 = surface_points[:-1, :-1,:]
+        points_du = surface_points[1:, :-1,:]
+        points_dv = surface_points[:-1, 1:,:]
+        points_du_dv = surface_points[1:, 1:,:]
+
+        areas_1 = np.linalg.norm(np.cross(points_du_dv - points_0, points_du - points_0), axis=2)/6.0
+        areas_2 = np.linalg.norm(np.cross(points_dv - points_0, points_du_dv - points_0), axis=2)/6.0
+        areas = areas_1 + areas_2
+        min_area = areas.min()
+        max_area = areas.max()
+        areas_range = max_area - min_area
+        info("Areas range: %s - %s", min_area, max_area)
+        if areas_range == 0:
+            areas = np.zeros((samples_u-1, samples_v-1))
+        else:
+            areas = (areas - min_area) / areas_range
+    else:
+        areas = np.zeros((samples_u-1, samples_v-1))
+        areas_range = 0
+
+    factors = max_curvatures + areas
+    factor_range = areas_range + curvatures_range
+    if by_area and by_curvature:
+        factors = factors / 2.0
+        factor_range = factor_range / 2.0
+    max_factor = factors.max()
+    if max_factor != 0:
+        factors = factors / max_factor
+    info("Factors: %s - %s (%s)", factors.min(), factors.max(), factor_range)
+    info("Areas: %s - %s", areas.min(), areas.max())
+    info("Curvatures: %s - %s", max_curvatures.min(), max_curvatures.max())
+
+    ppf_range = max_ppf - min_ppf
+
+    if not seed:
+        seed = 12345
+    random.seed(seed)
+    new_u = []
+    new_v = []
+    for i in range(samples_u-1):
+        u1 = us_range[i]
+        u2 = us_range[i+1]
+        for j in range(samples_v-1):
+            v1 = vs_range[j]
+            v2 = vs_range[j+1]
+            factor = factors[i,j]
+            if factor_range == 0:
+                ppf = (min_ppf + max_ppf)/2
+            else:
+                ppf = min_ppf + ppf_range * factor
+            #ppf = int(round(ppf))
+            ppf = ceil(ppf)
+#             if ppf > 1:
+#                 info("I %s, J %s, factor %s, PPF %s", i, j, factor, ppf)
+#                 info("U %s - %s, V %s - %s", u1, u2, v1, v2)
+            for k in range(ppf):
+                u = random.uniform(u1, u2)
+                v = random.uniform(v1, v2)
+                new_u.append(u)
+                new_v.append(v)
+
+    return us, vs, new_u, new_v
+
+def adaptive_subdivide(surface, samples_u, samples_v, by_curvature=True, by_area=True, add_points=None, min_ppf=1, max_ppf=5, seed=1):
+    us, vs, new_u, new_v = populate_surface_uv(surface, samples_u, samples_v, by_curvature, by_area, min_ppf, max_ppf, seed)
+    us_list = list(us) + new_u
+    vs_list = list(vs) + new_v
+    if add_points and len(add_points[0]) > 0:
+        us_list.extend([p[0] for p in add_points])
+        vs_list.extend([p[1] for p in add_points])
+    points_uv = [Site(u, v) for u, v in zip(us_list, vs_list)]
+    faces = computeDelaunayTriangulation(points_uv)
+    return np.array(us_list), np.array(vs_list), faces
+
