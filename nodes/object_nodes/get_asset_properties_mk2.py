647a4e78549664948aa23be3114ab109aca929db
zeffii <tetha.z@gmail.com>
2020-05-08 17:07:06 +0200

    wooop

--- nodes/object_nodes/get_asset_properties_mk2.py
+++ nodes/object_nodes/get_asset_properties_mk2.py
@@ -1,20 +1,9 @@
-# ##### BEGIN GPL LICENSE BLOCK #####
-#
-#  This program is free software; you can redistribute it and/or
-#  modify it under the terms of the GNU General Public License
-#  as published by the Free Software Foundation; either version 2
-#  of the License, or (at your option) any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with this program; if not, write to the Free Software Foundation,
-#  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
-#
-# ##### END GPL LICENSE BLOCK #####
+# This file is part of project Sverchok. It's copyrighted by the contributors
+# recorded in the version control history of the file, available from
+# its original location https://github.com/nortikin/sverchok/commit/master
+#  
+# SPDX-License-Identifier: GPL3
+# License-Filename: LICENSE
 
 import bpy
 from bpy.props import EnumProperty
@@ -198,7 +187,7 @@ class SvGetAssetPropertiesMK2(bpy.types.Node, SverchCustomTreeNode, SvAnimatable
         if self.image_name:
             img = data_list[self.image_name]
             if self.pass_pixels:
-                return [[img.pixels[:]]]
+                return [[img.pixels[:]], img.size[:]]
             else:
                 return [img]
         else:
@@ -241,11 +230,10 @@ class SvGetAssetPropertiesMK2(bpy.types.Node, SverchCustomTreeNode, SvAnimatable
         if not output_socket.is_linked:
             return
 
-        data_list = getattr(bpy.data, self.Mode)
-
         if self.Mode in {'objects', 'texts', 'images', 'grease_pencils'}:
             data_output = getattr(self, f"process_mode_{self.Mode}")()
         else:
+            data_list = getattr(bpy.data, self.Mode)
             data_output = data_list[:]
 
         output_socket.sv_set(data_output)
