b7499f15bac40f156887565e85a19264b47df7e7
zeffii <tetha.z@gmail.com>
2020-05-09 00:23:19 +0200

    move GP section of get_asset_propmk2 to ptr

--- nodes/object_nodes/get_asset_properties_mk2.py
+++ nodes/object_nodes/get_asset_properties_mk2.py
@@ -55,11 +55,17 @@ class SvGetAssetPropertiesMK2(bpy.types.Node, SverchCustomTreeNode, SvAnimatable
     bl_icon = 'SELECT_SET'
     sv_icon = 'SV_OBJECT_ID_SELECTOR'
 
+    def type_filter(self, object):
+        return object.type == self.Type
+
     def frame_updateNode(self, context):
         ''' must rebuild for each update'''
         self.frame_collection_name.clear()
-        gp_layer = bpy.data.grease_pencils[self.gp_name].layers[self.gp_layer]
-        for idx, f in enumerate(gp_layer.frames):
+        
+        if not (self.gp_pointer and self.gp_layer_pointer):
+           return 
+
+        for idx, f in enumerate(gp_layer_pointer.frames):
             self.frame_collection_name.add().name = str(idx) + ' | ' + str(f.frame_number)
 
         # updateNode(self, context)
@@ -72,7 +78,6 @@ class SvGetAssetPropertiesMK2(bpy.types.Node, SverchCustomTreeNode, SvAnimatable
 
         self.process()
 
-
     type_collection_name: bpy.props.CollectionProperty(type=bpy.types.PropertyGroup)
     frame_collection_name: bpy.props.CollectionProperty(type=bpy.types.PropertyGroup)
 
@@ -81,8 +86,6 @@ class SvGetAssetPropertiesMK2(bpy.types.Node, SverchCustomTreeNode, SvAnimatable
          'movieclips', 'node_groups', 'particles', 'scenes', 'screens', 'shape_keys',
          'sounds', 'speakers', 'texts', 'textures', 'worlds', 'objects']
 
-    def type_filter(self, object):
-        return object.type == self.Type
 
     T = ['MESH', 'CURVE', 'SURFACE', 'META', 'FONT', 'ARMATURE', 'GPENCIL',
          'LATTICE', 'EMPTY', 'CAMERA', 'LIGHT', 'SPEAKER']
@@ -92,20 +95,14 @@ class SvGetAssetPropertiesMK2(bpy.types.Node, SverchCustomTreeNode, SvAnimatable
 
     properties_to_skip_iojson: ["object_pointer", "image_pointer", "text_pointer"]
 
-    @property
-    def automated_props(self):
-        ...
-
     text_pointer: PointerProperty(type=bpy.types.Text, poll=lambda s, o: True, update=updateNode)
     object_pointer: PointerProperty(type=bpy.types.Object, poll=type_filter, update=updateNode)
     image_pointer: PointerProperty(type=bpy.types.Image, poll=lambda s, o: True, update=updateNode)
     pass_pixels: bpy.props.BoolProperty(update=updateNode)
 
     # GP props
-    # bpy.types.GreasePencil
-    # bpy.types.GreasePencilLayers
-    gp_name: bpy.props.StringProperty(update=updateNode)
-    gp_layer: bpy.props.StringProperty(update=updateNode)
+    gp_pointer: bpy.props.PointerProperty(type=bpy.types.GreasePencil, poll=lambda s, o: True, update=updateNode)
+    gp_layer_pointer: bpy.props.PointerProperty(type=bpy.types.GreasePencilLayers, poll=lambda s, o: True, update=updateNode)
     gp_frame_current: bpy.props.BoolProperty(default=True, update=updateNode)
     gp_frame_override: bpy.props.IntProperty(default=1, update=updateNode)
     gp_stroke_idx: bpy.props.IntProperty(update=updateNode)
@@ -119,64 +116,53 @@ class SvGetAssetPropertiesMK2(bpy.types.Node, SverchCustomTreeNode, SvAnimatable
     gp_pass_points: bpy.props.BoolProperty(default=True, update=updateNode)
 
     def draw_gp_options(self, context, layout):
-        # ------ TO DO --------------- POINTER
-
-        # -- points  [p.co for p in points]
-        # -- color   
-        #     -- color.color (stroke_color)
-        #     -- color.fill_color
-        #     -- color.file_alpha
-        # -- line_width
-        # -- draw_cyclic
-        #  --- / triangles (only set is useful...)
-        
-        layout.prop_search(self, 'gp_name', bpy.data, 'grease_pencils', text='name')
-        if not self.gp_name:
+        layout.prop_search(self, 'gp_pointer', bpy.data, 'grease_pencils', text='name')
+        if not self.gp_pointer:
             return
         
-        layout.prop_search(self, 'gp_layer', bpy.data.grease_pencils[self.gp_name], 'layers', text='layer')
-        if not self.gp_layer:
+        layout.prop_search(self, 'gp_layer_pointer', self.gp_pointer, 'layers', text='layer')
+        if not self.gp_layer_pointer:
             return
 
         layout.prop(self, 'gp_selected_frame_mode', expand=True)
-        gp_layer = bpy.data.grease_pencils[self.gp_name].layers[self.gp_layer]
         frame_data = None
         if self.gp_selected_frame_mode == 'active_frame':
-            frame_data = gp_layer.active_frame
+            frame_data = gp_layer_pointer.active_frame
         else:
             # maybe display uilist with frame_index and frame_nmber.
             layout.prop_search(self, 'gp_frame_pick', self, 'frame_collection_name')
         layout.prop(self, 'gp_pass_points', text='pass points')
 
+    def process_mode_grease_pencils(self):
+        data_list = bpy.data.grease_pencils
+        if not (self.gp_pointer and self.gp_layer_pointer):
+            return data_list[:]
 
-    def draw_buttons(self, context, layout):
-        # layout.operator('node.'   ,text='refresh from scene')
-        self.draw_animatable_buttons(layout, icon_only=True)
-        layout.row().prop(self, "Mode", text="data")
+        if self.gp_selected_frame_mode == 'active_frame':
+            if len(self.inputs) > 0 and self.inputs[0].is_linked:
 
-        if self.Mode == 'objects':
-            layout.prop(self, "Type", text="type")
-            layout.prop_search(self, 'object_pointer', bpy.data, 'objects', text='name', icon='OBJECT_DATA')
-        elif self.Mode == 'texts':
-            layout.prop_search(self, 'text_pointer', bpy.data, 'texts', text='name')
-        elif self.Mode == 'images':
-            layout.prop_search(self, 'image_pointer', bpy.data, 'images', text='name')
-            if self.image_pointer and self.image_pointer.name:
-                layout.prop(self, 'pass_pixels', text='pixels')
-                layout.label(text=f"dimensions: {self.image_pointer.size[:]}")
-        elif self.Mode == 'grease_pencils':
-            self.draw_gp_options(context, layout)
+                frame_number = self.inputs[0].sv_get()[0][0]
+                key = frame_from_available2(frame_number, self.gp_layer_pointer)
+                strokes = self.gp_layer_pointer.frames[key].strokes
+            else:
+                strokes = self.gp_layer_pointer.active_frame.strokes
+
+            if not strokes:
+                return []
+
+            if self.gp_pass_points:
+                return [[p.co[:] for p in s.points] for s in strokes]
+            else:
+                return strokes
         else:
-            col = layout.column()
-            col.alert = True
-            col.label(text=f"This node is not yet programmed to do")
-            col.label(text=f"anything sensible with bpy.data.{self.Mode}")
-
-
-    def sv_init(self, context):
-        self.outputs.new('SvStringsSocket', "Objects")
-        self.width = 210
-        self.Type = 'MESH'  # helps init the custom object prop_search
+            if self.gp_frame_pick:
+                idx_from_frame_pick = int(self.gp_frame_pick.split(' | ')[0])
+                frame_data = self.gp_layer_pointer.frames[idx_from_frame_pick]
+                if frame_data:
+                    if self.gp_pass_points:
+                        return [[p.co[:] for p in s.points] for s in frame_data.strokes]
+                    else:
+                        return strokes
 
     def process_mode_objects(self):
         data_list = bpy.data.objects
@@ -202,38 +188,33 @@ class SvGetAssetPropertiesMK2(bpy.types.Node, SverchCustomTreeNode, SvAnimatable
         else:
             return data_list[:]
 
-    def process_mode_grease_pencils(self):
-        # TODO FIXES POINTER NOT APPLIED YET
-        data_list = bpy.data.grease_pencils
-        if not (self.gp_name and self.gp_layer):
-            return []
+    def draw_buttons(self, context, layout):
+        # layout.operator('node.'   ,text='refresh from scene')
+        self.draw_animatable_buttons(layout, icon_only=True)
+        layout.row().prop(self, "Mode", text="data")
 
-        GP_and_layer = data_list[self.gp_name].layers[self.gp_layer]
-        if self.gp_selected_frame_mode == 'active_frame':
-            if len(self.inputs) > 0 and self.inputs[0].is_linked:
-
-                frame_number = self.inputs[0].sv_get()[0][0]
-                key = frame_from_available2(frame_number, GP_and_layer)
-                strokes = GP_and_layer.frames[key].strokes
-            else:
-                strokes = GP_and_layer.active_frame.strokes
-
-            if not strokes:
-                return []
-
-            if self.gp_pass_points:
-                return [[p.co[:] for p in s.points] for s in strokes]
-            else:
-                return strokes
+        if self.Mode == 'objects':
+            layout.prop(self, "Type", text="type")
+            layout.prop_search(self, 'object_pointer', bpy.data, 'objects', text='name', icon='OBJECT_DATA')
+        elif self.Mode == 'texts':
+            layout.prop_search(self, 'text_pointer', bpy.data, 'texts', text='name')
+        elif self.Mode == 'images':
+            layout.prop_search(self, 'image_pointer', bpy.data, 'images', text='name')
+            if self.image_pointer and self.image_pointer.name:
+                layout.prop(self, 'pass_pixels', text='pixels')
+                layout.label(text=f"dimensions: {self.image_pointer.size[:]}")
+        elif self.Mode == 'grease_pencils':
+            self.draw_gp_options(context, layout)
         else:
-            if self.gp_frame_pick:
-                idx_from_frame_pick = int(self.gp_frame_pick.split(' | ')[0])
-                frame_data = GP_and_layer.frames[idx_from_frame_pick]
-                if frame_data:
-                    if self.gp_pass_points:
-                        return [[p.co[:] for p in s.points] for s in frame_data.strokes]
-                    else:
-                        return strokes
+            col = layout.column()
+            col.alert = True
+            col.label(text=f"This node is not yet programmed to do")
+            col.label(text=f"anything sensible with bpy.data.{self.Mode}")
+
+    def sv_init(self, context):
+        self.outputs.new('SvStringsSocket', "Objects")
+        self.width = 210
+        self.Type = 'MESH'  # helps init the custom object prop_search
 
     def process(self):
         output_socket = self.outputs['Objects']
