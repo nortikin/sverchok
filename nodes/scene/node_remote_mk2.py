d0526d01ef7e6e0f9542f6b2a27a4bc80a7b2185
zeffii <tetha.z@gmail.com>
2020-05-09 15:10:50 +0200

    no substantial changes except get_bpy_data_from_name

--- nodes/scene/node_remote_mk2.py
+++ nodes/scene/node_remote_mk2.py
@@ -30,6 +30,7 @@ from bpy.props import (
 from sverchok.node_tree import SverchCustomTreeNode
 from sverchok.data_structure import dataCorrect, updateNode
 from sverchok.nodes.object_nodes.getsetprop import assign_data, types
+from sverchok.utils import register_multiple_classes, unregister_multiple_classes
 
 # can't use a PointerProperty for type=bpy.data.node_group
 # https://blender.stackexchange.com/questions/2075/assign-datablock-to-custom-property
