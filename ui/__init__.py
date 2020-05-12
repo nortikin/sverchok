f67a0dff5723d094e0fd622ee6fd2995aa25e092
Dealga McArdle <zeffii@users.noreply.github.com>
2020-05-12 16:36:36 +0200

    overloads G function nicely (#3213)

--- ui/__init__.py
+++ ui/__init__.py
@@ -2,7 +2,7 @@ ui_modules = [
     "color_def", "sv_IO_panel", "sv_examples_menu",
     "sv_panels", "nodeview_rclick_menu", "nodeview_space_menu",
     "monad", "sv_icons", "presets", "nodes_replacement", "sv_panel_display_nodes",
-    "sv_temporal_viewers", "sv_vep_connector",
+    "sv_temporal_viewers", "sv_vep_connector", "sv_overload_node_ot_translate",
     # bgl modules
     "bgl_callback_3dview", "bgl_callback_nodeview",
     # show git info
