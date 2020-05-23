fac33b1c1bb2fc2ab19f02c1b516e5384d2813f5
Dealga McArdle <zeffii@users.noreply.github.com>
2020-05-23 13:37:24 +0200

    testing different join approach (#3271)
    * testing different join approach
    
    * add isframed stub
    
    * docstring for are_nodes_in_same_frame function
    
    * this implements framing these nodes
    
    * this does fix the VD
    
    * fixes #3268

--- utils/macros/join_macros.py
+++ utils/macros/join_macros.py
@@ -16,7 +16,8 @@
 #
 # ##### END GPL LICENSE BLOCK #####
 
-from sverchok.utils.sv_node_utils import nodes_bounding_box
+from sverchok.utils.sv_node_utils import framed_nodes_bounding_box as bounding_box
+from sverchok.utils.sv_node_utils import are_nodes_in_same_frame
 
 
 def join_macros(context, operator, term, nodes, links):
@@ -27,44 +28,59 @@ def join_macros(context, operator, term, nodes, links):
     if not selected_nodes:
         operator.report({"ERROR_INVALID_INPUT"}, 'No selected nodes to join')
         return
+
     tree = nodes[0].id_data
     previous_state = tree.sv_process
     tree.sv_process = False
-    # get bounding box of all selected nodes
-    _, maxx, _, maxy = nodes_bounding_box(selected_nodes)
 
-    # find out which sockets to connect
-    socket_numbers = term.replace("join", "")
-    if len(socket_numbers) == 1: # one socket
-        socket_indices = [int(socket_numbers) - 1]
-    else: # multiple sockets
-        socket_indices = [int(n) - 1 for n in socket_numbers]
+    framed = are_nodes_in_same_frame(selected_nodes)
 
-    # Create List Join nodes
-    join_nodes=[]
-    for i, s in enumerate(socket_indices):
-        join_nodes.append(nodes.new('ListJoinNode'))
-        join_nodes[i].location = maxx + 100, maxy - (180+(22*(len(selected_nodes)))) * i
+    try:
+        # get bounding box of all selected nodes
+        _, maxx, _, maxy = bounding_box(selected_nodes)
 
-    sorted_nodes = sorted(selected_nodes, key=lambda n: n.location.y, reverse=True)
+        # find out which sockets to connect
+        socket_numbers = term.replace("join", "")
+        if len(socket_numbers) == 1: # one socket
+            socket_indices = [int(socket_numbers) - 1]
+        else: # multiple sockets
+            socket_indices = [int(n) - 1 for n in socket_numbers]
 
-    # link the nodes to ListJoin nodes
-    for i, node in enumerate(sorted_nodes):
-        for j, n in enumerate(socket_indices):
-            if len(node.outputs) > n:
-                links.new(node.outputs[n], join_nodes[j].inputs[i])
+        # Create List Join nodes
+        join_nodes=[]
+        for i, s in enumerate(socket_indices):
+            join_nodes.append(nodes.new('ListJoinNode'))
 
-    if all(node.outputs[0].bl_idname == "SvVerticesSocket" for node in sorted_nodes):
-        viewer_node = nodes.new("SvVDExperimental")
-        viewer_node.location = join_nodes[0].location.x + join_nodes[0].width + 100, maxy
+            join_nodes[i].location = maxx + 100, maxy - (180+(22*(len(selected_nodes)))) * i
+            if framed:
+                join_nodes[i].parent = framed
+            
 
-        # link the output switch node to the SvVDExperimental node
-        links.new(join_nodes[0].outputs[0], viewer_node.inputs[0])
-        if len(socket_indices) > 1:
-            links.new(join_nodes[1].outputs[0], viewer_node.inputs[socket_indices[1]])
-        if len(socket_indices) > 2:
-            links.new(join_nodes[2].outputs[0], viewer_node.inputs[socket_indices[2]])
+        sorted_nodes = sorted(selected_nodes, key=lambda n: n.location.y, reverse=True)
 
-    tree.sv_process = previous_state
-    tree.update()
-    operator.report({'INFO'}, 'Nodes Joined')
+        # link the nodes to ListJoin nodes
+        for i, node in enumerate(sorted_nodes):
+            for j, n in enumerate(socket_indices):
+                if len(node.outputs) > n:
+                    links.new(node.outputs[n], join_nodes[j].inputs[i])
+
+        if all(node.outputs[0].bl_idname == "SvVerticesSocket" for node in sorted_nodes):
+            viewer_node = nodes.new("SvVDExperimental")
+
+            viewer_node.location = join_nodes[0].absolute_location[0] + join_nodes[0].width + 100, maxy
+            if framed:
+                viewer_node.parent = framed
+
+            # link the output switch node to the SvVDExperimental node
+            links.new(join_nodes[0].outputs[0], viewer_node.inputs[0])
+            if len(socket_indices) > 1:
+                links.new(join_nodes[1].outputs[0], viewer_node.inputs[socket_indices[1]])
+            if len(socket_indices) > 2:
+                links.new(join_nodes[2].outputs[0], viewer_node.inputs[socket_indices[2]])
+
+        tree.sv_process = previous_state
+        tree.update()
+        operator.report({'INFO'}, 'Nodes Joined')
+    except Exception as err:
+        tree.sv_process = previous_state
+        operator.report({'ERROR'}, f'Nodes not joined, error:\n {err}')
--- utils/sv_node_utils.py
+++ utils/sv_node_utils.py
@@ -60,6 +60,40 @@ def nodes_bounding_box(selected_nodes):
 
     return minx, maxx, miny, maxy
 
+def framed_nodes_bounding_box(selected_nodes):
+    """
+    usage:
+    minx, maxx, miny, maxy = nodes_bounding_box(selected_nodes)
+    warning: don't call this on ReRoute or Frame nodes.
+    """
+    minx = +1e10
+    maxx = -1e10
+    miny = +1e10
+    maxy = -1e10
+    for node in selected_nodes:
+        
+        node_loc_x, node_loc_y = node.absolute_location
+
+        minx = min(minx, node_loc_x)
+        maxx = max(maxx, node_loc_x + node.width)
+        miny = min(miny, node_loc_y - node.height)
+        maxy = max(maxy, node_loc_y)
+
+    return minx, maxx, miny, maxy
+
+def are_nodes_in_same_frame(nodes):
+    """
+    input: a list of nodes (expects no Reroute or Frame nodes)
+    returns: 
+       - < False > if the set is not length one.
+       - < None > if no nodes are frames
+       - < FrameNode > if all nodes have the same parent
+    """
+    parents = set([node.parent for node in nodes])
+    if len(parents) == 1:
+        return list(parents)[0]
+
+
 def sync_pointer_and_stored_name(node, pointer_prop_name, prop_name):
     # in the event that the text datablock is renamed elsewhere, this will automatically
     # resync the stored name of the datablock. updates to datablock names do not 
