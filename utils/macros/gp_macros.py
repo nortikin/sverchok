b5eb31f8f62ecf20e7f364bb8e42221adc839d92
zeffii <tetha.z@gmail.com>
2020-05-25 21:07:04 +0200

    yo

--- utils/macros/gp_macros.py
+++ utils/macros/gp_macros.py
@@ -10,11 +10,9 @@ def gp_macro_one(context, operator, term, nodes, links):
         ['SvGetAssetPropertiesMK2', (0.00, 0.00)],
         ['SvPathLengthMk2Node', (250, 55)],
         ['SvScalarMathNodeMK4', (430, 115)],
-        ['Float2IntNode', (600, 50)],
-        ['SvGenNumberRange', (720, 90)],
-        ['SvInterpolationNodeMK3', (880, 40)],
-        ['LineConnectNodeMK2', (1060, -40)],
-        ['SvVDExperimental', (1245, 50)],
+        ['SvInterpolationNodeMK3', (680, 40)],
+        ['LineConnectNodeMK2', (860, -40)],
+        ['SvVDExperimental', (1045, 50)],
     ]
 
     made_nodes = []
@@ -24,36 +22,31 @@ def gp_macro_one(context, operator, term, nodes, links):
         n.location = node_location[0] + x, node_location[1] + y
         made_nodes.append(n)
 
-    # ID Selector
-    made_nodes[0].Mode = 'grease_pencils'  # the rest must be user driven
-    links.new(made_nodes[0].outputs[0], made_nodes[1].inputs[0])
+    # nodes!
+    obj_id, path_len, scalar_math, vec_int, uv_con, drawnode = made_nodes
 
-    # Scalar Math node
-    made_nodes[2].current_op = 'MUL'
-    made_nodes[2].y_ = 2.5
-    links.new(made_nodes[1].outputs[1], made_nodes[2].inputs[0])   # snlite-> math
-    links.new(made_nodes[2].outputs[0], made_nodes[3].inputs[0])   # math -> float
+    # -- node settings --
+    obj_id.Mode = 'grease_pencils' 
+    vec_int.infer_from_integer_input = True
+    scalar_math.current_op = 'MUL'
+    scalar_math.y_ = 2.5
+    uv_con.polygons = 'Edges'
+    uv_con.slice_check = False
+    uv_con.dir_check = 'U_dir'
 
-    # Float2Int node
-    # made_nodes[3]
-    links.new(made_nodes[3].outputs[0], made_nodes[4].inputs[2])
+    # ID Selector -> pathlen and vector interpolate
+    links.new(obj_id.outputs[0], path_len.inputs[0])
+    links.new(obj_id.outputs[0], vec_int.inputs[0])
 
-    # Float range
-    made_nodes[4].range_mode = 'RANGE_COUNT'
-    made_nodes[4].stop_float = 1.0
-    links.new(made_nodes[4].outputs[0], made_nodes[5].inputs[1])
+    # pathlen -> Vec int.
+    links.new(path_len.outputs[1], scalar_math.inputs[0])
+    
+    # Scalar Math node -> vec int
+    links.new(scalar_math.outputs[0], vec_int.inputs[1])
 
-    # Vector Interpolate
-    made_nodes[5]
-    links.new(made_nodes[0].outputs[0], made_nodes[5].inputs[0])
-    links.new(made_nodes[5].outputs[0], made_nodes[6].inputs[0])
+    # Vector Interpolate -> uv
+    links.new(vec_int.outputs[0], uv_con.inputs[0])
 
-    # UV connect
-    made_nodes[6].polygons = 'Edges'
-    made_nodes[6].slice_check = False
-    made_nodes[6].dir_check = 'U_dir'
-
-    # Viewer Draw
-    made_nodes[7]
-    links.new(made_nodes[6].outputs[0], made_nodes[7].inputs[0])
-    links.new(made_nodes[6].outputs[1], made_nodes[7].inputs[1])
+    # uvcon -> Viewer Draw
+    links.new(uv_con.outputs[0], drawnode.inputs[0])
+    links.new(uv_con.outputs[1], drawnode.inputs[1])
