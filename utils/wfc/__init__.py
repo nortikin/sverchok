297f3ef25810241db2cc47d34101714b9e7cc79e
Durman <sv@soluyanov.ru>
2020-06-11 22:33:29 +0400

    remove numpy implementation

--- utils/wfc/__init__.py
+++
@@ -1,3 +0,0 @@
-# conception - https://github.com/mxgmn/WaveFunctionCollapse
-# source code - https://github.com/ikarth/wfc_2019f
-# algorithm explanation - https://gridbugs.org/wave-function-collapse/
