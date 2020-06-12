759594e011b18cf494f78254316c3fd918be13d4
Durman <sv@soluyanov.ru>
2020-06-12 12:21:23 +0400

    appearance improvements

--- nodes/generators_extended/wfc_image.py
+++ nodes/generators_extended/wfc_image.py
@@ -5,9 +5,6 @@
 # SPDX-License-Identifier: GPL3
 # License-Filename: LICENSE
 
-import random
-import math
-from enum import Enum
 
 import numpy as np
 
@@ -29,21 +26,24 @@ class SvWFCTextureNode(bpy.types.Node, SverchCustomTreeNode):
     """
     Triggers: image
 
-    ...
-    ...
+    the node uses wave function collapse algorithm
+    some patterns can take long time to calculate
     """
     bl_idname = 'SvWFCTextureNode'
     bl_label = 'WFC Texture'
-    bl_icon = 'AUTOMERGE_ON'
+    bl_icon = 'FORCE_FORCE'
 
-    image_name: bpy.props.StringProperty(name="Image", default="", update=updateNode)
-    height: bpy.props.IntProperty(default=10, update=updateNode)
-    width: bpy.props.IntProperty(default=10, update=updateNode)
+    image_name: bpy.props.StringProperty(name="Image", default="", update=updateNode, description="Sample image")
+    height: bpy.props.IntProperty(default=10, min=1, max=100, update=updateNode, description="For output image")
+    width: bpy.props.IntProperty(default=10, min=1, max=100, update=updateNode, description="For output image")
     seed: bpy.props.IntProperty(update=updateNode)
-    pattern_size: bpy.props.IntProperty(default=3, min=1, max=5, update=updateNode)
-    rotate_patterns: bpy.props.BoolProperty(update=updateNode)
-    tiling_output: bpy.props.BoolProperty(update=updateNode)
-    periodic_input: bpy.props.BoolProperty(default=True, update=updateNode)
+    pattern_size: bpy.props.IntProperty(default=3, min=1, max=5, update=updateNode, description="Usually 2 or 3")
+    rotate_patterns: bpy.props.BoolProperty(update=updateNode, description="More complex result")
+    tiling_output: bpy.props.BoolProperty(update=updateNode,
+                                          description="Output would be able to cycle over UV directions")
+    periodic_input: bpy.props.BoolProperty(default=True, update=updateNode, description="Impact on creating patterns")
+    tries_number: bpy.props.IntProperty(default=1, min=1, max=10, update=updateNode,
+                                        description="If contradiction it will try calculate again")
 
     def sv_init(self, context):
         self.inputs.new("SvStringsSocket", "height").prop_name = 'height'
@@ -53,10 +53,16 @@ class SvWFCTextureNode(bpy.types.Node, SverchCustomTreeNode):
         self.outputs.new("SvColorSocket", "image")
 
     def draw_buttons(self, context, layout):
-        layout.prop_search(self, 'image_name', bpy.data, 'images', text='', icon='IMAGE')
-        layout.prop(self, 'rotate_patterns')
-        layout.prop(self, 'periodic_input')
-        layout.prop(self, 'tiling_output')
+        col = layout.column(align=True)
+        col.label(text="Sample image:")
+        col.prop_search(self, 'image_name', bpy.data, 'images', text='', icon='IMAGE')
+        col = layout.column(align=True)
+        col.prop(self, 'rotate_patterns')
+        col.prop(self, 'periodic_input')
+        col.prop(self, 'tiling_output')
+
+    def draw_buttons_ext(self, context, layout: 'UILayout'):
+        layout.prop(self, 'tries_number')
 
     def process(self):
         if not self.image_name:
@@ -65,15 +71,15 @@ class SvWFCTextureNode(bpy.types.Node, SverchCustomTreeNode):
         image = load_image(self.image_name)
         wave = WaveFunctionCollapse(
             image,
-            patter_size=self.pattern_size,
+            patter_size=self.inputs['pattern size'].sv_get()[0][0],
             periodic_input=self.periodic_input,
             rotate_patterns=self.rotate_patterns)
 
         output = wave.solve(
-            output_size=(self.width, self.height),
-            seed=self.seed,
+            output_size=(self.inputs['width'].sv_get()[0][0], self.inputs['height'].sv_get()[0][0]),
+            seed=self.inputs['seed'].sv_get()[0][0],
             tiling_output=self.tiling_output,
-            max_number_contradiction_tries=1)
+            max_number_contradiction_tries=self.tries_number)
 
         self.outputs['image'].sv_set(output)
 
