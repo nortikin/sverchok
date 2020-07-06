b68369d7ee137131cc21e22072d4ba224382d49c
Ilya Portnov <portnov@bk.ru>
2020-07-06 20:18:25 +0500

    Split surface.py.

--- utils/surface/__init__.py
+++ utils/surface/__init__.py
@@ -1,1 +1,10 @@
-# __init__.py
+# This file is part of project Sverchok. It's copyrighted by the contributors
+# recorded in the version control history of the file, available from
+# its original location https://github.com/nortikin/sverchok/commit/master
+#  
+# SPDX-License-Identifier: GPL3
+# License-Filename: LICENSE
+
+from sverchok.utils.surface.core import *
+from sverchok.utils.surface.sphere import *
+from sverchok.utils.surface.algorithms import *
