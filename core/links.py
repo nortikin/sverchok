a6783644a2af28c0d95786d36d079ed678fa8182
Ilya Portnov <portnov@bk.ru>
2020-08-04 10:34:42 +0500

    Comment out an annoying message :)

--- core/links.py
+++ core/links.py
@@ -168,7 +168,7 @@ class SvLinks:
         before_sv_links = self.sv_links_cache[tree_id]
 
         if not self.sv_links_cache[tree_id]:
-            print('there was no links memory, creating it')
+            #print('there was no links memory, creating it')
             self.create_new_links(node_tree)
             node_tree.nodes_dict.load_nodes(node_tree)
             return node_tree.nodes
