ebfc95a2a8c6849db88d18de142b9487c048f8e6
Victor Doval <10011941+vicdoval@users.noreply.github.com>
2020-08-08 19:35:31 +0200

    docs and refactor

--- docs/nodes/solid/solid_distance.rst
+++ docs/nodes/solid/solid_distance.rst
@@ -11,35 +11,42 @@ This node requires FreeCAD_ library to work.
 Functionality
 -------------
 
-The node returns the closest point to a vector that is over a solid surface. It will also accept "Solid Faces" and "Solid Edges"
+The node returns the distance between a Solid, a solid Face or a solid Edge to another Solid (or solid face, solid edge or vertex).
+
+It will also return which is the closest point between the selected objects and information about where this closest point is.
+
 
 Options
 -------
 
-**Mode**: Solid, Face or Edge
+**From**: Solid, Solid Face or Solid Edge
+
+**To**: Solid, Solid Face, Solid Edge or Vertex
 
 Inputs
 ------
 
 All inputs are vectorized and the data will be matched to the longest list
 
-- **Solid / Solid Face / Solid Edge**: Element to project points to.
-- **Vertices**: Vertices to make the analysis over.
+- **Solid / Solid Face / Solid Edge**: First element
+- ****Solid / Solid Face / Solid Edge / Vertices**: Second element
 
 Outputs
 -------
 
-- **Distance**: Minimum distance between point and Element.
-- **Closest Point**: Point in element where the distance is minimal
-- **Info**: returns a triple per vector with:
+- **Distance**: Minimum distance.
+- **Closest Point A and B**: Points in element where the distance is minimal
+- **Info A and B**: returns a triple per element with:
   - Nearest topology item type (Face, Edge or Vertex)
   - Nearest topology item index
-  - Nearest topology item parameter (UV coords for faces, U for edges and None for vertex)
+  - Nearest topology item parameter (UV coordinates for faces, T for edges and None for vertex)
 
 
 Examples
 --------
 
-.. image:: https://raw.githubusercontent.com/vicdoval/sverchok/docs_images/images_for_docs/solid/points_inside_solid/points_inside_solid_blender_sverchok_example.png
+.. image:: https://raw.githubusercontent.com/vicdoval/sverchok/docs_images/images_for_docs/solid/solid_distance/solid_distance_blender_sverchok_example_00.png
 
-.. image:: https://raw.githubusercontent.com/vicdoval/sverchok/docs_images/images_for_docs/solid/points_inside_solid/points_inside_solid_blender_sverchok_example_01.png
+.. image:: https://raw.githubusercontent.com/vicdoval/sverchok/docs_images/images_for_docs/solid/solid_distance/solid_distance_blender_sverchok_example_01.png
+
+.. image:: https://raw.githubusercontent.com/vicdoval/sverchok/docs_images/images_for_docs/solid/solid_distance/solid_distance_blender_sverchok_example_03.png
--- nodes/solid/solid_distance.py
+++ nodes/solid/solid_distance.py
@@ -13,10 +13,23 @@ else:
     import Part
     from FreeCAD import Base
 
+    def get_shape(mode, base_object):
+        if mode == 'Solid':
+            if isinstance(base_object, Part.Solid):
+                shape = base_object.OuterShell
+            else:
+                shape = base_object
+        elif mode == 'Face':
+            shape = base_object.face
+        else:
+            shape = base_object.edge
+        return shape
+
+
     class SvSolidDistanceNode(bpy.types.Node, SverchCustomTreeNode):
         """
-        Triggers: Closest point on Solid
-        Tooltip: Closest point to a vector on a solid surface, accepts also solid faces and solid edges
+        Triggers: Closest point on Solids
+        Tooltip: Distance between solids. Closest point on a solid surface, accepts also solid faces and solid edges
         """
         bl_idname = 'SvSolidDistanceNode'
         bl_label = 'Solid Distance'
@@ -31,17 +44,20 @@ else:
             ('Vertex', 'Vertex', '', 3),
 
         ]
-        def set_sockets(self,context):
+        def set_sockets(self, context):
             self.update_sockets()
             updateNode(self, context)
 
         def update_sockets(self):
-            self.inputs['Solid'].hide_safe = self.mode != 'Solid'
-            self.inputs['Solid Face'].hide_safe = self.mode != 'Face'
-            self.inputs['Solid Edge'].hide_safe = self.mode != 'Edge'
+            self.inputs['Solid A'].hide_safe = self.mode != 'Solid'
+            self.inputs['Solid Face A'].hide_safe = self.mode != 'Face'
+            self.inputs['Solid Edge A'].hide_safe = self.mode != 'Edge'
             self.inputs['Solid B'].hide_safe = self.mode_b != 'Solid'
             self.inputs['Solid Face B'].hide_safe = self.mode_b != 'Face'
             self.inputs['Solid Edge B'].hide_safe = self.mode_b != 'Edge'
+            self.inputs['Vertices'].hide_safe = self.mode_b != 'Vertex'
+            self.outputs['Closest Point B'].hide_safe = self.mode_b == 'Vertex'
+            self.outputs['Info B'].hide_safe = self.mode_b == 'Vertex'
 
 
         mode: EnumProperty(
@@ -57,20 +73,20 @@ else:
             update=set_sockets)
 
         def sv_init(self, context):
-            self.inputs.new('SvSolidSocket', "Solid")
-            self.inputs.new('SvSurfaceSocket', "Solid Face")
-            self.inputs.new('SvCurveSocket', "Solid Edge")
+            self.inputs.new('SvSolidSocket', "Solid A")
+            self.inputs.new('SvSurfaceSocket', "Solid Face A")
+            self.inputs.new('SvCurveSocket', "Solid Edge A")
             self.inputs.new('SvSolidSocket', "Solid B")
             self.inputs.new('SvSurfaceSocket', "Solid Face B")
             self.inputs.new('SvCurveSocket', "Solid Edge B")
             self.inputs.new('SvVerticesSocket', "Vertices")
             self.outputs.new('SvStringsSocket', "Distance")
-            self.outputs.new('SvVerticesSocket', "Closest Point")
-            self.outputs.new('SvStringsSocket', "Info")
+            self.outputs.new('SvVerticesSocket', "Closest Point A")
+            self.outputs.new('SvStringsSocket', "Info A")
             self.outputs.new('SvVerticesSocket', "Closest Point B")
             self.outputs.new('SvStringsSocket', "Info B")
-            self.mode= "Solid"
-            self.mode_b= "Solid"
+            self.mode = "Solid"
+            self.mode_b = "Solid"
             self.update_sockets()
 
         def draw_buttons(self, context, layout):
@@ -87,69 +103,53 @@ else:
                 return
 
 
-            solids_in = self.inputs[self["mode"]].sv_get()
+            objects_a = self.inputs[self["mode"]].sv_get()
             objects_b = self.inputs[self["mode_b"] + 3].sv_get()
-            # points = self.inputs[6].sv_get()
-
 
             distances_out = []
-            closest_points_out = []
-            infos_out = []
+            closest_points_out_a = []
+            infos_out_a = []
             closest_points_out_b = []
             infos_out_b = []
-            for solid, object_b, in zip(*mlr([solids_in, objects_b])):
+
+            for object_a, object_b, in zip(*mlr([objects_a, objects_b])):
                 distances = []
-                closest_points = []
-                infos = []
+                closest_points_a = []
+                infos_a = []
                 closest_points_b = []
                 infos_b = []
-
-                if self.mode == 'Solid':
-                    if isinstance(solid, Part.Solid):
-                        shape = solid.OuterShell
-                    else:
-                        shape = solid
-                elif self.mode == 'Face':
-                    shape = solid.face
-                else:
-                    shape = solid.edge
+                shape = get_shape(self.mode, object_a)
 
                 if self.mode_b == 'Vertex':
                     for v in object_b:
                         vertex = Part.Vertex(Base.Vector(v))
 
-                        d = shape.distToShape(vertex)
-                        distances.append(d[0])
-                        closest_points.append(d[1][0][0][:])
-                        infos.append(d[2][0][0:3])
+                        dist = shape.distToShape(vertex)
+                        distances.append(dist[0])
+                        closest_points_a.append(dist[1][0][0][:])
+                        infos_a.append(dist[2][0][0:3])
                 else:
-                    if self.mode_b == "Solid":
-                        if isinstance(solid, Part.Solid):
-                            shape_b = object_b.OuterShell
-                        else:
-                            shape_b = object_b
-                    elif self.mode == 'Face':
-                        shape_b = solid.face
-                    else:
-                        shape_b = solid.edge
-                    d = shape.distToShape(shape_b)
-                    distances.append(d[0])
-                    closest_points.append(d[1][0][0][:])
-                    infos.append(d[2][0][0:3])
-                    closest_points_b.append(d[1][0][1][:])
-                    infos_b.append(d[2][0][3:])
+                    shape_b = get_shape(self.mode_b, object_b)
+
+                    dist = shape.distToShape(shape_b)
+                    distances.append(dist[0])
+                    closest_points_a.append(dist[1][0][0][:])
+                    infos_a.append(dist[2][0][0:3])
+                    closest_points_b.append(dist[1][0][1][:])
+                    infos_b.append(dist[2][0][3:])
+                    
                     closest_points_out_b.append(closest_points_b)
                     infos_out_b.append(infos_b)
 
 
                 distances_out.append(distances)
-                closest_points_out.append(closest_points)
-                infos_out.append(infos)
+                closest_points_out_a.append(closest_points_a)
+                infos_out_a.append(infos_a)
 
 
             self.outputs['Distance'].sv_set(distances_out)
-            self.outputs['Closest Point'].sv_set(closest_points_out)
-            self.outputs['Info'].sv_set(infos_out)
+            self.outputs['Closest Point A'].sv_set(closest_points_out_a)
+            self.outputs['Info A'].sv_set(infos_out_a)
             self.outputs['Closest Point B'].sv_set(closest_points_out_b)
             self.outputs['Info B'].sv_set(infos_out_b)
 
