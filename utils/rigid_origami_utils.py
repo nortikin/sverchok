97c908f021fb831af6b23e8757bf8dac369b81bf
asahidari <asahidari@yahoo.co.jp>
2020-08-14 23:06:00 +0900

    Add header comments about license.

--- utils/rigid_origami_utils.py
+++ utils/rigid_origami_utils.py
@@ -1,3 +1,10 @@
+# This file is part of project Sverchok. It's copyrighted by the contributors
+# recorded in the version control history of the file, available from
+# its original location https://github.com/nortikin/sverchok/commit/master
+#
+# SPDX-License-Identifier: GPL3
+# License-Filename: LICENSE
+
 import numpy as np
 import math
 import copy
