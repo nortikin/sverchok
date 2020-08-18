f28f533e53b5363e4e6b127fff65c7c2a607151c
Durman <sv@soluyanov.ru>
2020-08-18 21:12:34 +0400

    add node logic

--- nodes/list_mutators/find_closest.py
+++ nodes/list_mutators/find_closest.py
@@ -5,6 +5,8 @@
 # SPDX-License-Identifier: GPL3
 # License-Filename: LICENSE
 
+import numpy as np
+
 import bpy
 
 from sverchok.node_tree import SverchCustomTreeNode
@@ -28,3 +30,16 @@ class SvFindClosest(bpy.types.Node, SverchCustomTreeNode):
     bl_idname = 'SvFindClosest'
     bl_label = 'Find closest'
     bl_icon = 'VIEWZOOM'
+
+    def process(self):
+        extended_data = np.array(node.inputs.data + [-np.inf, np.inf])
+        sorting_indexes = np.argsort(extended_data)
+        right_indexes = np.searchsorted(extended_data, node.inputs.values, sorter=sorting_indexes)
+        left_indexes = right_indexes - 1
+        left_distance = node.inputs.values - extended_data[sorting_indexes[left_indexes]]
+        left_distance = np.where(left_distance < 0, -left_distance, left_distance)
+        right_distance = extended_data[sorting_indexes[right_indexes]] - node.inputs.values
+        right_distance = np.where(right_distance < 0, -right_distance, right_distance)
+        result_indexes = np.where(left_distance < right_distance, left_indexes, right_indexes)
+        node.outputs.closest_indexes = sorting_indexes[result_indexes].tolist()
+        node.outputs.closest_values = extended_data[node.outputs.closest_indexes].tolist()
