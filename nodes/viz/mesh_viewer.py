24c5a8f3cf66e68dd0115bdd011e7d165adf1939
Durman <sv@soluyanov.ru>
2020-08-23 23:13:05 +0400

    add merge feature

--- nodes/viz/mesh_viewer.py
+++ nodes/viz/mesh_viewer.py
@@ -12,10 +12,12 @@ import bpy
 from bpy.props import BoolProperty
 from mathutils import Matrix
 
+from sverchok.nodes.matrix.apply_and_join import apply_and_join_python
 from sverchok.node_tree import SverchCustomTreeNode
 from sverchok.data_structure import updateNode
 from sverchok.utils.nodes_mixins.generating_objects import SvMeshData, SvViewerNode
 from sverchok.utils.handle_blender_data import correct_collection_length
+from sverchok.utils.sv_mesh_utils import mesh_join
 
 
 class SvMeshViewer(SvViewerNode, SverchCustomTreeNode, bpy.types.Node):
@@ -28,7 +30,7 @@ class SvMeshViewer(SvViewerNode, SverchCustomTreeNode, bpy.types.Node):
 
     mesh_data: bpy.props.CollectionProperty(type=SvMeshData)
 
-    merge: BoolProperty(default=False, update=updateNode)
+    is_merge: BoolProperty(default=False, update=updateNode, description="Merge all meshes into one object")
 
     auto_smooth: BoolProperty(
         default=False,
@@ -62,9 +64,12 @@ class SvMeshViewer(SvViewerNode, SverchCustomTreeNode, bpy.types.Node):
         row.prop_search(self, 'material', bpy.data, 'materials', text='', icon='MATERIAL_DATA')
         row.operator('node.sv_create_material', text='', icon='ADD')
 
-        col = layout.column()
-        col.active = False if self.inputs['matrix'].is_linked and self.apply_matrices_to == 'object' else True
-        col.prop(self, 'is_lock_origin', icon='LOCKED' if self.is_lock_origin else 'UNLOCKED')
+        row = layout.row(align=True)
+        col = row.column(align=True)
+        col.active = False if \
+            not self.is_merge and self.inputs['matrix'].is_linked and self.apply_matrices_to == 'object' else True
+        col.prop(self, 'is_lock_origin', text="Origin", icon='LOCKED' if self.is_lock_origin else 'UNLOCKED')
+        row.prop(self, 'is_merge', text='Merge', toggle=1, icon='AUTOMERGE_ON' if self.is_merge else 'AUTOMERGE_OFF')
 
     def draw_buttons_ext(self, context, layout):
         layout.prop(self, 'auto_smooth', text='smooth shade')
@@ -94,17 +99,38 @@ class SvMeshViewer(SvViewerNode, SverchCustomTreeNode, bpy.types.Node):
             return
 
         verts = self.inputs['vertices'].sv_get(deepcopy=False, default=[])
-        edges = self.inputs['edges'].sv_get(deepcopy=False, default=cycle([None]))
-        faces = self.inputs['faces'].sv_get(deepcopy=False, default=cycle([None]))
+        edges = self.inputs['edges'].sv_get(deepcopy=False, default=[[]])
+        faces = self.inputs['faces'].sv_get(deepcopy=False, default=[[]])
         mat_indexes = self.inputs['material_idx'].sv_get(deepcopy=False, default=[])
         matrices = self.inputs['matrix'].sv_get(deepcopy=False, default=[])
 
-        objects_number = max([len(verts), len(matrices)])  # todo if merged
+        # first step is merge everything if the option
+        if self.is_merge:
+            if matrices:
+                objects_number = max([len(verts), len(matrices)])
+                _, *join_mesh_data = list(zip(*zip(range(objects_number), cycle(verts), cycle(edges), cycle(faces),
+                                                   cycle(matrices))))
+                verts, edges, faces = apply_and_join_python(*join_mesh_data, True)
+                matrices = []
+            else:
+                verts, edges, faces = mesh_join(verts, edges if edges[0] else [], faces)  # function has good API
+                verts, edges, faces = [verts], [edges], [faces]
 
-        mesh_matrices = matrices if self.apply_matrices_to == 'mesh' else cycle([None])  # should be apply to mesh
+        objects_number = max([len(verts), len(matrices)])
+
+        # extract mesh matrices
+        if self.apply_matrices_to == 'mesh':
+            if matrices:
+                mesh_matrices = matrices
+            else:
+                mesh_matrices = cycle([None])
+        else:
+            mesh_matrices = cycle([None])
+
+        # extract object matrices
         if self.apply_matrices_to == 'object':
             if matrices:
-                obj_matrices = matrices  # should be apply to objects
+                obj_matrices = matrices
             else:
                 if self.is_lock_origin:
                     obj_matrices = cycle([Matrix.Identity(4)])
@@ -116,12 +142,15 @@ class SvMeshViewer(SvViewerNode, SverchCustomTreeNode, bpy.types.Node):
             else:
                 obj_matrices = []
 
+        # generate mesh data blocks
         correct_collection_length(self.mesh_data, objects_number)
-        for me_data, v, e, f, m in zip(self.mesh_data, cycle(verts), cycle(edges), cycle(faces), mesh_matrices):
+        create_mesh_data = zip(self.mesh_data, cycle(verts), cycle(edges), cycle(faces), cycle(mesh_matrices))
+        for me_data, v, e, f, m in create_mesh_data:
             me_data.regenerate_mesh(self.base_data_name, v, e, f, m)
 
+        # generate object data blocks
         self.regenerate_objects([self.base_data_name], [d.mesh for d in self.mesh_data], [self.collection])
-        [setattr(prop.obj, 'matrix_local', m) for prop, m in zip(self.object_data, obj_matrices)]
+        [setattr(prop.obj, 'matrix_local', m) for prop, m in zip(self.object_data, cycle(obj_matrices))]
 
         self.outputs['Objects'].sv_set([obj_data.obj for obj_data in self.object_data])
 
