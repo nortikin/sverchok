e5d8ace033bf9df46ac854bda104638adeb138f1
Ilya Portnov <portnov@bk.ru>
2020-08-27 21:43:37 +0500

    Remove excessive debug.

--- utils/surface/coons.py
+++ utils/surface/coons.py
@@ -82,7 +82,6 @@ def coons_surface(curve1, curve2, curve3, curve4):
 
     degree_u = nurbs_curves[0].get_degree()
     degree_v = nurbs_curves[1].get_degree()
-    #print(f"Target degrees UxV: {degree_u} x {degree_v}")
 
     knotvectors = [c.get_knotvector() for c in nurbs_curves]
     if not sv_knotvector.equal(knotvectors[0], knotvectors[2]):
@@ -90,19 +89,13 @@ def coons_surface(curve1, curve2, curve3, curve4):
     if not sv_knotvector.equal(knotvectors[1], knotvectors[3]):
         nurbs_curves[1], nurbs_curves[3] = unify_two_curves(nurbs_curves[1], nurbs_curves[3])
 
-    #nurbs_curves[2] = reverse_curve(nurbs_curves[2])
     nurbs_curves[0] = reverse_curve(nurbs_curves[0])
     nurbs_curves[3] = reverse_curve(nurbs_curves[3])
-    #nurbs_curves[1] = reverse_curve(nurbs_curves[1])
 
     ruled1 = nurbs_curves[0].make_ruled_surface(nurbs_curves[2], 0, 1)
-    #ruled1 = nurbs_curves[2].make_ruled_surface(nurbs_curves[0], 0, 1)
     ruled2 = nurbs_curves[1].make_ruled_surface(nurbs_curves[3], 0, 1).swap_uv()
-    #ruled2 = nurbs_curves[3].make_ruled_surface(nurbs_curves[1], 0, 1).swap_uv()
     ruled1 = ruled1.elevate_degree(SvNurbsSurface.V, target=degree_v)
     ruled2 = ruled2.elevate_degree(SvNurbsSurface.U, target=degree_u)
-
-    #print(f"Src: Ruled1 CP: {ruled1.get_control_points().shape}; Ruled2 CP: {ruled2.get_control_points().shape}")
 
     diff_1to2 = sv_knotvector.difference(ruled1.get_knotvector_v(), ruled2.get_knotvector_v())
     diff_2to1 = sv_knotvector.difference(ruled2.get_knotvector_u(), ruled1.get_knotvector_u())
@@ -112,22 +105,16 @@ def coons_surface(curve1, curve2, curve3, curve4):
     for u, count in diff_2to1:
         ruled2 = ruled2.insert_knot(SvNurbsSurface.U, u, count)
 
-    #print(f"Ruled1 CP: {ruled1.get_control_points().shape}; Ruled2 CP: {ruled2.get_control_points().shape}")
-
     linear_kv = sv_knotvector.generate(1, 2)
 
     c1_t_min, c1_t_max = nurbs_curves[0].get_u_bounds()
     c3_t_min, c3_t_max = nurbs_curves[2].get_u_bounds()
-    c2_t_min, c2_t_max = nurbs_curves[1].get_u_bounds()
-    c4_t_min, c4_t_max = nurbs_curves[3].get_u_bounds()
 
     pt1 = nurbs_curves[0].evaluate(c1_t_min)
     pt2 = nurbs_curves[0].evaluate(c1_t_max)
     pt3 = nurbs_curves[2].evaluate(c3_t_min)
     pt4 = nurbs_curves[2].evaluate(c3_t_max)
 
-    #linear_pts = np.array([[pt2,pt4], [pt1,pt3]])
-    #linear_pts = np.array([[pt4,pt2], [pt3,pt1]])
     linear_pts = np.array([[pt1,pt3], [pt2,pt4]])
     linear_weights = np.array([[1,1], [1,1]])
     bilinear = SvNurbsSurface.build(implementation,
@@ -141,13 +128,9 @@ def coons_surface(curve1, curve2, curve3, curve4):
     knotvector_u = ruled1.get_knotvector_u()
     knotvector_v = ruled2.get_knotvector_v()
     for u in sv_knotvector.get_internal_knots(knotvector_u):
-        #print(f"Insert U = {u}")
         bilinear = bilinear.insert_knot(SvNurbsSurface.U, u)
     for v in sv_knotvector.get_internal_knots(knotvector_v):
-        #print(f"Insert V = {v}")
         bilinear = bilinear.insert_knot(SvNurbsSurface.V, v)
-
-    #print(f"Bilinear CP: {bilinear.get_control_points().shape}")
 
     control_points = ruled1.get_control_points() + ruled2.get_control_points() - bilinear.get_control_points()
     weights = ruled1.get_weights() + ruled2.get_weights() - bilinear.get_weights()
@@ -155,5 +138,5 @@ def coons_surface(curve1, curve2, curve3, curve4):
                 degree_u, degree_v,
                 knotvector_u, knotvector_v,
                 control_points, weights)
-    return ruled1, ruled2, bilinear, result
+    return result
 
