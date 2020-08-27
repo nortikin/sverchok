06dfe8f72a539284ded2e8906d66f24bcfdeeddb
Ilya Portnov <portnov@bk.ru>
2020-08-27 21:53:03 +0500

    Handle all exceptions at once.

--- utils/surface/coons.py
+++ utils/surface/coons.py
@@ -7,6 +7,7 @@
 
 import numpy as np
 
+from sverchok.utils.logging import info, debug
 from sverchok.utils.curve import knotvector as sv_knotvector
 from sverchok.utils.curve.core import UnsupportedCurveTypeException
 from sverchok.utils.curve.nurbs import SvNurbsCurve, unify_two_curves
@@ -65,10 +66,10 @@ def coons_surface(curve1, curve2, curve3, curve4):
     nurbs_curves = [SvNurbsCurve.to_nurbs(c) for c in curves]
     if any(c is None for c in nurbs_curves):
         return SvCoonsSurface(*curves)
-    degrees = [c.get_degree() for c in nurbs_curves]
-    implementation = nurbs_curves[0].get_nurbs_implementation()
-
     try:
+        degrees = [c.get_degree() for c in nurbs_curves]
+        implementation = nurbs_curves[0].get_nurbs_implementation()
+
         if degrees[0] > degrees[2]:
             nurbs_curves[2] = nurbs_curves[2].elevate_degree(delta = degrees[0] - degrees[2])
         if degrees[2] > degrees[0]:
@@ -77,66 +78,67 @@ def coons_surface(curve1, curve2, curve3, curve4):
             nurbs_curves[3] = nurbs_curves[3].elevate_degree(delta = degrees[1] - degrees[3])
         if degrees[3] > degrees[1]:
             nurbs_curves[1] = nurbs_curves[1].elevate_degree(delta = degrees[3] - degrees[1])
-    except UnsupportedCurveTypeException:
+
+        degree_u = nurbs_curves[0].get_degree()
+        degree_v = nurbs_curves[1].get_degree()
+
+        knotvectors = [c.get_knotvector() for c in nurbs_curves]
+        if not sv_knotvector.equal(knotvectors[0], knotvectors[2]):
+            nurbs_curves[0], nurbs_curves[2] = unify_two_curves(nurbs_curves[0], nurbs_curves[2])
+        if not sv_knotvector.equal(knotvectors[1], knotvectors[3]):
+            nurbs_curves[1], nurbs_curves[3] = unify_two_curves(nurbs_curves[1], nurbs_curves[3])
+
+        nurbs_curves[0] = reverse_curve(nurbs_curves[0])
+        nurbs_curves[3] = reverse_curve(nurbs_curves[3])
+
+        ruled1 = nurbs_curves[0].make_ruled_surface(nurbs_curves[2], 0, 1)
+        ruled2 = nurbs_curves[1].make_ruled_surface(nurbs_curves[3], 0, 1).swap_uv()
+        ruled1 = ruled1.elevate_degree(SvNurbsSurface.V, target=degree_v)
+        ruled2 = ruled2.elevate_degree(SvNurbsSurface.U, target=degree_u)
+
+        diff_1to2 = sv_knotvector.difference(ruled1.get_knotvector_v(), ruled2.get_knotvector_v())
+        diff_2to1 = sv_knotvector.difference(ruled2.get_knotvector_u(), ruled1.get_knotvector_u())
+
+        for v, count in diff_1to2:
+            ruled1 = ruled1.insert_knot(SvNurbsSurface.V, v, count)
+        for u, count in diff_2to1:
+            ruled2 = ruled2.insert_knot(SvNurbsSurface.U, u, count)
+
+        linear_kv = sv_knotvector.generate(1, 2)
+
+        c1_t_min, c1_t_max = nurbs_curves[0].get_u_bounds()
+        c3_t_min, c3_t_max = nurbs_curves[2].get_u_bounds()
+
+        pt1 = nurbs_curves[0].evaluate(c1_t_min)
+        pt2 = nurbs_curves[0].evaluate(c1_t_max)
+        pt3 = nurbs_curves[2].evaluate(c3_t_min)
+        pt4 = nurbs_curves[2].evaluate(c3_t_max)
+
+        linear_pts = np.array([[pt1,pt3], [pt2,pt4]])
+        linear_weights = np.array([[1,1], [1,1]])
+        bilinear = SvNurbsSurface.build(implementation,
+                    1, 1,
+                    linear_kv, linear_kv,
+                    linear_pts, linear_weights)
+
+        bilinear = bilinear.elevate_degree(SvNurbsSurface.U, target=degree_u)
+        bilinear = bilinear.elevate_degree(SvNurbsSurface.V, target=degree_v)
+
+        knotvector_u = ruled1.get_knotvector_u()
+        knotvector_v = ruled2.get_knotvector_v()
+        for u in sv_knotvector.get_internal_knots(knotvector_u):
+            bilinear = bilinear.insert_knot(SvNurbsSurface.U, u)
+        for v in sv_knotvector.get_internal_knots(knotvector_v):
+            bilinear = bilinear.insert_knot(SvNurbsSurface.V, v)
+
+        control_points = ruled1.get_control_points() + ruled2.get_control_points() - bilinear.get_control_points()
+        weights = ruled1.get_weights() + ruled2.get_weights() - bilinear.get_weights()
+        result = SvNurbsSurface.build(implementation,
+                    degree_u, degree_v,
+                    knotvector_u, knotvector_v,
+                    control_points, weights)
+        return result
+    except UnsupportedCurveTypeException as e:
+        info("Can't create a native Coons surface from curves %s: %s", curves, e)
         return SvCoonsSurface(*curves)
-
-    degree_u = nurbs_curves[0].get_degree()
-    degree_v = nurbs_curves[1].get_degree()
-
-    knotvectors = [c.get_knotvector() for c in nurbs_curves]
-    if not sv_knotvector.equal(knotvectors[0], knotvectors[2]):
-        nurbs_curves[0], nurbs_curves[2] = unify_two_curves(nurbs_curves[0], nurbs_curves[2])
-    if not sv_knotvector.equal(knotvectors[1], knotvectors[3]):
-        nurbs_curves[1], nurbs_curves[3] = unify_two_curves(nurbs_curves[1], nurbs_curves[3])
-
-    nurbs_curves[0] = reverse_curve(nurbs_curves[0])
-    nurbs_curves[3] = reverse_curve(nurbs_curves[3])
-
-    ruled1 = nurbs_curves[0].make_ruled_surface(nurbs_curves[2], 0, 1)
-    ruled2 = nurbs_curves[1].make_ruled_surface(nurbs_curves[3], 0, 1).swap_uv()
-    ruled1 = ruled1.elevate_degree(SvNurbsSurface.V, target=degree_v)
-    ruled2 = ruled2.elevate_degree(SvNurbsSurface.U, target=degree_u)
-
-    diff_1to2 = sv_knotvector.difference(ruled1.get_knotvector_v(), ruled2.get_knotvector_v())
-    diff_2to1 = sv_knotvector.difference(ruled2.get_knotvector_u(), ruled1.get_knotvector_u())
-
-    for v, count in diff_1to2:
-        ruled1 = ruled1.insert_knot(SvNurbsSurface.V, v, count)
-    for u, count in diff_2to1:
-        ruled2 = ruled2.insert_knot(SvNurbsSurface.U, u, count)
-
-    linear_kv = sv_knotvector.generate(1, 2)
-
-    c1_t_min, c1_t_max = nurbs_curves[0].get_u_bounds()
-    c3_t_min, c3_t_max = nurbs_curves[2].get_u_bounds()
-
-    pt1 = nurbs_curves[0].evaluate(c1_t_min)
-    pt2 = nurbs_curves[0].evaluate(c1_t_max)
-    pt3 = nurbs_curves[2].evaluate(c3_t_min)
-    pt4 = nurbs_curves[2].evaluate(c3_t_max)
-
-    linear_pts = np.array([[pt1,pt3], [pt2,pt4]])
-    linear_weights = np.array([[1,1], [1,1]])
-    bilinear = SvNurbsSurface.build(implementation,
-                1, 1,
-                linear_kv, linear_kv,
-                linear_pts, linear_weights)
-
-    bilinear = bilinear.elevate_degree(SvNurbsSurface.U, target=degree_u)
-    bilinear = bilinear.elevate_degree(SvNurbsSurface.V, target=degree_v)
-
-    knotvector_u = ruled1.get_knotvector_u()
-    knotvector_v = ruled2.get_knotvector_v()
-    for u in sv_knotvector.get_internal_knots(knotvector_u):
-        bilinear = bilinear.insert_knot(SvNurbsSurface.U, u)
-    for v in sv_knotvector.get_internal_knots(knotvector_v):
-        bilinear = bilinear.insert_knot(SvNurbsSurface.V, v)
-
-    control_points = ruled1.get_control_points() + ruled2.get_control_points() - bilinear.get_control_points()
-    weights = ruled1.get_weights() + ruled2.get_weights() - bilinear.get_weights()
-    result = SvNurbsSurface.build(implementation,
-                degree_u, degree_v,
-                knotvector_u, knotvector_v,
-                control_points, weights)
-    return result
 
