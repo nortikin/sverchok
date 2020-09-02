e4852e1a194990efa32d5e81fa0a670ae5ebca49
Ilya Portnov <portnov@bk.ru>
2020-09-02 22:25:56 +0500

    Icon for "ruled solid".

--- nodes/solid/ruled_solid.py
+++ nodes/solid/ruled_solid.py
@@ -59,6 +59,7 @@ class SvRuledSolidNode(bpy.types.Node, SverchCustomTreeNode):
     bl_idname = 'SvRuledSolidNode'
     bl_label = 'Solid from two Faces'
     bl_icon = 'EDGESEL'
+    sv_icon = 'SV_RULED_SOLID'
     solid_catergory = "Operators"
 
     reverse1 : BoolProperty(
