Arc precision fix (#3526)
* Fix precision problems in evaluation of SvCircle curves.

This required a refactoring: `matrix` field was too hard to calculate
precisely, so I replaced it with `normal` + `vectorx` where possible.

* Circle (Curve) node: add "angle units" parameter.

When specifying angle in radians, it is too hard to get precisely closed
circle because of floating-point issues.

* Update documentation.

* Bugfix.

* Add test cases.

* "Fillet Polyline" precision fix.

* Error handling.

nodes/curve/arc_3pt.py
@@ -74,7 +74,10 @@ class SvArc3ptCurveNode(bpy.types.Node, SverchCustomTreeNode):
                     raise Exception("Can't build a circle by these points: {}, {}, {}".format(point1, point2, point3))
                 matrix = circle_data.get_matrix()
                 circle = SvCircle(matrix, circle_data.radius)
-                arc = SvCircle(matrix, circle_data.radius)
+                arc = SvCircle(radius=circle_data.radius,
+                                center=np.array(circle_data.center),
+                                normal=np.array(circle_data.normal),
+                                vectorx = np.array(circle_data.point1) - np.array(circle_data.center))
                 arc.u_bounds = (0.0, circle_data.arc_angle)
                 arcs_new.append(arc)
                 circles_new.append(circle)

tests/geom_tests.py
@@ -1,10 +1,14 @@
 
 import unittest
-from mathutils import Vector
+import numpy as np
+import math
+
+from mathutils import Vector, Matrix
 
 from sverchok.utils.logging import error
 from sverchok.utils.testing import *
 from sverchok.utils.geom import *
+from sverchok.utils.curve.primitives import SvCircle
 
 class GeometryTests(SverchokTestCase):
 
@@ -25,4 +29,39 @@ class GeometryTests(SverchokTestCase):
         output = calc_normal(inputs)
         expected_output = Vector((0, 0, 1))
         self.assertEquals(output, expected_output)
+
+    def test_circle_equal_1(self):
+        circle1 = SvCircle(Matrix(), 1.0)
+        x = np.array([1, 0, 0])
+        origin = np.array([0, 0, 0])
+        z = np.array([0, 0, 1])
+        circle2 = SvCircle(center=origin, normal=z, vectorx=x)
+
+        ts = np.linspace(0, pi/2, num=50)
+
+        pts1 = circle1.evaluate_array(ts)
+        pts2 = circle2.evaluate_array(ts)
+        self.assert_numpy_arrays_equal(pts1, pts2, precision=8)
+
+    def test_arc_values_1(self):
+        pt1 = np.array((-4, 2, 0))
+        pt2 = np.array((0, 2.5, 0))
+        pt3 = np.array((4, 2, 0))
+        eq = circle_by_three_points(pt1, pt2, pt3)
+        circle = SvCircle.from_equation(eq)
+        res1 = circle.evaluate(0)
+        self.assert_numpy_arrays_equal(res1, pt1, precision=6)
+        t_max = eq.arc_angle
+        res2 = circle.evaluate(t_max)
+        self.assert_numpy_arrays_equal(res2, pt3, precision=6)
+
+    def test_arc_derivative_1(self):
+        pt1 = (-5, 0, 0)
+        pt2 = (-4, 3, 0)
+        pt3 = (-3, 4, 0)
+        eq = circle_by_three_points(pt1, pt2, pt3)
+        circle = SvCircle.from_equation(eq)
+        dv = circle.tangent(0)
+        expected = np.array([0, 5, 0])
+        self.assert_numpy_arrays_equal(dv, expected, precision=8)
 

utils/geom.py
@@ -1964,33 +1964,39 @@ def circle_by_three_points(p1, p2, p3):
     factored out from basic_3pt_arc.py.
     """
     v1, v2, v3 = Vector(p1), Vector(p2), Vector(p3)
+    edge1 = v2 - v1
+    edge2 = v3 - v2
     edge1_mid = v1.lerp(v2, 0.5)
     edge2_mid = v2.lerp(v3, 0.5)
-    axis = mathutils.geometry.normal(v1, v2, v3)
-    mat_rot = Matrix.Rotation(math.radians(90.0), 4, axis)
 
-    mid1 = ((v1 - edge1_mid) @ mat_rot) + edge1_mid
-    mid2 = ((v2 - edge1_mid) @ mat_rot) + edge1_mid
-    mid3 = ((v2 - edge2_mid) @ mat_rot) + edge2_mid
-    mid4 = ((v3 - edge2_mid) @ mat_rot) + edge2_mid
-
-    r = mathutils.geometry.intersect_line_line(mid1, mid2, mid3, mid4)
-    if not r:
+    plane0 = PlaneEquation.from_three_points(v1, v2, v3)
+    plane1 = PlaneEquation.from_normal_and_point(edge1, edge1_mid)
+    plane2 = PlaneEquation.from_normal_and_point(edge2, edge2_mid)
+    axis = plane1.intersect_with_plane(plane2)
+    if not axis:
         return None
-    center, _ = r
+    center = plane0.intersect_with_line(axis)
+
+    dv1 = np.array(v1 - center)
+    dv3 = np.array(v3 - center)
+    radius = np.linalg.norm(dv1)
 
     # find arc angle.
-    alpha = (v1 - center).angle((v3 - center), 0)
-    beta = 2 * math.pi - alpha
-
-    interior_angle = (v1 - v2).angle(v3 - v2, 0)
+    e1 = np.array(v1 - v2)
+    e2 = np.array(v3 - v2)
+    cs = e1.dot(e2) / (np.linalg.norm(e1) * np.linalg.norm(e2))
+    interior_angle = np.arccos(cs)
     if interior_angle > 0.5 * math.pi:
         beta = 2*math.pi - 2*interior_angle
+    else:
+        cs = dv1.dot(dv3) / (radius*radius)
+        alpha = np.arccos(cs)
+        beta = 2 * math.pi - alpha
 
     circle = CircleEquation3D()
-    circle.radius = (v1 - center).length
+    circle.radius = radius
     circle.center = center
-    circle.normal = axis
+    circle.normal = axis.direction
     circle.point1 = np.array(v1)
     circle.arc_angle = beta
     return circle
@@ -2122,4 +2128,32 @@ def rotate_vector_around_vector(v, k, theta):
     ct, st = cos(theta), sin(theta)
 
     return ct * v + st * (k.cross(v)) + (1 - ct) * (k.dot(v)) * k
+
+def rotate_vector_around_vector_np(v, k, theta):
+    """
+    Rotate vector v around vector k by theta angle.
+    input: v, k - np.array of shape (3,); theta - float, in radians.
+    output: np.array.
+
+    This implements Rodrigues' formula: https://en.wikipedia.org/wiki/Rodrigues%27_rotation_formula
+    """
+    if not isinstance(v, np.ndarray):
+        v = np.array(v)
+    if not isinstance(k, np.ndarray):
+        k = np.array(k)
+    if k.ndim == 1:
+        k = k[np.newaxis]
+    k = k / np.linalg.norm(k, axis=1)
+
+    if isinstance(theta, np.ndarray):
+        ct, st = np.cos(theta)[np.newaxis].T, np.sin(theta)[np.newaxis].T
+    else:
+        ct, st = cos(theta), sin(theta)
+
+    s1 = ct * v
+    s2 = st * np.cross(k, v)
+    p1 = 1.0 - ct
+    p2 = np.apply_along_axis(lambda vi : k.dot(vi), 1, v)
+    s3 = p1 * p2 * k
+    return s1 + s2 + s3
 

utils/surface/algorithms.py
@@ -16,8 +16,8 @@ from sverchok.utils.math import (
     )
 from sverchok.utils.geom import (
         LineEquation, CircleEquation3D,
-        rotate_vector_around_vector, autorotate_householder,
-         autorotate_track, autorotate_diff
+        rotate_vector_around_vector, rotate_vector_around_vector_np,
+        autorotate_householder, autorotate_track, autorotate_diff
     )
 from sverchok.utils.curve.core import SvFlipCurve, UnsupportedCurveTypeException
 from sverchok.utils.curve.primitives import SvCircle
@@ -30,34 +30,6 @@ from sverchok.utils.surface.core import SvSurface
 from sverchok.utils.surface.nurbs import SvNurbsSurface
 from sverchok.utils.surface.data import *
 from sverchok.utils.logging import info, debug
-
-def rotate_vector_around_vector_np(v, k, theta):
-    """
-    Rotate vector v around vector k by theta angle.
-    input: v, k - np.array of shape (3,); theta - float, in radians.
-    output: np.array.
-
-    This implements Rodrigues' formula: https://en.wikipedia.org/wiki/Rodrigues%27_rotation_formula
-    """
-    if not isinstance(v, np.ndarray):
-        v = np.array(v)
-    if not isinstance(k, np.ndarray):
-        k = np.array(k)
-    if k.ndim == 1:
-        k = k[np.newaxis]
-    k = k / np.linalg.norm(k, axis=1)
-
-    if isinstance(theta, np.ndarray):
-        ct, st = np.cos(theta)[np.newaxis].T, np.sin(theta)[np.newaxis].T
-    else:
-        ct, st = cos(theta), sin(theta)
-
-    s1 = ct * v
-    s2 = st * np.cross(k, v)
-    p1 = 1.0 - ct
-    p2 = np.apply_along_axis(lambda vi : k.dot(vi), 1, v)
-    s3 = p1 * p2 * k
-    return s1 + s2 + s3
 
 class SvInterpolatingSurface(SvSurface):
     __description__ = "Interpolating"