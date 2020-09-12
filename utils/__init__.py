8a1c266712c70b9c54008db66ec631d2a06597f2
Ilya Portnov <portnov@bk.ru>
2020-09-13 00:01:59 +0500

    On "Quad Grid" sorting.

--- utils/__init__.py
+++ utils/__init__.py
@@ -144,4 +144,5 @@ utils_modules = [
     # geom 2d tools
     "geom_2d.lin_alg", "geom_2d.dcel", "geom_2d.dissolve_mesh", "geom_2d.merge_mesh", "geom_2d.intersections",
     "geom_2d.make_monotone", "geom_2d.sort_mesh", "geom_2d.dcel_debugger",
+    "quad_grid"
 ]
