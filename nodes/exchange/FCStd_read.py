70024f39fa7a2db5b06992eb5bbad92ccc36aa81
rastart <8501792+rastart@users.noreply.github.com>
2020-10-27 21:30:11 +0100

    Update FCStd_read.py

--- nodes/exchange/FCStd_read.py
+++ nodes/exchange/FCStd_read.py
@@ -13,29 +13,25 @@ else:
     from sverchok.utils.logging import info
 
     class SvReadFCStdNode(bpy.types.Node, SverchCustomTreeNode):
-    """
-    Triggers: Read FreeCAD file
-    Tooltip: import parts from a .FCStd file 
-    """
+        """
+        Triggers: Read FreeCAD file
+        Tooltip: import parts from a .FCStd file 
+        """
         bl_idname = 'SvReadFCStdNode'
         bl_label = 'Read FCStd'
         bl_icon = 'IMPORT'
         solid_catergory = "Inputs"
         
         read_update : BoolProperty(name="read_update", default=True, update = updateNode )
-        part_filter : StringProperty(name="part_filter", default="", description="use ',' to separate name with no space: part1,part2,... ")
         inv_filter : BoolProperty(name="inv_filter", default=False)
 
         def draw_buttons(self, context, layout):
-
-            layout.label(text="filter by name:")
-
             col = layout.column(align=True)
-            col.prop(self, 'part_filter',text="")
+            col.prop(self, 'read_update') 
             col.prop(self, 'inv_filter')
-            col.prop(self, 'read_update')          
-
+         
         def sv_init(self, context):
+            self.inputs.new('SvStringsSocket', "Part Filter")   
             self.inputs.new('SvFilePathSocket', "File Path")       
             self.outputs.new('SvSolidSocket', "Solid")
 
@@ -45,9 +41,13 @@ else:
             if not self.inputs['File Path'].is_linked:
                 return            
 
-            if self.read_update:
-                
+            if self.read_update:       
                 files = self.inputs['File Path'].sv_get()[0]
+                                               
+                part_filter = []
+                if self.inputs['Part Filter'].is_linked:
+                    part_filter = self.inputs['Part Filter'].sv_get()             
+             
                 solids = []
 
                 for f in files:
@@ -61,17 +61,8 @@ else:
                 return
 
 
-def LoadSolid(fc_file,part_filter,inv_filter):
+def LoadSolid(fc_file, part_filter, inv_filter):
     solids = []
-
-    if ',' in part_filter:
-        part_filter = part_filter.split(',')
-
-    elif not part_filter:
-        part_filter = ['all']
-
-    else:
-        part_filter = [part_filter]
 
     try:
 
@@ -84,9 +75,10 @@ def LoadSolid(fc_file,part_filter,inv_filter):
             if obj.Module in ('Part', 'PartDesign'):
                 
                 if not inv_filter:
-                    if obj.Label in part_filter or 'all' in part_filter:
-                        solids.append(obj.Shape)
 
+                    if obj.Label in part_filter or len(part_filter)==0:
+                        solids.append(obj.Shape)
+                        
                 else:
                     if not obj.Label in part_filter:
                         solids.append(obj.Shape)
