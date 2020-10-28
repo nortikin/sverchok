6751c99c7c7a3008222b2cb65f6569cd5d95fbb9
Victor Doval <10011941+vicdoval@users.noreply.github.com>
2020-10-28 08:44:53 +0100

    link to example of use

--- node_scripts/SNLite_templates/utils/nodetree_transfer.py
+++ node_scripts/SNLite_templates/utils/nodetree_transfer.py
@@ -7,5 +7,6 @@ out data      s
 #transfer data from another Sverchok Nodetree.
 #names can be passed from Note nodes
 #a Socket Converter node may be needed after this node for the data to be accepted by the next nodes
+#example of use at https://user-images.githubusercontent.com/10011941/97406572-b6ef6e00-18f9-11eb-8ea4-3a0e730cc598.png
 if nodetree_name and node_name and input_name:
     data = bpy.data.node_groups[nodetree_name].nodes[node_name].inputs[input_name].sv_get()
