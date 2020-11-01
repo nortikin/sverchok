be10cdeb284eb363fbafd741518ae7b4bf8ef3db
Durman <sv@soluyanov.ru>
2020-11-01 09:49:17 +0300

    Improving Tree structure API

--- tests/tree_structure_tests.py
+++ tests/tree_structure_tests.py
@@ -1,5 +1,3 @@
-from pathlib import Path
-
 import sverchok
 from sverchok.utils.testing import SverchokTestCase
 import sverchok.utils.tree_structure as ts
@@ -19,16 +17,28 @@ class TreeStructureTest(SverchokTestCase):
             tree = ts.Tree(new_tree)
             self.are_trees_equal(new_tree, tree)
 
+            with self.subTest(msg="Detecting changes in tree collections"):
+                new_tree.nodes.remove(tree.nodes['Number Range.001'].bl_tween)
+                changed_tree = ts.Tree(new_tree)
+                self.assertCountEqual(tree.nodes - changed_tree.nodes, (tree.nodes['Number Range.001'], ))
+                self.assertCountEqual(tree.links - changed_tree.links, tree.nodes['Number Range.001'].outputs[0].links)
+                self.assertRaises(TypeError, lambda: tree.nodes - {'Name 1', 'Name 2'})
+
+                tree.nodes['Note'].bl_tween.inputs.remove(tree.nodes['Note'].inputs[0].get_bl_socket(new_tree))
+                self.assertRaises(LookupError, tree.nodes['Note'].inputs[0].get_bl_socket, new_tree)
+
     def are_trees_equal(self, bl_tree, tree: ts.Tree):
         self.assertEqual(len(bl_tree.nodes), len(tree.nodes), msg="Wrong number of nodes")
         self.assertEqual(len(bl_tree.links), len(tree.links), msg="Wrong number of links")
+        self.assertEqual(bl_tree.name, tree.bl_tween.name)
+        self.assertEqual(bl_tree.tree_id, tree.id)
         for i, bl_node in enumerate(bl_tree.nodes):
             with self.subTest(msg=f'Check copy of "{bl_node.name}" node'):
                 node: ts.Node = tree.nodes[bl_node.name]
                 self.assertEqual(bl_node.name, node.name, msg="Node in nodes collection bu wrong name")
                 self.assertEqual(i, node.index)
                 self.assertEqual(bl_node.name, node.get_bl_node(bl_tree).name)
-                self.assertEqual(bl_node.name, node.get_bl_node(bl_tree, by_name=False).name)
+                self.assertEqual(bl_node.name, node.bl_tween.name)
                 self.assertEqual(len(bl_node.inputs), len(node.inputs))
                 self.assertEqual(len(bl_node.outputs), len(node.outputs))
                 for s_i, bl_socket in enumerate(bl_node.inputs):
@@ -47,6 +57,7 @@ class TreeStructureTest(SverchokTestCase):
                     self.assertEqual(socket.index, s_i)
                     self.assertEqual(bl_socket.identifier, socket.get_bl_socket(bl_tree).identifier)
                     self.assertEqual(bl_socket.is_linked, bool(socket.links))
+
         for i, (bl_link, link) in enumerate(zip(bl_tree.links, tree.links)):
             with self.subTest(msg=f'Check copy of link from={bl_link.from_node} to={bl_link.to_node}'):
                 link: ts.Link
@@ -56,7 +67,27 @@ class TreeStructureTest(SverchokTestCase):
                 self.assertEqual(bl_link.from_node.name, link.from_node.name)
                 self.assertEqual(bl_link.to_node.name, link.to_node.name)
 
+        self.assertTrue('Inset faces.001' in tree.nodes)
+        self.assertCountEqual((n.name for n in tree.nodes['Inset faces.001'].next_nodes), ('Mesh viewer', 'Switch.001'))
+        self.assertCountEqual((n.name for n in tree.nodes['Profile Parametric Mk3.001'].last_nodes),
+                              ('Formula.001', 'Number Range.004', 'Number Range.003', 'Number Range.002', 'Formula'))
+        self.assertRaises(LookupError, tree.nodes['Inset faces.001'].get_input_socket, ('False name', ))
+        self.assertRaises(LookupError, tree.nodes['Inset faces.001'].get_output_socket, ('False name', ))
+
+        with self.subTest(msg="Test update method"):
+            self.assertEqual(tree.nodes['Frame'].is_updated, False)
+            tree.nodes['Frame'].update()
+            self.assertEqual(tree.nodes['Frame'].is_updated, True)
+
 
 if __name__ == '__main__':
     import unittest
+    from coverage import Coverage
+    from pathlib import Path
+
+    cov = Coverage()
+    cov.start()
     unittest.main(exit=False)
+    cov.stop()
+    cov.save()
+    cov.html_report(ignore_errors=True)
--- utils/tree_structure.py
+++ utils/tree_structure.py
@@ -9,83 +9,29 @@
 from __future__ import annotations
 
 from collections import Mapping
-from typing import List, Iterable
+from typing import List, Iterable, TypeVar, TYPE_CHECKING, Dict, Any, Generic
 
 import bpy
 
 import sverchok.utils.tree_walk as tw
 
-
-class Tree(tw.Tree):
-    """
-    Structure similar to blender node groups but is more efficient in searching neighbours
-    Each time when nodes or links collections are changed the instance of the tree should be recreated
-    so it is immutable data structure
-    """
-    def __init__(self, bl_tree: bpy.types.NodeTree):
-        self._nodes = NodesCollection(bl_tree)
-        self._links = LinksCollection(bl_tree, self)
-
-    @property
-    def nodes(self) -> NodesCollection:
-        return self._nodes
-
-    @property
-    def links(self) -> LinksCollection:
-        return self._links
-
-
-class TreeCollections(Mapping):
-    """
-    The idea of this collection is to have access to its elements by their identifier
-    and meantime to have access to their indexes of true blender collections
-    so to get fast mapping between python and blender collections
-    downside is that this collection is immutable
-    because it impossible to predict order of Blender collection after changing of their content
-    """
-    def __init__(self):
-        self._dict = dict()
-
-    def __getitem__(self, item):
-        return self._dict[item]
-
-    def __iter__(self):
-        return iter(self._dict.values())
-
-    def __len__(self):
-        return len(self._dict)
-
-    def __contains__(self, item):
-        return item in self._dict
-
-
-class NodesCollection(TreeCollections):
-    def __init__(self, bl_tree: bpy.types.NodeTree):
-        super().__init__()
-        for i, bl_node in enumerate(bl_tree.nodes):
-            self._dict[bl_node.name] = Node.from_bl_node(bl_node, i)
-
-
-class LinksCollection(TreeCollections):
-    def __init__(self, bl_tree: bpy.types.NodeTree, tree: Tree):
-        super().__init__()
-        for i, bl_link in enumerate(bl_tree.links):
-            from_node = tree.nodes[bl_link.from_node.name]
-            from_socket = from_node.get_output_socket(bl_link.from_socket.identifier)
-            to_node = tree.nodes[bl_link.to_node.name]
-            to_socket = to_node.get_input_socket(bl_link.to_socket.identifier)
-
-            self._dict[(bl_link.from_node.name, bl_link.from_socket.identifier,
-                        bl_link.to_node.name, bl_link.to_socket.identifier)] = Link(from_socket, to_socket, i)
+if TYPE_CHECKING:
+    from sverchok.core.node_group import SvGroupTree
 
 
 class Node(tw.Node):
-    def __init__(self, name: str, index: int):
+    def __init__(self, name: str, index: int, tree: Tree):
         self.name = name
         self.select = False  # todo consider to remove
-        self._inputs = []
-        self._outputs = []
+        self.is_updated = False
+        self._inputs: List[Socket] = []
+        self._outputs: List[Socket] = []
         self._index = index
+        self._tree = tree
+
+    @property
+    def bl_tween(self) -> bpy.types.Node:
+        return self._tree.bl_tween.nodes[self._index]
 
     @property
     def index(self):
@@ -93,11 +39,11 @@ class Node(tw.Node):
         return self._index
 
     @property
-    def inputs(self) -> list:
+    def inputs(self) -> List[Socket]:
         return self._inputs
 
     @property
-    def outputs(self) -> list:
+    def outputs(self) -> List[Socket]:
         return self._outputs
 
     @property
@@ -110,12 +56,12 @@ class Node(tw.Node):
         """Returns all nodes which are linked wia the node input sockets"""
         return {other_s.node for s in self.inputs for other_s in s.linked_sockets}
 
-    def get_bl_node(self, tree: bpy.types.NodeTree, by_name=True) -> bpy.types.Node:
+    def get_bl_node(self, tree: SvGroupTree) -> bpy.types.Node:
         """
         Will return the node from given tree with the same name
-        if by_name is False it will use node index instead what will be faster
+        It is slower then `bl_tween` attribute but can return node from another given tree
         """
-        return tree.nodes[self.name if by_name else self.index]
+        return tree.nodes[self.name]
 
     def get_input_socket(self, identifier: str) -> Socket:
         """Search input socket by its identifier"""
@@ -132,9 +78,9 @@ class Node(tw.Node):
         raise LookupError(f'Socket "{identifier}" was not found in Node "{self.name}" outputs{self._outputs}')
 
     @classmethod
-    def from_bl_node(cls, bl_node: bpy.types.Node, index: int) -> Node:
+    def from_bl_node(cls, bl_node: bpy.types.Node, index: int, tree: Tree) -> Node:
         """Generate node and its sockets from Blender node instance"""
-        node = cls(bl_node.name, index)
+        node = cls(bl_node.name, index, tree)
         node.select = bl_node.select
         for in_socket in bl_node.inputs:
             node.inputs.append(Socket.from_bl_socket(node, in_socket))
@@ -142,8 +88,105 @@ class Node(tw.Node):
             node.outputs.append(Socket.from_bl_socket(node, out_socket))
         return node
 
+    def update(self):
+        bl_node = self.bl_tween
+        if hasattr(bl_node, 'process'):
+            bl_node.process()
+        self.is_updated = True
+
     def __repr__(self):
         return f'Node:"{self.name}"'
+
+
+NodeType = TypeVar('NodeType', bound=Node)
+
+
+class Tree(tw.Tree[NodeType]):
+    """
+    Structure similar to blender node groups but is more efficient in searching neighbours
+    Each time when nodes or links collections are changed the instance of the tree should be recreated
+    so it is immutable data structure
+    """
+    def __init__(self, bl_tree: SvGroupTree):
+        self._tree_id = bl_tree.tree_id
+        self._nodes = NodesCollection(bl_tree, self)
+        self._links = LinksCollection(bl_tree, self)
+
+        for i, tree in enumerate(bpy.data.node_groups):
+            if tree.name == bl_tree.name:
+                self._index = i
+                break
+
+    @property
+    def id(self) -> str:
+        return self._tree_id
+
+    @property
+    def bl_tween(self) -> SvGroupTree:
+        return bpy.data.node_groups[self._index]
+
+    @property
+    def nodes(self) -> NodesCollection:
+        return self._nodes
+
+    @property
+    def links(self) -> LinksCollection:
+        return self._links
+
+
+Element = TypeVar('Element')
+
+
+class TreeCollections(Mapping, Generic[Element]):
+    """
+    The idea of this collection is to have access to its elements by their identifier
+    and meantime to have access to their indexes of true blender collections
+    so to get fast mapping between python and blender collections
+    downside is that this collection is immutable
+    because it impossible to predict order of Blender collection after changing of their content
+    """
+    def __init__(self):
+        self._dict: Dict[Any, Element] = dict()
+
+    def __getitem__(self, item) -> Element:
+        return self._dict[item]
+
+    def __iter__(self) -> Iterable[Element]:
+        return iter(self._dict.values())
+
+    def __len__(self):
+        return len(self._dict)
+
+    def __contains__(self, item):
+        return item in self._dict
+
+    def __sub__(self, other) -> List[Element]:
+        if isinstance(other, TreeCollections):
+            return [self._dict[k] for k in (self._dict.keys() - other._dict.keys())]
+        else:
+            return NotImplemented
+
+
+class NodesCollection(TreeCollections[NodeType]):
+    def __init__(self, bl_tree: SvGroupTree, tree: Tree):
+        super().__init__()
+        for i, bl_node in enumerate(bl_tree.nodes):
+            self._dict[bl_node.name] = Node.from_bl_node(bl_node, i, tree)
+
+
+class LinksCollection(TreeCollections):
+    def __init__(self, bl_tree: SvGroupTree, tree: Tree):
+        super().__init__()
+        for i, bl_link in enumerate(bl_tree.links):
+            from_node = tree.nodes[bl_link.from_node.name]
+            from_socket = from_node.get_output_socket(bl_link.from_socket.identifier)
+            to_node = tree.nodes[bl_link.to_node.name]
+            to_socket = to_node.get_input_socket(bl_link.to_socket.identifier)
+
+            self._dict[(bl_link.from_node.name, bl_link.from_socket.identifier,
+                        bl_link.to_node.name, bl_link.to_socket.identifier)] = Link(from_socket, to_socket, i)
+
+    def __iter__(self) -> Iterable[Link]: return super().__iter__()
 
 
 class Socket:
@@ -166,7 +209,7 @@ class Socket:
     def links(self) -> list:
         return self._links
 
-    def get_bl_socket(self, bl_tree: bpy.types.NodeTree) -> bpy.types.NodeSocket:
+    def get_bl_socket(self, bl_tree: SvGroupTree) -> bpy.types.NodeSocket:
         """Search socket in given tree by its identifier"""
         bl_node = self.node.get_bl_node(bl_tree)
         for bl_socket in bl_node.outputs if self.is_output else bl_node.inputs:
