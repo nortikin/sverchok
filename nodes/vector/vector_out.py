7dad0fcbe1af5f124058d125281e3ed517e1a45c
Victor Doval <10011941+vicdoval@users.noreply.github.com>
2020-11-05 22:09:11 +0100

    Revert "Vector In and Vector Out to multilevel recursive (#3673)"
    This reverts commit a95871e3a5a77b13e63fda4654a07a638f6ab19d.

--- nodes/vector/vector_out.py
+++ nodes/vector/vector_out.py
@@ -17,77 +17,29 @@
 # ##### END GPL LICENSE BLOCK #####
 
 import bpy
-from bpy.props import BoolProperty, EnumProperty
+from bpy.props import BoolProperty
 from sverchok.node_tree import SverchCustomTreeNode
-from sverchok.data_structure import dataCorrect_np, updateNode, levels_of_list_or_np
+from sverchok.data_structure import dataCorrect_np, updateNode
 from numpy import ndarray, array
 
 def unpack_np(obj):
     return (obj[:, 0], obj[:, 1], obj[:, 2])
 
-
 def unpack_list(obj):
     return (list(x) for x in zip(*obj))
-
 
 def unpack_list_to_np(obj):
     return (array(x) for x in zip(*obj))
 
-
-def unpack(data, output_numpy):
-    X, Y, Z = [], [], []
-    if output_numpy:
-        unpack_func = unpack_np if isinstance(data[0], ndarray) else unpack_list_to_np
-    else:
-        unpack_func = unpack_list
-    for obj in data:
-        print(obj)
-        x_, y_, z_ = unpack_func(obj)
-        X.append(x_)
-        Y.append(y_)
-        Z.append(z_)
-    return X, Y, Z
-
-
-def unpack_multilevel(data, level, output_numpy):
-    if level > 3:
-        X, Y, Z = [], [], []
-        for obj in data:
-
-            x,y,z = unpack_multilevel(obj, level-1, output_numpy)
-            X.append(x)
-            Y.append(y)
-            Z.append(z)
-    elif level < 3:
-        X, Y, Z = unpack_multilevel([data], level+1, output_numpy)
-    else:
-        X,Y,Z = unpack(data, output_numpy)
-
-    return X,Y,Z
-
-
 class VectorsOutNode(bpy.types.Node, SverchCustomTreeNode):
-    """
-    Triggers: Separate XYZ
-    Tooltip:  Split X, Y and Z components from vector.
-    """
+    ''' Vectors out '''
     bl_idname = 'VectorsOutNode'
     bl_label = 'Vector out'
     sv_icon = 'SV_VECTOR_OUT'
-
     output_numpy: BoolProperty(
         name='Output NumPy',
         description='Output NumPy arrays',
         default=False, update=updateNode)
-    correct_output_modes = [
-        ('NONE', 'None', 'Leave at multi-object level (Advanced)', 0),
-        ('FLAT', 'Flat Output', 'Flat to object level', 2),
-    ]
-    correct_output: EnumProperty(
-        name="Simplify Output",
-        description="Behavior on different list lengths, object level",
-        items=correct_output_modes, default="FLAT",
-        update=updateNode)
 
     def sv_init(self, context):
         self.inputs.new('SvVerticesSocket', "Vectors")
@@ -98,28 +50,28 @@ class VectorsOutNode(bpy.types.Node, SverchCustomTreeNode):
 
     def draw_buttons_ext(self, ctx, layout):
         layout.prop(self, "output_numpy", toggle=False)
-        layout.prop(self, "correct_output")
 
     def rclick_menu(self, context, layout):
         layout.prop(self, "output_numpy", toggle=True)
-        layout.prop_menu_enum(self, 'correct_output')
 
     def process(self):
-        if not (self.inputs['Vectors'].is_linked and any(s.is_linked for s in self.outputs)):
-            return
+        if self.inputs['Vectors'].is_linked and any(s.is_linked for s in self.outputs):
+            xyz = self.inputs['Vectors'].sv_get(deepcopy=False)
 
-        vectors = self.inputs['Vectors'].sv_get(deepcopy=False)
-
-        if self.correct_output == 'FLAT':
-            data = dataCorrect_np(vectors)
-            xyz = unpack(data, self.output_numpy)
-        else:
-            input_level = levels_of_list_or_np(vectors)
-            xyz = unpack_multilevel(vectors, input_level, self.output_numpy)
-
-        for i, name in enumerate(['X', 'Y', 'Z']):
-            if self.outputs[name].is_linked:
-                self.outputs[name].sv_set(xyz[i])
+            data = dataCorrect_np(xyz)
+            X, Y, Z = [], [], []
+            if self.output_numpy:
+                unpack_func = unpack_np if isinstance(data[0], ndarray) else unpack_list_to_np
+            else:
+                unpack_func = unpack_list
+            for obj in data:
+                x_, y_, z_ = unpack_func(obj)
+                X.append(x_)
+                Y.append(y_)
+                Z.append(z_)
+            for i, name in enumerate(['X', 'Y', 'Z']):
+                if self.outputs[name].is_linked:
+                    self.outputs[name].sv_set([X, Y, Z][i])
 
 
 def register():
