b078593aa131ff52d99355e6ffb769570eefbfb0
Ilya Portnov <portnov@bk.ru>
2020-11-08 22:42:12 +0500

    Minor updates.

--- utils/surface/nurbs.py
+++ utils/surface/nurbs.py
@@ -903,7 +903,7 @@ def nurbs_sweep(path, profiles, ts, min_profiles, algorithm, knots_u = 'UNIFY', 
                 knots_u=knots_u, metric=metric,
                 implementation=implementation)
 
-def nurbs_birail(path1, path2, profiles, ts, min_profiles, knots_u = 'UNIFY', degree_v = None, metric = 'DISTANCE', scale_uniform = True, implementation = SvNurbsSurface.NATIVE):
+def nurbs_birail(path1, path2, profiles, ts1 = None, ts2 = None, min_profiles = 10, knots_u = 'UNIFY', degree_v = None, metric = 'DISTANCE', scale_uniform = True, implementation = SvNurbsSurface.NATIVE):
     """
     NURBS BiRail.
 
@@ -934,31 +934,37 @@ def nurbs_birail(path1, path2, profiles, ts, min_profiles, knots_u = 'UNIFY', de
     """
 
     n_profiles = len(profiles)
-    have_ts = ts is not None and len(ts) > 0
-    if have_ts and n_profiles != len(ts):
-        raise Exception(f"Number of profiles ({n_profiles}) is not equal to number of T values ({len(ts)})")
+    have_ts1 = ts1 is not None and len(ts1) > 0
+    have_ts2 = ts2 is not None and len(ts2) > 0
+    if have_ts1 and n_profiles != len(ts1):
+        raise Exception(f"Number of profiles ({n_profiles}) is not equal to number of T values ({len(ts1)})")
+    if have_ts2 and n_profiles != len(ts2):
+        raise Exception(f"Number of profiles ({n_profiles}) is not equal to number of T values ({len(ts2)})")
 
     if degree_v is None:
         degree_v = path1.get_degree()
 
     t_min_1, t_max_1 = path1.get_u_bounds()
     t_min_2, t_max_2 = path2.get_u_bounds()
-    if not have_ts:
+    if not have_ts1:
         ts1 = np.linspace(t_min_1, t_max_1, num=n_profiles)
+    if not have_ts2:
         ts2 = np.linspace(t_min_2, t_max_2, num=n_profiles)
 
     if n_profiles == 1:
         p = profiles[0]
         profiles = [p] * min_profiles
-        if not have_ts:
+        if not have_ts1:
             ts1 = np.linspace(t_min_1, t_max_1, num=min_profiles)
+        if not have_ts2:
             ts2 = np.linspace(t_min_2, t_max_2, num=min_profiles)
     elif n_profiles == 2 and n_profiles < min_profiles:
         coeffs = np.linspace(0.0, 1.0, num=min_profiles)
         p0, p1 = profiles
         profiles = [p0.lerp_to(p1, coeff) for coeff in coeffs]
-        if not have_ts:
+        if not have_ts1:
             ts1 = np.linspace(t_min_1, t_max_1, num=min_profiles)
+        if not have_ts2:
             ts2 = np.linspace(t_min_2, t_max_2, num=min_profiles)
     elif n_profiles < min_profiles:
         target_vs = np.linspace(0.0, 1.0, num=min_profiles)
@@ -967,8 +973,9 @@ def nurbs_birail(path1, path2, profiles, ts, min_profiles, knots_u = 'UNIFY', de
                     degree_v = min(max_degree, degree_v),
                     knots_u = knots_u,
                     implementation = implementation)
-        if not have_ts:
+        if not have_ts1:
             ts1 = np.linspace(t_min_1, t_max_1, num=min_profiles)
+        if not have_ts2:
             ts2 = np.linspace(t_min_2, t_max_2, num=min_profiles)
     else:
         profiles = repeat_last_for_length(profiles, min_profiles)
