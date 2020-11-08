6e072532e078dab45efa5810bda3482e1a3c0b9b
Ilya Portnov <portnov@bk.ru>
2020-11-08 20:52:56 +0500

    Nurbs Birail API: option for non-uniform scaling.

--- utils/surface/nurbs.py
+++ utils/surface/nurbs.py
@@ -903,7 +903,7 @@ def nurbs_sweep(path, profiles, ts, min_profiles, algorithm, knots_u = 'UNIFY', 
                 knots_u=knots_u, metric=metric,
                 implementation=implementation)
 
-def nurbs_birail(path1, path2, profiles, ts, min_profiles, knots_u = 'UNIFY', metric = 'DISTANCE', implementation = SvNurbsSurface.NATIVE):
+def nurbs_birail(path1, path2, profiles, ts, min_profiles, knots_u = 'UNIFY', metric = 'DISTANCE', scale_uniform = True, implementation = SvNurbsSurface.NATIVE):
 
     n_profiles = len(profiles)
     have_ts = ts is not None and len(ts) > 0
@@ -918,9 +918,9 @@ def nurbs_birail(path1, path2, profiles, ts, min_profiles, knots_u = 'UNIFY', me
 
     if n_profiles == 1:
         p = profiles[0]
+        profiles = [p] * min_profiles
         ts1 = np.linspace(t_min_1, t_max_1, num=min_profiles)
         ts2 = np.linspace(t_min_2, t_max_2, num=min_profiles)
-        profiles = [p] * min_profiles
     elif n_profiles == 2 and n_profiles < min_profiles:
         coeffs = np.linspace(0.0, 1.0, num=min_profiles)
         p0, p1 = profiles
@@ -951,8 +951,10 @@ def nurbs_birail(path1, path2, profiles, ts, min_profiles, knots_u = 'UNIFY', me
     scales = np.linalg.norm(binormals, axis=1, keepdims=True)
     binormals /= scales
     normals = np.cross(tangents, binormals)
+    normals /= np.linalg.norm(normals, axis=1, keepdims=True)
 
     tangents = np.cross(binormals, normals)
+    tangents /= np.linalg.norm(tangents, axis=1, keepdims=True)
 
     matrices = np.dstack((normals, binormals, tangents))
     matrices = np.transpose(matrices, axes=(0,2,1))
@@ -975,13 +977,28 @@ def nurbs_birail(path1, path2, profiles, ts, min_profiles, knots_u = 'UNIFY', me
             ])
         rotations.append(matrix)
 
+    scales = scales.flatten()
     placed_profiles = []
     for pt1, profile, scale, rotation, matrix in zip(points1, profiles, scales, rotations, matrices):
         t_min, t_max = profile.get_u_bounds()
         pr_start = profile.evaluate(t_min)
         pr_end = profile.evaluate(t_max)
         pr_length = np.linalg.norm(pr_end - pr_start)
-        cpts = [scale * (matrix @ rotation @ (pt - pr_start)) / pr_length + pt1 for pt in profile.get_control_points()]
+        scale /= pr_length
+        if scale_uniform:
+            scale_m = np.array([
+                    (scale, 0, 0),
+                    (0, scale, 0),
+                    (0, 0, scale)
+                ])
+        else:
+            scale_m = np.array([
+                    (1, 0, 0),
+                    (0, scale, 0),
+                    (0, 0, 1)
+                ])
+        cpts = [matrix @ scale_m @ rotation @ (pt - pr_start) + pt1 for pt in profile.get_control_points()]
+        cpts = np.array(cpts)
         profile = profile.copy(control_points = cpts)
         placed_profiles.append(profile)
 
