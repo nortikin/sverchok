e596601d8e5be43bc5f78cf33fe33891233f880f
DolphinDream <marius.giurgi@gmail.com>
2020-11-11 10:38:18 -0500

    Various renaming

--- nodes/transforms/3d_project.py
+++ nodes/transforms/3d_project.py
@@ -36,32 +36,32 @@ projection_type_items = [
     ("PERSPECTIVE",  "Perspective",  "Perspective projection", 0),
     ("ORTHOGRAPHIC", "Orthographic", "Orthographic projection", 1)]
 
-idMat = [[tuple(v) for v in Matrix()]]  # identity matrix
+id_mat = [[tuple(v) for v in Matrix()]]  # identity matrix
 
 EPSILON = 1e-10
 
 
-def projection_planar(verts3D, m, d, perspective):
+def projection_planar(verts_3D, m, d, perspective):
     """
-    Project the 3D verts onto a plane.
-     verts3D : vertices to project (perspective or ortographic)
-           m : transformation matrix of the projection plane (location & rotation)
-           d : distance between the projection point (focus) and the projection plane
+    Project the 3D verts onto a planar screen
+     verts_3D : vertices to project (perspective or ortographic)
+            m : transformation matrix of the projection plane (location & rotation)
+            d : distance between the projection point (focus) and the projection plane
     """
     ox, oy, oz = [m[0][3], m[1][3], m[2][3]]  # projection plane origin
     nx, ny, nz = [m[0][2], m[1][2], m[2][2]]  # projection plane normal
 
     # d = d if perspective else 1e10
 
-    vertList = []
-    for vert in verts3D:
+    vert_list = []
+    for vert in verts_3D:
         x, y, z = vert
         # vector relative to the plane origin (V-O)
         dx = x - ox
         dy = y - oy
         dz = z - oz
         # magnitude of the vector projected parallel to the plane normal
-        an = dx * nx + dy * ny + dz * nz
+        an = dx * nx + dy * ny + dz * nz + EPSILON
         # factor to scale the vector to touch the plane
         s = d / (d + an)
         # extended vector touching the plane
@@ -69,7 +69,7 @@ def projection_planar(verts3D, m, d, perspective):
         py = oy + s * (dy - an * ny)
         pz = oz + s * (dz - an * nz)
 
-        vertList.append([px, py, pz])
+        vert_list.append([px, py, pz])
 
     # Focus location m * D:
     #  Xx Yx Zx Tx        0     Tx - d * Zx
@@ -78,22 +78,22 @@ def projection_planar(verts3D, m, d, perspective):
     #  0  0  0  1         1     1
     focus = [[ox - d * nx, oy - d * ny, oz - d * nz]]
 
-    return vertList, focus
+    return vert_list, focus
 
 
-def projection_cylindrical(verts3D, m, d, perspective):
+def projection_cylindrical(verts_3D, m, d, perspective):
     """
-    Project the 3D verts onto a cylinder.
-     verts3D : vertices to project (perspective)
-           m : transformation matrix of the projection cylinder (location & rotation)
-           d : distance between the projection point (focus) and the projection cylinder (cylinder radius)
+    Project the 3D verts onto a cylinderical screen
+     verts_3D : vertices to project (perspective)
+            m : transformation matrix of the projection cylinder (location & rotation)
+            d : distance between the projection point (focus) and the projection cylinder (cylinder radius)
 
     """
     ox, oy, oz = [m[0][3], m[1][3], m[2][3]]  # projection cylinder origin
     nx, ny, nz = [m[0][2], m[1][2], m[2][2]]  # projection cylinder axis
 
-    vertList = []
-    for vert in verts3D:
+    vert_list = []
+    for vert in verts_3D:
         x, y, z = vert
         # vector relative to the cylinder origin (V-O)
         dx = x - ox
@@ -114,24 +114,24 @@ def projection_cylindrical(verts3D, m, d, perspective):
         yy = oy + dy * s
         zz = oz + dz * s
 
-        vertList.append([xx, yy, zz])
+        vert_list.append([xx, yy, zz])
 
     focus = [[ox, oy, oz]]
 
-    return vertList, focus
+    return vert_list, focus
 
 
-def projection_spherical(verts3D, m, d, perspective):
+def projection_spherical(verts_3D, m, d, perspective):
     """
-    Project the 3D verts onto a sphere.
-     verts3D : vertices to project (perspective)
-           m : transformation matrix of the projection sphere (location & rotation)
-           d : distance between the projection point (focus and the projection sphere (sphere radius)
+    Project the 3D verts onto a spherical screen
+     verts_3D : vertices to project (perspective)
+            m : transformation matrix of the projection sphere (location & rotation)
+            d : distance between the projection point (focus and the projection sphere (sphere radius)
     """
     ox, oy, oz = [m[0][3], m[1][3], m[2][3]]  # projection sphere origin
 
-    vertList = []
-    for vert in verts3D:
+    vert_list = []
+    for vert in verts_3D:
         x, y, z = vert
         # vector relative to the sphere origin (V-O)
         dx = x - ox
@@ -144,11 +144,11 @@ def projection_spherical(verts3D, m, d, perspective):
         yy = oy + dy * d/r
         zz = oz + dz * d/r
 
-        vertList.append([xx, yy, zz])
+        vert_list.append([xx, yy, zz])
 
     focus = [[ox, oy, oz]]
 
-    return vertList, focus
+    return vert_list, focus
 
 
 class Sv3DProjectNode(bpy.types.Node, SverchCustomTreeNode):
@@ -159,29 +159,29 @@ class Sv3DProjectNode(bpy.types.Node, SverchCustomTreeNode):
     bl_idname = 'Sv3DProjectNode'
     bl_label = '3D Projection'
 
-    projection_screen = EnumProperty(
+    projection_screen: EnumProperty(
         name="Screen", items=projection_screen_items, default="PLANAR", update=updateNode)
 
-    projection_type = EnumProperty(
+    projection_type: EnumProperty(
         name="Type", items=projection_type_items, default="PERSPECTIVE", update=updateNode)
 
-    distance = FloatProperty(
+    distance: FloatProperty(
         name="Distance", description="Projection Distance", default=2.0, update=updateNode)
 
     def sv_init(self, context):
         self.width = 180
-        self.inputs.new('VerticesSocket', "Verts")
-        self.inputs.new('StringsSocket', "Edges")
-        self.inputs.new('StringsSocket', "Polys")
+        self.inputs.new('SvVerticesSocket', "Verts")
+        self.inputs.new('SvStringsSocket', "Edges")
+        self.inputs.new('SvStringsSocket', "Polys")
         # projection screen location and orientation
-        self.inputs.new('MatrixSocket', "Matrix")
+        self.inputs.new('SvMatrixSocket', "Matrix")
         # distance from the projection point to the projection screen
-        self.inputs.new('StringsSocket', "D").prop_name = 'distance'
+        self.inputs.new('SvStringsSocket', "D").prop_name = 'distance'
 
-        self.outputs.new('VerticesSocket', "Verts")
-        self.outputs.new('StringsSocket', "Edges")
-        self.outputs.new('StringsSocket', "Polys")
-        self.outputs.new('VerticesSocket', "Focus")
+        self.outputs.new('SvVerticesSocket', "Verts")
+        self.outputs.new('SvStringsSocket', "Edges")
+        self.outputs.new('SvStringsSocket', "Polys")
+        self.outputs.new('SvVerticesSocket', "Focus")
 
     def draw_buttons(self, context, layout):
         layout.prop(self, "projection_screen", text="")
@@ -213,7 +213,7 @@ class Sv3DProjectNode(bpy.types.Node, SverchCustomTreeNode):
         else:
             input_p = [[]]
 
-        input_m = inputs["Matrix"].sv_get(default=idMat)
+        input_m = inputs["Matrix"].sv_get(default=id_mat)
 
         input_d = inputs["D"].sv_get()[0]
 
@@ -228,25 +228,25 @@ class Sv3DProjectNode(bpy.types.Node, SverchCustomTreeNode):
 
         perspective = self.projection_type == "PERSPECTIVE"
 
-        vertList = []
-        edgeList = []
-        polyList = []
-        focusList = []
+        vert_list = []
+        edge_list = []
+        poly_list = []
+        focus_list = []
         for v, e, p, m, d in zip(*params):
             verts, focus = projector(v, m, d, perspective)
-            vertList.append(verts)
-            edgeList.append(e)
-            polyList.append(p)
-            focusList.append(focus)
+            vert_list.append(verts)
+            edge_list.append(e)
+            poly_list.append(p)
+            focus_list.append(focus)
 
         if outputs["Verts"].is_linked:
-            outputs["Verts"].sv_set(vertList)
+            outputs["Verts"].sv_set(vert_list)
         if outputs["Edges"].is_linked:
-            outputs["Edges"].sv_set(edgeList)
+            outputs["Edges"].sv_set(edge_list)
         if outputs["Polys"].is_linked:
-            outputs["Polys"].sv_set(polyList)
+            outputs["Polys"].sv_set(poly_list)
         if outputs["Focus"].is_linked:
-            outputs["Focus"].sv_set(focusList)
+            outputs["Focus"].sv_set(focus_list)
 
 
 def register():
