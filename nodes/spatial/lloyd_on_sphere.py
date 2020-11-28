6caef5a92a5d232f88584c2ea1f38db95bf38aa1
Ilya Portnov <portnov@bk.ru>
2020-11-29 00:35:21 +0500

    Lloyd on sphere: fix nesting. [skip ci]

--- nodes/spatial/lloyd_on_sphere.py
+++ nodes/spatial/lloyd_on_sphere.py
@@ -70,7 +70,7 @@ class SvLloydOnSphereNode(bpy.types.Node, SverchCustomTreeNode):
         if self.inputs['Weights'].is_linked:
             weights_in = ensure_nesting_level(weights_in, 2, data_types=(SvScalarField,))
 
-        nested_output = input_level > 1
+        nested_output = input_level > 3
 
         verts_out = []
         for params in zip_long_repeat(center_in, radius_in, sites_in, iterations_in, weights_in):
