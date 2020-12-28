34391ae920ad860082fa5447d8a565341605cbb5
Ilya V. Portnov <portnov@bk.ru>
2020-12-28 11:25:38 +0500

    Fix.

--- ui/nodeview_space_menu.py
+++ ui/nodeview_space_menu.py
@@ -363,7 +363,7 @@ def unregister():
     for class_name in classes:
         bpy.utils.unregister_class(class_name)
     for category in presets.get_category_names():
-        if category in preset_category_menus[category]:
+        if category in preset_category_menus:
             bpy.utils.unregister_class(preset_category_menus[category])
 
     menu_class_by_title = dict()
