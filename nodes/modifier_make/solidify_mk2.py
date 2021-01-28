4fc7eb8633628dfdf4ec0e0578710ae1c3201c23
Victor Doval <10011941+vicdoval@users.noreply.github.com>
2021-01-28 09:20:40 +0100

    refactoring

--- nodes/modifier_make/solidify_mk2.py
+++ nodes/modifier_make/solidify_mk2.py
@@ -21,20 +21,35 @@ from bpy.props import FloatProperty, BoolProperty, EnumProperty
 import bmesh
 from itertools import cycle
 from sverchok.node_tree import SverchCustomTreeNode
-from sverchok.data_structure import updateNode, zip_long_repeat, enum_item_4
+from sverchok.data_structure import updateNode, zip_long_repeat, enum_item_4, numpy_full_list_cycle
 from sverchok.utils.sv_bmesh_utils import bmesh_from_pydata
+import numpy as np
 # by Linus Yng
 
-def create_edges(bm, v_len):
-    edges =[]
-    for edge in bm.edges[:]:
-        edges.append([v.index for v in edge.verts[:]])
-
+def create_edges(bm, edges, v_len):
+    if not edges:
+        edges = [[v.index for v in edge.verts[:]] for edge in bm.edges]
     edges_new = [[e[0]+v_len, e[1]+v_len] for e in edges]
     side_edges = [[v.index, v.index+v_len] for v in bm.verts if v.is_boundary]
-    return edges + edges_new +side_edges
+    return edges + edges_new + side_edges
 
-def solidify(vertices, faces, thickness, offset=None, even=True):
+def create_even_verts(bm_verts, thickness, offset):
+    new_verts = [(v.co + v.normal * t * v.calc_shell_factor() *((-o/2)+0.5))[:]
+                 for v, t, o in zip(bm_verts, cycle(thickness), cycle(offset))]
+
+    old_verts = [(v.co - v.normal * t * v.calc_shell_factor() * ((o/2)+0.5))[:]
+                 for v, t, o in zip(bm_verts, cycle(thickness), cycle(offset))]
+    return new_verts, old_verts
+
+def create_uneven_verts(bm_verts, thickness, offset):
+    new_verts = [(v.co + v.normal * t * ((-o/2) + 0.5))[:]
+                 for v, t, o in zip(bm_verts, cycle(thickness), cycle(offset))]
+    old_verts = [(v.co - v.normal * t * ((o/2) + 0.5))[:]
+                 for v, t, o in zip(bm_verts, cycle(thickness), cycle(offset))]
+
+    return new_verts, old_verts
+
+def solidify(vertices, edges, faces, thickness, offset=None, even=True, output_edges=True):
 
     if not faces or not vertices:
         return False
@@ -48,45 +63,46 @@ def solidify(vertices, faces, thickness, offset=None, even=True):
     verlen = set(range(len(vertices)))
 
     bm = bmesh_from_pydata(vertices, [], faces, normal_update=True)
-    geom_in = bm.verts[:] + bm.edges[:] + bm.faces[:]
 
     if len(offset) == 1 and offset == -1:
         if even:
-            new_verts = [(v.co+ v.normal * t * v.calc_shell_factor())[:] for v, t in zip(bm.verts, cycle(thickness))]
+            new_verts = [(v.co + v.normal * t * v.calc_shell_factor())[:]
+                         for v, t in zip(bm.verts, cycle(thickness))]
         else:
-            new_verts = [(v.co+ v.normal*t)[:] for v, t in zip(bm.verts, cycle(thickness))]
+            new_verts = [(v.co + v.normal * t)[:] for v, t in zip(bm.verts, cycle(thickness))]
 
         vertices_out = vertices +  new_verts
     else:
         if even:
-            new_verts=[(v.co + v.normal * t * v.calc_shell_factor() *((-o/2)+0.5))[:] for v, t, o in zip(bm.verts, cycle(thickness), cycle(offset))]
-            old_verts=[(v.co - v.normal * t * v.calc_shell_factor() * ((o/2)+0.5))[:] for v, t, o in zip(bm.verts, cycle(thickness), cycle(offset))]
-
+            new_verts, old_verts = create_even_verts(bm.verts, thickness, offset)
         else:
-            new_verts=[(v.co + v.normal * t * ((-o/2) + 0.5))[:] for v, t, o in zip(bm.verts, cycle(thickness), cycle(offset))]
-            old_verts=[(v.co - v.normal * t * ((o/2) + 0.5))[:] for v, t, o in zip(bm.verts, cycle(thickness), cycle(offset))]
+            new_verts, old_verts = create_uneven_verts(bm.verts, thickness, offset)
 
         vertices_out = old_verts +  new_verts
 
     v_len = len(vertices)
-    new_pols = [[c+v_len for c in p] for p in faces]
-    rim_pols = []
-    for e in bm.edges:
-        if e.is_boundary:
-            rim_pols.append([
-                e.verts[0].index,
-                e.verts[1].index,
-                e.verts[1].index + v_len,
-                e.verts[0].index + v_len])
 
+    if output_edges:
+        edges_out = create_edges(bm, edges, v_len)
+    else:
+        edges_out = []
+
+    new_pols = [[c + v_len for c in p] for p in faces]
+
+    rim_pols = [[e.verts[0].index,
+                 e.verts[1].index,
+                 e.verts[1].index + v_len,
+                 e.verts[0].index + v_len]
+                for e in bm.edges if e.is_boundary]
+                
     faces_out = [f[::-1] for f in faces] + new_pols + rim_pols
-    pol_group = [0]*len(faces) + [1]*len(new_pols) + [2]*len(rim_pols)
+    pol_group = [0] * len(faces) + [1] * len(new_pols) + [2] * len(rim_pols)
+
     new_verts_mask = [False]*len(vertices)+ [True]*len(new_verts)
 
-    edges_out = create_edges(bm, v_len)
     return (vertices_out, edges_out, faces_out, new_pols, rim_pols, pol_group, new_verts_mask)
 
-def solidify_blender(vertices, faces, t, offset=None, even=True):
+def solidify_blender(vertices, edges, faces, t, offset=None, even=True, output_edges=True):
 
     if not faces or not vertices:
         return False
@@ -108,8 +124,9 @@ def solidify_blender(vertices, faces, t, offset=None, even=True):
     bm.verts.index_update()
     bm.edges.index_update()
     bm.faces.index_update()
-    for edge in bm.edges[:]:
-        edges.append([v.index for v in edge.verts[:]])
+
+    if output_edges:
+        edges = [[v.index for v in edge.verts[:]] for edge in bm.edges[:]]
     verts = [vert.co[:] for vert in bm.verts[:]]
     for face in bm.faces:
         indexes = [v.index for v in face.verts[:]]
@@ -148,9 +165,11 @@ class SvSolidifyNodeMk2(bpy.types.Node, SverchCustomTreeNode):
     even: BoolProperty(
         name='Even Thickness', description='Mantain Thinkness by adjusting sharp corners',
         default=True, update=updateNode)
+
     def update_sockets(self, context):
         self.inputs['Offset'].hide_safe = self.implementation == 'Blender'
         updateNode(self, context)
+
     implementation: EnumProperty(
         name='Implementation', items=enum_item_4(['Sverchok', 'Blender']),
         description='Mantain Thinkness by adjusting sharp corners',
@@ -158,6 +177,7 @@ class SvSolidifyNodeMk2(bpy.types.Node, SverchCustomTreeNode):
 
     def sv_init(self, context):
         self.inputs.new('SvVerticesSocket', 'Vertices')
+        self.inputs.new('SvStringsSocket', 'Edges')
         self.inputs.new('SvStringsSocket', 'Polygons')
         self.inputs.new('SvStringsSocket', 'Thickness').prop_name = 'thickness'
         self.inputs.new('SvStringsSocket', 'Offset').prop_name = 'offset'
@@ -173,6 +193,7 @@ class SvSolidifyNodeMk2(bpy.types.Node, SverchCustomTreeNode):
     def draw_buttons(self, context, layout):
         if self.implementation == 'Sverchok':
             layout.prop(self, 'even')
+
     def draw_buttons_ext(self, context, layout):
         layout.prop(self, 'implementation')
         self.draw_buttons(context, layout)
@@ -185,34 +206,23 @@ class SvSolidifyNodeMk2(bpy.types.Node, SverchCustomTreeNode):
             return
 
         verts = self.inputs['Vertices'].sv_get(deepcopy=False)
+        edges = self.inputs['Edges'].sv_get(deepcopy=False, default=[[]])
         polys = self.inputs['Polygons'].sv_get(deepcopy=False)
         thickness = self.inputs['Thickness'].sv_get(deepcopy=False)
         offset = self.inputs['Offset'].sv_get(deepcopy=False)
 
-        verts_out = []
-        edges_out = []
-        polys_out = []
-        new_pols = []
-        rim_pols = []
-        pols_groups = []
-        new_verts_mask = []
         if self.implementation == 'Blender':
             func = solidify_blender
         else:
             func = solidify
 
-        for v, p, t, o in zip_long_repeat(verts, polys, thickness, offset):
-            res = func(v, p, t, o, self.even)
+        res = []
 
-            if not res:
-                return
-            verts_out.append(res[0])
-            edges_out.append(res[1])
-            polys_out.append(res[2])
-            new_pols.append(res[3])
-            rim_pols.append(res[4])
-            pols_groups.append(res[5])
-            new_verts_mask.append(res[6])
+        for v, e, p, t, o in zip_long_repeat(verts, edges, polys, thickness, offset):
+            res.append(func(v, e, p, t, o, self.even))
+
+        verts_out, edges_out, polys_out, new_pols, rim_pols, pols_groups, new_verts_mask = zip(*res)
+
 
         self.outputs['Vertices'].sv_set(verts_out)
         self.outputs['Edges'].sv_set(edges_out)
