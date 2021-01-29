79e56fab71f88d7f7e107cea6ebda9e9a87c90d4
nortikin <nikitronn@yandex.ru>
2021-01-29 18:57:14 +0300

    shapekeys snl

--- node_scripts/SNLite_templates/utils/shape_key_bake.py
+++ node_scripts/SNLite_templates/utils/shape_key_bake.py
@@ -25,7 +25,7 @@ pols = okok(pols)
 def make():
     # find mesh or create
     if not 'sv_shape_key' in bpy.data.meshes:
-        mesh = bpy.data.meshes.new('sv_shape_key')
+        mesh = bpy.data.meshes.new(name='sv_shape_key')
         mesh.from_pydata(verts,edges,pols)
     else:
         mesh = bpy.data.meshes['sv_shape_key']
@@ -35,23 +35,27 @@ def make():
         bpy.context.scene.objects.link(obj)
     else:
         obj = bpy.data.objects['sv_shape_key']
+    if not 'sv_shape_key' in bpy.context.scene.collection.objects:
+        bpy.context.scene.collection.objects.link(obj)
+
 
     # shapekeys adding
     # make shape key basis at first
     if not obj.data.shape_keys:
         sk0 = obj.shape_key_add(name='Basis')
+    else:
+        # name of new shape from frame number
+        kn = "sverchok %d"%frame
     key = bpy.data.shape_keys[obj.data.shape_keys.name]
     key.use_relative = False
 
-    # name of new shape from frame number
-    kn = "sverchok %d"%frame
 
     # check for equal length of vertices
     if len(obj.data.vertices) != len(verts):
         return
     # current frame to add
     if kn not in key.key_blocks:
-        sk = obj.shape_key_add(kn)
+        sk = obj.shape_key_add(name=kn)
         for i,v in enumerate(verts):
             key.key_blocks[kn].data[i].co = Vector(v)
 
