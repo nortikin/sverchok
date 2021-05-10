a759c2c01b466daee23cbce691c8212a190551d7
Dealga McArdle <zeffii@users.noreply.github.com>
2021-05-10 15:36:15 +0200

    add correct placing for hotswap (#4084)

--- utils/macros/hotswap_macros.py
+++ utils/macros/hotswap_macros.py
@@ -9,7 +9,13 @@ def swap_vd_mv(context, operator, term, nodes, links):
         tree = context.space_data.edit_tree
         nodes, links = tree.nodes, tree.links
         mv = tree.nodes.new('SvMeshViewer')
-        mv.location = loc
+
+        frame = active_node.parent
+        if frame:
+            mv.location = active_node.absolute_location
+            mv.parent = frame
+        else: 
+            mv.location = loc
 
         a_inputs = active_node.inputs
         link_matches = [[0, 0], [1, 1], [2, 2], [3, 4]]
