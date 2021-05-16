5a58945f6e91ee8e398b2bf6af6e368f60494f4c
Dealga McArdle <tetha.z@gmail.com>
2021-05-16 10:14:07 +0200

    add description

--- utils/modules/sdf_utils.py
+++ utils/modules/sdf_utils.py
@@ -4,14 +4,6 @@ def geometry_from_points(points, mode="python"):
     """
     using sdf module from:  https://github.com/fogleman/sdf
 
-    f.ex:
-        f = sdf.sphere()
-        points = f.generate(samples=1200)   # higher is slower, but more precise.
-        geom = geometry_from_points(points, mode="np")
-
-        >>> geom.verts
-        >>> geom.tris
-
     input:
         points: the direct output from `f.generate()`
         mode:   "np" (numpy), or "python"
@@ -19,7 +11,30 @@ def geometry_from_points(points, mode="python"):
         geom:   with a `.verts` atribute and `.tris`
                 verts and tris are output in numpy arrays if the mode is "np",
                 else standard python lists.
+
+    usage in snlite:
+
+        '''
+        in scale v
+        out verts v
+        out faces s
+        '''
+
+        from sdf import *
+        from sverchok.utils.modules.sdf_utils import geometry_from_points
+
+        f = box(2) & slab(z0=-0.5, z1=0.5).k(0.1)
+        f.scale((1, 0.5, 1))
+        f -= cylinder(0.25).circular_array(6, 0.6).k(0.05)
+
+        points = f.generate(samples=23200)
+
+        geom = geometry_from_points(points, mode="np")
+        verts.append(geom.verts)
+        faces.append(geom.tris)
+
     """
+
 
     geom = lambda: None
     num_points = len(points)
