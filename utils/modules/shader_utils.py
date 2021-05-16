c14023884f29d7a1bcb2f1c24291230571b0838c
Dealga McArdle <tetha.z@gmail.com>
2021-05-16 12:27:14 +0200

    tabs to spaces

--- utils/modules/shader_utils.py
+++ utils/modules/shader_utils.py
@@ -6,32 +6,32 @@
 # License-Filename: LICENSE
 
 class ShaderLib2D():
-	def __init__(self):
-		self.vectors = []
-		self.vertex_colors = []
-		self.indices = []
+    def __init__(self):
+        self.vectors = []
+        self.vertex_colors = []
+        self.indices = []
 
-	def add_rect(self, x, y, w, h, color):
-		addv = self.vectors.extend
-		addc = self.vertex_colors.extend
-		addi = self.indices.extend
-		"""
-		b - c
-	    | /	|
-		a - d
-		"""
-		a = (x, y)
-		b = (x, y - h)
-		c = (x + w, y - h)
-		d = (x + w, y)
-		offset = len(self.vectors)
-		addv([a, b, c, d])
-		addc([color for _ in range(4)])
-		addi([[offset + i for i in tri] for tri in [[0, 1, 2], [0, 2, 3]]])
+    def add_rect(self, x, y, w, h, color):
+        addv = self.vectors.extend
+        addc = self.vertex_colors.extend
+        addi = self.indices.extend
+        """
+        b - c
+        | / |
+        a - d
+        """
+        a = (x, y)
+        b = (x, y - h)
+        c = (x + w, y - h)
+        d = (x + w, y)
+        offset = len(self.vectors)
+        addv([a, b, c, d])
+        addc([color for _ in range(4)])
+        addi([[offset + i for i in tri] for tri in [[0, 1, 2], [0, 2, 3]]])
 
-	def compile(self):
-		geom = lambda: None
-		geom.vectors = self.vectors
-		geom.vertex_colors = self.vertex_colors
-		geom.indices = self.indices
-		return geom
+    def compile(self):
+        geom = lambda: None
+        geom.vectors = self.vectors
+        geom.vertex_colors = self.vertex_colors
+        geom.indices = self.indices
+        return geom
