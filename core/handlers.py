5fd7b4be9948011a1ba38205b76ddbdbee12df56
Dealga McArdle <tetha.z@gmail.com>
2021-05-25 18:48:28 +0200

    that event handler does not exist anymore

--- core/handlers.py
+++ core/handlers.py
@@ -207,7 +207,6 @@ def set_frame_change(mode):
     post = bpy.app.handlers.frame_change_post
     pre = bpy.app.handlers.frame_change_pre
 
-    # scene = bpy.app.handlers.scene_update_post
     # remove all
     if sv_update_handler in post:
         post.remove(sv_update_handler)
