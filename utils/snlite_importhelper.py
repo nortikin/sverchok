da35f4116d097f443aa6bc73a279fd73391e893d
Dealga McArdle <zeffii@users.noreply.github.com>
2021-06-12 14:05:23 +0200

    remove curvemapper from snlite (#4162)
    * remove curvemapper from snlite
    
    * using self.info
    
    * remove a few prints
    
    * use fstrings
    
    * no need to hasattr
    
    * less info
    
    * use debug
    
    * memo
    
    * complete > comment
    
    * function is no longer called
    
    * remove unused imports
    
    * these functions are no longer needed in snlite
    
    * prepare for new directive
    
    * typo
    
    * info <- print
    
    * postpone error reporting.. or execution
    
    * reorder test for unregister
    
    * Revert registration order
    
    * try >
    
    * fix logic >
    
    * seems ok
    
    * seems ok
    
    * seems ok
    
    * forgot to pass from self
    
    * typo
    
    * remix logic
    
    * maybe
    
    * printing only if execution_ends_early
    
    * use default to get content from socket
    
    * remove some debug prints
    
    * confirm it works. but i might remove for now
    
    * use default
    
    * remove print

--- utils/snlite_importhelper.py
+++ utils/snlite_importhelper.py
@@ -20,10 +20,7 @@ import re
 import bpy
 import ast
 
-from sverchok.utils.snlite_utils import get_valid_node
 
-
-TRIPPLE_QUOTES = '"""'
 UNPARSABLE = None, None, None, None
 
 sock_dict = {
@@ -50,10 +47,10 @@ def processed(str_in):
     return ast.literal_eval(b)
 
 
-def parse_socket_line(line):
+def parse_socket_line(node, line):
     lsp = line.strip().split()
     if not len(lsp) in {3, 5}:
-        print(line, 'is malformed')
+        self.info(f"{line} -> is malformed")
         return UNPARSABLE
     else:
         socket_type = sock_dict.get(lsp[2])
@@ -67,24 +64,20 @@ def parse_socket_line(line):
             nested = processed(lsp[4])
             return socket_type, socket_name, default, nested
 
-def parse_ui_line(L):
-    """
-    :    the bl_idname is needed only if it isn't ShaderNodeRGBCurve
-    :    expects the following format, comma separated
+def parse_required_socket_line(node, line):
+    # receives a line like
+    # required input sockets do not accept defaults or nested info, what would be the point?
+    # >in socketname sockettype
+    lsp = line.strip().split()
+    if len(lsp) == 3:
+        socket_type = sock_dict.get(lsp[2])
+        socket_name = lsp[1]
+        if not socket_type:
+            return UNPARSABLE
+        return socket_type, socket_name, None, None
 
-    ui = material_name, node_name
-    ui = material_name, node_name, bl_idname
-
-    something like:
-    ui = MyMaterial, RGB Curves
-
-    """
-    l = L[4:].strip()
-    items = [sl.strip() for sl in l.split(',')]
-    if len(items) == 2:
-        return dict(mat_name=items[0], node_name=items[1], bl_idname='ShaderNodeRGBCurve')
-    elif len(items) == 3:
-        return dict(mat_name=items[0], node_name=items[1], bl_idname=items[2])
+    self.info(f"{line} -> is malformed")
+    return UNPARSABLE
 
 
 def extract_directive_as_multiline_string(lines):
@@ -125,7 +118,7 @@ def parse_sockets(node):
         'inputs': [], 'outputs': [],
         'snlite_ui': [], 'includes': {},
         'custom_enum': [], 'custom_enum_2': [],
-        'callbacks': {}
+        'callbacks': {}, 'inputs_required': [],
     }
 
     directive = extract_directive_as_multiline_string(node.script_str)
@@ -138,12 +131,13 @@ def parse_sockets(node):
 
         if L.startswith('in ') or L.startswith('out '):
             socket_dir = L.split(' ')[0] + 'puts'
-            snlite_info[socket_dir].append(parse_socket_line(L))
+            snlite_info[socket_dir].append(parse_socket_line(node, L))
 
-        elif L.startswith('ui = '):
-            ui_dict = parse_ui_line(L)
-            if isinstance(ui_dict, dict):
-                snlite_info['snlite_ui'].append(ui_dict)
+        if L.startswith('>in '):
+            # one or more inputs can be required before processing/showing errors
+            input_info = parse_required_socket_line(node, L)
+            snlite_info['inputs'].append(input_info)
+            snlite_info['inputs_required'].append(input_info[1])
 
         elif L.startswith('inject'):
             if hasattr(node, 'inject_params'):
@@ -169,45 +163,3 @@ def parse_sockets(node):
 
 def are_matched(sock_, socket_description):
     return (sock_.bl_idname, sock_.name) == socket_description[:2]
-
-
-def get_rgb_curve(material_name, node_name):
-    '''
-    ShaderNodeRGBCurve support only
-    '''
-    m = bpy.data.materials.get(material_name)
-    node = m.node_tree.nodes.get(node_name)
-
-    out_list = []
-    for curve in node.mapping.curves:
-        points = curve.points
-        out_list.append([(p.handle_type, p.location[:]) for p in points])
-
-    x = 'ShaderNodeRGBCurve'
-    return dict(mat_name=material_name, node_name=node_name, bl_idname=x, data=out_list)
-
-
-def set_rgb_curve(data_dict):
-    '''
-    ShaderNodeRGBCurve support only
-    '''
-
-    mat_name = data_dict['mat_name']
-    node_name = data_dict['node_name']
-    bl_id = data_dict['bl_idname']
-
-    node = get_valid_node(mat_name, node_name, bl_id)
-
-    node.mapping.initialize()
-    data = data_dict['data']
-    for idx, curve in enumerate(node.mapping.curves):
-
-        # add extra points, if needed
-        extra = len(data[idx]) - len(curve.points)
-        _ = [curve.points.new(0.5, 0.5) for _ in range(extra)]
-
-        # set points to correspond with stored collection
-        for pidx, (handle_type, location) in enumerate(data[idx]):
-            curve.points[pidx].handle_type = handle_type
-            curve.points[pidx].location = location
-    node.mapping.update()
