4c5ae0094581d4f91b0d603874aa3dcf54a3b98a
Ilya Portnov <portnov@bk.ru>
2021-07-04 09:06:39 +0500

    Fix tests.

--- tests/dict_tests.py
+++ tests/dict_tests.py
@@ -86,6 +86,6 @@ class KnotvectorDictTests(SverchokTestCase):
         for k, m in curve2_kv:
             d.update(2, k, m)
 
-        expected_items = [(0, 4), (0.499, 3), (0.5, 3), (0.501, 3), (1, 4)]
+        expected_items = [(0, 4), (0.5, 3), (0.501, 3), (1, 4)]
         self.assertEquals(d.items(), expected_items)
 
