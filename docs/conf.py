815eefc65727799ae5f58c345cf7a20340f71773
Durman <sv@soluyanov.ru>
2021-07-11 16:12:34 +0400

    Change default theme
    regardless to execution environment. I guess this file was used to build on Read the Docs site. Now it will be used by Github actions.

--- docs/conf.py
+++ docs/conf.py
@@ -112,14 +112,11 @@ pygments_style = 'sphinx'
 
 # ------ Readthedocs -------------------------------------------
 
-# on_rtd is whether we are on readthedocs.org, this line of code grabbed from docs.readthedocs.org
-on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+import sphinx_rtd_theme
+html_theme = 'sphinx_rtd_theme'
 
-if not on_rtd:  # only import and set the theme if we're building docs locally
-    #import sphinx_rtd_theme
-    html_theme = 'sphinx_rtd_theme'
-    html_theme_path = ["_themes", ]
-    #html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+# Add any paths that contain custom themes here, relative to this directory.
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # otherwise, readthedocs.org uses their theme by default, so no need to specify it
 
@@ -133,10 +130,6 @@ if not on_rtd:  # only import and set the theme if we're building docs locally
 # further.  For a list of options available for each theme, see the
 # documentation.
 #html_theme_options = {}
-
-# Add any paths that contain custom themes here, relative to this directory.
-#html_theme_path = []
-html_theme_path = ["_themes", ]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
