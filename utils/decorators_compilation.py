34904af07ef9a9f7a9582423ae83c253354e3d34
Dealga McArdle <tetha.z@gmail.com>
2022-05-23 11:41:51 +0200

    fast fast fast

--- utils/decorators_compilation.py
+++ utils/decorators_compilation.py
@@ -16,8 +16,8 @@ local_numba_storage = {}
 def eejit(**kwargs):
     def wrapper(function_to_compile):
         if numba:
-            #from numba.pycc import CC
-            #cc = CC("numba_gofaster")
+            from numba.pycc import CC
+            cc = CC("numba_gofaster")
             function_name = function_to_compile.__name__
 
             if function_name not in local_numba_storage:
@@ -25,13 +25,17 @@ def eejit(**kwargs):
                 jitted_func = numba.njit(function_to_compile)
                 local_numba_storage[function_name] = jitted_func
 
-                #if kwargs.get('export'):
-                #    cc.export(kwargs['name'], kwargs['sig'])(jitted_func)
-                #    cc.compile()
+                if kwargs.get('export'):
+                    # it is not necessary to supply the compilation name explicitly
+                    compiled_name = kwargs.get("name", function_name)
+                    cc.export(kwargs['name'], kwargs['sig'])(jitted_func)
+                    cc.compile()
+
             # elif function_name in disk_numba_storoage
             #
             # --- read from disk, store in local_numba_storage
-            # 
+            #
+
             return local_numba_storage[function_name]
         else:
             return function_to_compile
