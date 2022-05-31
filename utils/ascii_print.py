3f4ef6308e16fd907da22f384539375c56d98635
Dealga McArdle <tetha.z@gmail.com>
2022-05-31 17:31:38 +0200

    add color to win10 startup

--- utils/ascii_print.py
+++ utils/ascii_print.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from sverchok.utils.development import get_version_string
 
 # pylint: disable=c0304
@@ -6,6 +7,7 @@ from sverchok.utils.development import get_version_string
 # pylint: disable=w1401
 
 def logo():
+
     l1 = r" ______ _    _ _______  ______ _______ _     _  _____  _     _"
     l2 = r"/______  \  /  |______ |_____/ |       |_____| |     | |____/ "
     l3 = r"______/   \/   |______ |    \_ |_____  |     | |_____| |    \_"
@@ -13,6 +15,12 @@ def logo():
     lines = [l1, l2, l3, l4]
 
     can_paint = os.name in {'posix'}
+    try:
+        if hasattr(sys, 'getwindowsversion'):
+            if sys.getwindowsversion().major == 10:
+                can_paint = True
+    except:
+        ...
 
     with_color = "\033[1;31m{0}\033[0m" if can_paint else "{0}"
     for line in lines:
