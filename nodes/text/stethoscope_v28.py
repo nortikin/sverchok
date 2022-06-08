9f35f6ea1ea42717391dcb8d5a003c0b67cbc8eb
Dealga McArdle <tetha.z@gmail.com>
2022-06-08 11:59:21 +0200

    revert fstr rounding for now..

--- nodes/text/stethoscope_v28.py
+++ nodes/text/stethoscope_v28.py
@@ -80,7 +80,10 @@ def parse_socket(socket, rounding, element_index, view_by_element, props):
     # http://stackoverflow.com/a/7584567/1243487
     rounded_vals = re.compile(r"\d*\.\d+")
 
-    def mround(match): f"{float(match.group()):.{rounding}g}"
+    # def mround(match): f"{float(match.group()):.{rounding}g}"
+    def mround(match):
+        format_string = "{{:.{0}g}}".format(rounding)
+        return format_string.format(float(match.group()))
 
     out = []
     for line in display_text:
