ed14a3ce458eadbdf49e1c41487ce6b1198f2a4a
Dealga McArdle <tetha.z@gmail.com>
2022-07-01 18:10:41 +0200

    add comment to example

--- node_scripts/SNLite_templates/templates/operator_example.py
+++ node_scripts/SNLite_templates/templates/operator_example.py
@@ -3,6 +3,8 @@ in verts v
 """
 
 def my_operator(self, context):
+    # this will have a limited knowledge of the globally available vars
+    # import bpy   if you need stuff from bpy ..etc.
     print(self, context, self.inputs['verts'].sv_get())
     return {'FINISHED'}
 
