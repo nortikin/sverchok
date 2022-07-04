b874ecacb43fff73a7d19df9704b61ea11320787
Dealga McArdle <tetha.z@gmail.com>
2022-07-04 11:56:57 +0200

    comment out stuff

--- node_scripts/SNLite_templates/templates/pip_install_example.py
+++ node_scripts/SNLite_templates/templates/pip_install_example.py
@@ -16,12 +16,12 @@ def install_whl(package_path):
     
 
 if __name__ == '__main__':
-    ## install_package(['--upgrade', 'pip'])  <-- may not be needed
+    ## install_package(['--upgrade', 'pip']) # <-- may not be needed
     # install_package('pandas')
-    ## install_package('gdal')  <-- may fail
-    ## install_package('fiona')  <-- may fail.
+    ## install_package('gdal') # <-- may fail
+    ## install_package('fiona') # <-- may fail.
     # install_whl(r"C:\Users\zeffi\Downloads\GDAL-3.4.2-cp310-cp310-win_amd64.whl")
     # install_whl(r"C:\Users\zeffi\Downloads\Fiona-1.8.21-cp310-cp310-win_amd64.whl")
     # install_package('geopandas')
-    # install_package('pygeos')   <-- optional
-    # install_package('shapely')  <-- optional
+    # install_package('pygeos')   # <-- optional
+    # install_package('shapely')  # <-- optional
