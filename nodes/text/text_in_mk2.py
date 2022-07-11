1c1677771e01d548ab54b102df652cac865d240a
Dealga McArdle <tetha.z@gmail.com>
2022-07-11 18:33:31 +0200

    remove from Note and Text In

--- nodes/text/text_in_mk2.py
+++ nodes/text/text_in_mk2.py
@@ -289,11 +289,9 @@ class SvTextInNodeMK2(bpy.types.Node, SverchCustomTreeNode):
 
             if n_id not in self.csv_data:
                 print("CSV auto reload failed, press update")
-                self.use_custom_color = True
                 self.color = FAIL_COLOR
                 return
 
-        self.use_custom_color = True
         self.color = READY_COLOR
         csv_data = self.csv_data[n_id]
         if not self.one_sock:
@@ -441,7 +439,6 @@ class SvTextInNodeMK2(bpy.types.Node, SverchCustomTreeNode):
             print(sys.exc_info()[-1].tb_frame.f_code)
             pass
 
-        self.use_custom_color = True
         if isinstance(data, (list, tuple)):
             self.list_data[n_id] = data
             self.color = READY_COLOR
@@ -460,7 +457,6 @@ class SvTextInNodeMK2(bpy.types.Node, SverchCustomTreeNode):
             self.reload_sv()
 
         if n_id not in self.list_data:
-            self.use_custom_color = True
             self.color = FAIL_COLOR
             return
 
@@ -501,7 +497,6 @@ class SvTextInNodeMK2(bpy.types.Node, SverchCustomTreeNode):
             if len(data) == 2 and data[0] in {'v', 's', 'm'}:
                 new_output_socket(self, named_socket, data[0])
             else:
-                self.use_custom_color = True
                 self.color = FAIL_COLOR
                 return
 
@@ -511,7 +506,6 @@ class SvTextInNodeMK2(bpy.types.Node, SverchCustomTreeNode):
         self.load_json_data()
 
         if n_id in self.json_data:
-            self.use_custom_color = True
             self.color = READY_COLOR
 
     def load_json_data(self):
@@ -528,7 +522,6 @@ class SvTextInNodeMK2(bpy.types.Node, SverchCustomTreeNode):
             print("Failed to load JSON data")
 
         if not json_data:
-            self.use_custom_color = True
             self.color = FAIL_COLOR
             return
 
@@ -545,11 +538,9 @@ class SvTextInNodeMK2(bpy.types.Node, SverchCustomTreeNode):
             self.reload_json()
 
         if n_id not in self.json_data:
-            self.use_custom_color = True
             self.color = FAIL_COLOR
             return
 
-        self.use_custom_color = True
         self.color = READY_COLOR
         json_data = self.json_data[n_id]
         for item in json_data:
@@ -584,7 +575,6 @@ class SvTextInNodeMK2(bpy.types.Node, SverchCustomTreeNode):
         #     print(sys.exc_info()[-1].tb_frame.f_code)
         #     pass
 
-        self.use_custom_color = True
         self.list_data[n_id] = data
         self.color = READY_COLOR
         self.current_text = self.text
@@ -602,7 +592,6 @@ class SvTextInNodeMK2(bpy.types.Node, SverchCustomTreeNode):
             self.reload_text()
 
         if n_id not in self.list_data:
-            self.use_custom_color = True
             self.color = FAIL_COLOR
             return
 
