b087b139c2dbf9f4683b5a11ffe87245c0c1b6ab
Ilya Portnov <portnov@bk.ru>
2022-09-29 11:40:41 +0500

    Move everything to utils/apidoc.py.

--- generate_api_documentation.sh
+++ generate_api_documentation.sh
@@ -3,12 +3,12 @@
 # If your blender is not available as just "blender" command, then you need
 # to specify path to blender when running this script, e.g.
 #
-# $ BLENDER=~/soft/blender-2.79/blender ./run_tests.sh
+# $ BLENDER=~/soft/blender-2.79/blender ./generate_api_documentation.sh
 #
 
 set -e
 
 BLENDER=${BLENDER:-blender}
 
-$BLENDER -b --addons sverchok --python gendocs.py --python-exit-code 1 -- $@
+$BLENDER -b --addons sverchok --python utils/apidoc.py --python-exit-code 1 -- $@
 
--- utils/apidoc.py
+++ utils/apidoc.py
@@ -9,30 +9,47 @@ import sys
 from os.path import join, dirname
 from os import makedirs
 
+from sverchok.utils.logging import getLogger
+
 try:
     import pdoc
 except ImportError:
     pdoc = None
 
-ROOT_MODULES = ['sverchok.utils', 'sverchok.ui']
+ROOT_MODULES = ['sverchok.data_structure', 'sverchok.node_tree', 'sverchok.core', 'sverchok.utils', 'sverchok.ui']
 
 def recursive_htmls(mod, parent=None):
     yield mod, mod.url(relative_to=None), mod.html()
     for submod in mod.submodules():
         yield from recursive_htmls(submod, parent=mod)
 
-def generate_api_documentation(root_directory):
+def generate_api_documentation(root_directory, logger=None):
     if pdoc is None:
         raise Exception("pdoc3 package is required in order to generate documentation")
-    print("Parsing modules...")
+    if logger is None:
+        logger = getLogger()
+    logger.info("Start generating API documentation")
     context = pdoc.Context()
     modules = [pdoc.Module(mod, context=context) for mod in ROOT_MODULES]
     pdoc.link_inheritance(context)
     for mod in modules:
         for mod, mod_url, mod_html in recursive_htmls(mod):
             makedirs(join(root_directory, dirname(mod_url)), exist_ok=True)
-            print(f"Writing module {mod.name} => {mod_url} documentation...")
+            logger.info(f"Writing module documentation: {mod_url}")
             with open(join(root_directory, mod_url), 'w') as f:
                 f.write(mod_html)
-    print("Done")
+    logger.info("API documentation generation done.")
 
+if __name__ == "__main__":
+    try:
+        argv = sys.argv
+        argv = argv[argv.index("--")+1:]
+        if argv:
+            output_directory = argv[0]
+        else:
+            output_directory = "./api_documentation"
+        generate_api_documentation(output_directory)
+        sys.exit(0)
+    except Exception as e:
+        print(e)
+        sys.exit(1)
