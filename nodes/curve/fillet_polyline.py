26bb7cc7a921f66d587c8744274a727b3b14311c
vkter <74725748+vkter@users.noreply.github.com>
2022-11-16 17:25:27 +0800

    Fillet clamp turned off by default

--- nodes/curve/fillet_polyline.py
+++ nodes/curve/fillet_polyline.py
@@ -1,9 +1,9 @@
 
 import numpy as np
 
-import bpy
+import bpy,math
 from bpy.props import FloatProperty, EnumProperty, BoolProperty, IntProperty
-
+from mathutils import Vector
 from sverchok.node_tree import SverchCustomTreeNode
 from sverchok.data_structure import updateNode, zip_long_repeat, repeat_last_for_length, ensure_nesting_level
 from sverchok.utils.logging import info, exception
@@ -21,15 +21,15 @@ class SvFilletPolylineNode(SverchCustomTreeNode, bpy.types.Node):
     bl_icon = 'OUTLINER_OB_EMPTY'
     sv_icon = 'SV_FILLET_POLYLINE'
 
-    factor : FloatProperty(
-        name = "Factor",
-        default = 1.0,
-        update = updateNode)
-
     radius : FloatProperty(
         name = "Radius",
         min = 0.0,
         default = 0.2,
+        update = updateNode)
+
+    clamp : BoolProperty(
+        name = "Clamp",
+        default = False,
         update = updateNode)
 
     concat : BoolProperty(
@@ -70,9 +70,12 @@ class SvFilletPolylineNode(SverchCustomTreeNode, bpy.types.Node):
         layout.label(text='Fillet mode:')
         layout.prop(self, 'arc_mode', text='')
         layout.prop(self, "concat")
+
         if self.concat:
             layout.prop(self, "scale_to_unit")
         layout.prop(self, "cyclic")
+
+        layout.prop(self,'clamp')
 
     def draw_buttons_ext(self, context, layout):
         self.draw_buttons(context, layout)
@@ -81,7 +84,6 @@ class SvFilletPolylineNode(SverchCustomTreeNode, bpy.types.Node):
     def sv_init(self, context):
         self.inputs.new('SvVerticesSocket', "Vertices")
         self.inputs.new('SvStringsSocket', "Radius").prop_name = 'radius'
-        self.inputs.new('SvStringsSocket', "Factor").prop_name = 'factor'
         self.outputs.new('SvCurveSocket', "Curve")
         self.outputs.new('SvMatrixSocket', "Centers")
 
@@ -105,7 +107,6 @@ class SvFilletPolylineNode(SverchCustomTreeNode, bpy.types.Node):
                 fillet = None
             else:
                 fillet = calc_fillet(v1, v2, v3, radius)
-
             if fillet is not None :
                 edge_direction = np.array(fillet.p1) - np.array(prev_edge_start)
                 edge_len = np.linalg.norm(edge_direction)
@@ -144,18 +145,49 @@ class SvFilletPolylineNode(SverchCustomTreeNode, bpy.types.Node):
         else:
             return curves, centers
 
-    def limit(self,vertices,radiuses,factors):
+    def limit(self,vertices,radiuses):
+        factor = 0.999
         if self.cyclic:
-            vertices = [vertices[-1]]+ vertices + [vertices[0]] 
+            vertices = [vertices[-1]] + vertices + [vertices[0]]
+        vertices = [Vector(v) for v in vertices]
         limit_radiuses = []
-        for v1,v2,v3,r,f in zip(vertices,vertices[1:],vertices[2:],radiuses,factors):
-            v1,v2,v3 = np.array(v1), np.array(v2), np.array(v3)
+        for n in range(len(vertices)-2):
+            v1,v2,v3,r = vertices[n],vertices[n+1],vertices[n+2],radiuses[n]
             vector1,vector2 = v1-v2,v3-v2
-            d1,d2 = np.linalg.norm(vector1),np.linalg.norm(vector2)
-            min_length = d1 if d1<d2 else d2
-            angle = np.arccos(np.dot(vector1,vector2)/(d1*d2))
-            max_r = float(np.tan(angle/2)*min_length*f/2)
-            r = max_r if r>max_r else r
+            d1,d2 = vector1.length,vector2.length
+            min_length1 = d1 if d1<d2 else d2
+
+            v_2,v_3,v_4 = v2,v3, v3 if v3 is vertices[-1] else vertices[n+3]
+            vector_1 ,vector_2 = vector2*-1,v_4-v_3
+            d_1,d_2 = d2 , vector_2.length
+            min_length2 = d_1 if d_1<d_2 else d_2
+
+            if d2 - min_length1 >= min_length2 :
+                if self.cyclic and n==0:
+                    min_length1 = min_length1/2
+                    vertices[-1] = (v1+v2)/2
+
+                angle = vector1.angle(vector2,0)
+                max_r = math.tan(angle/2)*min_length1
+            else:
+                vec2 = vector2.copy()
+                vec2.normalize()
+                vec_1 = vector_1.copy()
+                vec_1.normalize()
+                f_vector1 = vec2*min_length1
+                f_vector2 = vec_1*(d2-min_length2)
+
+                mid_vector = (f_vector1 + -f_vector2)/2
+                mid_vertex = v2 + mid_vector
+                vertices[n+1] = mid_vertex
+                min_length = mid_vector.length
+
+                if self.cyclic and n==0:
+                    vertices[-1] = v2 + vector1*(min_length/d1)
+                
+                angle = vector1.angle(vector2,0)
+                max_r = math.tan(angle/2)*min_length
+            r = max_r*factor if r>max_r else r
             limit_radiuses.append(r)
         return limit_radiuses
 
@@ -165,20 +197,18 @@ class SvFilletPolylineNode(SverchCustomTreeNode, bpy.types.Node):
 
         verts_s = self.inputs['Vertices'].sv_get()
         radius_s = self.inputs['Radius'].sv_get()
-        factor_s = self.inputs['Factor'].sv_get()
 
         verts_s = ensure_nesting_level(verts_s, 3)
         radius_s = ensure_nesting_level(radius_s, 2)
-        factor_s = ensure_nesting_level(factor_s,2)
 
         curves_out = []
         centers_out = []
-        for vertices, radiuses,factors in zip_long_repeat(verts_s, radius_s,factor_s):
+        for vertices, radiuses in zip_long_repeat(verts_s, radius_s):
             if len(vertices) < 3:
                 raise Exception("At least three vertices are required to make a fillet")
             radiuses = repeat_last_for_length(radiuses, len(vertices))
-            factors = repeat_last_for_length(factors, len(vertices))
-            radiuses = self.limit(vertices, radiuses,factors)
+            if self.clamp:
+                radiuses = self.limit(vertices, radiuses)
             curve, centers = self.make_curve(vertices, radiuses)
             curves_out.append(curve)
             centers_out.append(centers)
