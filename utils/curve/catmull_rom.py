942f4772d7c077b3a77891a5ba8b8408001b9015
Ilya Portnov <portnov@bk.ru>
2022-12-28 22:01:17 +0500

    conversion to NURBS.

--- utils/curve/catmull_rom.py
+++ utils/curve/catmull_rom.py
@@ -7,10 +7,24 @@
 
 import numpy as np
 
-from sverchok.utils.geom import Spline
+from sverchok.utils.geom import Spline, LineEquation
+from sverchok.utils.nurbs_common import SvNurbsMaths
 from sverchok.utils.curve.core import SvCurve
+from sverchok.utils.curve.bezier import SvBezierCurve
+from sverchok.utils.curve.nurbs_algorithms import concatenate_nurbs_curves
 
+bezierM = np.zeros((4,4))
+bezierM[:,0] = 1.0
+bezierM[3,:] = 1.0
+bezierM[1,1] = 1.0/3.0
+bezierM[2,1] = 2.0/3.0
+bezierM[2,2] = 1.0/3.0
+        
 class SvCatmullRomCurve(SvCurve):
+    """
+    Non-uniform Catmull-Rom cubic spline.
+    See https://en.wikipedia.org/wiki/Centripetal_Catmull%E2%80%93Rom_spline
+    """
     def __init__(self, tknots, points):
         self.points = np.asarray(points)
         self.tknots = np.asarray(tknots)
@@ -25,16 +39,14 @@ class SvCatmullRomCurve(SvCurve):
             tknots = Spline.create_knots(points, metric=metric)
 
         if cyclic:
-            points = np.insert(points, 0, points[-1], axis=0)
-            points = np.append(points, [points[-1]], axis=0)
+            points = np.concatenate(([points[-1]], points), axis=0)
+            points = np.append(points, [points[1], points[2]], axis=0)
 
             dt0 = tknots[1] - tknots[0]
             dt1 = tknots[-1] - tknots[-2]
             dt = (dt0 + dt1) * 0.5
-            t0 = tknots[0] - dt
-            tn = tknots[-1] + dt
-            tknots = np.insert(tknots, 0, t0, axis=0)
-            tknots = np.append(tknots, [tn], axis=0)
+            tknots = np.concatenate(([tknots[0] - dt], tknots), axis=0)
+            tknots = np.append(tknots, [tknots[-1] + dt, tknots[-1] + dt + dt0], axis=0)
         else:
             p0 = 2*points[0] - points[1]
             pn = 2*points[-1] - points[-2]
@@ -95,5 +107,109 @@ class SvCatmullRomCurve(SvCurve):
 
         c = (b1 * t2s - b2 * t1s) / t21
         return c
-    
+
+    def get_bezier_control_points(self):
+        n = len(self.tknots)
+        tk = self.tknots
+        t0 = tk[0:n-3]
+        t1 = tk[1:n-2]
+        t2 = tk[2:n-1]
+        t3 = tk[3:n]
+        dt10 = t1 - t0
+        dt20 = t2 - t0
+        dt30 = t3 - t0
+        dt21 = t2 - t1
+        dt31 = t3 - t1
+        dt32 = t3 - t2
+        
+        t01 = t0*t1
+        t02 = t0*t2
+        t12 = t1*t2
+        t13 = t1*t3
+        t23 = t2*t3
+        
+        t012 = t0*t1*t2
+        t013 = t0*t1*t3
+        t023 = t0*t2*t3
+        t123 = t1*t2*t3
+        
+        denom = np.zeros((n-3,4,4))
+        denom[:,0,0] = 1.0/(dt10*dt20*dt21)
+        denom[:,1,1] = 1.0/(dt10*dt21**2*dt31)
+        denom[:,2,2] = 1.0/(dt20*dt21**2*dt32)
+        denom[:,3,3] = 1.0/(dt21*dt31*dt32)
+        
+        numer = np.zeros((n-3,4,4))
+        numer[:,0,0] = t1*t2**2
+        numer[:,0,1] = -t2*(t023 + t1**2*t3 - t013 - t012)
+        numer[:,0,2] =  t1*(t123 + t023 - t013 - t0*t2**2)
+        numer[:,0,3] = -t1**2*t2
+        numer[:,1,0] = -t2*(t2 + 2*t1)
+        numer[:,1,1] = t2**2*t3 + t123 + t023 + t1**2*t3 - t013 - t1*t2**2 + t1**2*t2 - 3*t012
+        numer[:,1,2] = -(3*t123 + t023 - t013 - t1*t2**2 - t0*t2**2 + t1**2*t2 - t012 - t0*t1**2)
+        numer[:,1,3] = t1*(2*t2 + t1)
+        numer[:,2,0] = 2*t2 + t1
+        numer[:,2,1] = -(2*t23 + t13 - t12 - t02 + t1**2 - 2*t01)
+        numer[:,2,2] =   2*t23 + t13 - t2**2 + t12 - t02 - 2*t01
+        numer[:,2,3] = -(t2 + 2*t1)
+        numer[:,3,0] = -1.0
+        numer[:,3,1] = dt30
+        numer[:,3,2] = -dt30
+        numer[:,3,3] = 1.0
+        
+        U = np.zeros((n-3,4,4))
+        U[:,0,0] = 1.0
+        U[:,0,1] = t0
+        U[:,0,2] = t0**2
+        U[:,0,3] = t0**3
+        U[:,1,1] = dt30
+        U[:,1,2] = 2*t0*dt30
+        U[:,1,3] = 3*t0**2*dt30
+        U[:,2,2] = dt30**2
+        U[:,2,3] = 3*t0*dt30**2
+        U[:,3,3] = dt30**3
+        
+        bz = np.zeros((n-3,4,4))
+        bz[:] = bezierM
+        ms = (bz @ U @ numer) @ denom
+        cpts = []
+        for i,m in enumerate(ms):
+            ps = self.points[i:i+4]
+            cpt = m @ ps
+            cpts.append(cpt)
+        return np.array(cpts)
+
+    def to_bezier_segments(self):
+        segments = []
+        all_cpts = self.get_bezier_control_points()
+        for i, cpts in enumerate(all_cpts):
+            bezier = SvBezierCurve(cpts)
+            ts = self.tknots[i:i+4]
+            t1 = (ts[1] - ts[0]) / (ts[3] - ts[0])
+            t2 = (ts[2] - ts[0]) / (ts[3] - ts[0])
+            segment = bezier.cut_segment(t1, t2)
+            segments.append(segment)
+        return segments
+
+    def to_nurbs(self, implementation = SvNurbsMaths.NATIVE):
+        return concatenate_nurbs_curves(self.to_bezier_segments())
+
+    def get_control_points(self):
+        return self.to_nurbs().get_control_points()
+
+    def is_line(self):
+        pts = self.points
+        begin, end = pts[0], pts[-1]
+        # direction from first to last point of the curve
+        direction = end - begin
+        if np.linalg.norm(direction) < tolerance:
+            return True
+        line = LineEquation.from_direction_and_point(direction, begin)
+        distances = line.distance_to_points(pts[1:-1])
+        # Technically, this means that all control points lie
+        # inside the cylinder, defined as "distance from line < tolerance";
+        # As a consequence, the convex hull of control points lie in the
+        # same cylinder; and the curve lies in that convex hull.
+        return (distances < tolerance).all()
+        
 
