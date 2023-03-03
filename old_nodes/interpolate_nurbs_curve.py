948ed906f07ff7599e2617602c66071798879a57
rendetto <66558924+rendetto@users.noreply.github.com>
2023-03-03 15:41:54 +0200

    update logger for the deprecated node

--- old_nodes/interpolate_nurbs_curve.py
+++ old_nodes/interpolate_nurbs_curve.py
@@ -6,7 +6,6 @@ from bpy.props import FloatProperty, EnumProperty, BoolProperty, IntProperty
 
 from sverchok.node_tree import SverchCustomTreeNode
 from sverchok.data_structure import updateNode, zip_long_repeat
-from sverchok.utils.logging import info, exception
 from sverchok.utils.curve.nurbs import SvNurbsCurve, SvNativeNurbsCurve, SvGeomdlCurve
 from sverchok.utils.nurbs_common import SvNurbsMaths
 from sverchok.utils.math import supported_metrics
@@ -92,7 +91,7 @@ class SvExInterpolateNurbsCurveNode(SverchCustomTreeNode, bpy.types.Node):
                 implementation = SvNurbsCurve.NATIVE
                 metric = self.metric
 
-            curve = SvNurbsMaths.interpolate_curve(implementation, degree, vertices, metric=metric, cyclic=self.cyclic, logger=self.get_logger())
+            curve = SvNurbsMaths.interpolate_curve(implementation, degree, vertices, metric=metric, cyclic=self.cyclic, logger=self.sv_logger)
 
             points_out.append(curve.get_control_points().tolist())
             knots_out.append(curve.get_knotvector().tolist())
