5aa2bc2edb551bf72f49c964c8997aa5864713e1
Dealga McArdle <tetha.z@gmail.com>
2023-07-05 13:02:24 +0200

    add code documentation

--- utils/bgl_wrapper.py
+++ utils/bgl_wrapper.py
@@ -1,10 +1,20 @@
 # sverchok/utils/bgl_wrapper.py
 
+"""
+The bgl module is deprecated From Blender 3.5 onwards, this means we must use the gpu module to 
+achieve any/all features formerly provided by bgl. This wrapper aims to keep the code changes to
+a minimum while supporting the old bgl code where possbile. The module is also a test to see if
+we can achieve some type of feature parity between the versions.
+
+
+"""
+
+
 import bpy
 
 if bpy.app.version >= (3, 5, 0):
-    import gpu
 
+    import gpu
 
     BLEND = "ALPHA"
     GL_FRONT_AND_BACK = None
