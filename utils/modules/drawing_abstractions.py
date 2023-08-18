fb716ba4476381fc5688640cf5e3f8d59385de7e
Dealga McArdle <tetha.z@gmail.com>
2023-08-18 15:19:03 +0200

    testing gpu.state

--- utils/modules/drawing_abstractions.py
+++ utils/modules/drawing_abstractions.py
@@ -49,19 +49,23 @@ if bpy.app.version >= (3, 5, 0):
     class Drawing:
 
         set_wireframe_line = placeholder_function
-        set_wireframe_fill = placeholder_function
+
+        def set_wireframe_fill(self):
+            gpu.state.face_culling_set("FRONT")
+            gpu.state.front_facing_set(False)
+
         set_line_width = gpu.state.line_width_set
         reset_line_width = lambda self: gpu.state.line_width_set(1)
         set_point_size = gpu.state.point_size_set
         reset_point_size = lambda self: gpu.state.point_size_set(1)
-        enable_polygon_offset_fill = placeholder_function
+        enable_polygon_offset_fill = lambda self: gpu.state.face_culling_set("FRONT")
         disable_polygon_offset_fill = placeholder_function
         set_polygon_offset_amounts = placeholder_function
 
-        enable_blendmode = placeholder_function
-        disable_blendmode = placeholder_function
-        enable_depth_test = placeholder_function
-        disable_depth_test = placeholder_function
+        enable_blendmode = lambda self: gpu.state.blend_set("ALPHA")
+        disable_blendmode = lambda self: gpu.state.blend_set("NONE")
+        enable_depth_test = lambda self: gpu.state.depth_test_set("ALWAYS")
+        disable_depth_test = lambda self: gpu.state.depth_test_set("NONE")
 
         new_buffer_texture = placeholder_function
         get_buffer = placeholder_function
