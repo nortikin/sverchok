fix #5034. Apply Vector Field Node performance proposal solution. v003. Escape calculation of vertices by polimorphizm. Some 'normal_array' of surface calcs source vertices so there is no need calc vertices by evaluate_array. So written function 'normal_vertices_array' is SvSurface and in some inheritance classes that return both params if needed.

utils/surface/algorithms.py
@@ -159,11 +159,7 @@ class SvInterpolatingSurface(SvSurface):
 #         normals = [self._normal(u, v) for u,v in zip(us, vs)]
 #         return np.array(normals)
 
-    def normal_array(self, us, vs):
-        result_normals, *_ = self.normal_array_with_source_vertices(us, vs)
-        return result_normals
-
-    def normal_array_with_source_vertices(self, us, vs):
+    def normal_vertices_array(self, us, vs):
         h = 0.001
         _points         = np.empty( (0, 3), dtype=np.float64)
         _points_u_h     = np.empty( (0, 3), dtype=np.float64)
@@ -316,7 +312,7 @@ class SvDeformedByFieldSurface(SvSurface):
         normal = normal / n
         return normal
 
-    def normal_array(self, us, vs):
+    def normal_vertices_array(self, us, vs):
         surf_vertices = self.evaluate_array(us, vs)
         u_plus = self.evaluate_array(us + self.normal_delta, vs)
         v_plus = self.evaluate_array(us, vs + self.normal_delta)
@@ -329,7 +325,7 @@ class SvDeformedByFieldSurface(SvSurface):
         #if norm != 0:
         normal = normal / norm
         #self.info("Normals: %s", normal)
-        return normal
+        return normal, surf_vertices
 
 class SvRevolutionSurface(SvSurface):
     __description__ = "Revolution"

utils/surface/core.py
@@ -38,6 +38,10 @@ class SvSurface(object):
         return normal
 
     def normal_array(self, us, vs):
+        normal, *_ = self.normal_vertices_array(us, vs)
+        return normal
+
+    def normal_vertices_array(self, us, vs):
         surf_vertices = self.evaluate_array(us, vs)
         u_plus = self.evaluate_array(us + self.normal_delta, vs)
         v_plus = self.evaluate_array(us, vs + self.normal_delta)
@@ -50,7 +54,7 @@ class SvSurface(object):
         #if norm != 0:
         normal = normal / norm
         #self.info("Normals: %s", normal)
-        return normal
+        return normal, surf_vertices
 
     def derivatives_data_array(self, us, vs):
         if hasattr(self, 'normal_delta'):
@@ -403,7 +407,7 @@ class SvLambdaSurface(SvSurface):
     def normal(self, u, v):
         return self.normal_array(np.array([u]), np.array([v]))[0]
 
-    def normal_array(self, us, vs):
+    def normal_vertices_array(self, us, vs):
         surf_vertices = self.evaluate_array(us, vs)
         u_plus = self.evaluate_array(us + self.normal_delta, vs)
         v_plus = self.evaluate_array(us, vs + self.normal_delta)
@@ -416,5 +420,5 @@ class SvLambdaSurface(SvSurface):
         #if norm != 0:
         normal = normal / norm
         #self.info("Normals: %s", normal)
-        return normal
+        return normal, surf_vertices
 