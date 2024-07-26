6e65325e1414f18d291e64135268a560214a4558
satabol <satabol@yandex.ru>
2024-07-26 10:44:47 +0300

    - append string list mode - append list select units system and units for some elements

--- nodes/number/list_input.py
+++ nodes/number/list_input.py
@@ -17,7 +17,7 @@
 # ##### END GPL LICENSE BLOCK #####
 
 import bpy
-from bpy.props import EnumProperty, FloatVectorProperty, IntProperty, FloatProperty, IntVectorProperty, BoolProperty, CollectionProperty
+from bpy.props import EnumProperty, FloatVectorProperty, IntProperty, FloatProperty, IntVectorProperty, BoolProperty, StringProperty, CollectionProperty
 
 from sverchok.node_tree import SverchCustomTreeNode
 from sverchok.data_structure import updateNode, multi_socket
@@ -397,6 +397,88 @@ class SvListInputColorEntry(bpy.types.PropertyGroup):
         update=update_entry,
     ) # type: ignore
 
+
+class SvListInputStringEntry(bpy.types.PropertyGroup):
+
+    # share data with elements
+    def get_elem(self):
+        return self.elem
+
+    def set_elem(self, value):
+        self.elem = value
+
+    def update_entry(self, context):
+        # TODO: Если нод не активен, то изменения текста через контролы выбора файла или каталога
+        # не работают и выходные значения нода не обновляются. Как-то надо найти нод, к
+        # которому относятся эти контролы.
+        if hasattr(context, 'node'):
+            updateNode(context.node, context)
+        elif hasattr(context, 'active_node'):
+            updateNode(context.active_node, context)
+        else:
+            sv_logger.debug("Node is not defined in this context, so will not update the node.")
+
+    item_enable : BoolProperty(
+        name = "Enable",
+        description = "Enable Element in list",
+        default=True,
+        update=update_entry,
+        ) # type: ignore
+        
+    elem: StringProperty(
+        name = "String of text",
+        default = "",
+        update=update_entry,
+    ) # type: ignore
+    
+    NONE: StringProperty(
+        name = "String of text",
+        default = "",
+        subtype='NONE',
+        get = get_elem, set = set_elem,
+        #update=update_entry,
+    ) # type: ignore
+    
+    FILE_PATH: StringProperty(
+        name = "String of text",
+        default = "",
+        subtype='FILE_PATH',
+        get = get_elem, set = set_elem,
+        #update=update_entry,
+    ) # type: ignore
+    
+    DIR_PATH: StringProperty(
+        name = "String of text",
+        default = "",
+        subtype='DIR_PATH',
+        get = get_elem, set = set_elem,
+        #update=update_entry,
+    ) # type: ignore
+    
+    FILE_NAME: StringProperty(
+        name = "String of text",
+        default = "",
+        subtype='FILE_NAME',
+        get = get_elem, set = set_elem,
+        #update=update_entry,
+    ) # type: ignore
+    
+    BYTE_STRING: StringProperty(
+        name = "String of text",
+        default = "",
+        subtype='BYTE_STRING',
+        get = get_elem, set = set_elem,
+        #update=update_entry,
+    ) # type: ignore
+    
+    PASSWORD: StringProperty(
+        name = "String of text",
+        default = "",
+        subtype='PASSWORD',
+        get = get_elem, set = set_elem,
+        #update=update_entry,
+    ) # type: ignore
+
 def Correct_ListInput_Length(self, context):
     if(self.mode=='BOOL_LIST_MODE'):
         correct_collection_length(self.bool_list_items, self.bool_list_counter)
@@ -408,6 +490,8 @@ def Correct_ListInput_Length(self, context):
         correct_collection_length(self.vector_list_items, self.vector_list_counter)
     elif(self.mode=='COLOR_LIST_MODE'):
         correct_collection_length(self.color_list_items, self.color_list_counter)
+    elif(self.mode=='STRING_LIST_MODE'):
+        correct_collection_length(self.string_list_items, self.string_list_counter)
     else:
         raise Exception(f"[func: Correct_ListInput_Length] unknown mode {self.mode}.")
     updateNode(self, context)
@@ -424,6 +508,8 @@ def invert_ListInput_Mask(self, context):
         items = self.vector_list_items
     elif(self.mode=='COLOR_LIST_MODE'):
         items = self.color_list_items
+    elif(self.mode=='STRING_LIST_MODE'):
+        items = self.string_list_items
     else:
         raise Exception(f"[func: invert_ListInput_Mask] unknown mode {self.mode}.")
     for item in items:
@@ -503,6 +589,14 @@ class SvListInputNodeMK2(Show3DProperties, SverchCustomTreeNode, bpy.types.Node)
         update=Correct_ListInput_Length)  # type: ignore
     color_list_items : CollectionProperty(type=SvListInputColorEntry) # type: ignore
 
+    string_list_counter: IntProperty(
+        name='string_list_counter',
+        description='String',
+        default=1,
+        min=1,
+        update=Correct_ListInput_Length)  # type: ignore
+    string_list_items : CollectionProperty(type=SvListInputStringEntry) # type: ignore
+
     invert_mask: BoolProperty(
         name = "Mask",
         description = "Invert list of mask",
@@ -533,6 +627,9 @@ class SvListInputNodeMK2(Show3DProperties, SverchCustomTreeNode, bpy.types.Node)
         elif self.mode == 'COLOR_LIST_MODE':
             self.set_output_socketype(['SvVerticesSocket'])
             self.outputs['data_output'].label = 'Colors'
+        elif self.mode == 'STRING_LIST_MODE':
+            self.set_output_socketype(['SvVerticesSocket'])
+            self.outputs['data_output'].label = 'Strings'
         else:
             raise Exception(f"[func: changeMode] unknown mode {self.mode}.")
         Correct_ListInput_Length(self, context)
@@ -543,7 +640,8 @@ class SvListInputNodeMK2(Show3DProperties, SverchCustomTreeNode, bpy.types.Node)
         ("INT_LIST_MODE", "Int", "Integer", "", 1),
         ("FLOAT_LIST_MODE", "Float", "Float", "", 2),
         ("VECTOR_LIST_MODE", "Vector", "Vector", "", 3),
-        ("COLOR_LIST_MODE", "Color", "Color", "", 4)
+        ("COLOR_LIST_MODE", "Color", "Color", "", 4),
+        ("STRING_LIST_MODE", "Text", "Text", "", 5)
     ]
 
     mode: EnumProperty(
@@ -615,6 +713,187 @@ class SvListInputNodeMK2(Show3DProperties, SverchCustomTreeNode, bpy.types.Node)
         #update=changeSubTypeFloat
     ) # type:  ignore
 
+
+    subtypes_string = [
+        # https://docs.blender.org/api/current/bpy_types_enum_items/property_subtype_string_items.html#rna-enum-property-subtype-string-items
+        ("NONE", "None", "None", "", 0),
+        ("FILE_PATH", "File Path", "File Path", "", 1),
+        ("DIR_PATH", "Directory Path", "Directory Path", "", 2),
+        ("FILE_NAME", "File Name", "File Name", "", 3),
+        ("BYTE_STRING", "Byte String", "Byte String", "", 4),
+        ("PASSWORD", "Password", "Password", "A string that is displayed hidden (’********’)", 5),
+    ]
+    
+    subtype_string: EnumProperty(
+        name='Subtypes',
+        items=subtypes_string,
+        default='NONE',
+        #update=changeSubTypeFloat
+    ) # type:  ignore
+
+    # share data with elements
+    def get_unit_system(self):
+        system_unit = bpy.data.scenes['Scene'].unit_settings.system
+        res = [e for e in self.unit_systems if e[0]==system_unit][0][4]
+        return res
+
+    def set_unit_system(self, value):
+        bpy.data.scenes['Scene'].unit_settings.system = self.unit_systems[value][0]
+        pass
+
+    unit_systems = [
+        ("NONE", "None", "None", "", 0),
+        ("METRIC", "Metric", "Metric", "", 1),
+        ("IMPERIAL", "Imperial", "Imperial]", "", 2),
+    ]
+    unit_system: EnumProperty(
+        name='Unit System',
+        items=unit_systems,
+        default='NONE',
+        get=get_unit_system, set=set_unit_system,
+        #update=changeSubTypeFloat
+    ) # type:  ignore
+
+    def get_length_unit_metric(self):
+        length_unit = bpy.data.scenes['Scene'].unit_settings.length_unit
+        res = [e for e in self.length_units_metric if e[0]==length_unit][0][4]
+        return res
+
+    def set_length_unit_metric(self, value):
+        bpy.data.scenes['Scene'].unit_settings.length_unit = self.length_units_metric[value][0]
+        pass
+
+    length_units_metric = [
+        ("ADAPTIVE", "Adaptive", "Adaptive", "", 0),
+        ("KILOMETERS", "Kilometers", "Kilometers", "10^3", 1),
+        ("METERS", "Meters", "Meters", "1", 2),
+        ("CENTIMETERS", "Centimeters", "Centimeters", "10^-2", 3),
+        ("MILLIMETERS", "Millimeters", "Millimeters", "", 4),
+        ("MICROMETERS", "Micrometers", "Micrometers", "", 5),
+    ]
+    length_unit_metric: EnumProperty(
+        name='Length unit metric',
+        items=length_units_metric,
+        default='METERS',
+        get=get_length_unit_metric, set=set_length_unit_metric,
+        #update=changeSubTypeFloat
+    ) # type:  ignore
+
+    def get_length_unit_imperial(self):
+        length_unit = bpy.data.scenes['Scene'].unit_settings.length_unit
+        res = [e for e in self.length_units_imperial if e[0]==length_unit][0][4]
+        return res
+
+    def set_length_unit_imperial(self, value):
+        bpy.data.scenes['Scene'].unit_settings.length_unit = self.length_units_imperial[value][0]
+        pass
+
+    length_units_imperial = [
+        ("ADAPTIVE", "Adaptive", "Adaptive", "", 0),
+        ("MILES", "Miles", "Miles", "", 1),
+        ("FEET", "Feet", "Feet", "1", 2),
+        ("INCHES", "Inches", "Inches", "", 3),
+        ("THOU", "Thou", "Thow", "", 4),
+    ]
+    length_unit_imperial: EnumProperty(
+        name='Length unit imperial',
+        items=length_units_imperial,
+        default='FEET',
+        get=get_length_unit_imperial, set=set_length_unit_imperial,
+        #update=changeSubTypeFloat
+    ) # type:  ignore
+
+    def get_rotation_unit(self):
+        system_rotation = bpy.data.scenes['Scene'].unit_settings.system_rotation
+        res = [e for e in self.system_rotations if e[0]==system_rotation][0][4]
+        return res
+
+    def set_rotation_unit(self, value):
+        bpy.data.scenes['Scene'].unit_settings.system_rotation = self.system_rotations[value][0]
+        pass
+
+    system_rotations = [
+        ("RADIANS", "Radians", "Radians", "", 0),
+        ("DEGREES", "Degrees", "Degrees", "", 1),
+    ]
+    system_rotation: EnumProperty(
+        name='Rotation unit',
+        items=system_rotations,
+        default='RADIANS',
+        get=get_rotation_unit, set=set_rotation_unit,
+        #update=changeSubTypeFloat
+    ) # type:  ignore
+
+    def get_temperature_unit_metric(self):
+        temperature_unit = bpy.data.scenes['Scene'].unit_settings.temperature_unit
+        res = [e for e in self.temperature_units_metric if e[0]==temperature_unit][0][4]
+        return res
+
+    def set_temperature_unit_metric(self, value):
+        bpy.data.scenes['Scene'].unit_settings.temperature_unit = self.temperature_units_metric[value][0]
+        pass
+
+    temperature_units_metric = [
+        ("ADAPTIVE", "Adaptive", "Adaptive", "", 0),
+        ("KELVIN", "Kelvin", "Kelvin", "", 1),
+        ("CELSIUS", "Celsius", "Celsius", "", 2),
+    ]
+    temperature_unit_metric: EnumProperty(
+        name='Temperature unit metric',
+        items=temperature_units_metric,
+        default='CELSIUS',
+        get=get_temperature_unit_metric, set=set_temperature_unit_metric,
+        #update=changeSubTypeFloat
+    ) # type:  ignore
+
+    def get_temperature_unit_imperial(self):
+        temperature_unit = bpy.data.scenes['Scene'].unit_settings.temperature_unit
+        res = [e for e in self.temperature_units_imperial if e[0]==temperature_unit][0][4]
+        return res
+
+    def set_temperature_unit_imperial(self, value):
+        bpy.data.scenes['Scene'].unit_settings.temperature_unit = self.temperature_units_imperial[value][0]
+        pass
+
+    temperature_units_imperial = [
+        ("ADAPTIVE", "Adaptive", "Adaptive", "", 0),
+        ("KELVIN", "Kelvin", "Kelvin", "", 1),
+        ("FAHRENHEIT", "Fahrenheit", "Fahrenheit", "", 2),
+    ]
+    temperature_unit_imperial: EnumProperty(
+        name='Temperature unit imperial',
+        items=temperature_units_imperial,
+        default='FAHRENHEIT',
+        get=get_temperature_unit_imperial, set=set_temperature_unit_imperial,
+        #update=changeSubTypeFloat
+    ) # type:  ignore
+
+    def get_time_unit(self):
+        time_unit = bpy.data.scenes['Scene'].unit_settings.time_unit
+        res = [e for e in self.time_units if e[0]==time_unit][0][4]
+        return res
+
+    def set_time_unit(self, value):
+        bpy.data.scenes['Scene'].unit_settings.time_unit = self.time_units[value][0]
+        pass
+
+    time_units = [
+        ("ADAPTIVE", "Adaptive", "Adaptive", "", 0),
+        ("DAYS", "Days", "Days", "", 1),
+        ("HOURS", "Hours", "Hours", "", 2),
+        ("MINUTES", "Minutes", "Minutes", "", 3),
+        ("SECONDS", "Seconds", "Seconds", "", 4),
+        ("MILLISECONDS", "Milliseconds", "Milliseconds", "", 5),
+        ("MICROSECONDS", "Microseconds", "Microseconds", "", 6),
+    ]
+    time_unit: EnumProperty(
+        name='Time unit',
+        items=time_units,
+        default='SECONDS',
+        get=get_time_unit, set=set_time_unit,
+        #update=changeSubTypeFloat
+    ) # type:  ignore
+
     base_name = 'data '
     multi_socket_type = 'SvStringsSocket'
 
@@ -648,6 +927,8 @@ class SvListInputNodeMK2(Show3DProperties, SverchCustomTreeNode, bpy.types.Node)
             self.outputs.new('SvVerticesSocket', 'data_output').label = 'Vectors'
         elif self.mode == 'COLOR_LIST_MODE':
             self.outputs.new('SvVerticesSocket', 'data_output').label = 'Colors'
+        elif self.mode == 'STRING_LIST_MODE':
+            self.outputs.new('SvVerticesSocket', 'data_output').label = 'Strings'
         else:
             raise Exception(f"[func: sv_init] unknown mode {self.mode}.")
         Correct_ListInput_Length(self, context)
@@ -666,16 +947,56 @@ class SvListInputNodeMK2(Show3DProperties, SverchCustomTreeNode, bpy.types.Node)
             layout.prop(self, "vector_list_counter", text="List Length (vectors)")
         elif self.mode == 'COLOR_LIST_MODE':
             layout.prop(self, "color_list_counter", text="List Length (colors)")
+        elif self.mode == 'STRING_LIST_MODE':
+            layout.prop(self, "string_list_counter", text="List Length (strings)")
         else:
             raise Exception(f"[func: draw_buttons] unknown mode {self.mode}.")
 
         layout.row().prop(self, "mode", expand=True)
+        
+        r_unit_system = layout.row().split(factor=0.3)
+        r_unit_system.column().label(text="Unit system:")
+        if self.mode=='FLOAT_LIST_MODE' or self.mode=='VECTOR_LIST_MODE':
+            r_unit_system.row().prop(self, "unit_system", expand=True)
+        else:
+            r_unit_system.column().label(text='-----')
+
+        r_subtype_split1 = layout.row().split(factor=0.3)
+        r_subtype_split1.column().label(text="Subtype:")
+        r_subtype_split2 = r_subtype_split1.column().split(factor=0.5)
         if self.mode=='FLOAT_LIST_MODE':
-            layout.row().prop(self, "subtype_float", expand=False, text='subtype')
+            r_subtype_split2.column().prop(self, "subtype_float", expand=False, text='')
         elif self.mode=='VECTOR_LIST_MODE':
-            layout.row().prop(self, "subtype_vector", expand=False, text='subtype')
+            r_subtype_split2.row().prop(self, "subtype_vector", expand=False, text='')
         elif self.mode=='COLOR_LIST_MODE':
-            layout.row().prop(self, "subtype_color", expand=False, text='subtype')
+            r_subtype_split2.row().prop(self, "subtype_color", expand=False, text='')
+        elif self.mode=='STRING_LIST_MODE':
+            r_subtype_split2.row().prop(self, "subtype_string", expand=False, text='')
+
+        if self.unit_system=='METRIC':
+            if self.mode=='FLOAT_LIST_MODE' and self.subtype_float=='DISTANCE' or self.mode=='VECTOR_LIST_MODE' and (self.subtype_vector=='TRANSLATION' or self.subtype_vector=='XYZ_LENGTH'):
+                r_subtype_split2.column().prop(self, 'length_unit_metric', expand=False, text='')
+            elif self.mode=='FLOAT_LIST_MODE' and self.subtype_float=='ANGLE' or self.mode=='VECTOR_LIST_MODE' and (self.subtype_vector=='EULER' or self.subtype_vector=='AXISANGLE'):
+                r_subtype_split2.column().prop(self, 'system_rotation', expand=False, text='')
+            elif self.mode=='FLOAT_LIST_MODE' and self.subtype_float=='TEMPERATURE':
+                r_subtype_split2.column().prop(self, 'temperature_unit_metric', expand=False, text='')
+            else:
+                r_subtype_split2.column().label(text='-----')
+        elif self.unit_system=='IMPERIAL':
+            if self.mode=='FLOAT_LIST_MODE' and self.subtype_float=='DISTANCE' or self.mode=='VECTOR_LIST_MODE' and (self.subtype_vector=='TRANSLATION' or self.subtype_vector=='XYZ_LENGTH'):
+                r_subtype_split2.column().prop(self, 'length_unit_imperial', expand=False, text='')
+            elif self.mode=='FLOAT_LIST_MODE' and self.subtype_float=='ANGLE' or self.mode=='VECTOR_LIST_MODE' and (self.subtype_vector=='EULER' or self.subtype_vector=='AXISANGLE'):
+                r_subtype_split2.column().prop(self, 'system_rotation', expand=False, text='')
+            elif self.mode=='FLOAT_LIST_MODE' and self.subtype_float=='TEMPERATURE':
+                r_subtype_split2.column().prop(self, 'temperature_unit_imperial', expand=False, text='')
+            else:
+                r_subtype_split2.column().label(text='-----')
+        else:
+            if self.mode=='FLOAT_LIST_MODE' and self.subtype_float=='ANGLE':
+                r_subtype_split2.column().prop(self, 'system_rotation', expand=False, text='')
+            else:
+                r_subtype_split2.column().label(text='-----')
+
             
         layout.row().prop(self, "invert_mask", text="Invert mask", toggle=True)
         #layout.row().prop(self, "copy_clipboard", text="Copy data to clipboard", toggle=True)
@@ -790,6 +1111,22 @@ class SvListInputNodeMK2(Show3DProperties, SverchCustomTreeNode, bpy.types.Node)
                 s_r2_c2 = s_r2.column()
                 s_r2_c2.label(text=s_r2_c2_label1)
                 pass
+        elif self.mode == 'STRING_LIST_MODE':
+            for I, elem in enumerate(self.string_list_items):
+                s = col.row().split(factor=0.8)
+                s_r1 = s.row()
+                s_r1.prop(elem, self.subtype_string, icon_only=True)
+                s_r2 = s.row()
+                s_r2_c1 = s_r2.column()
+                s_r2_c1.prop(elem, f'item_enable', icon_only=True)
+                if elem.item_enable==False:
+                    s_r2_c2_label1 = '-'
+                else:
+                    s_r2_c2_label1 = f'{J}'
+                    J+=1
+                s_r2_c2 = s_r2.column()
+                s_r2_c2.label(text=s_r2_c2_label1)
+                pass
         else:
             raise Exception(f"[func: draw_buttons] unknown mode {self.mode}.")
 
@@ -828,6 +1165,8 @@ class SvListInputNodeMK2(Show3DProperties, SverchCustomTreeNode, bpy.types.Node)
                 lst = [[tuple(Vector(elem.elem)) for elem in self.vector_list_items if elem.item_enable==True]]
             elif self.mode == 'COLOR_LIST_MODE':
                 lst = [[tuple(elem.elem) for elem in self.color_list_items if elem.item_enable==True]]
+            elif self.mode == 'STRING_LIST_MODE':
+                lst = [[elem.elem for elem in self.string_list_items if elem.item_enable==True]]
             else:
                 raise Exception(f"[func: process] unknown mode {self.mode}.")
             
@@ -835,5 +1174,5 @@ class SvListInputNodeMK2(Show3DProperties, SverchCustomTreeNode, bpy.types.Node)
             
         self.outputs[0].sv_set(data)
 
-classes = [SvListInputBoolEntry, SvListInputIntEntry, SvListInputFloatEntry, SvListInputVectorEntry, SvListInputColorEntry, SvListInputNodeMK2]
+classes = [SvListInputBoolEntry, SvListInputIntEntry, SvListInputFloatEntry, SvListInputVectorEntry, SvListInputColorEntry, SvListInputStringEntry, SvListInputNodeMK2]
 register, unregister = bpy.utils.register_classes_factory(classes)
