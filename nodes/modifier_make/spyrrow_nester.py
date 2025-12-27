d949de68dbe07260a2f7daa462af21b02428f523
Ilya V. Portnov <portnov@bk.ru>
2025-12-27 21:23:26 +0500

    "Spyrrow nester" node.

---
+++ nodes/modifier_make/spyrrow_nester.py
@@ -0,0 +1,245 @@
+# ##### BEGIN GPL LICENSE BLOCK #####
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation; either version 2
+#  of the License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software Foundation,
+#  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#
+# ##### END GPL LICENSE BLOCK #####
+
+import bpy
+from bpy.props import BoolVectorProperty, EnumProperty, BoolProperty, FloatProperty, IntProperty
+
+from sverchok.core.sv_custom_exceptions import SvInvalidInputException, SvInvalidResultException
+from sverchok.node_tree import SverchCustomTreeNode
+from sverchok.data_structure import ensure_nesting_level, zip_long_repeat, updateNode
+from sverchok.utils.spyrrow import SpyrrowSolver
+from sverchok.dependencies import spyrrow
+
+class SvSpyrrowNesterNode(SverchCustomTreeNode, bpy.types.Node):
+    """
+    Triggers: Spyrrow Nesting Packing
+    Tooltip: Solve strip packing (nesting) problems
+    """
+    bl_idname = 'SvSpyrrowNesterNode'
+    bl_label = "Spyrrow Nester"
+    bl_icon = 'OUTLINER_OB_EMPTY'
+    sv_icon = 'SV_DUAL_MESH'
+    sv_dependencies = ['spyrrow']
+
+    activate : BoolProperty(
+        name = "Activate",
+        default = True,
+        update = updateNode)
+
+    early_termination : BoolProperty(
+        name = "Allow early termination",
+        description = "Whether to allow early termination of the algorithm",
+        default = True,
+        update = updateNode)
+
+    quadtree_depth : IntProperty(
+        name = "Quadtree depth",
+        description = "Maximum depth of the quadtree used by the collision detection engine jagua-rs. Must be positive, common values are 3,4,5",
+        default = 4,
+        min = 1, max = 8,
+        update = updateNode)
+
+    min_separation : FloatProperty(
+        name = "Separation",
+        description = "Minimum required distance between packed items",
+        default = 0.0,
+        min = 0.0,
+        update = updateNode)
+
+    total_time : IntProperty(
+        name = "Total time",
+        description = "Total time budget in seconds",
+        min = 1,
+        default = 10,
+        update = updateNode)
+
+    use_all_cores : BoolProperty(
+        name = "Use all CPU cores",
+        default = True,
+        update = updateNode)
+    
+    num_workers : IntProperty(
+        name = "Threads",
+        description = "Number of threads used by the collision detection engine during exploration",
+        default = 4,
+        min = 1,
+        update = updateNode)
+
+    stripe_height : FloatProperty(
+        name = "Strip Height",
+        description = "The fixed height of the strip",
+        default = 4.0,
+        min = 0.0,
+        update = updateNode)
+
+    item_count : IntProperty(
+        name = "Count",
+        default = 1,
+        min = 1,
+        update = updateNode)
+
+    random_seed : IntProperty(
+        name = "Seed",
+        default = 0,
+        update = updateNode)
+
+    restrict_angle : IntProperty(
+        name = "Angle",
+        default = 90,
+        min = 0,
+        update = updateNode)
+
+    rotation_modes = [
+        ('NO', "No rotations", "Disallow rotations", 0),
+        ('ANY', "Arbitrary rotation", "Arbitrary rotation", 1),
+        ('STEP', "Restrict by step", "Restrict rotation by step, e.g. 0, 30, 60...", 2),
+        ('ANGLES', "Only specified angles", "Allow rotations by specified angles", 3)
+    ]
+
+    def update_sockets(self, context):
+        self.inputs['Angle'].hide_safe = self.rotation_mode in {'NO', 'ANY'}
+        updateNode(self, context)
+
+    rotation_mode : EnumProperty(
+        name = "Rotation",
+        items = rotation_modes,
+        default = 'NO',
+        update = update_sockets)
+
+    flat_output : BoolProperty(
+        name = "Flat output",
+        default = True,
+        update = updateNode)
+
+    def sv_init(self, context):
+        self.inputs.new('SvVerticesSocket', 'Vertices')
+        self.inputs.new('SvStringsSocket', 'Faces')
+        self.inputs.new('SvStringsSocket', 'Count').prop_name = 'item_count'
+        self.inputs.new('SvStringsSocket', 'StripeHeight').prop_name = 'stripe_height'
+        self.inputs.new('SvStringsSocket', 'MinSeparation').prop_name = 'min_separation'
+        self.inputs.new('SvStringsSocket', 'Angle').prop_name = 'restrict_angle'
+        self.inputs.new('SvStringsSocket', 'Seed').prop_name = 'random_seed'
+        self.outputs.new('SvVerticesSocket', 'Vertices')
+        self.outputs.new('SvStringsSocket', 'Faces')
+        self.update_sockets(context)
+
+    def draw_buttons(self, context, layout):
+        layout.prop(self, 'activate', toggle = True, icon = 'CHECKMARK')
+        layout.label(text="Rotation:")
+        layout.prop(self, 'rotation_mode', text='')
+        layout.prop(self, 'total_time')
+        layout.prop(self, 'early_termination')
+        layout.prop(self, 'use_all_cores')
+        if not self.use_all_cores:
+            layout.prop(self, 'num_workers')
+        layout.prop(self, 'flat_output')
+
+    def draw_buttons_ext(self, context, layout):
+        self.draw_buttons(context, layout)
+        layout.prop(self, 'quadtree_depth')
+
+    def get_config(self, min_separation, seed):
+        return spyrrow.StripPackingConfig(
+                early_termination = self.early_termination,
+                quadtree_depth = self.quadtree_depth,
+                min_items_separation = min_separation,
+                total_computation_time = self.total_time,
+                num_workers = self.num_workers if not self.use_all_cores else None,
+                seed = seed)
+
+    def sort_verts(self, verts, faces):
+        if not faces:
+            return verts
+        else:
+            if len(faces) != 1:
+                raise SvInvalidInputException("Each item must have exactly one face")
+            face = faces[0]
+            verts = [verts[j] for j in face]
+            return verts
+
+    def prepare_angles(self, angles):
+        #print("Src A", angles)
+        if self.rotation_mode == 'NO':
+            angles = []
+        elif self.rotation_mode == 'ANY':
+            angles = None
+        elif self.rotation_mode == 'STEP':
+            if len(angles) != 1:
+                raise SvInvalidInputException("Exactly one angle value must be provided")
+            angles = list(range(0, 360, angles[0]))
+        #print("Res A", angles)
+        return angles
+
+    def process(self):
+        if not any((s.is_linked for s in self.outputs)):
+            return
+
+        verts_out = []
+        faces_out = []
+        if self.activate:
+            verts_s = self.inputs['Vertices'].sv_get()
+            if self.inputs['Faces'].is_linked:
+                faces_s = self.inputs['Faces'].sv_get()
+                faces_s = ensure_nesting_level(faces_s, 5)
+            else:
+                faces_s = [[[0]]]
+            height_s = self.inputs['StripeHeight'].sv_get()
+            min_separation_s = self.inputs['MinSeparation'].sv_get()
+            seed_s = self.inputs['Seed'].sv_get()
+            count_s = self.inputs['Count'].sv_get()
+            angle_s = self.inputs['Angle'].sv_get()
+
+            height_s = ensure_nesting_level(height_s, 2)
+            min_separation_s = ensure_nesting_level(min_separation_s, 2)
+            count_s = ensure_nesting_level(count_s, 3)
+            seed_s = ensure_nesting_level(seed_s, 2)
+            verts_s = ensure_nesting_level(verts_s, 5)
+            angle_s = ensure_nesting_level(angle_s, 4)
+
+            for params in zip_long_repeat(verts_s, faces_s, angle_s, count_s, height_s, min_separation_s, seed_s):
+                for verts_l, faces_l, angle_l, count_l, height, min_separation, seed in zip_long_repeat(*params):
+                    config = self.get_config(min_separation, seed)
+                    solver = SpyrrowSolver(config, height)
+                    for verts, faces, angles, count in zip_long_repeat(verts_l, faces_l, angle_l, count_l):
+                        verts = self.sort_verts(verts, faces)
+                        angles = self.prepare_angles(angles)
+                        solver.add_item(verts, count = count, allowed_orientations = angles)
+
+                    problem_verts = []
+                    problem_faces = []
+                    for item in solver.solve():
+                        item_verts = item.calc_verts()
+                        problem_verts.append(item_verts)
+                        face = list(range(len(item_verts)))
+                        faces = [face]
+                        problem_faces.append(faces)
+                    if self.flat_output:
+                        verts_out.extend(problem_verts)
+                        faces_out.extend(problem_faces)
+                    else:
+                        verts_out.append(problem_verts)
+                        faces_out.append(problem_faces)
+        self.outputs['Vertices'].sv_set(verts_out)
+        self.outputs['Faces'].sv_set(faces_out)
+
+def register():
+    bpy.utils.register_class(SvSpyrrowNesterNode)
+
+def unregister():
+    bpy.utils.unregister_class(SvSpyrrowNesterNode)
+
---
+++ utils/spyrrow.py
@@ -0,0 +1,87 @@
+# This file is part of project Sverchok. It's copyrighted by the contributors
+# recorded in the version control history of the file, available from
+# its original location https://github.com/nortikin/sverchok/commit/master
+#
+# SPDX-License-Identifier: GPL3
+# License-Filename: LICENSE
+
+import numpy as np
+from math import sin, cos, radians
+
+from mathutils import Matrix
+
+from sverchok.core.sv_custom_exceptions import InvalidStateError
+from sverchok.dependencies import spyrrow
+
+def rotate(points, angle):
+    angle = radians(angle)
+    points = np.array(points)
+    m = [[cos(angle), -sin(angle)], [sin(angle), cos(angle)]]
+    m = np.array(m)
+    return (m @ points.T).T
+
+def translate(points, vector):
+    return points + np.array(vector)
+
+def to_2d(verts):
+    return [(v[0], v[1]) for v in verts]
+
+def to_3d(verts):
+    return [(v[0], v[1], 0) for v in verts]
+
+class SpyrrowSolutionItem:
+    def __init__(self, placed_item, verts2d):
+        self.placed_item = placed_item
+        self.verts2d = verts2d
+
+    def calc_verts(self):
+        verts2d = translate(rotate(self.verts2d, self.placed_item.rotation), self.placed_item.translation)
+        return to_3d(verts2d)
+    
+    def calc_matrix(self):
+        rotation = Matrix.Rotation(radians(self.placed_item.rotation), 4, 'Z')
+        v = self.placed_item.translation
+        v = [v[0], v[1], 0]
+        translation = Matrix.Translation(v)
+        return translation @ rotation
+
+class SpyrrowSolver:
+    def __init__(self, config, strip_height):
+        self.instance = None
+        self.config = config
+        self.strip_height = strip_height
+        self.solution = None
+        self.items = []
+        self.verts2d = dict()
+
+    def add_item(self, verts, count = 1, allowed_orientations = None):
+        if self.instance is not None:
+            raise InvalidStateError("Spyrrow instance has already been initialized")
+        j = len(self.items)
+        verts = to_2d(verts)
+        id = f"object_{j}"
+        item = spyrrow.Item(
+                id, verts,
+                demand = count,
+                allowed_orientations = allowed_orientations)
+        self.verts2d[id] = verts
+        self.items.append(item)
+
+    def solve(self):
+        if self.instance is not None:
+            raise InvalidStateError("solve() has already been called")
+        self.instance = spyrrow.StripPackingInstance(
+                    "spyrrow",
+                    strip_height = self.strip_height,
+                    items = self.items
+                )
+        print("Starting Spyrrow solver")
+        self.solution = self.instance.solve(self.config)
+        print("Spyrrow solver done")
+        result = []
+        for placed_item in self.solution.placed_items:
+            verts = self.verts2d[placed_item.id]
+            item = SpyrrowSolutionItem(placed_item, verts)
+            result.append(item)
+        return result
+
