732e83705e9b09c19ccf0e03d616a7c3b088bfca
Ilya V. Portnov <portnov@bk.ru>
2026-01-24 11:49:45 +0500

    On node UI.

--- nodes/surface/concat_surfaces.py
+++ nodes/surface/concat_surfaces.py
@@ -92,7 +92,7 @@ class SvConcatSurfacesNode(SverchCustomTreeNode, bpy.types.Node):
     def draw_buttons(self, context, layout):
         layout.prop(self, 'input_mode', text='')
         layout.prop(self, 'direction', expand=True)
-        layout.prop(self, 'use_nurbs')
+        layout.prop(self, 'use_nurbs', text='')
 
     def draw_buttons_ext(self, context, layout):
         self.draw_buttons(context, layout)
