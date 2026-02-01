05e057651b9de40c485c88fe2c01444c7c0de1de
Ilya V. Portnov <portnov@bk.ru>
2026-02-01 22:04:38 +0500

    Fix imports

--- utils/surface/algorithms.py
+++ utils/surface/algorithms.py
@@ -31,7 +31,7 @@ from sverchok.utils.curve.algorithms import (
             reparametrize_curve,
             SvCurveOnSurface
         )
-from sverchok.utils.surface.core import SvReparametrizedSurface, SvSurface, UnsupportedSurfaceTypeException, other_direction
+from sverchok.utils.surface.core import SvReparametrizedSurface, SvSurface, UnsupportedSurfaceTypeException, other_direction, SurfaceDirection
 from sverchok.utils.surface.nurbs import SvNurbsSurface
 from sverchok.utils.surface.data import *
 from sverchok.utils.nurbs_common import SvNurbsBasisFunctions, SvNurbsMaths
